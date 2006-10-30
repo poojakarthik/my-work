aea33bb2f5a31bb8706cbca3fb5e6d37f18711f6
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2006-10-30 05:04:00 +0000

    building application dir

---
+++ framework/application/definitions.php
@@ -0,0 +1,50 @@
+<?php
+//----------------------------------------------------------------------------//
+// (c) copyright 2006 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//
+// DEFINITIONS
+//----------------------------------------------------------------------------//
+/**
+ * DEFINITIONS
+ *
+ * Global Definitions
+ *
+ * This file exclusively declares global constants
+ *
+ * @file		definitions.php
+ * @language	PHP
+ * @package		framework
+ * @author		Rich Davis
+ * @version		6.10
+ * @copyright	2006 VOIPTEL Pty Ltd
+ * @license		NOT FOR EXTERNAL DISTRIBUTION
+ *
+ */
+ 
+ 
+//----------------------------------------------------------------------------//
+// CONSTANTS
+//--------------------------------------------------------------------------
+
+ 
+// Reporting constants
+define("AUTOMATED_REPORT_HEADER", 	"===================================\n" .
+									"THIS IS AN AUTOMATED REPORT MESSAGE\n" .
+									"===================================\n\n");
+									
+define("AUTOMATED_REPORT_FOOTER", 	"\n -- END OF REPORT --\n");
+
+
+// Data Access constants
+define("DATABASE_URL", "10.11.12.13");
+define("DATABASE_NAME", "richdavis");
+define("DATABASE_USER", "richdavis");
+define("DATABASE_PWORD", "rich");
+
+define("DATABASE_ERROR_TABLE", "Error")
+?>
