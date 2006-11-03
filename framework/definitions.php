fd8d6a49f7b2031bf7784432500d5d7228186b97
Rich Davis <rdavis@yellowbilling.com.au>
2006-11-03 00:01:26 +0000

    Added CDR Type Definitions

--- framework/definitions.php
+++ framework/definitions.php
@@ -31,11 +31,17 @@
 // CONSTANTS
 //----------------------------------------------------------------------------//
 
+// CDR TYPES
+define("CDR_UNTIEL_RSLCOM"			, 1);
+define("CDR_UNTIEL_COMMANDER"		, 2);
+define("CDR_OPTUS_STANDARD"			, 3);
+define("CDR_AAPT_STANDARD"			, 4);
+define("CDR_ISEEK_STANDARD"			, 5);
 
 // ERROR TABLE
-define("FATAL_ERROR_LEVEL", 10000);
+define("FATAL_ERROR_LEVEL"			, 10000);
 
-define("NON_FATAL_TEST_EXCEPTION", 1337);
-define("FATAL_TEST_EXCEPTION", 80085);
+define("NON_FATAL_TEST_EXCEPTION"	, 1337);
+define("FATAL_TEST_EXCEPTION"		, 80085);
 
 ?>
