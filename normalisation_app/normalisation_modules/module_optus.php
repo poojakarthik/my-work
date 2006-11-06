4c26cb3ec37c8f2cbc9167ec8018421392c41d4c
Rich Davis <rdavis@yellowbilling.com.au>
2006-11-06 01:26:06 +0000

    Updated module_optus - file format definition

--- normalisation_app/normalisation_modules/module_optus.php
+++ normalisation_app/normalisation_modules/module_optus.php
@@ -78,29 +78,105 @@ class NormalisationModuleOptus extends NormalisationModule
 		$arrDefine ['RecordType']	['Start']		= 0;
 		$arrDefine ['RecordType']	['Length']		= 2;
 		
-		$arrDefine ['CDRId']		['Start']		= 2;
-		$arrDefine ['CDRId']		['Length']		= 12;
+		$arrDefine ['CDRId']		['Start']		= 12;
+		$arrDefine ['CDRId']		['Length']		= 2;
 		
 		$arrDefine ['AccountNo']	['Start']		= 14;
 		$arrDefine ['AccountNo']	['Length']		= 14;
 		
-		$arrDefine ['CDRId']		['Start']		= 4;
-		$arrDefine ['CDRId']		['Length']		= 12;
+		$arrDefine ['ServiceNo']	['Start']		= 28;
+		$arrDefine ['ServiceNo']	['Length']		= 24;
 		
-		$arrDefine ['CDRId']		['Start']		= 4;
-		$arrDefine ['CDRId']		['Length']		= 12;
+		$arrDefine ['ServiceNoType']['Start']		= 52;
+		$arrDefine ['ServiceNoType']['Length']		= 6;
 		
-		$arrDefine ['CDRId']		['Start']		= 4;
-		$arrDefine ['CDRId']		['Length']		= 12;
+		$arrDefine ['PointOrigin']	['Start']		= 58;
+		$arrDefine ['PointOrigin']	['Length']		= 24;
 		
-		$arrDefine ['CDRId']		['Start']		= 4;
-		$arrDefine ['CDRId']		['Length']		= 12;
+		$arrDefine ['PointTarget']	['Start']		= 82;
+		$arrDefine ['PointTarget']	['Length']		= 24;
 		
-		$arrDefine ['CDRId']		['Start']		= 4;
-		$arrDefine ['CDRId']		['Length']		= 12;
+		$arrDefine ['Jurisdiction']	['Start']		= 106;
+		$arrDefine ['Jurisdiction']	['Length']		= 6;
 		
-		$arrDefine ['CDRId']		['Start']		= 4;
-		$arrDefine ['CDRId']		['Length']		= 12;
+		$arrDefine ['CallDate']		['Start']		= 112;
+		$arrDefine ['CallDate']		['Length']		= 14;
+				
+		$arrDefine ['BillClass']	['Start']		= 126;
+		$arrDefine ['BillClass']	['Length']		= 6;
+		
+		$arrDefine ['TypeIdUsage']	['Start']		= 132;
+		$arrDefine ['TypeIdUsage']	['Length']		= 6;
+		
+		$arrDefine ['ElementId']	['Start']		= 138;
+		$arrDefine ['ElementId']	['Length']		= 6;
+		
+		$arrDefine ['Units']		['Start']		= 144;
+		$arrDefine ['Units']		['Length']		= 10;
+		
+		$arrDefine ['Completed']	['Start']		= 154;
+		$arrDefine ['Completed']	['Length']		= 3;
+		
+		$arrDefine ['Latitude']		['Start']		= 157;
+		$arrDefine ['Latitude']		['Length']		= 8;
+		
+		$arrDefine ['Longitude']	['Start']		= 165;
+		$arrDefine ['Longitude']	['Length']		= 8;
+		
+		$arrDefine ['OriginDesc']	['Start']		= 173;
+		$arrDefine ['OriginDesc']	['Length']		= 32;
+		
+		$arrDefine ['TargetDesc']	['Start']		= 205;
+		$arrDefine ['TargetDesc']	['Length']		= 32;
+		
+		$arrDefine ['RatePeriod']	['Start']		= 237;
+		$arrDefine ['RatePeriod']	['Length']		= 1;
+		
+		$arrDefine ['RatedUnits']	['Start']		= 238;
+		$arrDefine ['RatedUnits']	['Length']		= 10;
+		
+		$arrDefine ['ThirdUnits']	['Start']		= 258;		// Unused
+		$arrDefine ['ThirdUnits']	['Length']		= 10;
+		
+		$arrDefine ['FileId']		['Start']		= 268;
+		$arrDefine ['FileId']		['Length']		= 10;
+		
+		$arrDefine ['OldSeqNo']		['Start']		= 278;		// For withdrawn CDRs only
+		$arrDefine ['OldSeqNo']		['Length']		= 6;
+		
+		$arrDefine ['Amount']		['Start']		= 284;		// Charge in cents
+		$arrDefine ['Amount']		['Length']		= 8;
+		
+		$arrDefine ['RateClass']	['Start']		= 292;
+		$arrDefine ['RateClass']	['Length']		= 6;
+		
+		$arrDefine ['ProviderClass']['Start']		= 298;
+		$arrDefine ['ProviderClass']['Length']		= 6;
+		
+		$arrDefine ['ProviderId']	['Start']		= 304;
+		$arrDefine ['ProviderId']	['Length']		= 6;
+		
+		$arrDefine ['CurrencyCode']	['Start']		= 310;
+		$arrDefine ['CurrencyCode']	['Length']		= 6;
+		
+		$arrDefine ['EquimentType']	['Start']		= 316;
+		$arrDefine ['EquimentType']	['Length']		= 6;
+		
+		$arrDefine ['ServiceClass']	['Start']		= 322;		// Unused
+		$arrDefine ['ServiceClass']	['Length']		= 6;
+		
+		$arrDefine ['RateUnitsType']['Start']		= 328;
+		$arrDefine ['RateUnitsType']['Length']		= 6;
+		
+		$arrDefine ['DistBandId']	['Start']		= 334;
+		$arrDefine ['DistBandId']	['Length']		= 3;
+		
+		$arrDefine ['ZoneClass']	['Start']		= 337;
+		$arrDefine ['ZoneClass']	['Length']		= 3;
+		
+		$arrDefine ['CDRStatus']	['Start']		= 340;
+		$arrDefine ['CDRStatus']	['Length']		= 3;
+
 		
 		$this->_arrDefineCarrier = $arrDefine;
 		
@@ -136,6 +212,12 @@ class NormalisationModuleOptus extends NormalisationModule
 			return CDR_CANT_NORMALISE_HEADER;
 		}
 		
+		// Make sure the record is a CDR
+		if ((int)$this->_FetchRawCDR('RecordType') != 50)
+		{
+			return CDR_CANT_NORMALISE_NON_CDR;
+		}
+		
 		// covert CDR string to array
 		$this->_SplitRawCDR($arrCDR["CDR.CDR"]);
 	
@@ -157,43 +239,42 @@ class NormalisationModuleOptus extends NormalisationModule
 		//--------------------------------------------------------//
 		
 		// FNN
-		$mixValue = $this->_FetchRawCDR('ColumnName1');
+		$mixValue = $this->_FetchRawCDR('ServiceNo');
 		$this->_AppendCDR('FNN', $mixValue);
 		
 		// CarrierRef
-		$mixValue = $this->_GenerateUID($arrCDR["FileName"], $arrCDR["CDR.SequenceNo"]);
+		$mixValue = $this->_FetchRawCDR('CDRId');
 		$this->_AppendCDR('CarrierRef', $mixValue);
 		
 		// StartDatetime
-		$mixValue = $this->_FetchRawCDR('ColumnName2');
+		$mixValue = ConvertTime($this->_FetchRawCDR('CallDate'));
 		$this->_AppendCDR('StartDatetime', $mixValue);
 		
 		// Units
-		$mixValue = $this->_FetchRawCDR('');
+		$mixValue = $this->_FetchRawCDR('Units');
 		$this->_AppendCDR('Units', $mixValue);
 		
 		// Description
-		$mixValue = $this->_FetchRawCDR('');
+		$mixValue = $this->_FetchRawCDR('TypeIdUsage');				// Need to dereference
 		$this->_AppendCDR('Description', $mixValue);
 		
 		// RecordType
-		$mixValue = $this->_FetchRawCDR(''); // needs to match database
+		//$mixValue = $this->_FetchRawCDR('');						// needs to match database
 		$this->_AppendCDR('RecordType', $mixValue);
 		
 		// ServiceType
-		$mixValue = $this->_FetchRawCDR('');
-		$this->_AppendCDR('ServiceType', $mixValue);
+		$this->_AppendCDR('ServiceType', SERVICE_TYPE_LAND_LINE);
 
 		//--------------------------------------------------------//
 		// Optional Fields
 		//--------------------------------------------------------//
 
 		// Source
-		$mixValue = $this->_FetchRawCDR('');
+		$mixValue = $this->_FetchRawCDR('PointOrigin');
 		$this->_AppendCDR('Source', $mixValue);
 		
 		// Destination
-		$mixValue = $this->_FetchRawCDR('');
+		$mixValue = $this->_FetchRawCDR('PointTarget');
 		$this->_AppendCDR('Destination', $mixValue);
 		
 		// EndDatetime
@@ -212,6 +293,16 @@ class NormalisationModuleOptus extends NormalisationModule
 		
 		// return output array
 		return $this->_OutputCDR();
+	}
+	
+	function ConvertTime($strTime)
+	{
+		$strReturn 	= substr($strTime, 0, 4);				// Year
+		$strReturn .=  "-" . substr($strTime, 4, 2);		// Month
+		$strReturn .=  "-" . substr($strTime, 6, 2);		// Day
+		$strReturn .=  substr($strTime, 8, 2) . ":" . substr($strTime, 10, 2) . ":" . substr($strTime, 12, 2);			// Time
+		
+		return $strReturn;
 	}
 }
 
