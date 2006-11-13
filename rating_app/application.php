60a6332f261d2f1db9ae8bdcebb16c29d18b33cf
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2006-11-13 02:52:42 +0000

    added skel rating app

---
+++ rating_app/application.php
@@ -0,0 +1,59 @@
+<?php
+//----------------------------------------------------------------------------//
+// (c) copyright 2006 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// application
+//----------------------------------------------------------------------------//
+/**
+ * application
+ *
+ * Contains all classes for the application
+ *
+ * Contains all classes for the application
+ *
+ * @file		application.php
+ * @language	PHP
+ * @package		Skeleton_application
+ * @author		Jared 'flame' Herbohn
+ * @version		6.10
+ * @copyright	2006 VOIPTEL Pty Ltd
+ * @license		NOT FOR EXTERNAL DISTRIBUTION
+ *
+ */
+
+// Application entry point - create an instance of the application object
+$appSkel = new ApplicationSkel();
+
+// finished
+echo("\n-- End of Skeleton --\n");
+die();
+
+
+
+//----------------------------------------------------------------------------//
+// ApplicationSkel
+//----------------------------------------------------------------------------//
+/**
+ * ApplicationSkel
+ *
+ * Skeleton Module
+ *
+ * Skeleton Module
+ *
+ *
+ * @prefix		app
+ *
+ * @package		skeleton_application
+ * @class		ApplicationSkel
+ */
+ class ApplicationSkel extends ApplicationBaseClass
+ {
+ 
+ }
+
+
+?>
---
+++ rating_app/config.php
@@ -0,0 +1,34 @@
+<?php
+//----------------------------------------------------------------------------//
+// (c) copyright 2006 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//
+// DEFINITIONS
+//----------------------------------------------------------------------------//
+/**
+ * config
+ *
+ * ApplicationConfig Definitions
+ *
+ * This file exclusively declares application config
+ *
+ * @file		config.php
+ * @language	PHP
+ * @package		framework
+ * @author		Jared 'flame' Herbohn
+ * @version		6.10
+ * @copyright	2006 VOIPTEL Pty Ltd
+ * @license		NOT FOR EXTERNAL DISTRIBUTION
+ *
+ */
+ 
+ 
+//----------------------------------------------------------------------------//
+// CONFIG
+//----------------------------------------------------------------------------//
+
+?>
