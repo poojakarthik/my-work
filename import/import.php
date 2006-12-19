a2fbde39fea779a763a528a3f66bafdebdd4790b
Rich Davis <rdavis@yellowbilling.com.au>
2006-12-19 05:58:41 +0000

    Import now actually writes to database_define.php, rather than simply spitting out some data on the screen ;)

--- import/import.php
+++ import/import.php
@@ -1,5 +1,130 @@
 <pre>
 <?
+//----------------------------------------------------------------------------//
+// (c) copyright 2006 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// import
+//----------------------------------------------------------------------------//
+/**
+ * import
+ *
+ * Writes the database_define.php file
+ *
+ * Writes the database_define.php file
+ *
+ * @file		import.php
+ * @language	PHP
+ * @package		import
+ * @author		Bashkim 'Bash' Isai, Rich 'Waste' Davis
+ * @version		6.12
+ * @copyright	2006 VOIPTEL Pty Ltd
+ * @license		NOT FOR EXTERNAL DISTRIBUTION
+ *
+ */
+
+//----------------------------------------------------------------------------//
+// !!!! USAGE !!!
+//----------------------------------------------------------------------------//
+/*
+ * Use the GET variable "l" to specify the location of your database_define.php
+ * You must include the trailing "/", and must not include the file basename.
+ * 
+ * Example URL:  "http://localhost/vixen/import/import.php?l=/home/vixen/framework/"
+ * 				 This will produce a file called "database_define.php" at "/home/vixen/framework/"
+ *  
+ */
+	
+	
+	// Define basic file format
+	$strFileContents = "<?php\n".
+"//----------------------------------------------------------------------------// 
+// (c) copyright 2006 VOIPTEL Pty Ltd 
+// 
+// NOT FOR EXTERNAL DISTRIBUTION 
+//----------------------------------------------------------------------------// 
+ 
+ 
+//----------------------------------------------------------------------------// 
+// database_define 
+//----------------------------------------------------------------------------// 
+/** 
+ * database_define 
+ * 
+ * Defines database tables for use in the application 
+ * 
+ * Defines database tables for use in the application 
+ * 
+ * @file		database_define.php 
+ * @language	PHP 
+ * @package		framework 
+ * @author		Jared 'flame' Herbohn 
+ * @version		".str_replace("0", "", date("y.m"))." 
+ * @copyright	2006 VOIPTEL Pty Ltd 
+ * @license		NOT FOR EXTERNAL DISTRIBUTION 
+ * 
+ */ 
+ 
+ 
+ 
+//----------------------------------------------------------------------------// 
+// database table define format 
+//----------------------------------------------------------------------------// 
+ /* 
+	\$arrDefine['Name']		= \"\";			// Table name 
+	\$arrDefine['Type']		= \"MYISAM\";		// optional Table type, defaults to	'MYISAM' 
+	\$arrDefine['Id']		= \"Id\";			// optional Primary auto index column name, defaults to	'Id' 
+ 	
+	\$arrDefine['Index'][] 		= \"\";		// optional array of index column names 
+	\$arrDefine['Unique'][] 		= \"\";		// optional array of unique column names 
+ 	
+	// DO NOT Define the ID column here ! it will be added automatically 
+	\$arrDefine['Column'][\$strName]['Type'] 			= \"\";			// Validation type: s, i etc 
+	\$arrDefine['Column'][\$strName]['SqlType'] 		= \"\";			// Sql Type: Varchar(5), Int etc 
+	\$arrDefine['Column'][\$strName]['Null'] 			= TRUE|FALSE;	// optional, defaults to FALSE (NOT NULL) 
+	\$arrDefine['Column'][\$strName]['Default'] 		= \"\";			// optional default value 
+	\$arrDefine['Column'][\$strName]['Attributes'] 	= \"\";			// optional attributes 
+ */ 
+ 
+ 
+ 
+//----------------------------------------------------------------------------// 
+// skeleton table define 
+//----------------------------------------------------------------------------// 
+/* 
+	// clean reused temporary array 
+	unset(\$arrDefine); 
+ 	
+	// Define Table 
+	\$arrDefine['Name']		= \"\"; 
+	\$arrDefine['Type']		= \"MYISAM\"; 
+	\$arrDefine['Id']		= \"Id\";  
+	\$arrDefine['Index'][] 		= \"\"; 
+	\$arrDefine['Unique'][] 		= \"\"; 
+ 	
+	// Define Columns 
+	\$strName = \"\"; 
+		\$arrDefine['Column'][\$strName]['Type'] 			= \"\"; 
+		\$arrDefine['Column'][\$strName]['SqlType'] 		= \"\"; 
+		\$arrDefine['Column'][\$strName]['Null'] 			= FALSE; 
+		\$arrDefine['Column'][\$strName]['Default'] 		= \"\"; 
+		\$arrDefine['Column'][\$strName]['Attributes'] 	= \"\"; 
+ 		
+	// Save Table Define 
+	\$GLOBALS['arrDatabaseTableDefine'][\$arrDefine['Name']] = \$arrDefine; 
+ 	
+*/ 
+ 
+ 		
+ 		
+	unset(\$arrDefine);";
+	
+	
+	
+	
 	
 	$_DATABASE = "bash";
 	
@@ -9,22 +134,20 @@
 	
 	while ($_TABLE = mysql_fetch_row ($_TABLES)) {
 		$_FIELDS = mysql_query ("SHOW COLUMNS FROM " . $_DATABASE . "." . $_TABLE [0]);
-		
-		?>
-		
-	unset($arrDefine);
 	
-	//----------------------------------------------------------------------------//
-	// Table: <?=$_TABLE [0] . "\n"?>
-	//----------------------------------------------------------------------------//
-	
-	$arrDefine['Name']		= "<?=$_TABLE [0]?>";
-	$arrDefine['Type']		= "InnoDB";
-	$arrDefine['Id']		= "Id";
-	$arrDefine['Index'][] 		= "";
-	$arrDefine['Unique'][] 		= "";
-		
-		<?
+		$strFileContents.= " 
+ 	
+	//----------------------------------------------------------------------------// 
+	// Table: ".$_TABLE [0] . "\n".
+"	//----------------------------------------------------------------------------// 
+	 
+	\$arrDefine['Name']		= \"".$_TABLE[0]."\"; 
+	\$arrDefine['Type']		= \"InnoDB\"; 
+	\$arrDefine['Id']		= \"Id\"; 
+	\$arrDefine['Index'][] 		= \"\"; 
+	\$arrDefine['Unique'][] 		= \"\"; 
+		 
+		";
 		
 		while ($_FIELD = mysql_fetch_assoc ($_FIELDS)) {
 			if ($_FIELD ['Field'] != "Id") {
@@ -89,23 +212,64 @@
 					$_FIELD ['ObLib'] = "dataCDATA";
 				}
 				
-				?>
-				
-	// Define Columns
-	$strName = "<?=$_FIELD ['Field']?>";
-		$arrDefine['Column'][$strName]['Type'] 			= "<?=$_FIELD ['RefType']?>";
-		$arrDefine['Column'][$strName]['SqlType'] 		= "<?=$_FIELD ['Type']?>";
-		$arrDefine['Column'][$strName]['Null'] 			= <?=(($_FIELD ['Null'] === "YES") ? "TRUE" : "FALSE")?>;
-		$arrDefine['Column'][$strName]['Default'] 		= <?=(($_FIELD ['Default'] === null) ? "null" : "\"" . $_FIELD ['Default'] . "\"")?>;
-		$arrDefine['Column'][$strName]['ObLib'] 		= "<?=$_FIELD ['ObLib']?>";
-		
-				<?
+				$strFileContents .= " 
+				 
+	// Define Columns 
+	\$strName = \"".$_FIELD ['Field']."\"; 
+		\$arrDefine['Column'][\$strName]['Type'] 			= \"".$_FIELD ['RefType']."\"; 
+		\$arrDefine['Column'][\$strName]['SqlType'] 		= \"".$_FIELD ['Type']."\"; 
+		\$arrDefine['Column'][\$strName]['Null'] 			= ".(($_FIELD ['Null'] === "YES") ? "TRUE" : "FALSE")."; 
+		\$arrDefine['Column'][\$strName]['Default'] 		= ".(($_FIELD ['Default'] === null) ? "null" : "\"" . $_FIELD ['Default'] . "\"")."; 
+		\$arrDefine['Column'][\$strName]['ObLib'] 		= \"".$_FIELD ['ObLib']."\"; 
+		 
+				";
 			}
 		}
-	?>	
+	$strFileContents .= "	 
 	// Save Table Define
-	$GLOBALS['arrDatabaseTableDefine'][$arrDefine['Name']] = $arrDefine;
-	<?
+	\$GLOBALS['arrDatabaseTableDefine'][\$arrDefine['Name']] = \$arrDefine; 
+	";
+	
 	}
 	
-?>
+	$strFileContents .= "\n\n?>";
+	
+	// Write to file
+	if ($_GET['l'])
+	{
+		$strFileLocation = $_GET['l']."database_define.php";
+	}
+	else
+	{
+		$strFileLocation = "/home/richdavis/vixen/framework/database_define.php";
+	}
+	
+	if (file_exists($strFileLocation))
+	{
+		// Delete backup file if one exists
+		if (file_exists($strFileLocation.".bak"))
+		{
+			echo "Deleting old backup file...\t".$strFileLocation.".bak\n";
+			unlink($strFileLocation.".bak");
+		}
+		
+		echo "Making backup file...\t\t".$strFileLocation.".bak\n";
+		// Make a backup
+		rename($strFileLocation, $strFileLocation.".bak");
+	}
+	echo "Writing new file...\t\t".$strFileLocation."\n";
+	try
+	{
+		$ptrFile = fopen($strFileLocation, "w");
+		fwrite($ptrFile, $strFileContents);
+		fclose($ptrFile);
+	}
+	catch (Exception $e)
+	{
+		echo "Unable to write file.\n\n".$e;
+		die();
+	}
+	echo "File successfully written!";
+	
+	?>
+</pre>
