60af852bd8d53b590480897c8585f47fce05add7
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2007-01-02 05:37:22 +0000

    x

--- rating_app/application.php
+++ rating_app/application.php
@@ -122,7 +122,7 @@ die();
 															"AND RateGroup.ServiceType = <ServiceType>" .
 															"AND Service.Id = ServiceRateGroup.Service");
 								
-		$strWhere					= "(ISNULL(ClosedOn) OR ClosedOn > <Date>)";
+		$strWhere					= "(ISNULL(ClosedOn) OR ClosedOn > <Date>) ";
 		$strWhere					.="AND (FNN = <FNN> OR (FNN != <FNN> AND Indial100 = 1 AND FNN LIKE CONCAT(SUBSTRING(<FNN>, -2, 2)), '__'))";		
 		$this->_selServiceByFNN		= new StatementSelect(	"Service",
 															"Id",
