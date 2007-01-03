e69d5d2ff87a60b1afd10d272231e0ea3f3bac47
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2007-01-03 03:20:54 +0000

    x

--- billing_app/modules/module_printing.php
+++ billing_app/modules/module_printing.php
@@ -567,14 +567,14 @@
 			$strInvoiceTable = 'Invoice';
 		}
 		$qryBuildFile	= new Query();
-		$strColumns		= "CONCAT('10', LPAD(CAST($strInvoiceTable.Id AS CHAR(10)), 10, ' '), InvoiceOutput.Data)";
+		$strColumns		= "CONCAT('10', LPAD(CAST($strInvoiceTable.Id AS CHAR), 10, ' '), InvoiceOutput.Data)";
 		$strWhere		= "InvoiceOutput.InvoiceRun = '$strInvoiceRun'";
 		$strQuery		=	"SELECT $strColumns INTO OUTFILE '$strFilename'\n" .
 							"FROM InvoiceOutput JOIN $strInvoiceTable USING (Account)\n".
 							"WHERE $strWhere\n";
 		if($bolSample)
 		{
-			$strQuery .= rand(0, (int)$arrMetaData['Invoices']).", ".BILL_PRINT_SAMPLE_LIMIT;
+			$strQuery .= "LIMIT ".rand(0, (int)$arrMetaData['Invoices']).", ".BILL_PRINT_SAMPLE_LIMIT;
 		}
 		$qryBuildFile->Execute($strQuery);
 		Debug($qryBuildFile->Error());
