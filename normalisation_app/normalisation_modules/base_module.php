54e76d4340186e23b93c9bb4ebe25ef4a36c1cc2
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2007-01-03 21:43:08 +0000

    ..

--- normalisation_app/normalisation_modules/base_module.php
+++ normalisation_app/normalisation_modules/base_module.php
@@ -240,7 +240,7 @@ abstract class NormalisationModule
 		$this->_rptNormalisationReport 	= $rptNormalisationReport;
 		
 		$this->_selFindOwner 			= new StatementSelect("Service", "AccountGroup, Account, Id", "FNN = <fnn> AND (CAST(<date> AS DATE) BETWEEN CreatedOn AND ClosedOn OR ISNULL(ClosedOn))", "CreatedOn DESC", "1");
-		$this->_selFindRecordType		= new StatementSelect("RecordType", "Id", "ServiceType = <ServiceType> AND Code = <Code>", "", "1");
+		$this->_selFindRecordType		= new StatementSelect("RecordType", "Id, Context", "ServiceType = <ServiceType> AND Code = <Code>", "", "1");
 		$this->_selFindRecordCode		= new StatementSelect("RecordTypeTranslation", "Code", "Carrier = <Carrier> AND CarrierCode = <CarrierCode>", "", "1");
 		$this->_selFindOwnerIndial100	= new StatementSelect("Service", "AccountGroup, Account, Id", "(FNN LIKE <fnn>) AND (Indial100 = TRUE)AND (CAST(<date> AS DATE) BETWEEN CreatedOn AND ClosedOn OR ISNULL(ClosedOn))", "CreatedOn DESC", "1");
 	
