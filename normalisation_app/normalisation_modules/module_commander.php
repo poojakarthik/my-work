45db12d0b800a2bdc43841eb3f11d852e87ef15a
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2007-01-03 21:58:00 +0000

    ..

--- normalisation_app/normalisation_modules/module_commander.php
+++ normalisation_app/normalisation_modules/module_commander.php
@@ -170,7 +170,9 @@ class NormalisationModuleCommander extends NormalisationModule
 		if ($this->_intContext > 0)
 		{
 			$mixCarrierCode 				= $this->_FetchRawCDR('RateId');
+			Debug($mixCarrierCode);
 			$arrDestinationCode 			= $this->FindDestination($mixCarrierCode);
+			Debug($arrDestinationCode['Description']);
 			if ($arrDestinationCode)
 			{
 				$this->_AppendCDR('DestinationCode', $arrDestinationCode['Code']);
