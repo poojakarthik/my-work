f5a6d4956ebbdea12fe6ed1f07e33d967706b57c
Rich Davis <rdavis@yellowbilling.com.au>
2007-01-03 06:40:08 +0000

    bug fix in billing app

--- billing_app/modules/module_printing.php
+++ billing_app/modules/module_printing.php
@@ -569,7 +569,7 @@
 		$qryBuildFile	= new Query();
 		$strColumns		= "CONCAT('10', LPAD(CAST($strInvoiceTable.Id AS CHAR), 10, ' '), InvoiceOutput.Data)";
 		$strWhere		= "InvoiceOutput.InvoiceRun = '$strInvoiceRun'";
-		$strQuery		=	"SELECT $strColumns INTO OUTFILE '$strFilename' FIELDS TERMINATED BY '' LINES TERMINATED BY '\\n'\n" .
+		$strQuery		=	"SELECT $strColumns INTO OUTFILE '$strFilename' FIELDS TERMINATED BY '' FIELDS ESCAPED BY '' LINES TERMINATED BY '\\n'\n" .
 							"FROM InvoiceOutput JOIN $strInvoiceTable USING (Account)\n".
 							"WHERE $strWhere\n";
 		if($bolSample)
