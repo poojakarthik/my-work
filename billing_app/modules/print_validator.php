bc0372091631bf5ca4c348d438f3d01b118af775
Rich Davis <rdavis@yellowbilling.com.au>
2007-01-09 06:39:41 +0000

    fixes in billing module

--- billing_app/modules/print_validator.php
+++ billing_app/modules/print_validator.php
@@ -44,7 +44,7 @@ require_once("../config.php");
 
 $strPath		= "/home/vixen_bill_output/sample/";
 $strPDFPath		= "/home/vixen_bill_output/sample/pdf/";
-$strFilename	= $strPath."sample".date("Y-m-d")."vbf";
+$strFilename	= $strPath."sample".date("Y-m-d").".vbf";
 
 $GLOBALS['FileFormat']	= $arrDefine;
 
