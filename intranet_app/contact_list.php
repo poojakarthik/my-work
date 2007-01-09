637f4c1ac3751aa9e7cd909a1f980e17760c2b99
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2007-01-09 01:14:48 +0000

    working on contact list

--- intranet_app/contact_list.php
+++ intranet_app/contact_list.php
@@ -6,14 +6,16 @@
 	// NOT FOR EXTERNAL DISTRIBUTION
 	//----------------------------------------------------------------------------//
 	
-	require ("config/application_loader.php");
+	// call application loader
+	require ('config/application_loader.php');
 	
-	// If the User is not logged into the system
-	if (!$athAuthentication->isAuthenticated ())
-	{
-		// Foward to Login Interface
-		header ("Location: login.php"); exit;
-	}
+	// set page details
+	$arrPage['PopUp']		= FALSE;
+	$arrPage['Permission']	= PERMISSION_OPERATOR;
+	$arrPage['Modules']		= MODULE_BASE | MODULE_SERVICE | MODULE_ACCOUNT;
+	
+	// call application
+	require ('config/application.php');
 	
 	$docDocumentation->Explain ("Account");
 	$docDocumentation->Explain ("Service");
