3c2157c94f987b2400dee3e97a50f3ab8e4e22d4
Rich Davis <rdavis@yellowbilling.com.au>
2007-01-16 02:49:58 +0000

    billing app debugging

--- billing_app/modules/module_printing.php
+++ billing_app/modules/module_printing.php
@@ -659,7 +659,7 @@
 			$strInvoiceTable = 'Invoice';
 		}
 		$qryBuildFile	= new Query();
-		$strColumns		= "'1000', LPAD(CAST($strInvoiceTable.Id AS CHAR), 10, '0'), InvoiceOutput.Data";
+		$strColumns		= "'0010', LPAD(CAST($strInvoiceTable.Id AS CHAR), 10, '0'), InvoiceOutput.Data";
 		$strWhere		= "InvoiceOutput.InvoiceRun = '$strInvoiceRun' AND InvoiceOutput.InvoiceRun = $strInvoiceTable.InvoiceRun";
 		$strQuery		=	"SELECT $strColumns INTO OUTFILE '$strFilename' FIELDS TERMINATED BY '' ESCAPED BY '' LINES TERMINATED BY '\\n'\n" .
 							"FROM InvoiceOutput JOIN $strInvoiceTable USING (Account)\n".
