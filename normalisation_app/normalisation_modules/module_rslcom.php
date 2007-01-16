72a6f7da71fba4a3c28eab7422b6843273209a7b
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2007-01-16 21:30:25 +0000

    ..

--- normalisation_app/normalisation_modules/module_rslcom.php
+++ normalisation_app/normalisation_modules/module_rslcom.php
@@ -285,7 +285,8 @@ class NormalisationModuleRSLCOM extends NormalisationModule
 		
 		// Cost
 		$mixValue						= $this->_FetchRawCDR('Price');
-		$mixValue						= str_replace($mixValue, '','$');
+		echo "$mixValue \n";
+		$mixValue						= str_replace('$', '', $mixValue);
 		echo "$mixValue \n";
 		$this->_AppendCDR('Cost', (float)$mixValue);
 		
