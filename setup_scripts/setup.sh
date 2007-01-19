589626a18d81c968eeae3f881c5d797556a4b329
Rich Davis <rdavis@yellowbilling.com.au>
2007-01-19 00:27:48 +0000

    updated setup script to create logging directories

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -27,9 +27,21 @@ mkdir -m 770 /home/vixen_bill_output/sample/pdf
 
 mkdir -m 700 /home/vixen_invoices
 
+mkdir -m 700 /home/vixen_log
+mkdir -m 700 /home/vixen_log/billing_app
+mkdir -m 700 /home/vixen_log/charges_app
+mkdir -m 700 /home/vixen_log/collection_app
+mkdir -m 700 /home/vixen_log/master
+mkdir -m 700 /home/vixen_log/mistress
+mkdir -m 700 /home/vixen_log/normalisation_app
+mkdir -m 700 /home/vixen_log/payment_app
+mkdir -m 700 /home/vixen_log/provisioning_app
+mkdir -m 700 /home/vixen_log/rating_app
+
 # chown dirs
 chown -R www-data.www-data /home/vixen_download
 chown -R www-data.www-data /home/vixen_import
 chown -R www-data.www-data /home/vixen_upload
 chown -R www-data.mysql /home/vixen_bill_output
 chown -R www-data.www-data /home/vixen_invoices
+chown -R www-data.www-data /home/vixen_log
