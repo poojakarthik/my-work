d6d3f2f8455573409c3c5f253f90774019b7aac6
Rich Davis <rdavis@yellowbilling.com.au>
2007-01-22 01:29:58 +0000

    fixed a couple of bugs

--- framework/framework.php
+++ framework/framework.php
@@ -495,7 +495,7 @@
 	 	else
 	 	{
 	 		$arrParams['fnn'] 	= substr((string)$strFNN, 0, -2) . "__";
-	 		$arrParams['date'] 	= ; //TODO!rich! todays date
+	 		$arrParams['date'] 	= date("Y-m-d", time()); //TODO!rich! todays date
 	 		$intResult = $this->_selFindOwnerIndial100->Execute($arrParams);
 	 		
 	 		if ($intResult === FALSE)
