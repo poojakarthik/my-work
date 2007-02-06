5cdb1ee535b7060d2718db07dfee8d0a92a2e928
Rich Davis <rdavis@yellowbilling.com.au>
2007-02-06 05:14:41 +0000

    bug fix in billing

--- billing_app/modules/module_etech.php
+++ billing_app/modules/module_etech.php
@@ -1109,7 +1109,7 @@
 				break;	
 		}
 		
-		$selMetaData = new StatementSelect($strInvoiceTable, "MIN(Id) AS MinId, MAX(Id) AS MaxId, COUNT(Id) AS Invoices, InvoiceRun", "Status = ".INVOICE_PRINT, NULL, NULL, "InvoiceRun");
+		$selMetaData = new StatementSelect($strInvoiceTable, "MIN(Id) AS MinId, MAX(Id) AS MaxId, COUNT(Id) AS Invoices, InvoiceRun", "1", NULL, NULL, "Status HAVING Status = ".INVOICE_PRINT);
 		if ($selMetaData->Execute() === FALSE)
 		{
 			Debug('$selMetaData : '.$selMetaData->Error());
