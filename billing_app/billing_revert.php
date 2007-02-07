5ab7b7ccdbac1719a21aba8525865b75425bd11a
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2007-02-07 02:21:16 +0000

    stuff

---
+++ billing_app/billing_revert.php
@@ -0,0 +1,37 @@
+<?php
+//----------------------------------------------------------------------------//
+// (c) copyright 2006 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// Revert a comitted billing run
+//----------------------------------------------------------------------------//
+
+// get invoice run
+$strInvoiceRun = trim($argv[1]);
+
+// die if no invoice run is set
+if (!$strInvoiceRun)
+{
+	echo("\n\n-- FAIL : No Invoice Run --\n");
+	Die();
+}
+
+// load application
+require_once('application_loader.php');
+
+// Application entry point - create an instance of the application object
+$appBilling = new ApplicationBilling($arrConfig);
+
+// revert bill
+$bolResponse = $appBilling->RevertInvoiceRun($strInvoiceRun);
+
+$appBilling->FinaliseReport();
+
+// finished
+echo("\n\n-- End of Billing --\n");
+die();
+
+?>
---
+++ billing_app/fuck_etech.php
@@ -0,0 +1,103 @@
+<?php
+//----------------------------------------------------------------------------//
+// (c) copyright 2006 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// Execute a billing run
+//----------------------------------------------------------------------------//
+
+// load application
+require_once('application_loader.php');
+
+// load etech modules
+require_once($strModuleDir."etech_reader.php");
+require_once($strModuleDir."etech_biller.php");
+
+$suxEtech = new EtechReader();
+$etbEtech = new EtechBiller();
+
+$stdout = fopen("php://stdout","w"); 
+function CliEcho($strOutput)
+{
+	$stdout = $GLOBALS['stdout'];
+	fwrite($stdout, $strOutput."\n");
+}
+CliEcho("Starting");
+
+// set location of file
+$strFilePath = "/home/vixen/etech_bills/2006/12/inv_telcoblue_20070105_1167948643.txt";
+
+// open file
+$suxEtech->OpenFile($strFilePath);
+
+// read file
+$intCount = 0;
+while($arrLine = $suxEtech->FetchNext())
+{
+	$intCount++;
+	if ($intCount > 1000)
+	{
+		break;
+	}
+	// check line type
+	switch($arrLine['_LineType'])
+	{
+		case 'ERROR':
+			CliEcho("Error at Line {$arrLine['_LineNo']}");
+			break;
+			
+		case 'DATA':
+			// check table type
+			switch ($arrLine['_Table'])
+			{
+				case 'CDR':
+					$mixReturn = $etbEtech->FindCDR($arrLine);
+					if ($mixReturn === FALSE)
+					{
+						CliEcho("CDR NOT FOUND for : ".$arrLine['FNN']);
+						print_r($arrLine);
+						Die;
+					}
+					else
+					{
+						CliEcho($arrLine['FNN']." = ".$mixReturn);
+					}
+					break;
+				
+				case 'ServiceTypeTotal':
+					// Ignore
+					break;
+					
+				case 'ServiceTotal':
+					// Ignore
+					break;
+					
+				case 'Invoice':
+					// Ignore
+					break;
+				
+				case 'Other':
+					// Ignore
+					break;
+					
+				default:
+					CliEcho("Unknown Table ({$arrLine['_Table']}) at Line {$arrLine['_LineNo']}");
+					break;
+			}
+			break;
+			
+		// match line
+		default:
+			CliEcho("Unknown Type at Line {$arrLine['_LineNo']}");
+			break;
+	}
+}
+
+CliEcho("\n\nDone");
+fclose($stdout);
+Die();
+
+?>
