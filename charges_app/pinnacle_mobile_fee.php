c89e1191062631d448eb47dee14e835df3bfabcd
Rich Davis <rdavis@yellowbilling.com.au>
2007-03-03 03:48:54 +0000

    work on charges engine

--- charges_app/pinnacle_mobile_fee.php
+++ charges_app/pinnacle_mobile_fee.php
@@ -14,6 +14,7 @@ require_once('application_loader.php');
 $appCharge = new ApplicationCharge($arrConfig);
 
 // Execute the application
+echo ("\n\nPinnacle Mobile Services");
 $intCount = $appCharge->AddPinnacleMobileFees();
 
 // finished
