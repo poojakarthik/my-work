19f5278691a342bc98c6cbae1d98b04e310e55d4
Rich Davis <rdavis@yellowbilling.com.au>
2007-03-05 04:17:57 +0000

    work on provisioning interface

---
+++ intranet_app/js/provisioning.js
@@ -0,0 +1,45 @@
+
+
+
+ 	//------------------------------------------------------------------------//
+	// ShowResidential
+	//------------------------------------------------------------------------//
+	/**
+	* ShowResidential()
+	*
+	* Shows Residential-only fields, and hides Business-only fields
+	*
+	* Shows Residential-only fields, and hides Business-only fields
+	*
+	* @method
+	*/
+	function ShowResidential(void)
+	{
+		// show each of the residential-only fields
+		document.getElementById('ResidentialSpecific').display	= 'block';
+		
+		// hide each of the business-only fields
+		document.getElementById('BusinessSpecific').display		= 'none';
+	}
+	
+ 	//------------------------------------------------------------------------//
+	// ShowBusiness
+	//------------------------------------------------------------------------//
+	/**
+	* ShowBusiness()
+	*
+	* Shows Business-only fields, and hides Residential-only fields
+	*
+	* Shows Business-only fields, and hides Residential-only fields
+	*
+	* @method
+	*/
+	function ShowBusiness(void)
+	{
+		// show each of the business-only fields
+		document.getElementById('BusinessSpecific').display		= 'block';
+		
+		// hide each of the residential-only fields
+		document.getElementById('ResidentialSpecific').display	= 'none';
+	}
+
--- intranet_app/xsl/content/service/provisioning/serviceaddress_update.xsl
+++ intranet_app/xsl/content/service/provisioning/serviceaddress_update.xsl
@@ -15,7 +15,7 @@
 		<!-- TODO!bash! [  DONE  ]		Rename this page and stick all of the 'Service address' pages in the provisioning folder where they belong -->
 		<script language="javascript" src="js/ABN.js"></script>
 		<script language="javascript" src="js/note_add.js"></script>
-		<!-- <script language="javascript" src="js/provisioning.js"</script> -->
+		<script language="javascript" src="js/provisioning.js"</script>
 		
 		<table border="0" width="100%" cellpadding="0" cellspacing="0">
 			<tr>
