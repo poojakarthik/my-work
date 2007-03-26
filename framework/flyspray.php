4c668776121f709960ca377f63d411254380ba94
Nathan Abussi <voiptel+nate@yellowbilling.com.au>
2007-03-26 06:17:16 +0000

    svn flyspray integration testing

--- framework/flyspray.php
+++ framework/flyspray.php
@@ -6,7 +6,11 @@ require_once("/usr/share/flyspray/htdocs/header.php");
 
 $user = new User(1);
 
-if(backend::add_comment(1, "TEST"))
+$arrTask = array();
+
+$arrTask['task_id'] = 1 ;
+
+if(backend::add_comment($arrTask, "TEST"))
 {
 	echo "True";
 }
