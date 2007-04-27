bb52b621348e00a2b66b0d59b9d74b78e17da2e2
Rich Davis <rdavis@yellowbilling.com.au>
2007-04-27 00:30:19 +0000

    remote_copy.php is now usable

--- setup_scripts/remote_update.php
+++ setup_scripts/remote_update.php
@@ -37,7 +37,8 @@ define("REMOTE_SERVER_CATWALK"	, 4);
 define("PATH_PACKAGE_VIXEN"		, 1);
 define("PATH_PACKAGE_BACKEND"	, 2);
 define("PATH_PACKAGE_INTRANET"	, 3);
-define("PATH_PACKAGE_OTHER"		, 4);
+define("PATH_PACKAGE_FRAMEWORK"	, 4);
+define("PATH_PACKAGE_OTHER"		, 5);
  
 //----------------------------------------------------------------------------//
 // Configuration
@@ -69,17 +70,18 @@ $arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'normalisation_app';
 $arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'payment_app';
 $arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'provisioning_app';
 $arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'rating_app';
-$arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'framework';
-$arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'import';
 $arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'backup_scripts';
 $arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'setup_scripts';
 $arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'screen_scrape';
-$arrConfig['Package']	[PATH_PACKAGE_BACKEND]		['Path']	[]	= 'oblib';
 
 $arrConfig['Package']	[PATH_PACKAGE_INTRANET]		['Name']		= 'Entire Intranet';
 $arrConfig['Package']	[PATH_PACKAGE_INTRANET]		['Path']	[]	= 'intranet_app';
-$arrConfig['Package']	[PATH_PACKAGE_INTRANET]		['Path']	[]	= 'oblib';
-$arrConfig['Package']	[PATH_PACKAGE_INTRANET]		['Path']	[]	= 'framework';
+$arrConfig['Package']	[PATH_PACKAGE_INTRANET]		['Path']	[]	= 'images';
+
+$arrConfig['Package']	[PATH_PACKAGE_FRAMEWORK]	['Name']		= 'Framework';
+$arrConfig['Package']	[PATH_PACKAGE_FRAMEWORK]	['Path']	[]	= 'oblib';
+$arrConfig['Package']	[PATH_PACKAGE_FRAMEWORK]	['Path']	[]	= 'framework';
+$arrConfig['Package']	[PATH_PACKAGE_FRAMEWORK]	['Path']	[]	= 'import';
 
 $arrConfig['Package']	[PATH_PACKAGE_OTHER]		['Name']		= 'Choose a different path';
 
@@ -164,6 +166,9 @@ $arrConfig['Server']	[REMOTE_SERVER_SKEL]	['RemotePath']	= 'root vixen directory
 	 */
 	function __construct($arrConfig)
 	{
+		// Call parent constructor
+		parent::__construct();
+		
 		// Set up config
 		$this->_arrConfig = $arrConfig;
 		
@@ -190,7 +195,7 @@ $arrConfig['Server']	[REMOTE_SERVER_SKEL]	['RemotePath']	= 'root vixen directory
 	function RemoteCopy()
 	{
 		// Create interface
-		$mixServer = $this->_DrawInterface($this->_arrConfig['Server'], "Please choose a server to update:");
+		$mixServer = $this->_DrawMenu($this->_arrConfig['Server'], "Please choose a server to update:");
 		if ($mixServer === FALSE)
 		{
 			// return
@@ -199,23 +204,50 @@ $arrConfig['Server']	[REMOTE_SERVER_SKEL]	['RemotePath']	= 'root vixen directory
 		}
 		
 		// Choose package or path to copy
-		$mixPackage = $this->_DrawInterface($this->_arrConfig['Package'], "Please choose the package to copy:");
+		$mixPackage = $this->_DrawMenu($this->_arrConfig['Package'], "Please choose the package to copy:");
 		if ($mixPackage === FALSE)
 		{
 			// return
 			$this->_EchoCLS();
 			return TRUE;
 		}
-		$mixPaths = $this->_arrConfig['Package'][$mixPackage]['Path'];
+		if ($mixPackage === PATH_PACKAGE_OTHER)
+		{
+			// Prompt the user for a path
+			$arrPaths[] = $this->_DrawPrompt("Please enter a path to copy:", Array(&$this, "_VixenFileExists"), "Invalid path specified!", $this->_arrConfig['Application']['LocalPath']);
+		}
+		else
+		{
+			// Get the list of paths to copy
+			$arrPaths = $this->_arrConfig['Package'][$mixPackage]['Path'];
+		}
 		
 		// Choose Copy mode
-		$mixMode = $this->_DrawInterface($this->_arrConfig['CopyMode'], "Please choose the copy mode:");
+		$mixMode = $this->_DrawMenu($this->_arrConfig['CopyMode'], "Please choose the copy mode:");
 		if ($mixMode === FALSE)
 		{
 			// return
 			$this->_EchoCLS();
 			return TRUE;
 		}
+		
+		// Confirmation
+		$mixConfirm = $this->_DrawPrompt("Are you sure you want to copy these files?", "/^(yes)|(no)$/misU", "Please type 'yes' or 'no'", "[Yes | No] ? ");
+		switch (true)
+		{
+			// Yes; continue
+			case (strtoupper($mixConfirm) == 'YES'):
+				break;
+				
+			// No; exit
+			default:
+				// return
+				$this->_EchoCLS();
+				return TRUE;
+		}
+		
+		// Clear screen to allow for nice output
+		$this->_EchoCLS();
 		
 		// Instanciate the RemoteCopy object
 		$rcpRemoteCopy	= NULL;
@@ -226,15 +258,16 @@ $arrConfig['Server']	[REMOTE_SERVER_SKEL]	['RemotePath']	= 'root vixen directory
 		{
 			case PROTOCOL_SSH2:
 				$rcpRemoteCopy = new RemoteCopySSH($strServer, $strUserName, $strPassword);
+				$strMessage = "Connected to SSH2 server '$strServer'!\n\n";
 				break;
 			
 			case PROTOCOL_FTP:
 				$rcpRemoteCopy = new RemoteCopyFTP($strServer, $strUserName, $strPassword);
+				$strMessage = "Connected to FTP server '$strServer'!\n\n";
 				break;
 			
 			default:
 				// return
-				$this->_EchoCLS();
 				echo "Fatal Error: Invalid Protocol Specified!\n\n";
 				return FALSE;
 		}
@@ -243,38 +276,51 @@ $arrConfig['Server']	[REMOTE_SERVER_SKEL]	['RemotePath']	= 'root vixen directory
 		if (is_string($mixResult = $rcpRemoteCopy->Connect()))
 		{
 			// return
-			$this->_EchoCLS();
 			echo "Fatal Error: $mixResult\n\n";
 			return FALSE;
 		}
 		
+		echo $strMessage;
+		ob_flush();
+		
 		// Copy
-		foreach ($mixPaths as $strPath)
+		$this->Framework->StartWatch();
+		foreach ($arrPaths as $strPath)
 		{
 			$strLocalPath	= $this->_arrConfig['Application']['LocalPath'].$strPath;
 			$strRemotePath	= $this->_arrConfig['Server']['RemotePath'].$strPath;
-			//$rcpRemoteCopy->Copy($strLocalPath, $strRemotePath, $mixMode);
+			$rcpRemoteCopy->Copy($strLocalPath, $strRemotePath, $mixMode);
+		}
+		
+		// Success!
+		$intTime = $this->Framework->SplitWatch();
+		echo "Copy Completed in $intTime seconds.\n\n";
+		
+		// Disconnect
+		if (is_string($mixResult = $rcpRemoteCopy->Disconnect()))
+		{
+			echo "Warning: $mixResult\n\n";
 		}
 		
 		return TRUE;
 	}
 
 	//------------------------------------------------------------------------//
-	// _DrawInterface
+	// _DrawMenu
 	//------------------------------------------------------------------------//
 	/**
-	 * _DrawInterface()
+	 * _DrawMenu()
 	 *
 	 * Draws the Main menu, and retrieves user input
 	 *
 	 * Draws the Main menu, and retrieves user input
 	 * 
 	 * @return	mixed		FALSE	: exit the program
-	 * 						integer	: Server to copy to
+	 * 						integer	: menu option selected
 	 *
 	 * @method
 	 */
-	function _DrawInterface($arrItems, $strTitle)
+	function _DrawMenu($arrItems, $strTitle)
 	{
 		// Clear the screen
 		$this->_EchoCLS();
@@ -323,6 +369,135 @@ $arrConfig['Server']	[REMOTE_SERVER_SKEL]	['RemotePath']	= 'root vixen directory
 			return $intInput;
 		}
 	}
+
+	//------------------------------------------------------------------------//
+	// _DrawPrompt
+	//------------------------------------------------------------------------//
+	/**
+	 * _DrawPrompt()
+	 *
+	 * Draws a prompt, and retrieves user input
+	 *
+	 * Draws a prompt, and retrieves user input
+	 * 
+	 * @param	string		$strTitle		Title for the prompt
+	 * @param	mixed		$mixValidate	optional Regular Expression to use for validation, or Custom Validation method for us in call_user_func()
+	 * @param	string		$strPrompt		optional Custom prompt text (defaults to '? ')
+	 * 
+	 * @return	mixed		FALSE	: exit the program
+	 * 						string	: input string
+	 *
+	 * @method
+	 */
+	function _DrawPrompt($strTitle, $mixValidate = NULL, $strError = "Invalid input, please try again!", $strPrompt = '? ')
+	{
+		// Clear the screen
+		$this->_EchoCLS();
+		
+		// Draw the application title
+		$this->_EchoPos($this->_arrConfig['Application']['Title'], $this->_arrConfig['Application']['TitleX'], $this->_arrConfig['Application']['TitleY']);
+		
+		// Draw the prompt title
+		//$this->_EchoPos("Please select a server to update:", $this->_arrConfig['Application']['MenuTitleX'], $this->_arrConfig['Application']['MenuTitleY']);
+		$this->_EchoPos($strTitle, $this->_arrConfig['Application']['MenuTitleX'], $this->_arrConfig['Application']['MenuTitleY']);
+		
+		// Draw the prompt window
+		$intMinX	= $this->_arrConfig['Application']['MenuX'];
+		$intMinY	= $this->_arrConfig['Application']['MenuY']+1;
+		/*$intWidth	= 60;
+		$intHeight	= 3;
+		$this->_DrawWindow($intMinX, $intMinY, $intWidth, $intHeight);*/
+		
+		// Await input
+		$strInput = NULL;
+		while ($strInput === FALSE || $strInput === NULL)
+		{
+			// Was there bad input?
+			if ($strInput === FALSE)
+			{
+				// Write error message
+				$this->_EchoPos(str_repeat(' ', 80), $intMinX, $intMinY+2);
+				$this->_EchoPos("\033[0;31m".$strError."\033[0;30m", $intMinX, $intMinY+2, FALSE);
+			}
+			
+			// Redraw the prompt line
+			$this->_EchoPos(str_repeat(' ', 80), $intMinX, $intMinY);
+			$this->_EchoPos($strPrompt, $intMinX, $intMinY, FALSE);
+			ob_flush();
+			
+			// Get input
+			$strInput = trim(fgets(STDIN));
+			
+			// Validate
+			switch (true)
+			{
+				// Run validation function
+				case is_array($mixValidate):
+				case function_exists($mixValidate):
+					$strInput = (call_user_func($mixValidate, $strInput)) ? $strInput : FALSE;
+					break;
+				
+				// Run regex
+				case $mixValidate:
+					$strInput = (preg_match($mixValidate, $strInput)) ? $strInput : FALSE;
+					break;
+			}
+		}
+		
+		return $strInput;
+	}
+
+	//------------------------------------------------------------------------//
+	// _DrawWindow
+	//------------------------------------------------------------------------//
+	/**
+	 * _DrawWindow()
+	 *
+	 * Draws an ASCII window
+	 *
+	 * Draws an ASCII window
+	 * 
+	 * @param	string		$strTitle		Title for the prompt
+	 * @param	string		$strValidate	optional Regular Expression to use for validation, or Custom Validation method
+	 * @param	string		$strPrompt		optional Custom prompt text (defaults to '? ')
+	 * 
+	 * @return	mixed		FALSE	: exit the program
+	 * 						string	: input string
+	 *
+	 * @method
+	 */
+	function _DrawWindow($intMinX, $intMinY, $intWidth, $intHeight, $bolShadow = TRUE)
+	{		
+		// Draw the Window
+		
+		// Set the draw colour
+		echo "\033[0;47m";
+		ob_flush();
+		
+		// foreach row
+		for ($intRow = $intMinY; $intRow < ($intMinY+$intHeight); $intRow++)
+		{
+			// Draw a Line
+			$strOutput = str_repeat(' ', $intWidth);
+			echo "\033[$intRow;{$intMinX}H".$strOutput;
+		}
+		
+		// Draw the window shadow
+		if ($bolShadow)
+		{
+			// Right shadow
+			// TODO
+			
+			// Bottom Shadow
+			// TODO
+		}
+		ob_flush();
+		
+		// Return Draw colour to Black
+		echo "\033[0;30m";
+		ob_flush();
+	}
+	
 
 	//------------------------------------------------------------------------//
 	// _EchoPos
@@ -381,6 +556,27 @@ $arrConfig['Server']	[REMOTE_SERVER_SKEL]	['RemotePath']	= 'root vixen directory
 			$this->_EchoPos(str_repeat(' ', 80), 0, $i);
 		}
 		$this->_EchoPos("", 0, 0, FALSE);
+	}
+
+	//------------------------------------------------------------------------//
+	// _VixenFileExists
+	//------------------------------------------------------------------------//
+	/**
+	 * _VixenFileExists()
+	 *
+	 * Performs a file_exists() for a viXen path
+	 *
+	 * Performs a file_exists() for a viXen path
+	 * 
+	 * @param	string	$strPath	viXen path to validate
+	 * 
+	 * @return	boolean
+	 *
+	 * @method
+	 */
+	function _VixenFileExists($strPath)
+	{
+		return (bool)file_exists($this->_arrConfig['Application']['LocalPath'].$strPath);
 	}
 	
 	
