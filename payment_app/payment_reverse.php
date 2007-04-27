21bc85fa2c016153a96a29acb75b60457b949d18
Rich Davis <rdavis@yellowbilling.com.au>
2007-04-27 02:21:49 +0000

    added script to test payment reversal

---
+++ payment_app/payment_reverse.php
@@ -0,0 +1,25 @@
+<?php
+//----------------------------------------------------------------------------//
+// (c) copyright 2006 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// import and process payments
+//----------------------------------------------------------------------------//
+require_once('application_loader.php');
+
+// Application entry point - create an instance of the application object
+$appPayment = new ApplicationPayment($arrConfig);
+
+// Execute the application
+$appPayment->ReversePayment(35643);
+$appPayment->ReversePayment(34942, 22);
+
+// finished
+echo("\n-- End of Payments --\n");
+echo "</pre>";
+die();
+
+?>
