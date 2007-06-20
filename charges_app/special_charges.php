8ebd2f7711eea412fdf0aae6ed57cdd930871b6c
Rich Davis <rdavis@yellowbilling.com.au>
2007-06-20 05:18:37 +0000

    prepared charges app for automation

--- charges_app/special_charges.php
+++ charges_app/special_charges.php
@@ -1,24 +1,48 @@
 <?php
 //----------------------------------------------------------------------------//
-// (c) copyright 2006 VOIPTEL Pty Ltd
+// (c) copyright 2007 VOIPTEL Pty Ltd
 //
 // NOT FOR EXTERNAL DISTRIBUTION
 //----------------------------------------------------------------------------//
 
 //----------------------------------------------------------------------------//
-// add recurring charges
+// add special charges
 //----------------------------------------------------------------------------//
 require_once('application_loader.php');
 
 // Application entry point - create an instance of the application object
 $appCharge = new ApplicationCharge($arrConfig);
 
-// Execute the application
-$appCharge->Execute();
+// Remove/Mark Inbound S&E CDRs
+$appCharge->MarkInboundSAndECDR();
+
+// Add Landline S&E Credits
+$appCharge->AddLLSAndECredits();
+
+// Add Inbound Service Fee
+$appCharge->AddActiveInboundFees();
+
+// Add Pinnacle Mobile Fees
+$appCharge->AddPinnacleMobileFees();
 
 // finished
-echo("\n-- End of Charges::Recurring --\n");
-echo "</pre>";
-die();
+echo("\n-- End of Charges::Special --\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
