ef2fe11594091bc255ddfb1ac41bb42461e3897e
Jared Herbohn <voiptel+flame@yellowbilling.com.au>
2007-06-26 06:17:49 +0000

    added page link

---
+++ ui_app/vixen.php
@@ -0,0 +1,14 @@
+<?php
+
+// Work out the application template and method from the URL
+// takes a URL like : http://.../vixen.php/ApplicationTemplate/Method/?Object.Property=Value
+$arrScript 		= explode('.php', $_SERVER['PHP_SELF'], 2);
+$strScript 		= ltrim($arrScript[1], '/'); 
+$arrScript 		= explode('/', $strScript);
+$strTemplate 	= $arrScript[0];
+$strMethod 		= $arrScript[1];
+
+// load stuff here
+//TODO!!!!
+
+?>
