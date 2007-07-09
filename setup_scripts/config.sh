513d61a955a189522f0c3c4de4cb68316f293149
Rich Davis <rdavis@yellowbilling.com.au>
2007-07-09 03:46:30 +0000

    fixed (?) new config.sh

--- setup_scripts/config.sh
+++ setup_scripts/config.sh
@@ -13,49 +13,8 @@ mkdir -m 755 /etc/vixen
 rm vixen.conf
 
 # write conf file
-FileVixenConf="<?php
-//----------------------------------------------------------------------------//
-// /etc/vixen/vixen.conf
-//----------------------------------------------------------------------------//
+FileVixenConf="<?php\n//----------------------------------------------------------------------------//\n// /etc/vixen/vixen.conf\n//----------------------------------------------------------------------------//\n\n// Data Access constants\n\n\$strDBServer = 'DPS';\n//\$strDBServer = 'CATWALK';\n//\$strDBServer = 'MINX';\n\n// Defaults\n\$strDBUser        = 'vixen';\n\$strDBPassword    = 'V1x3n';\n\$strDBDatabase    = 'vixen';\nswitch ($strDBServer)\n{\n	case 'DPS':\n		\$strDBURL		= '10.11.12.13';\n		break;\n\n	case 'MINX':\n		\$strDBURL		= '10.11.12.16';\n		break;\n	\n	case 'CATWALK':\n		\$strDBURL		= '10.11.12.14';\n		\$strDBPassword		= 'vixen';\n		\$strDBDatabase		= 'vixenworking';\n		break;\n\n	default:\n		throw new Exception('Bad Database Connection Definition');\n		die;\n}\n\n\n\$GLOBALS['**arrDatabase']['URL']		= \$strDBURL;\n\$GLOBALS['**arrDatabase']['User']		= \$strDBUser;\n\$GLOBALS['**arrDatabase']['Password']	= \$strDBPassword;\n\$GLOBALS['**arrDatabase']['Database']	= \$strDBDatabase;\n?>\n"
 
-// Data Access constants
-
-\$strDBServer = 'DPS';
-//\$strDBServer = 'CATWALK';
-//\$strDBServer = 'MINX';
-
-// Defaults
-\$strDBUser        = 'vixen';
-\$strDBPassword    = 'V1x3n';
-\$strDBDatabase    = 'vixen';
-switch ($strDBServer)
-{
-	case 'DPS':
-		\$strDBURL		= '10.11.12.13';
-		break;
-
-	case 'MINX':
-		\$strDBURL		= '10.11.12.16';
-		break;
-	
-	case 'CATWALK':
-		\$strDBURL		= '10.11.12.14';
-		\$strDBPassword		= 'vixen';
-		\$strDBDatabase		= 'vixenworking';
-		break;
-
-	default:
-		throw new Exception('Bad Database Connection Definition');
-		die;
-}
-
-
-\$GLOBALS['**arrDatabase']['URL']		= \$strDBURL;
-\$GLOBALS['**arrDatabase']['User']		= \$strDBUser;
-\$GLOBALS['**arrDatabase']['Password']	= \$strDBPassword;
-\$GLOBALS['**arrDatabase']['Database']	= \$strDBDatabase;
-?>
-"
 echo "$FileVixenConf" > /etc/vixen/vixen.conf
 
 # set permissions
