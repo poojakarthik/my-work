3a9382b9833822d326c5a5c6aa7e3902d6a36f4e
Rich Davis <rdavis@yellowbilling.com.au>
2007-08-17 04:09:41 +0000

    * completed Unitel DSC module

---
+++ provisioning_app/modules/unitel/import_dsc.php
@@ -0,0 +1,400 @@
+<?php
+//----------------------------------------------------------------------------//
+// (c) copyright 2007 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// import_dsc
+//----------------------------------------------------------------------------//
+/**
+ * import_dsc
+ *
+ * Parses a Unitel Daily Status Change file
+ *
+ * Parses a Unitel Daily Status Change file
+ *
+ * @file		import_dsc.php
+ * @language	PHP
+ * @package		provisioning
+ * @author		Rich "Waste" Davis
+ * @version		7.07
+ * @copyright	2007 VOIPTEL Pty Ltd
+ * @license		NOT FOR EXTERNAL DISTRIBUTION
+ *
+ */
+
+//----------------------------------------------------------------------------//
+// ImportUnitelDSC
+//----------------------------------------------------------------------------//
+/**
+ * ImportUnitelDSC
+ *
+ * Parses a Unitel Daily Status Change file
+ *
+ * Parses a Unitel Daily Status Change file
+ *
+ * @prefix		imp
+ *
+ * @package		provisioning
+ * @class		ImportUnitelDSC
+ */
+ class ImportUnitelDSC extends ImportBase
+ {
+ 	//------------------------------------------------------------------------//
+	// __construct
+	//------------------------------------------------------------------------//
+	/**
+	 * __construct()
+	 *
+	 * Constructor
+	 *
+	 * Constructor
+	 * 
+	 * @return	ImportBase
+	 *
+	 * @method
+	 */
+ 	function __construct()
+ 	{
+ 		// Parent Constructor
+ 		parent::__construct();
+ 		
+ 		// Carrier
+ 		$this->intCarrier = 1;
+		
+		//##----------------------------------------------------------------##//
+		// Define File Format
+		//##----------------------------------------------------------------##//
+		
+		// define row start (account for header rows)
+		// Row numbers start at 1
+		// for a file without any header row, set this to 1
+		// for a file with 1 header row, set this to 2
+		$this->_intStartRow = 2;
+ 		
+ 		// Delimiter
+ 		$this->_strDelimiter	= "";
+ 		
+		$arrDefine['RecordType']	['Start']		= 0;
+		$arrDefine['RecordType']	['Length']		= 1;
+		
+		$arrDefine['Sequence']		['Start']		= 1;
+		$arrDefine['Sequence']		['Length']		= 5;
+		
+		$arrDefine['OrderId']		['Start']		= 6;
+		$arrDefine['OrderId']		['Length']		= 9;
+
+		$arrDefine['OrderType']		['Start']		= 15;
+		$arrDefine['OrderType']		['Length']		= 2;
+		
+		$arrDefine['OrderYear']		['Start']		= 17;
+		$arrDefine['OrderYear']		['Length']		= 4;
+		
+		$arrDefine['OrderMonth']	['Start']		= 21;
+		$arrDefine['OrderMonth']	['Length']		= 2;
+		
+		$arrDefine['OrderDay']		['Start']		= 23;
+		$arrDefine['OrderDay']		['Length']		= 2;
+		
+		$arrDefine['ServiceNo']		['Start']		= 25;
+		$arrDefine['ServiceNo']		['Length']		= 29;
+		
+		$arrDefine['Basket']		['Start']		= 54;
+		$arrDefine['Basket']		['Length']		= 3;
+		
+		$arrDefine['EffectiveYear']	['Start']		= 57;
+		$arrDefine['EffectiveYear']	['Length']		= 4;
+		
+		$arrDefine['EffectiveMonth']['Start']		= 61;
+		$arrDefine['EffectiveMonth']['Length']		= 2;
+		
+		$arrDefine['EffectiveDay']	['Start']		= 63;
+		$arrDefine['EffectiveDay']	['Length']		= 2;
+		
+		$arrDefine['NewNo']			['Start']		= 65;
+		$arrDefine['NewNo']			['Length']		= 29;
+		
+		$arrDefine['ReasonCode']	['Start']		= 94;
+		$arrDefine['ReasonCode']	['Length']		= 3;
+		
+		$arrDefine['LostTo']		['Start']		= 97;
+		$arrDefine['LostTo']		['Length']		= 3;
+		
+		$arrDefine['RSLReference']	['Start']		= 100;
+		$arrDefine['RSLReference']	['Length']		= 9;
+		
+		$this->_arrDefine = $arrDefine;
+ 	}
+ 	
+ 	//------------------------------------------------------------------------//
+	// PreProcess
+	//------------------------------------------------------------------------//
+	/**
+	 * PreProcess()
+	 *
+	 * Pre-processes a file
+	 *
+	 * Pre-processes a file
+	 * 
+	 * @param	array	$arrRawData		File Data to parse
+	 * 
+	 * @return	array					Parsed data
+	 *
+	 * @method
+	 */
+ 	function PreProcess($arrRawData)
+ 	{
+ 		// foreach line
+ 		$strLastFNN		= NULL;
+ 		$strNewLine		= "";
+ 		$arrParsedData	= Array();
+ 		foreach ($arrRawData as $strLine)
+ 		{
+ 			// Check the FNN
+ 			$strFNN = trim(substr($strLine, 25, 29));
+ 			//Debug($strFNN);
+ 			
+ 			// If this line has the same FNN as the last line, merge
+ 			if ($strFNN === $strLastFNN)
+ 			{
+ 				$strNewLine			.= $strLine;
+ 			}
+ 			elseif ($strNewLine)
+ 			{
+ 				$arrParsedData[]	= $strNewLine;
+ 				$strNewLine			= $strLine;
+ 			}
+ 			
+ 			$strLastFNN = $strFNN;
+ 		}
+ 		
+ 		// Return the new data
+ 		//Debug($arrParsedData);
+ 		return $arrParsedData;
+ 	}
+ 	
+ 	//------------------------------------------------------------------------//
+	// Normalise
+	//------------------------------------------------------------------------//
+	/**
+	 * Normalise()
+	 *
+	 * Normalises a line from a Provisioning File
+	 *
+	 * Normalises a line from a Provisioning File
+	 * 
+	 * @param	string	$strLine		Line to parse
+	 * 
+	 * @return	array					Parsed data
+	 *
+	 * @method
+	 */
+ 	function Normalise($strLine)
+ 	{
+ 		// Split the line on \n
+ 		$arrLines = explode("\n", trim($strLine));
+ 		
+ 		// There should be 5-6 lines, 1 for each basket
+ 		$arrPDR		= Array();
+ 		$arrBaskets	= Array();
+ 		foreach ($arrLines as $strSubLine)
+ 		{
+ 			// Split the sub-line using the file definition
+ 			$arrData = $this->_SplitLine($strSubLine);
+ 			
+ 			//Debug($arrData);
+ 			
+ 			//----------------------------------------------------------------//
+ 			// FNN
+ 			$arrPDR['FNN']	= trim($arrData['ServiceNo']);
+ 			//----------------------------------------------------------------//
+ 			
+ 			//----------------------------------------------------------------//
+ 			// Type
+			switch ($arrData['OrderType'])
+			{
+				case "11":	// Migration Request
+				case "12":	// Churn to eBill
+					$arrPDR['Type']	= REQUEST_FULL_SERVICE;
+					break;
+				case "13":	// Virtual PreSelection
+					$arrPDR['Type']	= REQUEST_PRESELECTION;
+					break;
+				case "52":
+					$arrPDR['Type']	= REQUEST_FULL_SERVICE_REVERSE;
+					break;
+				case "53":
+					$arrPDR['Type']	= REQUEST_PRESELECTION_REVERSE;
+					break;
+				default:
+					// Either unhandled or not required
+					break;
+			}
+ 			switch ($arrData['RecordType'])
+ 			{
+				case "S":	// Gain - new service
+					$arrPDR['Type']			= REQUEST_FULL_SERVICE;
+					$arrPDR['Description']	= "Service Gained";
+					
+					// This can span over multiple lines in the file
+					$arrBaskets[]	= (int)$arrData['Basket'];
+					break;
+					
+				case "G":	// Gain - reversal
+					$arrPDR['Type']			= REQUEST_FULL_SERVICE;
+					$arrPDR['Description']	= "Service Gained by Reversal";
+					
+					// This can span over multiple lines in the file
+					$arrBaskets[]	= (int)$arrData['Basket'];
+					break;
+					
+				case "E":	// Loss - commercial churn
+				case "O":	// Loss - other ASD
+				case "L":	// Loss - other CSP
+					if ($arrPDR['Type'] || (int)$arrData['Basket'] != 6)
+					{
+						// If there are multiple lines, Full Service Loss
+						$arrPDR['Type']			= REQUEST_LOSS_FULL;
+						$arrPDR['Description']	= "Service lost to Carrier #{$arrData['LostTo']}";
+					}
+					else
+					{
+						// If there is only Basket 6, then Preslection Loss
+						$arrPDR['Type']			= REQUEST_LOSS_PRESELECT;
+						$arrPDR['Description']	= "Preselection lost to Carrier #{$arrData['LostTo']}";
+					}
+					
+					// This can span over multiple lines in the file
+					$arrBaskets[]	= (int)$arrData['Basket'];
+					break;
+					
+				case "X":	// Loss - cancellation
+					$arrPDR['Type']			= REQUEST_LOSS_FULL;
+					$arrPDR['Description']	= "Service Cancelled";
+					
+					// This can span over multiple lines in the file
+					$arrBaskets[]	= (int)$arrData['Basket'];
+					break;
+					
+				case "N":	// Change - number
+				case "M":	// Change - address
+				case "B":	// Change - number & address
+					$arrPDR['Type']				= REQUEST_CHANGE_ADDRESS;
+					$arrPDR['Description']		= "Address Changed";
+					break;
+					
+				case "P":	// Order pending with Telstra
+					$intRequestStatus			= REQUEST_STATUS_PENDING;
+					$arrPDR['Description']		= "Order Pending with Telstra";
+					break;
+					
+				case "W":	// Order waiting to be processed
+					$arrPDR['RequestStatus']	= REQUEST_STATUS_PENDING;
+					$arrPDR['Description']		= "Order Pending with Unitel";
+					break;
+					
+				case "A":	// Order actioned by WeBill
+					$arrPDR['RequestStatus']	= REQUEST_STATUS_PENDING;
+					$arrPDR['Description']		= "Order accepted by Unitel";
+					break;
+					
+				case "D":	// Order disqualified by WeBill
+					$arrPDR['RequestStatus']	= REQUEST_STATUS_REJECTED;
+					$arrPDR['Description']		= "Order Rejected by Unitel - <Reason>";
+					break;
+					
+				case "R":	// Order rejected by Telstra
+					$arrPDR['RequestStatus']	= REQUEST_STATUS_REJECTED;
+					$arrPDR['Description']		= "Order Rejected by Telstra - <Reason>";
+					break;
+					
+				case "C":	// Order completed by Telstra
+					$arrPDR['RequestStatus']	= REQUEST_STATUS_COMPLETED;
+					$arrPDR['Description']		= "Order completed by Telstra";
+					break;
+					
+				default:	// Unknown Record Type
+					return RESPONSE_STATUS_CANT_NORMALISE;
+ 			}
+ 			//----------------------------------------------------------------//
+ 			
+ 			//----------------------------------------------------------------//
+ 			// CarrierRef
+ 			$arrPDR['CarrierRef'] = (int)$arrData['OrderId'];
+ 			//----------------------------------------------------------------//
+ 			
+ 			//----------------------------------------------------------------//
+ 			// Description
+ 			// Handled Elsewhere
+ 			//----------------------------------------------------------------//
+ 			
+ 			//----------------------------------------------------------------//
+ 			// EffectiveDate
+ 			if ($arrData['EffectiveYear'])
+ 			{
+ 				$arrPDR['EffectiveDate']	= "{$arrData['EffectiveYear']}-{$arrData['EffectiveMonth']}-{$arrData['EffectiveDay']}";
+ 			}
+ 			if ($arrData['OrderYear'])
+ 			{
+ 				$arrPDR['OrderDate']		= "{$arrData['OrderYear']}-{$arrData['OrderMonth']}-{$arrData['OrderDay']}";
+ 			}
+ 			//----------------------------------------------------------------//
+ 		}
+ 		
+		//----------------------------------------------------------------//
+		// Find Owner
+		if ($arrOwner = FindFNNOwner($arrPDR['FNN'], $arrPDR['EffectiveDate']))
+		{
+			$arrPDR = array_merge($arrOwner, $arrPDR);
+		}
+		else
+		{
+			$arrPDR['Status']	= RESPONSE_STATUS_BAD_OWNER;
+		}
+		//----------------------------------------------------------------//
+ 		
+ 		if (!$arrPDR['Type'])
+ 		{
+ 			Debug($arrData);
+ 		}
+ 		
+ 		return $arrPDR;
+ 	}
+ 	
+ 	
+ 	//------------------------------------------------------------------------//
+	// LinkToRequest
+	//------------------------------------------------------------------------//
+	/**
+	 * LinkToRequest()
+	 *
+	 * Attempts to link a Response to a Request
+	 *
+	 * Attempts to link a Response to a Request
+	 * 
+	 * @param	array	$arrResponse	Response to match against
+	 * 
+	 * @return	integer					Request Id
+	 *
+	 * @method
+	 */
+	 function LinkToRequest($arrResponse)
+	 {
+	 	// Match by CarrierRef
+	 	if ($arrResponse['CarrierRef'])
+	 	{
+	 		if ($this->_selRequestByCarrierRef->Execute($arrResponse))
+	 		{
+	 			// Found a match, return the Id
+	 			$arrReturn = $this->_selRequestByCarrierRef->Fetch();
+	 			return $arrReturn['Id'];
+	 		}
+	 	}
+	 	
+	 	// Run the default matcher
+	 	return parent::LinkToRequest($arrResponse);
+	 }
+ }
+?>
