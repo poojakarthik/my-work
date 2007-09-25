2c7e5235167d47c1bfccccf949fcc500f02a0dd9
Rich Davis <rdavis@yellowbilling.com.au>
2007-09-25 02:44:41 +0000

    fixed a bill printing bug

--- billing_app/billing_regenerate.php
+++ billing_app/billing_regenerate.php
@@ -20,6 +20,99 @@ $appBilling = new ApplicationBilling($arrConfig);
 
 // ACCOUNTS
 $arrAccounts = Array();
+
+// Billing Samples
+// Voicetalk customers
+$arrAccounts[]	= 1000162906;
+$arrAccounts[]	= 1000162059;
+$arrAccounts[]	= 1000159470;
+$arrAccounts[]	= 1000162528;
+$arrAccounts[]	= 1000007031;
+$arrAccounts[]	= 1000162445;
+$arrAccounts[]	= 1000161645;
+$arrAccounts[]	= 1000162437;
+$arrAccounts[]	= 1000162825;
+$arrAccounts[]	= 1000159466;
+$arrAccounts[]	= 1000159893;
+$arrAccounts[]	= 1000010134;
+$arrAccounts[]	= 1000161816;
+$arrAccounts[]	= 1000163105;
+//$arrAccounts[]	= 1000161744;
+
+// w/ Inbound Services
+$arrAccounts[]	= 1000157275;
+$arrAccounts[]	= 1000157088;
+$arrAccounts[]	= 1000162420;
+$arrAccounts[]	= 1000155448;
+$arrAccounts[]	= 1000157889;
+$arrAccounts[]	= 1000155226;
+$arrAccounts[]	= 1000162199;
+$arrAccounts[]	= 1000154974;
+$arrAccounts[]	= 1000154909;
+//$arrAccounts[]	= 1000157423;
+$arrAccounts[]	= 1000156613;
+$arrAccounts[]	= 1000154838;
+$arrAccounts[]	= 1000157129;
+$arrAccounts[]	= 1000160069;
+$arrAccounts[]	= 1000160496;
+
+// w/ Mobiles
+$arrAccounts[]	= 1000157278;
+$arrAccounts[]	= 1000163258;
+$arrAccounts[]	= 1000158462;
+$arrAccounts[]	= 1000160638;
+$arrAccounts[]	= 1000157524;
+$arrAccounts[]	= 1000159582;
+$arrAccounts[]	= 1000162277;
+$arrAccounts[]	= 1000162126;
+$arrAccounts[]	= 1000157548;
+$arrAccounts[]	= 1000158255;
+$arrAccounts[]	= 1000158558;
+$arrAccounts[]	= 1000159107;
+$arrAccounts[]	= 1000157175;
+$arrAccounts[]	= 1000158156;
+$arrAccounts[]	= 1000159979;
+
+// w/ Cost Centres
+$arrAccounts[]	= 1000155448;
+$arrAccounts[]	= 1000157203;
+$arrAccounts[]	= 1000155054;
+//$arrAccounts[]	= 1000155021;
+$arrAccounts[]	= 1000156265;
+$arrAccounts[]	= 1000155104;
+$arrAccounts[]	= 1000158291;
+$arrAccounts[]	= 1000157570;
+$arrAccounts[]	= 1000155313;
+
+// other
+$arrAccounts[]	= 1000160299;
+$arrAccounts[]  = 1000007119;
+$arrAccounts[]	= 1000162429;
+$arrAccounts[]	= 1000163167;
+
+// ELB Accounts
+$arrAccounts[]	= 1000156611;
+$arrAccounts[]	= 1000160843;
+$arrAccounts[]	= 1000157789;
+
+// Indial 100 without ELB (Itemisation was missing)
+$arrAccounts[]	= 1000158008;
+$arrAccounts[]	= 1000160250;
+
+// Telco Blue
+$arrAccounts[]	= 1000154811;
+
+// New Mobile Plan Accounts
+$arrAccounts[]	= 1000162273;
+$arrAccounts[]	= 1000164826;
+$arrAccounts[]	= 1000164618;
+$arrAccounts[]	= 1000164236;
+$arrAccounts[]	= 1000162139;
+
+// Recurring Charges
+$arrAccounts[]	= 1000158996;
+$arrAccounts[]	= 1000164686;
+
 // Accounts with Special Charges double-applied
 /*$arrAccount[]	= 1000154803;
 $arrAccount[]	= 1000154838;
@@ -888,7 +981,7 @@ $arrAccount[]	= 1000162273;
 	die;
 }*/
 // REGENERATE
-$intInvoices = $appBilling->RegenerateAccounts($arrAccount, TRUE);
+$intInvoices = $appBilling->RegenerateAccounts($arrAccounts, TRUE);
 
 // ROLLBACK TRANSACTION
 //TransactionRollback();
