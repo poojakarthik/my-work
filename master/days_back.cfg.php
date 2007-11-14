4f4f417fdfc5a8a9837b4abcc14ca973fead0a92
Rich Davis <rdavis@yellowbilling.com.au>
2007-11-14 04:07:45 +0000

    Master now requires a config file path to be specified at the command line

---
+++ master/days_back.cfg.php
@@ -0,0 +1,69 @@
+<?php
+
+$arrConfig['MaxRuns']			= 100000;
+$arrConfig['Sleep']				= 60;
+$arrConfig['Verbose']			= FALSE;
+$arrConfig['GMTOffset']			= 10;
+$arrConfig['RefreshRate']		= 1;
+
+
+$arrScript	= Array();
+
+	$arrScript['StartTime']                 =       3600*11;	// 1100
+	$arrScript['FinishTime']                =       3600*15;	// 1500
+	$arrScript['Interval']                  =       2419200;	// 28 Days (this is really irrelevant)
+	$arrScript['RecurringDay']				= 		-17;
+	$arrScript['DieOnChildDeath']			=		TRUE;
+	
+	$arrScript['SubScript'] = Array();
+		
+		// Billing Samples
+		$arrSubscript = Array();
+		$arrSubscript['Command']	=       'php /usr/share/vixen/master/test.php';
+		$arrSubscript['Directory']	=       '/usr/share/vixen/master/';
+		$arrScript['SubScript']['CopyDB']				= $arrSubscript;
+	
+$arrConfig['Script']['Today']	= $arrScript;
+
+
+$arrScript	= Array();
+
+	$arrScript['StartTime']                 =       3600*2;	// 1100
+	$arrScript['FinishTime']                =       3600*3;	// 1300
+	$arrScript['Interval']                  =       2419200;	// 28 Days (this is really irrelevant)
+	$arrScript['RecurringDay']				= 		-18;
+	$arrScript['DieOnChildDeath']			=		TRUE;
+	
+	$arrScript['SubScript'] = Array();
+		
+		// Billing Samples
+		$arrSubscript = Array();
+		$arrSubscript['Command']	=       'php /usr/share/vixen/master/test.php';
+		$arrSubscript['Directory']	=       '/usr/share/vixen/master/';
+		$arrScript['SubScript']['CopyDB']				= $arrSubscript;
+	
+$arrConfig['Script']['Minus18']	= $arrScript;
+
+
+$arrScript	= Array();
+
+	$arrScript['StartTime']                 =       3600*2;	// 1100
+	$arrScript['FinishTime']                =       3600*3;	// 1300
+	$arrScript['Interval']                  =       2419200;	// 28 Days (this is really irrelevant)
+	$arrScript['RecurringDay']				= 		-10;
+	$arrScript['DieOnChildDeath']			=		TRUE;
+	
+	$arrScript['SubScript'] = Array();
+		
+		// Billing Samples
+		$arrSubscript = Array();
+		$arrSubscript['Command']	=       'php /usr/share/vixen/master/test.php';
+		$arrSubscript['Directory']	=       '/usr/share/vixen/master/';
+		$arrScript['SubScript']['CopyDB']				= $arrSubscript;
+	
+$arrConfig['Script']['Minus10']	= $arrScript;
+
+
+
+
+?>
--- master/master.sh
+++ master/master.sh
@@ -10,6 +10,22 @@
 // master application
 //----------------------------------------------------------------------------//
 require_once('../framework/require.php');
+$arrConfig = LoadApplication();
+
+
+// Parse command line parameters for config file
+if (!$argv[1])
+{
+	CliEcho("\nYou must pass a vaild Master Config File as the first parameter!\n");
+	die;
+}
+$strFilename	= trim($argv[1]);
+if (!file_exists($strFilename))
+{
+	CliEcho("\nFile '$strFilename does not exist!'\n");
+	die;
+}
+require_once(strFilename);
 
 // Application entry point - create an instance of the application object
 $appMaster = new ApplicationMaster($arrConfig);
