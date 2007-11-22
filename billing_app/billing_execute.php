f5b8e9edf117f0065a0cc5d457456cc36c061f1c
Rich Davis <rdavis@yellowbilling.com.au>
2007-11-22 05:36:44 +0000

    re-enabled billing execute

--- billing_app/billing_execute.php
+++ billing_app/billing_execute.php
@@ -21,7 +21,7 @@ $appBilling = new ApplicationBilling($arrConfig);
 // execute bill
 $strDateTime = date("Y-m-d H:i:s");
 SendEmail('turdminator@hotmail.com, mark.s@yellowbilling.com.au', "viXen Billing Started @ $strDateTime", "viXen Billing Started @ $strDateTime");
-//$bolResponse = $appBilling->Execute();
+$bolResponse = $appBilling->Execute();
 $strDateTime = date("Y-m-d H:i:s");
 SendEmail('turdminator@hotmail.com, mark.s@yellowbilling.com.au', "viXen Billing Ended @ $strDateTime", "viXen Billing Started @ $strDateTime");
 
