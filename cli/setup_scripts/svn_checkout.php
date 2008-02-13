796778270d5abf5185d50d46c957eee9eb7806fd
Rich Davis <rdavis@yellowbilling.com.au>
2008-02-13 01:48:32 +0000

    added svn_checkout.php, which checks out to a given path

---
+++ cli/setup_scripts/svn_checkout.php
@@ -0,0 +1,25 @@
+<?php
+
+ob_start();
+$strPath	= rtrim(trim($argv[1]), '/');
+echo "\nExporting to '$strPath'...\t\t\t";
+ob_flush();
+
+if(!file_exists($strPath))
+{
+	echo "[ FAILED ]\n\t- Directory does not exist\n\n";
+	die;
+}
+
+// Remove existing Flex files
+shell_exec("rm -Rf $strPath/*");
+
+// get latest version
+shell_exec("svn export --non-interactive --force --no-auth-cache --username export --password export http://192.168.2.13/svn_vixen $strPath");
+
+// set file permissions
+shell_exec("chmod -R 0777 $strPath/*");
+
+echo "[   OK   ]\n\n";
+die;
+?>
