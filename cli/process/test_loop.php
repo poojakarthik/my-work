854783847b1bd06b59401a7a5bffa0be03fa433e
Rich Davis <rdavis@yellowbilling.com.au>
2008-02-14 02:46:46 +0000

    fixed a bug in the test_loop.php script

--- cli/process/test_loop.php
+++ cli/process/test_loop.php
@@ -2,11 +2,12 @@
 require_once("../../flex.require.php");
 
 CliEcho("Starting! (PID: ".getmypid().")");
-$intLoop	= 12;
-while (true)
+$intLoop	= 0;
+while ($intLoop < 12)
 {
 	sleep(5);
 	CliEcho("Loop #$intLoop");
+	$intLoop++;
 }
 CliEcho("Complete!");
 die;
