8b30e52221a7b6526061f2b72f6791e39b1b3104
SVN Export <voiptel+svn-export@yellowbilling.com.au>
2008-03-18 01:36:11 +0000

    Changed employee management to use flex framework

--- html/intranet/employee_list.php
+++ html/intranet/employee_list.php
@@ -5,26 +5,6 @@
 	//
 	// NOT FOR EXTERNAL DISTRIBUTION
 	//----------------------------------------------------------------------------//
-	
-	// call application loader
-	require ('config/application_loader.php');
-	
-	// set page details
-	$arrPage['PopUp']		= FALSE;
-	$arrPage['Permission']	= PERMISSION_ADMIN;
-	$arrPage['Modules']		= MODULE_BASE | MODULE_EMPLOYEE;
-	
-	// call application
-	require ('config/application.php');
-	
-	
-	// Start a new Employee Search
-	$emsEmployees = $Style->attachObject (new Employees(
-		isset ($_GET ['rangePage']) ? $_GET ['rangePage'] : 1, 
-		isset ($_GET ['rangeLength']) ? $_GET ['rangeLength'] : 30));
-
-	
-	// Output the Result
-	$Style->Output ('xsl/content/employee/list.xsl');
+	header("Location: flex.php/Employee/EmployeeList/");
 	
 ?>
