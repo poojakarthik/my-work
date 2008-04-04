299cf5609bc1ac79f3a7c27b65b62ccae5248c7c
Rich Davis <rdavis@yellowbilling.com.au>
2008-04-04 05:09:16 +0000

    provisioning will now require all of the module files

--- cli/provisioning/config.php
+++ cli/provisioning/config.php
@@ -30,6 +30,19 @@
 //----------------------------------------------------------------------------//
 // CONFIG
 //----------------------------------------------------------------------------//
+VixenRequire('cli/provisioning/modules/export_base.php');
+VixenRequire('cli/provisioning/modules/import_base.php');
 
+VixenRequire('cli/provisioning/modules/optus/export_bar.php');
+VixenRequire('cli/provisioning/modules/optus/export_deactivate.php');
+VixenRequire('cli/provisioning/modules/optus/export_preselect_reverse.php');
+VixenRequire('cli/provisioning/modules/optus/export_preselect.php');
+VixenRequire('cli/provisioning/modules/optus/export_unbar.php');
+VixenRequire('cli/provisioning/modules/optus/import_ppr.php');
 
+VixenRequire('cli/provisioning/modules/unitel/export_daily_order.php');
+VixenRequire('cli/provisioning/modules/unitel/export_preselection.php');
+VixenRequire('cli/provisioning/modules/unitel/import_dsc.php');
+VixenRequire('cli/provisioning/modules/unitel/import_line_status.php');
+VixenRequire('cli/provisioning/modules/unitel/import_preselection.php');
 ?>
