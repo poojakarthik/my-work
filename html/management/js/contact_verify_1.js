ca22d838dad9f7c6ee192e9a2d576ba88959d739
SVN Export <voiptel+svn-export@yellowbilling.com.au>
2008-05-15 04:08:15 +0000

    * Fix up dodgey name

--- html/management/js/contact_verify_1.js
+++ html/management/js/contact_verify_1.js
@@ -1,30 +1,30 @@
-	
-	// Usage:
-	// <script language="javascript" src="js/contact_list_1.js"></script>
-	// <form onsubmit="return contactList1 (this)">
-	
-	function contactList1 (form)
+
+// Usage:
+// <script language="javascript" src="js/contact_verify_1.js"></script>
+// <form onsubmit="return contactList1 (this)">
+
+function contactList1 (form)
+{
+	var count = 0;
+
+	if (form.elements ["ui-Account"].value != "")			count += 1;
+	if (form.elements ["ui-ABN"].value != "")				count += 1;
+	if (form.elements ["ui-ACN"].value != "")				count += 1;
+	if (form.elements ["ui-Invoice"].value != "")			count += 1;
+	if (form.elements ["ui-FNN"].value != "")				count += 1;
+	if (form.elements ["ui-BusinessName"].value != "")		count += 1;
+	if (form.elements ["ui-Contact-First"].value != "")		count += 0.5;
+	if (form.elements ["ui-Contact-Last"].value != "")		count += 0.5;
+
+	if (count != 1)
 	{
-		var count = 0;
-		
-		if (form.elements ["ui-Account"].value != "")			count += 1;
-		if (form.elements ["ui-ABN"].value != "")				count += 1;
-		if (form.elements ["ui-ACN"].value != "")				count += 1;
-		if (form.elements ["ui-Invoice"].value != "")			count += 1;
-		if (form.elements ["ui-FNN"].value != "")				count += 1;
-		if (form.elements ["ui-BusinessName"].value != "")		count += 1;
-		if (form.elements ["ui-Contact-First"].value != "")		count += 0.5;
-		if (form.elements ["ui-Contact-Last"].value != "")		count += 0.5;
-		
-		if (count != 1)
-		{
-			alert (
+		alert (
 				"You did not fill in this form correctly.  Please try again."
-			);
-			
-			return false;
-		}
-		
-		return true;
+			  );
+
+		return false;
 	}
-	
+
+	return true;
+}
+
