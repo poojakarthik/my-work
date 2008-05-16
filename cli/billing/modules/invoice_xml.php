297d89b1a57d6e6cb8031dea7ef711c306414eea
Rich Davis <rdavis@yellowbilling.com.au>
2008-05-16 03:17:25 +0000

    bug fixes in invoice xml output

--- cli/billing/modules/invoice_xml.php
+++ cli/billing/modules/invoice_xml.php
@@ -99,7 +99,7 @@
 		$xmlDocument	= $this->_AddElement($this->_domDocument, 'Document');
 		$this->_AddElement($xmlDocument, 'DocumentType', 'DOCUMENT_TYPE_INVOICE');
 		$this->_AddElement($xmlDocument, 'CustomerGroup', GetConstantName($arrCustomer['CustomerGroup'], 'CustomerGroup'));
-		$this->_AddElement($xmlDocument, 'CreationDate', date('Y-m-d H:i:s'), strtotime($arrInvoice['CreatedOn']));
+		$this->_AddElement($xmlDocument, 'CreationDate', date('Y-m-d H:i:s', strtotime($arrInvoice['CreatedOn'])));
 		$this->_AddElement($xmlDocument, 'DeliveryMethod', GetConstantName($arrInvoice['DeliveryMethod'], 'DeliveryMethod'));
 		
 		//--------------------------------------------------------------------//
