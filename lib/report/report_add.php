654df151d1642164c467dc85f4fb97f219d24cd5
Rich Davis <rdavis@yellowbilling.com.au>
2008-05-21 05:57:02 +0000

    added a new Data Report for listing Duplicate CDR Files

--- lib/report/report_add.php
+++ lib/report/report_add.php
@@ -22,14 +22,14 @@ $arrSQLSelect	= Array();
 $arrSQLFields	= Array();
 
 // General Data
-$arrDataReport['Name']			= "Percentage Collected for a Given Invoice Run";
-$arrDataReport['Summary']		= "Shows the Percentage collected to date for a specified Invoice Run";
-$arrDataReport['FileName']		= "Percentage Collected from <InvoiceRun::Label> Invoice Run as of <DATETIME()>";
+$arrDataReport['Name']			= "Duplicate Unbilled CDR Files in a given Period";
+$arrDataReport['Summary']		= "Displays a list of CDR Files which have duplicate unbilled CDRs in them.";
+$arrDataReport['FileName']		= "Duplicate Unbilled CDR Files as of <DATETIME()>";
 $arrDataReport['RenderMode']	= REPORT_RENDER_INSTANT;
 $arrDataReport['Priviledges']	= 2147483648;
 $arrDataReport['CreatedOn']		= date("Y-m-d");
-$arrDataReport['SQLTable']		= "Invoice";
-$arrDataReport['SQLWhere']		= "InvoiceRun = <InvoiceRun>";
+$arrDataReport['SQLTable']		= "CDR JOIN FileImport ON CDR.File = FileImport.Id";
+$arrDataReport['SQLWhere']		= "Status = ".CDR_DUPLICATE;
 $arrDataReport['SQLGroupBy']	= "InvoiceRun";
 
 // Documentation Reqs
@@ -37,41 +37,20 @@ $arrDocReq[]	= "DataReport";
 $arrDataReport['Documentation']	= serialize($arrDocReq);
 
 // SQL Select
-$arrSQLSelect['Invoice Grand Total']	['Value']	= "SUM(CASE WHEN Total+Tax > 0 THEN Total+Tax ELSE 0 END)";
-$arrSQLSelect['Invoice Grand Total']	['Type']	= EXCEL_TYPE_CURRENCY;
+$arrSQLSelect['File Name']				['Value']	= "FileExport.FileName";
 
-$arrSQLSelect['Total Collected']		['Value']	= "SUM(CASE WHEN Total+Tax > 0 THEN Total+Tax ELSE 0 END) - SUM(CASE WHEN Total+Tax > 0 THEN Balance ELSE 0 END)";
-$arrSQLSelect['Total Collected']		['Type']	= EXCEL_TYPE_CURRENCY;
+$arrSQLSelect['Duplicate CDRs']			['Value']	= "COUNT(CDR.Id)";
+$arrSQLSelect['Duplicate CDRs']			['Type']	= EXCEL_TYPE_INTEGER;
 
-$arrSQLSelect['Total Outstanding']		['Value']	= "SUM(CASE WHEN Total+Tax >= 0 THEN Balance ELSE 0 END)";
-$arrSQLSelect['Total Outstanding']		['Type']	= EXCEL_TYPE_CURRENCY;
+$arrSQLSelect['Total CDR Cost']			['Value']	= "SUM(CDR.Cost)";
+$arrSQLSelect['Total CDR Cost']			['Type']	= EXCEL_TYPE_CURRENCY;
 
-$arrSQLSelect['Percent Collected']		['Value']	= "1 - (SUM(CASE WHEN Total+Tax > 0 THEN Balance ELSE 0 END) / SUM(CASE WHEN Total+Tax > 0 THEN Total+Tax ELSE 0 END))";
-$arrSQLSelect['Percent Collected']		['Type']	= EXCEL_TYPE_PERCENTAGE;
+$arrSQLSelect['Import Date']			['Value']	= "DATE_FORMAT(FileExport.ImportedOn, '%d/%m/%Y %H:%i:%s')";
 
 $arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
 
 
 // SQL Fields
-$arrColumns = Array();
-$arrColumns['Label']	= "DATE_FORMAT(BillingDate, '%d %M %Y')";
-$arrColumns['Value']	= "InvoiceRun";
-
-$arrSelect = Array();
-$arrSelect['Table']		= "InvoiceRun";
-$arrSelect['Columns']	= $arrColumns;
-$arrSelect['Where']		= "BillingDate > '2007-03-01'";
-$arrSelect['OrderBy']	= "BillingDate DESC";
-$arrSelect['Limit']		= NULL;
-$arrSelect['GroupBy']	= NULL;
-$arrSelect['ValueType']	= "dataString";
-
-$arrSQLFields['InvoiceRun']	= Array(
-										'Type'					=> "StatementSelect",
-										'DBSelect'				=> $arrSelect,
-										'Documentation-Entity'	=> "DataReport",
-										'Documentation-Field'	=> "BillingDate",
-									);
 $arrDataReport['SQLFields'] = serialize($arrSQLFields);
 
 //----------------------------------------------------------------------------//
--- lib/report/report_source.php
+++ lib/report/report_source.php
@@ -807,4 +807,62 @@ $arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
 // SQL Fields
 $arrDataReport['SQLFields'] = serialize($arrSQLFields);
 
+
+
+//----------------------------------------------------------------------------//
+// Percentage Collected for a Given Invoice Run
+//----------------------------------------------------------------------------//
+// General Data
+$arrDataReport['Name']			= "Percentage Collected for a Given Invoice Run";
+$arrDataReport['Summary']		= "Shows the Percentage collected to date for a specified Invoice Run";
+$arrDataReport['FileName']		= "Percentage Collected from <InvoiceRun::Label> Invoice Run as of <DATETIME()>";
+$arrDataReport['RenderMode']	= REPORT_RENDER_INSTANT;
+$arrDataReport['Priviledges']	= 2147483648;
+$arrDataReport['CreatedOn']		= date("Y-m-d");
+$arrDataReport['SQLTable']		= "Invoice";
+$arrDataReport['SQLWhere']		= "InvoiceRun = <InvoiceRun>";
+$arrDataReport['SQLGroupBy']	= "InvoiceRun";
+
+// Documentation Reqs
+$arrDocReq[]	= "DataReport";
+$arrDataReport['Documentation']	= serialize($arrDocReq);
+
+// SQL Select
+$arrSQLSelect['Invoice Grand Total']	['Value']	= "SUM(CASE WHEN Total+Tax > 0 THEN Total+Tax ELSE 0 END)";
+$arrSQLSelect['Invoice Grand Total']	['Type']	= EXCEL_TYPE_CURRENCY;
+
+$arrSQLSelect['Total Collected']		['Value']	= "SUM(CASE WHEN Total+Tax > 0 THEN Total+Tax ELSE 0 END) - SUM(CASE WHEN Total+Tax > 0 THEN Balance ELSE 0 END)";
+$arrSQLSelect['Total Collected']		['Type']	= EXCEL_TYPE_CURRENCY;
+
+$arrSQLSelect['Total Outstanding']		['Value']	= "SUM(CASE WHEN Total+Tax >= 0 THEN Balance ELSE 0 END)";
+$arrSQLSelect['Total Outstanding']		['Type']	= EXCEL_TYPE_CURRENCY;
+
+$arrSQLSelect['Percent Collected']		['Value']	= "1 - (SUM(CASE WHEN Total+Tax > 0 THEN Balance ELSE 0 END) / SUM(CASE WHEN Total+Tax > 0 THEN Total+Tax ELSE 0 END))";
+$arrSQLSelect['Percent Collected']		['Type']	= EXCEL_TYPE_PERCENTAGE;
+
+$arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
+
+
+// SQL Fields
+$arrColumns = Array();
+$arrColumns['Label']	= "DATE_FORMAT(BillingDate, '%d %M %Y')";
+$arrColumns['Value']	= "InvoiceRun";
+
+$arrSelect = Array();
+$arrSelect['Table']		= "InvoiceRun";
+$arrSelect['Columns']	= $arrColumns;
+$arrSelect['Where']		= "BillingDate > '2007-03-01'";
+$arrSelect['OrderBy']	= "BillingDate DESC";
+$arrSelect['Limit']		= NULL;
+$arrSelect['GroupBy']	= NULL;
+$arrSelect['ValueType']	= "dataString";
+
+$arrSQLFields['InvoiceRun']	= Array(
+										'Type'					=> "StatementSelect",
+										'DBSelect'				=> $arrSelect,
+										'Documentation-Entity'	=> "DataReport",
+										'Documentation-Field'	=> "BillingDate",
+									);
+$arrDataReport['SQLFields'] = serialize($arrSQLFields);
+
  ?>
