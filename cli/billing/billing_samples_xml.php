e7beb93b0d9fb5e904f49579bc6222db0f32ad0b
Rich Davis <rdavis@yellowbilling.com.au>
2008-05-23 04:29:44 +0000

    added an xml version of billing_samples

---
+++ cli/billing/billing_samples_xml.php
@@ -0,0 +1,116 @@
+<?php
+//----------------------------------------------------------------------------//
+// (c) copyright 2008 VOIPTEL Pty Ltd
+//
+// NOT FOR EXTERNAL DISTRIBUTION
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// Reprint invoices for a defined list of accounts
+//----------------------------------------------------------------------------//
+
+
+// load application
+require_once("../../flex.require.php");
+$arrConfig = LoadApplication();
+
+// load remote copy
+VixenRequire('lib/framework/remote_copy.php');
+
+// Application entry point - create an instance of the application object
+$appBilling = new ApplicationBilling($arrConfig);
+
+// Get Command-line Params
+switch (strtoupper(trim($argv[1])))
+{
+	case 'SILVER':
+		$strMode	= "Silver";
+		break;
+	
+	case 'BRONZE':
+		$strMode	= "Bronze";
+		break;
+	
+	case 'GOLD':
+		$strMode	= "Gold";
+		break;
+	
+	default:
+		Debug("No mode specified! ('bronze', 'silver' or 'gold')");
+		die;
+}
+
+Debug("[ GENERATING ".strtoupper($strMode)." SAMPLES ]");
+
+// Get list of Accounts
+$arrAccounts		= Array();
+$selSampleAccounts	= new StatementSelect("Account", "Id", "Sample != 0");
+$selSampleAccounts->Execute();
+while ($arrAccount = $selSampleAccounts->Fetch())
+{
+	$arrAccounts[]	= $arrAccount['Id'];
+}
+
+// Get InvoiceRun from InvoiceTemp table
+$selInvoiceRun	= new StatementSelect("InvoiceTemp", "InvoiceRun", "1", NULL, 1);
+$selInvoiceRun->Execute();
+if (!($arrInvoiceRun = $selInvoiceRun->Fetch()))
+{
+	// No Temporary InvoiceRun
+	CliEcho("There is no temporary Invoice Run!");
+	die;
+}
+$strInvoiceRun	= $arrInvoiceRun['InvoiceRun'];
+
+// Email Status
+$strDateTime = date("Y-m-d H:i:s");
+SendEmail('turdminator@hotmail.com, mark.s@yellowbilling.com.au', "viXen Billing::{$strMode}Samples Started @ $strDateTime", "viXen Billing Started @ $strDateTime");
+
+// Try to change to XML directory
+$strWorkingDir		= getcwd();
+$strFullDirectory	= INVOICE_XML_PATH.$strInvoiceRun.'/';
+$strSampleDirectory	= $strFullDirectory."samples/";
+if (chdir($strFullDirectory))
+{
+	// Create Sample PDFs
+	foreach ($arrAccounts as $intAccount)
+	{
+		CliEcho(shell_exec("php cli.php -x$strFullDirectory -f$strSampleDirectory -oEMAIL"));
+	}
+	
+	// ZIP samples, copy to public location
+	chdir($strSampleDirectory);
+	$strBaseZipname = date("F")." $strMode Samples";
+	$strZipname		= $strBaseZipname.'.zip';
+	$intVersion		= 1;
+	while (file_exists($strZipname))
+	{
+		$intVersion++;
+		$strZipname	= $strBaseZipname." MK$intVersion.zip";
+	}
+	echo shell_exec("zip -qj '$strZipname' *.pdf");
+	
+	$strCustomerName	= str_replace('working', '', $GLOBALS['**arrCustomerConfig']['Customer']);
+	$strDir				= date("Y/m/", strtotime("-2 days", time()));
+	
+	$rcpRemoteCopySamples = new RemoteCopySSH("10.50.50.131", "rdavis", "password");
+	if (is_string($mixResult = $rcpRemoteCopySamples->Connect("10.50.50.131", "rdavis", "password")))
+	{
+		echo "$mixResult \n";
+	}
+	$rcpRemoteCopySamples->Copy($strDownloadDir.$strZipname, "/data/www/samples.yellowbilling.com.au/html/$strCustomerName/$strDir", RCOPY_BACKUP);
+	$rcpRemoteCopySamples->Disconnect();
+}
+else
+{
+	// No XML Data
+	CliEcho("No XML Data exists for temporary Invoice Run ($strInvoiceRun)");
+}
+
+// Email Status
+$strDateTime = date("Y-m-d H:i:s");
+SendEmail('turdminator@hotmail.com, mark.s@yellowbilling.com.au', "viXen Billing::ReprintSamples Ended @ $strDateTime", "viXen Billing Ended @ $strDateTime");
+
+// finished
+CliEcho("\n\n-- End of Billing --");
+?>
