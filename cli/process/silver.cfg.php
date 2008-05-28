c730bda76e3945513970fdf4e30a574f136961aa
Rich Davis <rdavis@yellowbilling.com.au>
2008-05-28 01:51:25 +0000

    process application should now support new billing processes

--- cli/process/silver.cfg.php
+++ cli/process/silver.cfg.php
@@ -14,80 +14,80 @@ $arrConfig = Array();
 
 // Normalisation
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php normalisation.php -i';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'normalisation/';
-$arrConfig['Normalise']		= $arrSubscript;
+$arrSubscript['Command']			= 'php normalisation.php -i';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'normalisation/';
+$arrConfig['Normalise']				= $arrSubscript;
 
 // Rating
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php rating.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'rating/';
-$arrConfig['Rate']		= $arrSubscript;
+$arrSubscript['Command']			= 'php rating.php';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'rating/';
+$arrConfig['Rate']					= $arrSubscript;
 
 // Rate LL S&E Credits
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php rate_ll_se_credits.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'rating/';
+$arrSubscript['Command']			= 'php rate_ll_se_credits.php';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'rating/';
 $arrConfig['RateLLSECredits']		= $arrSubscript;
 
 // Backup Invoice Output
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php backup_invoice_output.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'billing/';
+$arrSubscript['Command']			= 'php backup_invoice_output.php';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'billing/';
 $arrConfig['BackupInvoiceOutput']	= $arrSubscript;
 
 // Check Un-Invoiced Special Charges
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php charges_check_special.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'charges/';
+$arrSubscript['Command']			= 'php charges_check_special.php';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'charges/';
 $arrConfig['CheckSpecialCharges']	= $arrSubscript;
 
 // Special Charges
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php special_charges.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'charges/';
+$arrSubscript['Command']			= 'php special_charges.php';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'charges/';
 $arrConfig['SpecialCharges']		= $arrSubscript;
 
 // Recurring Charges
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php recurring_charges.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'charges/';
+$arrSubscript['Command']			= 'php recurring_charges.php';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'charges/';
 $arrConfig['RecurringCharges']		= $arrSubscript;
 
 // Payments
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php payments.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'payment/';
+$arrSubscript['Command']			= 'php payments.php';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'payment/';
 $arrConfig['Payments']				= $arrSubscript;
 
 // Check CDR Files
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php cdrcheck.php -v';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'billing/';
-$arrConfig['CDRFileCheck']				= $arrSubscript;		
+$arrSubscript['Command']			= 'php cdrcheck.php -v';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'billing/';
+$arrConfig['CDRFileCheck']			= $arrSubscript;		
 
 // Billing Execute
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php billing_execute.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'billing/';
-$arrConfig['BillExecute']				= $arrSubscript;
+$arrSubscript['Command']			= 'php billing_execute.php silver';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'billing/';
+$arrConfig['BillExecute']			= $arrSubscript;
 
 // Billing Print
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php billing_print.php BILL_FLEX_XML';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'billing/';
+$arrSubscript['Command']			= 'php billing_print.php BILL_FLEX_XML';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'billing/';
 $arrConfig['BillPrint']				= $arrSubscript;
 
 // Billing Samples
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php billing_samples.php silver';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'billing/';
-//$arrConfig['BillSamples']				= $arrSubscript;
+$arrSubscript['Command']			= 'php billing_samples_list.php silver';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'billing/';
+$arrConfig['BillSamples']			= $arrSubscript;
 
 // Management Reports
 $arrSubscript = Array();
-$arrSubscript['Command']	=       'php billing_reports.php';
-$arrSubscript['Directory']	=       BACKEND_BASE_PATH.'billing/';
+$arrSubscript['Command']			= 'php billing_reports.php';
+$arrSubscript['Directory']			= BACKEND_BASE_PATH.'billing/';
 //$arrConfig['ManagementReports']		= $arrSubscript;
 
 ?>
