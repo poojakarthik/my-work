fa2da92d2b500a93c9b2f7751dd915ad8be0fe75
Rich Davis <rdavis@yellowbilling.com.au>
2008-06-05 00:10:26 +0000

    $refMysqliConnection is Public again

--- lib/framework/db_access.php
+++ lib/framework/db_access.php
@@ -74,7 +74,7 @@
 	 *
 	 * @property
 	 */
-	private $refMysqliConnection;
+	public $refMysqliConnection;
 	
  	//------------------------------------------------------------------------//
 	// DataAccess() - Constructor
