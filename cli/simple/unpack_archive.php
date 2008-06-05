34c4c6497f4f0d206e250700555dc09384110b21
Rich Davis <rdavis@yellowbilling.com.au>
2008-06-05 01:03:24 +0000

    added debugging to UnpackArchive()

--- cli/simple/unpack_archive.php
+++ cli/simple/unpack_archive.php
@@ -3,7 +3,7 @@
 // Framework
 require_once("../../flex.require.php");
 
-$strSourcePath			= "/home/richdavis/Desktop/archivetest/__extract/";
+$strSourcePath			= "/home/richdavis/Desktop/archivetest/__extract/archivetest.tar";
 $strDestinationPath		= NULL;
 $bolJunkPaths			= FALSE;
 $strPassword			= NULL;
@@ -11,5 +11,13 @@ $strType				= NULL;
 
 $mixReturn	= UnpackArchive($strSourcePath, $strDestinationPath, $bolJunkPaths, $strPassword, $strType);
 Debug($mixReturn);
+
+shell_exec("chmod -R 0777 ".dirname($strSourcePath));
+
+if ($strDestinationPath)
+{
+	shell_exec("chmod -R 0777 $strDestinationPath");
+}
+
 die;
 ?>
