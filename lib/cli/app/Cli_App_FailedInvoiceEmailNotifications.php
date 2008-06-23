b15feb378683c8ccf4a0732f3aa0347153b97fbf
Hadrian Oliver <holiver@yellowbilling.com.au>
2008-06-23 06:29:23 +0000

    Added empty log file check

--- lib/cli/app/Cli_App_FailedInvoiceEmailNotifications.php
+++ lib/cli/app/Cli_App_FailedInvoiceEmailNotifications.php
@@ -25,12 +25,17 @@ class Cli_App_FailedInvoiceEmailNotifications extends Cli
 
 			// Need to parse the log file here to extract the rejected email addresses
 			$log = file_get_contents($pathToLog);
+			if (!$log)
+			{
+				$this->log("Log file '$pathToLog' is empty.");
+				return 0;
+			}
+
 			$matches = array();
 			preg_match_all("/.+ said: [45]{1,1}[0-9]{2,2} .*\<([^\>]+)\>/", $log, $matches);
 			@$arrEmailAddresses = array_unique($matches[1]);
 			if (!$arrEmailAddresses || !count($arrEmailAddresses))
 			{
-				$this->log($log);
 				$this->log('No bounced messages detected.');
 				return 0;
 			}
