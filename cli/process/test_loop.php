0f94b52cda928814115a83471fc179ab2b86aaf0
Rich Davis <rdavis@yellowbilling.com.au>
2008-06-23 02:33:12 +0000

    test_loop.php shoul now accept a paramter

--- cli/process/test_loop.php
+++ cli/process/test_loop.php
@@ -1,9 +1,11 @@
 <?php
 require_once("../../flex.require.php");
 
+$intTotalLoops	= ($argv[1]) ? (int)$argv[1] : 10;
+
 CliEcho("Starting! (PID: ".getmypid().")");
 $intLoop	= 0;
-while ($intLoop < 12)
+while ($intLoop < $intTotalLoops)
 {
 	sleep(5);
 	CliEcho("Loop #$intLoop");
