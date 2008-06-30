913419a06641e80703318aa893671e584486cbda
Rich Davis <rdavis@yellowbilling.com.au>
2008-06-30 04:41:32 +0000

    collection create_carrier_module.php should no longer look for config files

---
+++ cli/collection/create_carrier_module.php
@@ -0,0 +1,50 @@
+<?php
+
+// Load Framework
+require_once("../../flex.require.php");
+LoadApplication();
+
+CliEcho("\n[ CREATE COLLECTION CARRIER MODULE ]\n");
+
+if (is_int(stripos($argv[1], '?')))
+{
+	// Print Help
+	CliEcho("** Usage: 'php create_carrier_module.php <Carrier.Id> <ClassName> <ConfigFile>'\n");
+	CliEcho("<Carrier.Id>\t: The Id for the Carrier this module is to be created for");
+	CliEcho("<ClassName>\t: The Class of the module to create");
+	CliEcho();
+	die;
+}
+
+$intCarrier		= (int)$argv[1];
+$strClassName	= trim($argv[2]);
+$strConfigPath	= trim($argv[3]);
+
+if (!GetConstantName($intCarrier, 'Carrier'))
+{
+	CliEcho("ERROR: '$intCarrier' is not a valid Carrier Id!\n");
+	die;
+}
+elseif (!class_exists($strClassName) || !is_subclass_of($strClassName, 'CarrierModule'))
+{
+	CliEcho("ERROR: '$strClassName' is not a valid CarrierModule Class!\n");
+	die;
+}
+
+CliEcho("Creating new Module...\t\t\t", FALSE);
+$objModule	= new $strClassName($intCarrier);
+
+if (!$objModule)
+{
+	CliEcho("[ FAILED ]\n\tERROR: There was an error instanciating the CarrierModule Class '$strClassName'\n");
+	die;
+}
+
+if (($mixResult = $objModule->CreateModuleConfig()) !== TRUE)
+{
+	CliEcho("[ FAILED ]\n\t".$mixResult."\n");
+	die;
+}
+
+CliEcho("[   OK   ]\n");
+?>
