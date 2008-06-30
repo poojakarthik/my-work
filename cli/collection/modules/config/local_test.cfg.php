010176317252d4b801ce4cd4f0b4df3059b46581
Rich Davis <rdavis@yellowbilling.com.au>
2008-06-30 03:24:49 +0000

    modifications to the local file test collection script

--- cli/collection/modules/config/local_test.cfg.php
+++ cli/collection/modules/config/local_test.cfg.php
@@ -15,7 +15,7 @@ $arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['PathDefine']		= $arrArchiveFiles
 $arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['Regex']			= "/^test_archive_(\d+).zip$/";
 $arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['Uniqueness']		= "FileName = <FileName> AND SHA1 = <SHA1>";
 $arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['FTPMode']			= FTP_BINARY;
-$arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['Paths']			[]	= '/home/rdavis/';
+$arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['Paths']			[]	= '/home/rdavis/collection_test/';
 $arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['DownloadOnly']	= TRUE;
 $arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['ArchiveType']		= 'zip';
 
@@ -23,14 +23,14 @@ $arrStructure[FILE_IMPORT_FLEX_TEST_ARCHIVE]		['ArchiveType']		= 'zip';
 $arrStructure[FILE_IMPORT_FLEX_TEST_1]				['Regex']		= "/^test1_(\d+).txt$/";
 $arrStructure[FILE_IMPORT_FLEX_TEST_1]				['Uniqueness']	= "FileName = <FileName> AND SHA1 = <SHA1>";
 $arrStructure[FILE_IMPORT_FLEX_TEST_1]				['FTPMode']		= FTP_BINARY;
-$arrStructure[FILE_IMPORT_FLEX_TEST_1]				['Paths']		[]	= '/home/rdavis/test1/';
-$arrStructure[FILE_IMPORT_FLEX_TEST_1]				['Paths']		[]	= '/home/rdavis/test1/archive/';
+$arrStructure[FILE_IMPORT_FLEX_TEST_1]				['Paths']		[]	= '/home/rdavis/collection_test/test1/';
+$arrStructure[FILE_IMPORT_FLEX_TEST_1]				['Paths']		[]	= '/home/rdavis/collection_test/test1/archive/';
 
 $arrStructure[FILE_IMPORT_FLEX_TEST_2]				['Regex']		= "/^test2_(\d+).txt$/";
 $arrStructure[FILE_IMPORT_FLEX_TEST_2]				['Uniqueness']	= "FileName = <FileName> AND SHA1 = <SHA1>";
 $arrStructure[FILE_IMPORT_FLEX_TEST_2]				['FTPMode']		= FTP_BINARY;
-$arrStructure[FILE_IMPORT_FLEX_TEST_2]				['Paths']		[]	= '/home/rdavis/test1/';
-$arrStructure[FILE_IMPORT_FLEX_TEST_2]				['Paths']		[]	= '/home/rdavis/test2/';
+$arrStructure[FILE_IMPORT_FLEX_TEST_2]				['Paths']		[]	= '/home/rdavis/collection_test/test1/';
+$arrStructure[FILE_IMPORT_FLEX_TEST_2]				['Paths']		[]	= '/home/rdavis/collection_test/test2/';
 
 // Config
 $arrModuleConfig['FileDefine']		['Value']		= $arrStructure;
