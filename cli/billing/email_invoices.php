92e6632c4ad75b50d99d641fd44c9035f362421b
Rich Davis <rdavis@yellowbilling.com.au>
2008-07-01 07:21:26 +0000

    email_invoices.php now takes an InvoiceRun as a parameter, and should now work for Flex XML Invoices

--- cli/billing/email_invoices.php
+++ cli/billing/email_invoices.php
@@ -15,12 +15,21 @@
 require_once("../../flex.require.php");
 $arrConfig = LoadApplication();
 
+// Check Parameters
+$strInvoiceRun	= $argv[1];
+$selInvoiceRun	= new StatementSelect("InvoiceRun", "Id", "InvoiceRun = <InvoiceRun>");
+if (!$selInvoiceRun->Execute(Array('InvoiceRun' => $strInvoiceRun)))
+{
+	CliEcho("\n'$strInvoiceRun' is not a valid InvoiceRun!\n");
+	exit(1);
+}
+
 // Application entry point - create an instance of the application object
 $appBilling = new ApplicationBilling($arrConfig);
 
 // execute bill
-//$strPath = FILES_BASE_PATH."invoices/".date("Y/n/", strtotime("-1 month", time()));
-$strPath = "/home/richdavis/Desktop/Invoices/";
+$strPath = FILES_BASE_PATH."invoices/pdf/$strInvoiceRun/";
+//$strPath = "/home/richdavis/Desktop/Invoices/";
 $bolResponse = $appBilling->EmailInvoicePDFs($strPath);
 
 $appBilling->FinaliseReport();
