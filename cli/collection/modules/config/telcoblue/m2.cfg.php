7f03fd875b694fcb86eaba168bc95babe8140d64
Rich Davis <rdavis@yellowbilling.com.au>
2008-07-08 23:58:20 +0000

    changed directory structure for collection config temp files

---
+++ cli/collection/modules/config/telcoblue/m2.cfg.php
@@ -0,0 +1,25 @@
+<?php
+
+// CDR
+$arrStructure[FILE_IMPORT_CDR_M2_STANDARD]				['Regex']			= "/^(cdr|bli)_[a-z0-9]+_[a-z0-9]+_\d+_\d+_\d{8}\.txt$/";
+$arrStructure[FILE_IMPORT_CDR_M2_STANDARD]				['Uniqueness']		= "FileName = <FileName> AND SHA1 = <SHA1>";
+$arrStructure[FILE_IMPORT_CDR_M2_STANDARD]				['DownloadUnique']	= TRUE;
+$arrStructure[FILE_IMPORT_CDR_M2_STANDARD]				['Paths']			[]	= '/home/telcoblue/Incoming/cdr/m2/';
+
+// Config
+$arrModuleConfig['FileDefine']		['Value']		= $arrStructure;
+
+// Output
+$strOutputFile	= basename(__FILE__, '.cfg.php').'.serialised';
+@unlink($strOutputFile);
+
+if (file_put_contents($strOutputFile, serialize($arrStructure)))
+{
+	echo "\nSerialised Data successfully dumped to '$strOutputFile'.\n\n";
+}
+else
+{
+	echo "\nUnable to dump serialised data to '$strOutputFile'.\n\n";
+}
+
+?>
