a1358525c53675a7f0dab5b61e9937cc8764e36c
Rich Davis <rdavis@yellowbilling.com.au>
2008-07-22 00:54:20 +0000

    added local cdr collection dfefinitions for iseek an aapt

---
+++ cli/collection/modules/config/telcoblue/iseek_local.cfg.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("../../../../../flex.require.php");
+
+// CDR
+$arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['Regex']			= "/^tap_isk4_\d{14}_\d{8}_\d{6}_a_s\.dat$/i";
+$arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['Uniqueness']		= "FileName = <FileName> AND SHA1 = <SHA1>";
+$arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['DownloadUnique']	= TRUE;
+$arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['Paths']			[]	= '/home/telcoblue/Incoming/cdr/aapt/';
+
+// Config
+$arrModuleConfig['FileDefine']		['Value']		= $arrStructure;
+
+// Output
+$strOutputFile	= basename(__FILE__, '.cfg.php').'.serialised';
+@unlink($strOutputFile);
+
+if (file_put_contents($strOutputFile, serialize($arrStructure)))
+{
+	echo "\nSerialised Data successfully dumped to '$strOutputFile'.\n\n";
+}
+else
+{
+	echo "\nUnable to dump serialised data to '$strOutputFile'.\n\n";
+}
+
+?>
