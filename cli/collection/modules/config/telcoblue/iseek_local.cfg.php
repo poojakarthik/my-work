bb1fc289511068303b830c7e24296cb869613151
Rich Davis <rdavis@yellowbilling.com.au>
2008-07-22 01:00:19 +0000

    added local cdr collection dfefinitions for iseek an aapt

--- cli/collection/modules/config/telcoblue/iseek_local.cfg.php
+++ cli/collection/modules/config/telcoblue/iseek_local.cfg.php
@@ -6,7 +6,7 @@ require_once("../../../../../flex.require.php");
 $arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['Regex']			= "/^tap_isk4_\d{14}_\d{8}_\d{6}_a_s\.dat$/i";
 $arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['Uniqueness']		= "FileName = <FileName> AND SHA1 = <SHA1>";
 $arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['DownloadUnique']	= TRUE;
-$arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['Paths']			[]	= '/home/telcoblue/Incoming/cdr/aapt/';
+$arrStructure[FILE_IMPORT_CDR_OPTUS_STANDARD]				['Paths']			[]	= '/home/telcoblue/Incoming/cdr/iseek/';
 
 // Config
 $arrModuleConfig['FileDefine']		['Value']		= $arrStructure;
