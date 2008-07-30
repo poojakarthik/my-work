8f7534e03dadb86dbbc419b3c1a8e2c67bfda7f1
Hadrian Oliver <holiver@yellowbilling.com.au>
2008-07-30 07:29:55 +0000

    Added support for multiple-directory paths in URLs.
    E.g. flex.php/Ticketing/Ticket/View
    
    In the above example the array [0=>'View'] would be passed to the 'Application_Handler_Ticketing::Ticket()' function as the first parameter.

--- html/ui/classes/application/Application_Page.php
+++ html/ui/classes/application/Application_Page.php
@@ -55,7 +55,7 @@ class Application_Page extends Page
 		$arrObject['Name']		= $strName;
 		$arrObject['Id']		= $strId;
 		$arrObject['Column']	= $intColumn;
-		$arrObject['Object']	= new $strClassName($intContext, $strId);
+		$arrObject['Object']	= new $strClassName($intContext, $strId, $this->mxdDataToRender);
 		if ($this->IsModal())
 		{
 			$arrObject['Object']->SetModal(TRUE);
@@ -265,9 +265,11 @@ class Application_Page extends Page
 		}
 		$strEmployeeConsoleLink = Href()->EmployeeConsole();
 
-		echo "<html>
+		echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
 	<head>
-		<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
+		<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />
+		<meta name=\"generator\" content=\"Flex\" />
 		<title>Flex - $this->_strPageName</title>
 		<base href='$strBaseDir'/>\n";
 		$this->RenderHeaderJS();
