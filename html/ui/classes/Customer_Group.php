28dfc77e55847c4b053a35c18d4be7a02c06c6e2
Hadrian Oliver <holiver@yellowbilling.com.au>
2008-07-30 07:25:19 +0000

    Added base class for Customer Group objects (currently just an abstraction layer for accessing global constants for customer groups (evil!))

---
+++ html/ui/classes/Customer_Group.php
@@ -0,0 +1,59 @@
+<?php
+
+class Customer_Group 
+{
+	protected $arrProperties = array();
+
+	protected function __construct($arrProps)
+	{
+		$this->arrProperties = $arrProps;
+	}
+
+	public function __get($property)
+	{
+		switch (strtolower($property))
+		{
+			case 'id':
+			case 'value':
+				return $this->arrProperties['Id'];
+			case 'name':
+			case 'description':
+				return $this->arrProperties['Description'];
+			case 'constant':
+				return $this->arrProperties['Constant'];
+		}
+	}
+
+	public static function getAvailablePrioritiesForUser($user)
+	{
+		$available = array();
+		if ($user->isUser())
+		{
+			$available = self::listAll();
+		}
+		return $available;
+	}
+
+	public static function listAll()
+	{
+		static $instances;
+		if (!isset($instances))
+		{
+			$instances = array();
+			foreach ($GLOBALS['*arrConstant']['CustomerGroup'] as $id => $props)
+			{
+				$props['Id'] = $id;
+				$instances[$id] = new self($props);
+			}
+		}
+		return $instances;
+	}
+
+	public static function getForId($id)
+	{
+		$instances = self::listAll();
+		return $instances[$id];
+	}
+}
+
+?>
