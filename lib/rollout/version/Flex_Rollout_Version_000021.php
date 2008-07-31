c03fd0cd55795d94b44632d5fcfb86256ab6159c
Joel Dawkins <jdawkins@yellowbilling.com.au>
2008-07-31 03:15:00 +0000

    The new created_on fields in the DirectDebit and CreditCard tables will be created as TIMESTAMPs instead of DATETTIMEs and they default to the current time

--- lib/rollout/version/Flex_Rollout_Version_000021.php
+++ lib/rollout/version/Flex_Rollout_Version_000021.php
@@ -3,8 +3,8 @@
 /**
  * Version 21 (twenty-one) of database update.
  * This version: -
- *	1:	Adds the created_on property to the CreditCard table
- *	2:	Adds the created_on property to the DirectDebit table
+ *	1:	Adds the created_on property to the CreditCard table (of type TIMESTAMP)
+ *	2:	Adds the created_on property to the DirectDebit table (of type TIMESTAMP)
  */
 
 class Flex_Rollout_Version_000021 extends Flex_Rollout_Version
@@ -17,7 +17,7 @@ class Flex_Rollout_Version_000021 extends Flex_Rollout_Version
 
 		// 1. Add the "created_on" field to the CreditCard table
 		$strSQL = "	ALTER TABLE `CreditCard` 
-					ADD `created_on` DATETIME NOT NULL COMMENT 'The timestamp at which the credit card details were entered into flex' AFTER `Archived`;
+					ADD `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The timestamp at which the credit card details were entered into flex' AFTER `Archived`;
 		";
 
 		if (!$qryQuery->Execute($strSQL))
@@ -29,7 +29,7 @@ class Flex_Rollout_Version_000021 extends Flex_Rollout_Version
 
 		// 2. Add the "created_on" field to the DirectDebit table
 		$strSQL = "	ALTER TABLE `DirectDebit` 
-					ADD `created_on` DATETIME NOT NULL COMMENT 'The timestamp at which the direct debit account details were entered into flex' AFTER `Archived`;
+					ADD `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The timestamp at which the direct debit account details were entered into flex' AFTER `Archived`;
 		";
 
 		if (!$qryQuery->Execute($strSQL))
