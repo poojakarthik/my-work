9292a6046e130a0025fb71bde2798c17b0bbc287
Hadrian Oliver <holiver@yellowbilling.com.au>
2008-08-05 07:01:59 +0000

    Added new file (committing before eclipse decides to delete it)

---
+++ lib/ticketing/Ticketing_Attachment_Blacklist_Status.php
@@ -0,0 +1,50 @@
+<?php
+
+class Ticketing_Attachment_Blacklist_Status
+{
+	protected $arrProperties = array();
+
+	protected function __construct($arrProps)
+	{
+		$this->arrProperties = $arrProps;
+	}
+
+	public function __get($property)
+	{
+		switch (strtolower($property))
+		{
+			case 'id':
+			case 'value':
+				return $this->arrProperties['Id'];
+			case 'name':
+				return $this->arrProperties['Name'];
+			case 'description':
+				return $this->arrProperties['Description'];
+			case 'constant':
+				return $this->arrProperties['Constant'];
+		}
+	}
+
+	public static function listAll()
+	{
+		static $instances;
+		if (!isset($instances))
+		{
+			$instances = array();
+			foreach ($GLOBALS['*arrConstant'][strtolower(__CLASS__)] as $id => $props)
+			{
+				$props['Id'] = $id;
+				$instances[$id] = new self($props);
+			}
+		}
+		return $instances;
+	}
+
+	public static function getForId($id)
+	{
+		$instances = self::listAll();
+		return $instances[$id];
+	}
+}
+
+?>
