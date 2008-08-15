f5e596b164a6bed5031d0824904d22c86991c163
Rich Davis <rdavis@yellowbilling.com.au>
2008-08-15 06:50:04 +0000

    Payment require.php should now include the Direct Debit files

--- cli/payment/require.php
+++ cli/payment/require.php
@@ -27,11 +27,20 @@
  *
  */
  
+$strApplicationDir	= "cli/payment/";
+ 
 // load modules
-$strApplicationDir = "cli/payment/modules/";
-VixenRequire($strApplicationDir."base_module.php");
-VixenRequire($strApplicationDir."module_billexpress.php");
-VixenRequire($strApplicationDir."module_bpay_westpac.php");
-VixenRequire($strApplicationDir."module_securepay.php");
+$strModuleDir 		= "cli/payment/modules/";
+VixenRequire($strModuleDir."base_module.php");
+VixenRequire($strModuleDir."module_billexpress.php");
+VixenRequire($strModuleDir."module_bpay_westpac.php");
+VixenRequire($strModuleDir."module_securepay.php");
+
+$strDirectDebitDir	= "cli/payment/directdebit/";
+VixenRequire($strApplicationDir."Payment_DirectDebit.php");
+VixenRequire($strDirectDebitDir."Payment_DirectDebit_File.php");
+VixenRequire($strDirectDebitDir."securepay/file/Payment_DirectDebit_File_SecurePay_BankTransfer.php");
+VixenRequire($strDirectDebitDir."securepay/file/Payment_DirectDebit_File_SecurePay_CreditCard.php");
+
 
 ?>
