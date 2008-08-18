84e8785d42fe6961f5219896f79c6dd36c43acc2
Ryan Forrester <rforrester@yellowbilling.com.au>
2008-08-18 05:47:19 +0000

    adding payment page.

--- html/customer/app_template/console.php
+++ html/customer/app_template/console.php
@@ -43,8 +43,100 @@
  */
 class AppTemplateConsole extends ApplicationTemplate
 {
+	
+	 // Make a payment.
+	 function Pay()
+	 {
+		// Check user authorization and permissions
+		AuthenticatedUser()->CheckClientAuth();
+		
+		// Retrieve the client's details
+		DBO()->Contact->Id = AuthenticatedUser()->_arrUser['Id'];
+		if (!DBO()->Contact->Load())
+		{
+			// This should never actually occur because if the contact can't be loaded then AuthenticatedUser()->CheckClientAuth() would have failed
+			DBO()->Error->Message = "The contact with contact id: ". DBO()->Contact->Id->Value ." could not be found";
+			$this->LoadPage('error');
+			return FALSE;
+		}
+		
+		if (DBO()->Account->Id->Value)
+		{
+			// A specific account has been specified, so load the details of it
+			// DBO()->Account->Id has already been initialised
+		}
+		else
+		{
+			// No specific account has been specified, so load the contact's primary account
+			DBO()->Account->Id = DBO()->Contact->Account->Value;
+		}
+		
+		// Load the clients primary account
+		DBO()->Account->Load();
+
+		// If the user can view all accounts in their account group then load these too
+		if (DBO()->Contact->CustomerContact->Value)
+		{
+			DBL()->Account->AccountGroup = DBO()->Contact->AccountGroup->Value;
+			DBL()->Account->Archived = 0;
+			DBL()->Account->Load();
+		}
+		// Make sure that the Account requested belongs to the account group that the contact belongs to
+		$bolUserCanViewAccount = FALSE;
+		if (AuthenticatedUser()->_arrUser['CustomerContact'])
+		{
+			// The user can only view the account, if it belongs to the account group that they belong to
+			if (AuthenticatedUser()->_arrUser['AccountGroup'] == DBO()->Account->AccountGroup->Value)
+			{
+				$bolUserCanViewAccount = TRUE;
+			}
+		}
+		elseif (AuthenticatedUser()->_arrUser['Account'] == DBO()->Account->Id->Value)
+		{
+			// The user can only view the account, if it is their primary account
+			$bolUserCanViewAccount = TRUE;
+		}
+		
+		if (!$bolUserCanViewAccount)
+		{
+			// The user does not have permission to view the requested account
+			BreadCrumb()->Console();
+			BreadCrumb()->SetCurrentPage("Error");
+			DBO()->Error->Message = "ERROR: The user does not have permission to view account# ". DBO()->Account->Id->Value;
+			$this->LoadPage('Error');
+			return FALSE;
+		}
+
+		// Calculate the Account Balance
+		DBO()->Account->CustomerBalance = $this->Framework->GetAccountBalance(DBO()->Account->Id->Value);
+		
+		// Calculate the Account Overdue Amount
+		$fltOverdue = $this->Framework->GetOverdueBalance(DBO()->Account->Id->Value);
+		if ($fltOverdue < 0)
+		{
+			$fltOverdue = 0;
+		}
+		DBO()->Account->Overdue = $fltOverdue;
+		
+		// Calculate the Account's total unbilled adjustments (inc GST)
+		DBO()->Account->UnbilledAdjustments = $this->Framework->GetUnbilledCharges(DBO()->Account->Id->Value);
+		
+		// Calculate the total unbilled CDRs for the account (inc GST), omitting Credit CDRs
+		DBO()->Account->UnbilledCDRs = AddGST(UnbilledAccountCDRTotal(DBO()->Account->Id->Value, TRUE));
+		
+		// Setup BreadCrumb Menu
+		$strWelcome = "Welcome " . DBO()->Contact->Title->Value ." " . DBO()->Contact->FirstName->Value ." ". DBO()->Contact->LastName->Value .". You are currently logged into your account\n";
+		BreadCrumb()->SetCurrentPage($strWelcome);
+		
+		$this->LoadPage('pay');
+
+		return TRUE;	 	
+	 }
+	 
+	 
+	 
 	//------------------------------------------------------------------------//
-	// Home
+	// Edit account details.
 	//------------------------------------------------------------------------//
 	/**
 	 * Home()
@@ -144,6 +236,8 @@ class AppTemplateConsole extends ApplicationTemplate
 
 		return TRUE;	 	
 	 }
+	 
+	 
 	function Home()
 	{
 		// Check user authorization and permissions
