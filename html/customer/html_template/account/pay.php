79200d4da88cf2c2e3971925b02dc6ba65a6a623
Ryan Forrester <rforrester@yellowbilling.com.au>
2008-08-18 05:48:03 +0000

    actual payment page.

---
+++ html/customer/html_template/account/pay.php
@@ -0,0 +1,83 @@
+<?php
+//----------------------------------------------------------------------------//
+// HtmlTemplateAccountDetails
+//----------------------------------------------------------------------------//
+/**
+ * HtmlTemplateAccountDetails
+ *
+ * HTML Template object for the Account Details
+ *
+ * HTML Template object for the Account Details
+ *
+ *
+ * @prefix	<prefix>
+ *
+ * @package	web_app
+ * @class	HtmlTemplateAccountDetails
+ * @extends	HtmlTemplate
+ */
+ 
+
+
+ class HtmlTemplateAccountPay extends HtmlTemplate
+{
+	//------------------------------------------------------------------------//
+	// _intContext
+	//------------------------------------------------------------------------//
+	/**
+	 * _intContext
+	 *
+	 * the context in which the html object will be rendered
+	 *
+	 * the context in which the html object will be rendered
+	 *
+	 * @type		integer
+	 *
+	 * @property
+	 */
+	public $_intContext;
+
+	//------------------------------------------------------------------------//
+	// __construct
+	//------------------------------------------------------------------------//
+	/**
+	 * __construct
+	 *
+	 * Constructor
+	 *
+	 * Constructor - java script required by the HTML object is loaded here
+	 *
+	 * @param	int		$intContext		context in which the html object will be rendered
+	 *
+	 * @method
+	 */
+	function __construct($intContext)
+	{
+		$this->_intContext = $intContext;
+	}
+	
+	//------------------------------------------------------------------------//
+	// Render
+	//------------------------------------------------------------------------//
+	/**
+	 * Render()
+	 *
+	 * Render this HTML Template
+	 *
+	 * Render this HTML Template
+	 *
+	 * @method
+	 */
+	 
+	function Render()
+	{
+		echo "<div class='NarrowContent'>\n";
+	
+		echo "TO Add: Credit card form...";
+		echo "<div class='Seperator'></div>\n";
+		
+		echo "</div>\n";
+	}
+}
+
+?>
