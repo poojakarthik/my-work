03baa1b64f6347ad67e29d20839add375b29a507
Rich Davis <rdavis@yellowbilling.com.au>
2008-08-18 03:58:36 +0000

    fixed bugs in direct debit

--- cli/payment/application.php
+++ cli/payment/application.php
@@ -808,7 +808,7 @@
 	 	{
 	 		// Retrieve Direct Debit Scheduling Details, and determine if today is the Invoice Due Date
 	 		$selSchedule	= new StatementSelect(	"InvoiceRun LEFT JOIN automatic_invoice_run_event ON InvoiceRun.Id = automatic_invoice_run_event.invoice_run_id",
-														"InvoiceRun.InvoiceRun, InvoiceRun.BillingDate, automatic_invoice_run_event.scheduled_datetime, automatic_invoice_run_event.actioned_datetime, automatic_invoice_run_event.id",
+														"InvoiceRun.InvoiceRun, InvoiceRun.BillingDate, automatic_invoice_run_event.scheduled_datetime, automatic_invoice_run_event.actioned_datetime, automatic_invoice_run_event.id AS id",
 														"automatic_invoice_action_id = ".AUTOMATIC_INVOICE_ACTION_DIRECT_DEBIT,
 														"InvoiceRun.BillingDate DESC, automatic_invoice_run_event.id DESC",
 														"1");
