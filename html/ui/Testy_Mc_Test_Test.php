2dbe40d2f9f6bea72f6178de5a8c9187bd5e3096
Ryan Forrester <rforrester@yellowbilling.com.au>
2008-08-19 07:19:47 +0000

    part 3

--- html/ui/Testy_Mc_Test_Test.php
+++ html/ui/Testy_Mc_Test_Test.php
@@ -1,2 +1,3 @@
 This is to check what happens when you commit a file in svn, then someone else modifies it but doesn't commit, then you move the file and commit, then they commit.
 
+Ryan added this bit
