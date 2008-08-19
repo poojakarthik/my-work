8a9177d855c7db9aac8108b1e93c86018260dea0
Ryan Forrester <rforrester@yellowbilling.com.au>
2008-08-19 05:39:27 +0000

    success html template

---
+++ html/customer/html_template/account/edit_successful.php
@@ -0,0 +1,84 @@
+<?php
+//----------------------------------------------------------------------------//
+// HtmlTemplateAccountDetails
+//----------------------------------------------------------------------------//
+/**
+ * HtmlTemplateAccountDetails
+ *
+ * HTML Template object for the Account Details
+ *
+ * HTML Template object for the Account Details
+ *
+ *
+ * @prefix	<prefix>
+ *
+ * @package	web_app
+ * @class	HtmlTemplateAccountDetails
+ * @extends	HtmlTemplate
+ */
+ 
+
+
+ class HtmlTemplateAccountEditSuccessful extends HtmlTemplate
+{
+	//------------------------------------------------------------------------//
+	// _intContext
+	//------------------------------------------------------------------------//
+	/**
+	 * _intContext
+	 *
+	 * the context in which the html object will be rendered
+	 *
+	 * the context in which the html object will be rendered
+	 *
+	 * @type		integer
+	 *
+	 * @property
+	 */
+	public $_intContext;
+
+	//------------------------------------------------------------------------//
+	// __construct
+	//------------------------------------------------------------------------//
+	/**
+	 * __construct
+	 *
+	 * Constructor
+	 *
+	 * Constructor - java script required by the HTML object is loaded here
+	 *
+	 * @param	int		$intContext		context in which the html object will be rendered
+	 *
+	 * @method
+	 */
+	function __construct($intContext)
+	{
+		$this->_intContext = $intContext;
+	}
+	
+	//------------------------------------------------------------------------//
+	// Render
+	//------------------------------------------------------------------------//
+	/**
+	 * Render()
+	 *
+	 * Render this HTML Template
+	 *
+	 * Render this HTML Template
+	 *
+	 * @method
+	 */
+	 
+	function Render()
+	{
+		echo "<div class='NarrowContent'>\n";
+
+		print "Thank you for taking the time to update your account,<br/><font color=\"green\">your changes have been completed. <img src=\"/" . CUSTOMER_URL_NAME . "/trunk/html/images/generic/check.gif\"></font><br/><br/>";
+		
+		echo "<div class='Seperator'></div>\n";
+		
+		echo "</div>\n";
+	}
+}
+
+?>
