3baab327a969fa393b0165920ba67bb7c98ba7f8
Joel Dawkins <jdawkins@yellowbilling.com.au>
2008-08-19 07:36:32 +0000

    Removed test file

--- html/customer/Testy_Mc_Test_Test.php
+++
@@ -1,3 +0,0 @@
-This is to check what happens when you commit a file in svn, then someone else modifies it but doesn't commit, then you move the file and commit, then they commit.
-
-blah blah blah.  Now I'm moved.
