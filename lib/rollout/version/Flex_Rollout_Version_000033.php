90ce7fe09cebdefa92d20d464ad9b2900f984076
Rich Davis <rdavis@yellowbilling.com.au>
2008-08-22 03:10:06 +0000

    Rollout v33 now also adds a Mobile Other Charges & Credits Record Type

--- lib/rollout/version/Flex_Rollout_Version_000033.php
+++ lib/rollout/version/Flex_Rollout_Version_000033.php
@@ -4,6 +4,7 @@
  * Version 33 (thirty-three) of database update.
  * This version: -
  *	1:	Adds Mobile to 13/1300 RecordType 
+ *	2:	Adds Mobile Other Charges & Credits RecordType
  */
 
 class Flex_Rollout_Version_000033 extends Flex_Rollout_Version
@@ -18,9 +19,9 @@ class Flex_Rollout_Version_000033 extends Flex_Rollout_Version
 		$arrUpdateDBs[]		= $GLOBALS['**arrDatabase']['cdr']['Database'];
 		$arrUpdateDBs[]		= $GLOBALS['**arrDatabase']['flex']['Database'];
 		
-		// Adds Mobile to 13/1300 RecordType
 		foreach ($arrUpdateDBs as $strUpdateDB)
 		{
+			//	1:	Adds Mobile to 13/1300 RecordType
 			$strSQL = " INSERT INTO {$strUpdateDB}.RecordType
 							(Code, Name, Description, ServiceType, Context, Required, Itemised, GroupId, DisplayType) " .
 							"VALUES ('OneThree', 'Mobile to 1300', 'Mobile to 13/1300', 101, 0, 1, 1, 0, 3)";
@@ -35,6 +36,22 @@ class Flex_Rollout_Version_000033 extends Flex_Rollout_Version
 				throw new Exception(__CLASS__ . ' Failed to Set Mobile to 13/1300\'s GroupId in '.$strUpdateDB.'. ' . $qryQuery->Error());
 			}
 			$this->rollbackSQL[] = "DELETE FROM {$strUpdateDB}.RecordType WHERE Code = 'OneThree' AND ServiceType = 101;";
+			
+			//	2:	Adds Mobile Other Charges & Credits RecordType
+			$strSQL = " INSERT INTO {$strUpdateDB}.RecordType
+							(Code, Name, Description, ServiceType, Context, Required, Itemised, GroupId, DisplayType) " .
+							"VALUES ('OC&C', 'Other Charges & Credits', 'Other Charges & Credits', 101, 3, 1, 1, 0, 2)";
+			if (!$qryQuery->Execute($strSQL))
+			{
+				throw new Exception(__CLASS__ . ' Failed to add Mobile Other Charges & Credits in '.$strUpdateDB.'. ' . $qryQuery->Error());
+			}
+			$strSQL 		= " UPDATE {$strUpdateDB}.RecordType
+								SET GroupId = Id WHERE Code = 'OC&C' AND ServiceType = 101";
+			if (!($intInsertId = $qryQuery->Execute($strSQL)))
+			{
+				throw new Exception(__CLASS__ . ' Failed to Set Mobile Other Charges & Credits\' GroupId in '.$strUpdateDB.'. ' . $qryQuery->Error());
+			}
+			$this->rollbackSQL[] = "DELETE FROM {$strUpdateDB}.RecordType WHERE Code = 'OC&C' AND ServiceType = 101;";
 		}
 		
 	}
