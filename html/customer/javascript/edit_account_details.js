9cd616c39739f8387bcdc072bfb8369dc13f4f9a
Ryan Forrester <rforrester@yellowbilling.com.au>
2008-08-27 23:25:29 +0000

    javascript email address syntax checking.

--- html/customer/javascript/edit_account_details.js
+++ html/customer/javascript/edit_account_details.js
@@ -3,10 +3,16 @@ function validate_required(field,alerttxt)
 	with (field)
 	{
 		if (value==null||value=="")
-		{alert(alerttxt);return false;}
-		else {return true}
+		{
+			alert(alerttxt);
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
 }
+
 function validate_form(thisform)
 {
 	with (thisform)
@@ -17,11 +23,21 @@ function validate_form(thisform)
 		{mixContact_FirstName.focus();return false;}
 		if (validate_required(mixContact_LastName,"LastName must be filled out!")==false)
 		{mixContact_LastName.focus();return false;}
-		if (validate_required(mixContact_Email,"Email must be filled out!")==false)
-		{mixContact_Email.focus();return false;}
 		if (validate_required(mixAccount_Suburb,"Suburb must be filled out!")==false)
 		{mixAccount_Suburb.focus();return false;}
-		if (validate_required(mixAccount_Address1,"Address1 must be filled out!")==false)
+		if (validate_required(mixAccount_Address1,"Street Address must be filled out!")==false)
 		{mixAccount_Address1.focus();return false;}
+		if (validate_required(mixContact_Email,"Email must be filled out!")==false)
+		{
+			mixContact_Email.focus();
+			return false;
+		}
 	}
+
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(thisform.mixContact_Email.value))
+	{
+		return (true)
+	}
+	alert("Invalid E-mail Address! Please re-enter.")
+	return (false)
 }
