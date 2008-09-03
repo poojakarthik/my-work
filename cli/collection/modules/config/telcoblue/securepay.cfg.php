e2018138245999fce61030eecb9255c8099f5168
Rich Davis <rdavis@yellowbilling.com.au>
2008-09-03 01:21:48 +0000

    added some config for BCS

--- cli/collection/modules/config/telcoblue/securepay.cfg.php
+++ cli/collection/modules/config/telcoblue/securepay.cfg.php
@@ -3,7 +3,7 @@
 require_once("../../../../../flex.require.php");
 
 // CDR
-$arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_SECUREPAY_STANDARD]				['Regex']			= "/^[a-z]{3}\d{2}_\d{4}-\d{2}-\d{2}_\d{2}_\d{1,4}\.txt$/i";
+$arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_SECUREPAY_STANDARD]				['Regex']			= "/^[a-z]{3}\d{2}_\d{4}-\d{2}-\d{2}_\d{2}_\d{1,9}\.txt$/i";
 $arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_SECUREPAY_STANDARD]				['Uniqueness']		= "FileName = <FileName> AND SHA1 = <SHA1>";
 $arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_SECUREPAY_STANDARD]				['DownloadUnique']	= TRUE;
 $arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_SECUREPAY_STANDARD]				['Paths']			[]	= '/home/telcoblue/Incoming/secure_pay/';
