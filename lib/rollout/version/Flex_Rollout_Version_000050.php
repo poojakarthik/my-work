d07272d8a42a45a2f35df9982e958d737c2e669f
Joel Dawkins <jdawkins@yellowbilling.com.au>
2008-09-08 03:52:54 +0000

    Committed rollout version 50 which includes all table modifications required of the Customer Status functionality

--- lib/rollout/version/Flex_Rollout_Version_000050.php
+++ lib/rollout/version/Flex_Rollout_Version_000050.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Version XX (ex-ex) of database update.
+ * Version 50 (Fifty) of database update.
  * This version: -
  *	1:	Adds customer_status table
  *	2:	Populates the customer_status table
@@ -14,7 +14,7 @@
  *	9:	Adds INDEX (invoice_run_id/account) to the automatic_invoice_action_history table
  */
 
-class Flex_Rollout_Version_0000XX extends Flex_Rollout_Version
+class Flex_Rollout_Version_000050 extends Flex_Rollout_Version
 {
 	private $rollbackSQL = array();
 	
@@ -33,7 +33,7 @@ class Flex_Rollout_Version_0000XX extends Flex_Rollout_Version
 						default_overdue_action_description VARCHAR(1000) NOT NULL COMMENT 'Default description of what the user should do with the customer when the customer is overdue',
 						precedence INT(10) NOT NULL COMMENT 'Order in which the statuses are tested. Customer status with precedence 1 will be tested before customer status with precidence 2.  Customer is assigned the first status which it satisfies',
 						test VARCHAR(255) NOT NULL COMMENT 'Identifies the ''test'' which is used to check that the criteria has been met'
-					) ENGINE = innodb COMMENT = 'Defines the various Customer Statuses and how they are tested';";
+					) ENGINE = innodb COMMENT = 'Defines the Customer Statuses and how they are tested';";
 		if (!$qryQuery->Execute($strSQL))
 		{
 			throw new Exception(__CLASS__ . ' Failed to create customer_status table. ' . $qryQuery->Error());
@@ -47,13 +47,13 @@ class Flex_Rollout_Version_0000XX extends Flex_Rollout_Version
 					(3, 'I', 'Account has dispute with invoice', 'Dispute handled by CSM or TL.', 'Collect undisputed amount (overdue amount).  Dispute handled by CSM or TL.', 3, 'AccountInDispute'),
 					(4, 'H', 'Account has gone to Austral', 'Customer to be handled by Credit Control department.', 'Customer to be handled by Credit Control department.', 4, 'AccountWithAustral'),
 					(5, 'G', 'Account is ready for Austral but has not gone there yet', 'Customer to be handled by Credit Control department.', 'Customer to be handled by Credit Control department.', 5, 'AccountReadyForAustral'),
-					(6, 'F', 'Final Demand has been sent (implies the account was still unpaid at day 36 and TDC has been applied)', 'Incoming call from customer must be redirected to Credit Control department.', 'Incoming call from customer must be redirected to Credit Control department.', 6, 'AccountSentFinalDemand'),
-					(7, 'E', 'Account has been auto barred (implies the account was still unpaid at day 29)', 'Lift barring if barred.', 'Payment must be received by credit card on the spot to automatically lift barring.', 7, 'AccountHasBeenAutoBarred'),
-					(8, 'D', 'Overdue Notice has been sent (implies the account was still unpaid at day 21)', 'Assist with any enquiry', 'Payment must be taken before being assisted with any other enquiry', 8, 'AccountSentOverdueNotice'),
-					(9, 'C', 'Non-contracted customer (doesn''t have at least one 24 month contract that has not transpired) AND not yet sent Overdue Notice', 'Assist customer and get on DD, if they are not already on it.  Promote contracted product.', 'Payment must be taken before being assisted with any other enquiry.  Assist customer and get on DD, if they are not already on it.  Promote contracted product.', 9, 'NotIn24MonthContractAndNotYetSentOverdueNotice'),
-					(10, 'B', 'Contracted customer (has at least one 24 month contract that has not transpired) AND not on Direct Debit AND not yet sent Overdue Notice', 'VIP treatment every time', 'Payment must be taken before being assisted with any other enquiry. VIP treatment every time', 10, 'In24MonthContractAndNotDirectDebitAndNotYetSentOverdueNotice'),
-					(11, 'A', 'Contracted customer (has at least one 24 month contract that has not transpired) AND pays by Direct Debit AND not emailed their bill', 'VIP treatment every time', 'Payment must be taken before being assisted with any other enquiry. VIP treatment every time', 11, 'In24MonthContractAndDirectDebitAndNotEmailedInvoice'),
-					(12, 'AAA', 'Contracted customer (has at least one 24 month contract that has not transpired) AND pays by Direct Debit AND emailed their bill', 'VIP treatment every time', 'Payment must be taken before being assisted with any other enquiry. VIP treatment every time', 12, 'In24MonthContractAndDirectDebitAndEmailedInvoice');";
+					(6, 'F', 'Final Demand has been sent (implies TDC has been applied)', 'Incoming call from customer must be redirected to Credit Control department.', 'Incoming call from customer must be redirected to Credit Control department.', 6, 'AccountSentFinalDemand'),
+					(7, 'E', 'Account has been auto barred', 'Lift barring if manually barred', 'Payment must be received by credit card on the spot to automatically lift barring.  If barring was manually carried out, then manually lift the barring, when payment is made.', 7, 'AccountHasBeenAutoBarred'),
+					(8, 'D', 'Overdue Notice has been sent', 'Assist with any enquiry', 'Payment must be taken before being assisted with any other enquiry', 8, 'AccountSentOverdueNotice'),
+					(9, 'C', 'Non-contracted customer (doesn''t have any outstanding 24 month contracted services) AND not yet sent Overdue Notice', 'Assist customer and get on DD, if they are not already on it.  Promote contracted product.', 'Payment must be taken before being assisted with any other enquiry.  Assist customer and get on DD, if they are not already on it.  Promote contracted product.', 9, 'NotIn24MonthContractAndNotYetSentOverdueNotice'),
+					(10, 'B', 'Contracted customer (has at least one service with an outstanding 24 month contract) AND not on Direct Debit AND not yet sent Overdue Notice', 'VIP treatment every time', 'Payment must be taken before being assisted with any other enquiry. VIP treatment every time', 10, 'In24MonthContractAndNotDirectDebitAndNotYetSentOverdueNotice'),
+					(11, 'A', 'Contracted customer (has at least one service with an outstanding 24 month contract) AND pays by Direct Debit AND not emailed their bill', 'VIP treatment every time', 'Payment must be taken before being assisted with any other enquiry. VIP treatment every time', 11, 'In24MonthContractAndDirectDebitAndNotEmailedInvoice'),
+					(12, 'AAA', 'Contracted customer (has at least one service with an outstanding 24 month contract) AND pays by Direct Debit AND emailed their bill', 'VIP treatment every time', 'Payment must be taken before being assisted with any other enquiry. VIP treatment every time', 12, 'In24MonthContractAndDirectDebitAndEmailedInvoice');";
 		if (!$qryQuery->Execute($strSQL))
 		{
 			throw new Exception(__CLASS__ . ' Failed to populate the customer_status table. ' . $qryQuery->Error());
@@ -68,7 +68,7 @@ class Flex_Rollout_Version_0000XX extends Flex_Rollout_Version
 						user_role_id BIGINT(20) UNSIGNED NOT NULL COMMENT 'FK user_role table',
 						description VARCHAR(1000) NOT NULL COMMENT 'description of the required action that the employee must take when of this role/customer_status',
 						overdue_description VARCHAR(1000) NOT NULL COMMENT 'description of the required action that the employee must take when of this role/customer_status and the customer has an overdue amount'
-					) ENGINE = innodb COMMENT = 'Defines the action to be taken by a user, for a customer of a given customer status';";
+					) ENGINE = innodb COMMENT = 'Action to be taken, for a given customer status';";
 		if (!$qryQuery->Execute($strSQL))
 		{
 			throw new Exception(__CLASS__ . ' Failed to create customer_status_action table. ' . $qryQuery->Error());
@@ -83,7 +83,7 @@ class Flex_Rollout_Version_0000XX extends Flex_Rollout_Version
 						invoice_run_id BIGINT(20) UNSIGNED NOT NULL COMMENT 'FK InvoiceRun table',
 						last_updated DATETIME NOT NULL COMMENT 'time at which the status was last calculated',
 						customer_status_id BIGINT(20) UNSIGNED NOT NULL COMMENT 'FK customer_status table'
-					) ENGINE = innodb COMMENT = 'Defines most recently calculated customer status for a given invoice run/account';";
+					) ENGINE = innodb COMMENT = 'Customer status for a given invoice run/account';";
 		if (!$qryQuery->Execute($strSQL))
 		{
 			throw new Exception(__CLASS__ . ' Failed to create customer_status_history table. ' . $qryQuery->Error());
@@ -109,12 +109,16 @@ class Flex_Rollout_Version_0000XX extends Flex_Rollout_Version
 					(1, 'Flex Admin', 'Flex System Administrator', 'USER_ROLE_FLEX_ADMIN'),
 					(2, 'Manager', 'Manager', 'USER_ROLE_MANAGER'),
 					(3, 'Team Leader', 'Team Leader', 'USER_ROLE_TEAM_LEADER'),
-					(4, 'Customer Service Representitive', 'Customer Service Representitive', 'USER_ROLE_CUSTOMER_SERVICE_REPRESENTITIVE'),
+					(4, 'Customer Service Representative', 'Customer Service Representative', 'USER_ROLE_CUSTOMER_SERVICE_REPRESENTATIVE'),
 					(5, 'Credit Control Manager', 'Credit Control Manager', 'USER_ROLE_CREDIT_CONTROL_MANAGER'),
 					(6, 'Sales', 'Sales', 'USER_ROLE_SALES'),
 					(7, 'Admin Manager', 'Admin Manager', 'USER_ROLE_ADMIN_MANAGER'),
-					(8, 'Adminion', 'Admin Minion', 'USER_ROLE_ADMINION'),
-					(9, 'Accounts', 'Accounts', 'USER_ROLE_ACCOUNTS');";
+					(8, 'Credit Control', 'Credit Control', 'USER_ROLE_CREDIT_CONTROL'),
+					(9, 'Accounts', 'Accounts', 'USER_ROLE_ACCOUNTS'),
+					(10, 'Admin-Sales Support', 'Admin-Sales Support', 'USER_ROLE_ADMIN_SALES_SUPPORT'),
+					(11, 'Customer Service Supervisor', 'Customer Service Supervisor', 'USER_ROLE_CUSTOMER_SERVICE_SUPERVISOR'),
+					(12, 'Trainer', 'Trainer', 'USER_ROLE_TRAINER'),
+					(13, 'Wholesale Manager', 'Wholesale Manager', 'USER_ROLE_WHOLESALE_MANAGER');";
 		if (!$qryQuery->Execute($strSQL))
 		{
 			throw new Exception(__CLASS__ . ' Failed to populate the user_role table. ' . $qryQuery->Error());
@@ -144,6 +148,7 @@ class Flex_Rollout_Version_0000XX extends Flex_Rollout_Version
 			throw new Exception(__CLASS__ . ' Failed to add index (account_id/invoice_run_id) to customer_status_history table. ' . $qryQuery->Error());
 		}
 		$this->rollbackSQL[] = "ALTER TABLE `automatic_invoice_action_history` DROP INDEX `invoice_run_id_account`;";
+		
 	}
 	
 	function rollback()
