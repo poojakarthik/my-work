3157bc1923b466e0bbf457a7603e5b10b074af0c
Rich Davis <rdavis@yellowbilling.com.au>
2008-09-09 02:12:53 +0000

    Rollout v51 now also adds the RecurringCharge.in_advance field

--- lib/rollout/version/Flex_Rollout_Version_000051.php
+++ lib/rollout/version/Flex_Rollout_Version_000051.php
@@ -4,6 +4,7 @@
  * Version 51 of database update.
  * This version: -
  *	1:	Fixes spelling mistakes in the provisioning_type table
+ *	2:	Adds the RecurringCharge.in_advance field
  */
 
 class Flex_Rollout_Version_000051 extends Flex_Rollout_Version
@@ -29,6 +30,14 @@ class Flex_Rollout_Version_000051 extends Flex_Rollout_Version
 			throw new Exception(__CLASS__ . ' Failed to fix spelling mistake for id 917 in the provisioning_type table. ' . $qryQuery->Error());
 		}
 		$this->rollbackSQL[] = "UPDATE provisioning_type SET name = 'Preselection Lost (Diconnected)', description = 'Preselection Lost (Diconnected)' WHERE id = 917;";
+		
+		// 2:	Adds the RecurringCharge.in_advance field
+		$strSQL = "ALTER TABLE RecurringCharge ADD in_advance TINYINT(1) NOT NULL COMMENT '1: Charged in Advance; 0: Charged in Arrears' AFTER Continuable;";
+		if (!$qryQuery->Execute($strSQL))
+		{
+			throw new Exception(__CLASS__ . ' Failed to add the RecurringCharge.in_advance field. ' . $qryQuery->Error());
+		}
+		$this->rollbackSQL[] = "ALTER TABLE RecurringCharge DROP in_advance;";
 		
 	}
 	
