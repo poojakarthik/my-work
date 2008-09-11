bb6fd94d0f1857e33212a5ab3e7bf207e603611b
Joel Dawkins <jdawkins@yellowbilling.com.au>
2008-09-11 01:25:27 +0000

    This class now references the database to pull all Customer Group information, instead of the globally defined CustomerGroup information

--- lib/classes/Customer_Group.php
+++ lib/classes/Customer_Group.php
@@ -1,52 +1,103 @@
 <?php
 
+//----------------------------------------------------------------------------//
+// Customer_Group
+//----------------------------------------------------------------------------//
+/**
+ * Customer_Group
+ *
+ * Models a single customer group.  Also includes other customer group functionality
+ *
+ * Models a single customer group.  Also includes other customer group functionality
+ *
+ * @class	Customer_Group
+ */
 class Customer_Group 
 {
+	private $id								= NULL;
+	private $internalName					= NULL;
+	private $externalName					= NULL;
+	private $outboundEmail					= NULL;
+	private $flexUrl						= NULL;
+	private $emailDomain					= NULL;
+	private $customerPrimaryColor			= NULL;
+	private $customerSecondaryColor			= NULL;
+	private $customerLogo					= NULL;
+	private $customerLogoType				= NULL;
+	private $customerBreadcrumbMenuColor	= NULL;
+	private $customerExitUrl				= NULL;
+	private $billPayBillerCode				= NULL;
+	private $abn							= NULL;
+	private $acn							= NULL;
+	private $businessPhone					= NULL;
+	private $businessFax					= NULL;
+	private $businessWeb					= NULL;
+	private $businessContactEmail			= NULL;
+	private $businessInfoEmail				= NULL;
+	private $customerServicePhone			= NULL;
+	private $customerServiceEmail			= NULL;
+	private $customerServiceContactName		= NULL;
+	private $businessPayableName			= NULL;
+	private $businessPayableAddress			= NULL;
+	private $businessFriendlyName			= NULL;
+	private $businessFriendlyNamePossessive	= NULL;
+	private $creditCardPaymentPhone			= NULL;
+	private $faultsPhone					= NULL;
+	
+	private $value							= NULL;
+	private $name							= NULL;
+	private $description					= NULL;
+	private $constant						= NULL;
+	
+	
 	protected $arrProperties = array();
 
-	protected function __construct($arrProps)
+	//------------------------------------------------------------------------//
+	// __construct
+	//------------------------------------------------------------------------//
+	/**
+	 * __construct()
+	 *
+	 * constructor
+	 * 
+	 * constructor
+	 *
+	 * @param		array	$arrProperties 	Optional.  Associative array defining a customer group with keys for each field of the CustomerGroup table
+	 * @return		void
+	 * @constructor
+	 */
+	private function __construct($arrProperties=NULL)
 	{
-		$this->arrProperties = $arrProps;
-	}
-
-	public function __get($property)
-	{
-		switch (strtolower($property))
+		if ($arrProperties)
 		{
-			case 'id':
-			case 'value':
-				return $this->arrProperties['Id'];
-			case 'name':
-			case 'description':
-				return $this->arrProperties['Description'];
-			case 'constant':
-				return $this->arrProperties['Constant'];
+			$this->init($arrProperties);
 		}
 	}
 
-	public static function getAvailablePrioritiesForUser($user)
+	public static function getAll($bolForceRefresh=FALSE)
 	{
-		$available = array();
-		if ($user->isUser())
-		{
-			$available = self::listAll();
-		}
-		return $available;
+		return self::listAll($bolForceRefresh);
 	}
 
-	public static function listAll()
+	public static function listAll($bolForceRefresh=FALSE)
 	{
-		static $instances;
-		if (!isset($instances))
+		static $arrCustomerGroups;
+		if (!isset($arrCustomerGroups) || $bolForceRefresh)
 		{
-			$instances = array();
-			foreach ($GLOBALS['*arrConstant']['CustomerGroup'] as $id => $props)
+			$arrCustomerGroups = array();
+			
+			$selCustomerGroups = new StatementSelect("CustomerGroup", self::getColumns(), "", "InternalName ASC");
+			if (($outcome = $selCustomerGroups->Execute()) === FALSE)
 			{
-				$props['Id'] = $id;
-				$instances[$id] = new self($props);
+				throw new Exception("Failed to retrieve all Customer Groups: ". $selCustomerGroups->Error());
+			}
+	
+			while ($arrCustomerGroup = $selCustomerGroups->Fetch())
+			{
+				$arrCustomerGroups[$arrCustomerGroup['Id']] = new self($arrCustomerGroup);
 			}
 		}
-		return $instances;
+		return $arrCustomerGroups;
 	}
 
 	public static function getForId($id)
@@ -55,6 +106,134 @@ class Customer_Group
 		$id = intval($id);
 		return (array_key_exists($id, $instances)) ? $instances[$id] : NULL;
 	}
+	
+	//------------------------------------------------------------------------//
+	// getColumns
+	//------------------------------------------------------------------------//
+	/**
+	 * getColumns()
+	 *
+	 * Returns array defining the columns of the CustomerGroup table
+	 * 
+	 * Returns array defining the columns of the CustomerGroup table
+	 *
+	 * @return		array	
+	 * @method
+	 */
+	protected static function getColumns()
+	{
+		return array(
+						"Id",
+						"InternalName",
+						"ExternalName",
+						"OutboundEmail",
+						"flex_url",
+						"email_domain",
+						"customer_primary_color",
+						"customer_secondary_color",
+						"customer_logo",
+						"customer_logo_type",
+						"customer_breadcrumb_menu_color",
+						"customer_exit_url",
+						"bill_pay_biller_code",
+						"abn",
+						"acn",
+						"business_phone",
+						"business_fax",
+						"business_web",
+						"business_contact_email",
+						"business_info_email",
+						"customer_service_phone",
+						"customer_service_email",
+						"customer_service_contact_name",
+						"business_payable_name",
+						"business_payable_address",
+						"business_friendly_name",
+						"business_friendly_name_possessive",
+						"credit_card_payment_phone",
+						"faults_phone"
+					);
+	}
+
+	//------------------------------------------------------------------------//
+	// init
+	//------------------------------------------------------------------------//
+	/**
+	 * init()
+	 *
+	 * Initialises the Customer_Group object
+	 * 
+	 * Initialises the Customer_Group object
+	 *
+	 * @param		array	$arrProperties		assoc array modelling record of CustomerGroup table
+	 * @return		void
+	 * @method
+	 */
+	private function init($arrProperties)
+	{
+		foreach($arrProperties as $name => $value)
+		{
+			$this->{self::tidyName($name)} = $value;
+		}
+
+		// Constant Group stuff
+		$this->name			= $this->internalName;
+		$this->description	= $this->internalName;
+		$this->constant		= "CUSTOMER_GROUP_" . strtoupper(str_replace(" ", "_", $this->internalName));
+		$this->value		= $this->id;
+	}
+
+	//------------------------------------------------------------------------//
+	// __get
+	//------------------------------------------------------------------------//
+	/**
+	 * __get()
+	 *
+	 * accessor method
+	 * 
+	 * accessor method
+	 *
+	 * @param	string	$strName	name of property to get. in either of the formats xxxYyyZzz or xxx_yyy_zzz 
+	 * @return	void
+	 * @method
+	 */
+	public function __get($strName)
+	{
+		if ($strName[0] === '_')
+		{
+			// Don't allow access to data attributes that start with '_'
+			return NULL;
+		}
+		if (property_exists($this, $strName) || (($strName = self::tidyName($strName)) && property_exists($this, $strName)))
+		{
+			return $this->{$strName};
+		}
+		return NULL;
+	}
+	
+	//------------------------------------------------------------------------//
+	// tidyName
+	//------------------------------------------------------------------------//
+	/**
+	 * tidyName()
+	 *
+	 * Converts a string from xxx_yyy_zzz to xxxYyyZzz
+	 * 
+	 * Converts a string from xxx_yyy_zzz to xxxYyyZzz
+	 * If the string is already in the xxxYxxZzz format, then it will not be changed
+	 *
+	 * @param	string	$strName
+	 * @return	string
+	 * @method
+	 */
+	private function tidyName($name)
+	{
+		$tidy = str_replace(' ', '', ucwords(str_replace('_', ' ', $name)));
+		$tidy[0] = strtolower($tidy[0]);
+		return $tidy;
+	}
+
+	
 }
 
 ?>
