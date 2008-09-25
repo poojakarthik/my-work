c28a78cdfc06ba11c0dec147c4e0cb03de847872
Ryan Forrester <rforrester@yellowbilling.com.au>
2008-09-25 06:30:23 +0000

    update to rollout 59

--- lib/rollout/version/Flex_Rollout_Version_000059.php
+++ lib/rollout/version/Flex_Rollout_Version_000059.php
@@ -20,7 +20,7 @@ class Flex_Rollout_Version_000059 extends Flex_Rollout_Version
 		{
 			throw new Exception(__CLASS__ . ' Failed to run query1. ' . $result->getMessage());
 		}
-		$strSQL = "CREATE TABLE `flex_dev`.`customer_faq` (
+		$strSQL = "CREATE TABLE `customer_faq` (
 		`id` MEDIUMINT( 11 ) NOT NULL AUTO_INCREMENT COMMENT 'Field to record faq ids',
 		`title` VARCHAR( 255 ) NULL COMMENT 'field for faq title',
 		`contents` TEXT NULL COMMENT 'field for faq contents',
@@ -42,7 +42,7 @@ class Flex_Rollout_Version_000059 extends Flex_Rollout_Version
 		{
 			throw new Exception(__CLASS__ . ' Failed to run query3. ' . $result->getMessage());
 		}
-		$this->rollbackSQL[] = "CREATE TABLE `flex_dev`.`customer_faq` (
+		$this->rollbackSQL[] = "CREATE TABLE `customer_faq` (
 		`id` MEDIUMINT( 11 ) NOT NULL AUTO_INCREMENT COMMENT 'Field to record faq ids',
 		`title` VARCHAR( 255 ) NULL COMMENT 'field for faq title',
 		`contents` TEXT NULL COMMENT 'field for faq contents',
