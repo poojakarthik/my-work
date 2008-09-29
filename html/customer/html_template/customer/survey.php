1021b0b75f61199642de92a67357264ec8a0d341
Ryan Forrester <rforrester@yellowbilling.com.au>
2008-09-29 08:35:50 +0000

    survey changes

--- html/customer/html_template/customer/survey.php
+++ html/customer/html_template/customer/survey.php
@@ -72,30 +72,137 @@
 	function Render()
 	{
 
+		$arrInputTypes = array(
+			"select" => "<select name='[question]'>", 
+			"text" => "<input type='text' name='[question]'>", 
+			"textarea" => "<textarea name='[question]'>",
+			"checkbox" => "<input type='checkbox' name='[question]'>"
+		);
+
+		$arrEndInputTypes = array(
+			"select" => "</select>", 
+			"text" => "", 
+			"textarea" => "</textarea>",
+			"checkbox" => ""
+		);
+		$arrInputDropDown = array(
+			"select" => "<option value='[option]'>[option]</option>", 
+			"text" => "", 
+			"textarea" => "",
+			"checkbox" => ""
+		);
+
+		$mixOutPut .= "";
+		$intLoopStarted = FALSE;
+		$IntQuestionId = NULL; // by default, no question is selected
+		$arrSurveyCount = array();
+		$intQuestionNum=NULL;
+		$numbers = array();
 		foreach(DBO()->Survey->Results->Value as $results)
 		{
 			foreach($results as $key=>$val){
 				$$key=$val;
 			}
 
-			$mixQuestions .= "question: $question,  option_name: $option_name, response_type: $response_type<br>\n";
-			unset($question,$option_name,$response_type);
+			$mixQuestionStart = "
+			<div class='customer-standard-table-title-style-password'>$question</div>
+			<div class='GroupedContent'>
+			<TABLE class=\"customer-standard-table-style\">
+			<TR VALIGN=\"TOP\">
+				<TD>";
+			
+			$mixQuestionEnd .= "
+				</TD>
+			</TR>
+			</TABLE>
+			</div>
+			<br/>";
+
+			$foo = $numbers[$question_id];
+			if($foo=="")
+			{
+				echo "S$question_id";
+				$numbers[$question_id]="$question_id";
+				//$mixOutPut .= "$mixQuestionStart";
+			}
+
+			$count++;
+			$mixCheckBoxStart = "";
+			$mixCheckBoxEnd = "";
+			
+
+			if($intQuestionNum == NULL)
+			{
+				$mixOutPut .= "<br>START $question_id<br>";
+			}
+			if($intQuestionNum !== NULL && $intQuestionNum !== $question_id)
+			{
+				$mixOutPut .= "<br>END $question_id<br><br>START $question_id<br>";
+			}
+
+			if($response_type == "checkbox")
+			{
+				$mixCheckBoxStart = "";
+				$mixCheckBoxEnd = "$option_name<br>";	
+			}
+			// First Item
+			if($IntQuestionId == NULL)
+			{
+				$mixOutPut .= "" . "debug: 1" . str_replace("[question]","strAnswer[$question_id]","$arrInputTypes[$response_type]") . "$mixCheckBoxEnd\n";				
+				$intLoopStarted = TRUE;
+			}
+			if($IntQuestionId == "$question_id" || $IntQuestionId == NULL)
+			{
+				if($response_type == "checkbox")
+				{
+					$mixOutPut .= "
+					debug: 2" . "$mixCheckBoxStart" . str_replace("[question]","arrAnswer[$id]","$arrInputTypes[$response_type]") . "$mixCheckBoxEnd\n";
+				}
+				$mixOutPut .= "debug: 3" . str_replace("[option]","$option_name","$arrInputDropDown[$response_type]") . "\n";
+			}
+
+			// Last Item...
+			if($IntQuestionId !== "$question_id" && $IntQuestionId !== NULL)
+			{
+				$IntPrevious = $question_id-1;
+				$mixOutPut .= "$arrEndInputTypes[$response_type]" . "\n";
+				//echo "Ending...$IntPrevious<br>\n";
+				$IntQuestionId = NULL;
+				// start new question..
+				$mixOutPut .= "" . "debug: 4" . "$mixCheckBoxStart" . str_replace("[question]","strAnswer[$question_id]","$arrInputTypes[$response_type]") . "$mixCheckBoxEnd \n";
+				$mixOutPut .= "" . "debug: 5" . str_replace("[option]","$option_name","$arrInputDropDown[$response_type]") . "\n";
+				$intLoopStarted = TRUE;
+			}
+			$IntQuestionId = "$question_id";
+			$intQuestionNum = $question_id;
+
+			if($foo!==$question_id)
+			{
+				echo "E, ";
+				//$mixOutPut .= "$mixQuestionEnd";
+			}
+
+		}
+		if($intLoopStarted == TRUE)
+		{
+			$mixOutPut .= "debug: 6" . "$arrEndInputTypes[$response_type]" . "\n";
+			//echo "Ending...$question_id<br>\n";
 		}
 
 		echo "<div class='customer-standard-display-title'>&nbsp;</div><br/><br/>";
-		echo "<form method=\"GET\" action=\"./flex.php/Console/Survey/\">";
 
 		echo "
 		<div class='customer-standard-table-title-style-confirm-details'>$title</div>
 		<div class='GroupedContent'>
 		<table class=\"customer-standard-table-style\">
 		<tr>
-			<td>$mixQuestions</td>
+			<td>Please complete all fields below</td>
 		</tr>
 		</table>
 		</div><br/>";
 
-		echo "</FORM>";
+		echo $mixOutPut;
+
 	}
 }
 
