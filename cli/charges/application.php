1316966fd47387415fc9b84578f299a4ec6d219e
Rich Davis <rdavis@yellowbilling.com.au>
2008-09-30 21:39:20 +0000

    fixed a bug in recurring charges

--- cli/charges/application.php
+++ cli/charges/application.php
@@ -69,7 +69,7 @@
 								  "AND RecurringFreq > 0 " .
 								  "AND RecurringCharge.Archived = 0 " .
 								  "AND Account.Archived = ".ACCOUNT_STATUS_ACTIVE." " .
-								  "AND Service.Status = ".SERVICE_ACTIVE." AND " .
+								  "AND (Service.Status = ".SERVICE_ACTIVE." OR Service.Status IS NULL) " .
 								  "(" .
 								  "		Continuable = 1 " .
 								  "		OR " .
