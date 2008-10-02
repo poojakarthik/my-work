8069cb1fda47e3ec169009d2903f4b4ebde8f725
Joel Dawkins <jdawkins@yellowbilling.com.au>
2008-10-02 03:33:19 +0000

    Fixed up some formatting issues in the code

--- html/admin/classes/json/handler/customer/JSON_Handler_Customer_Verification.php
+++ html/admin/classes/json/handler/customer/JSON_Handler_Customer_Verification.php
@@ -229,17 +229,17 @@ class JSON_Handler_Customer_Verification extends JSON_Handler
 					$arrEmail = NULL;
 				}
 				
-if (strlen($objContact->password) != 0)
-{
-				// All contacts have a password
-				$arrPassword = array(	"Sha1"		=> $objContact->password,
-										"Weight"	=> 10
-									);
-}
-else
-{
-	$arrPassword = NULL;
-}				
+				if (strlen($objContact->password) != 0)
+				{
+					// All contacts have a password
+					$arrPassword = array(	"Sha1"		=> $objContact->password,
+											"Weight"	=> 10
+										);
+				}
+				else
+				{
+					$arrPassword = NULL;
+				}				
 				$arrCustomer['Contacts'][$objContact->id] = array(	"Id"			=> $objContact->id,
 																	"Name"			=> htmlspecialchars($objContact->getName()),
 																	"Verifiable"	=> array(	"DOB"		=> $arrDOB,
