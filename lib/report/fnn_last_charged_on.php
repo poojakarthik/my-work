117e8b370ad64dca518bc72da3e09db3d447f7da
Rich Davis <rdavis@yellowbilling.com.au>
2008-10-06 22:48:59 +0000

    added a report which finds the earliest and latest CDR dates for a CSV of FNNs

---
+++ lib/report/fnn_last_charged_on.php
@@ -0,0 +1,58 @@
+<?php
+
+// Framework
+require_once("../../flex.require.php");
+
+// Statements
+$selLastChargedOn	= new StatementSelect("Service", "MIN(EarliestCDR) AS MinEarliestCDR, MAX(LatestCDR) AS MaxLatestCDR", "FNN = <FNN>");
+
+// Source file
+$strInputPath	= $argv[1];
+if (!file_exists($strInputPath))
+{
+	throw new Exception("CSV file '{$strInputPath}' does not exist!");
+}
+
+$strOutputPath	= dirname($strInputPath).basename($strInputPath, 'csv').'.output.csv';
+
+// Open the CSV files
+$resInputFile	= fopen($strInputPath, 'r');
+$resOutputFile	= fopen($strOutputPath, 'w');
+if ($resInputFile && $resOutputFile)
+{
+	// Parse each line
+	while ($arrLine = fgetcsv($strInputPath))
+	{
+		// Get each FNN from the line
+		foreach ($arrLine as $strFNN)
+		{			
+			$arrOutputLine	= Array($strFNN);
+			
+			// Find the CDR Dates
+			$strFNN	= trim($strFNN);
+			if ($selLastChargedOn->Execute(Array('FNN'=>$strFNN)) === FALSE)
+			{
+				throw new Exception($selLastChargedOn->Error());
+			}
+			
+			if (!($arrLastChargedOn = $selLastChargedOn->Fetch()))
+			{
+				$arrLastChargedOn	= Array();
+			}
+			
+			$arrOutputLine[]	= $arrLastChargedOn['MinEarliestCDR'];
+			$arrOutputLine[]	= $arrLastChargedOn['MaxLatestCDR'];
+		}
+	}
+	
+	// Close the files
+	fclose($resOutputFile);
+	fclose($resInputFile);
+}
+else
+{
+	throw new Exception("Unable to open file '{$strInputPath}' or '{$strOutputPath}'");
+}
+
+
+?>
