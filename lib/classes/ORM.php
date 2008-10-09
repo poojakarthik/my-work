41dd3913854cbe0e61925618caa7e2b2a05668a5
Rich Davis <rdavis@yellowbilling.com.au>
2008-10-09 05:31:22 +0000

    fixed a bug in the ORM base class

--- lib/classes/ORM.php
+++ lib/classes/ORM.php
@@ -192,58 +192,5 @@ abstract class ORM
 	{
 		return $this->_arrProperties;
 	}
-	
-	//------------------------------------------------------------------------//
-	// _preparedStatement
-	//------------------------------------------------------------------------//
-	/**
-	 * _preparedStatement()
-	 *
-	 * Access a Static Cache of Prepared Statements used by Invoice
-	 *
-	 * Access a Static Cache of Prepared Statements used by Invoice
-	 * 
-	 * @param	string		$strStatement						Name of the statement
-	 * 
-	 * @return	Statement										The requested Statement
-	 *
-	 * @method
-	 */
-	abstract private static function _preparedStatement($strStatement);
-	/*{
-		/*throw new Exception();
-		
-		static	$arrPreparedStatements	= Array();
-		if (isset($arrPreparedStatements[$strStatement]))
-		{
-			return $arrPreparedStatements[$strStatement];
-		}
-		else
-		{
-			switch ($strStatement)
-			{
-				// SELECTS
-				case 'selById':
-					$arrPreparedStatements[$strStatement]	= new StatementSelect("", "*", "id = <Id>", NULL, 1);
-					break;
-				
-				// INSERTS
-				case 'insSelf':
-					$arrPreparedStatements[$strStatement]	= new StatementInsert("");
-					break;
-				
-				// UPDATE BY IDS
-				case 'ubiSelf':
-					$arrPreparedStatements[$strStatement]	= new StatementUpdateById("");
-					break;
-				
-				// UPDATES
-				
-				default:
-					throw new Exception(__CLASS__."::{$strStatement} does not exist!");
-			}
-			return $arrPreparedStatements[$strStatement];
-		}
-	}*/
 }
 ?>
