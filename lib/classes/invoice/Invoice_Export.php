f6224402af60d00825ef9e43d00a297e12788919
Rich Davis <rdavis@yellowbilling.com.au>
2008-10-10 04:02:36 +0000

    fixed a bug in Invoice_Export

--- lib/classes/invoice/Invoice_Export.php
+++ lib/classes/invoice/Invoice_Export.php
@@ -124,7 +124,7 @@ class Invoice_Export
 		$selServiceInstances	= self::_preparedStatement('selServiceInstances');
 		foreach ($arrAccountFNNs as $intKey=>$arrService)
 		{
-			Cli_App_Billing::debug($arrService);
+			//Cli_App_Billing::debug($arrService);
 			
 			// Get details from the Current Service
 			$arrService['invoice_run_id']	= $arrInvoice['invoice_run_id'];
@@ -137,7 +137,7 @@ class Invoice_Export
 				$arrServiceDetails	= $selServiceDetails->Fetch();
 				$arrService			= array_merge($arrService, $arrServiceDetails);
 				
-				Cli_App_Billing::debug($arrService);
+				//Cli_App_Billing::debug($arrService);
 				
 				// Is this the Primary FNN?
 				$arrService['Primary']		= ($arrService['FNN'] >= $arrService['RangeStart'] && $arrService['FNN'] <= $arrService['RangeEnd']) ? TRUE : FALSE;
@@ -474,7 +474,7 @@ class Invoice_Export
 					$arrService['RatePlan']		= "RatePlan.Name";
 					$arrPreparedStatements[$strStatement]	= new StatementSelect(	"((((Service JOIN ServiceTotal ON ServiceTotal.Service = Service.Id) JOIN RatePlan ON ServiceTotal.RatePlan = RatePlan.Id) LEFT JOIN CostCentre ON CostCentre.Id = Service.CostCentre) LEFT JOIN ServiceExtension ON (ServiceExtension.Service = Service.Id AND ServiceExtension.Archived = 0)) LEFT JOIN CostCentre CostCentreExtension ON ServiceExtension.CostCentre = CostCentreExtension.Id",
 																					$arrService,
-																					"ServiceTotal.invoice_run_id = <invoice_run_id> AND ServiceTotal.Id = <CurrentId> AND (ServiceExtension.Name IS NULL OR ServiceExtension.Name = <Extension>)",
+																					"ServiceTotal.invoice_run_id = <invoice_run_id> AND ServiceTotal.Service = <CurrentId> AND (ServiceExtension.Name IS NULL OR ServiceExtension.Name = <Extension>)",
 																					"Service.ServiceType, Service.FNN, ServiceExtension.Name",
 																					NULL,
 																					"Service.FNN, ServiceExtension.Name");
