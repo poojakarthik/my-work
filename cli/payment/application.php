33acfd21b746654cbb76854433668b91088b566f
Rich Davis <rdavis@yellowbilling.com.au>
2008-10-15 04:39:07 +0000

    Direct Debits will now ensure that it has been scheduled before it actually runs

--- cli/payment/application.php
+++ cli/payment/application.php
@@ -811,7 +811,7 @@
 	 		// Retrieve Direct Debit Scheduling Details, and determine if today is the Invoice Due Date
 	 		$selSchedule	= new StatementSelect(	"InvoiceRun LEFT JOIN automatic_invoice_run_event ON InvoiceRun.Id = automatic_invoice_run_event.invoice_run_id",
 														"InvoiceRun.Id AS Id, InvoiceRun.Id AS invoice_run_id, InvoiceRun.BillingDate, automatic_invoice_run_event.scheduled_datetime, automatic_invoice_run_event.actioned_datetime, automatic_invoice_run_event.id AS id",
-														"scheduled_datetime IS NOT NULL, actioned_datetime IS NULL AND automatic_invoice_action_id = ". AUTOMATIC_INVOICE_ACTION_DIRECT_DEBIT,
+														"scheduled_datetime IS NOT NULL AND actioned_datetime IS NULL AND automatic_invoice_action_id = ". AUTOMATIC_INVOICE_ACTION_DIRECT_DEBIT,
 														"InvoiceRun.BillingDate DESC, automatic_invoice_run_event.id DESC",
 														"1");
 			
