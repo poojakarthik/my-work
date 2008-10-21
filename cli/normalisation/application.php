740acf502a011cbdcf80b68adb9f3d78ef0d2249
Rich Davis <rdavis@yellowbilling.com.au>
2008-10-21 00:05:49 +0000

    fixed a normalisation duplicate bug

--- cli/normalisation/application.php
+++ cli/normalisation/application.php
@@ -706,20 +706,20 @@
 				
 				// Report
 				$qryQuery	= Query();
-				$strFindDuplicateSQL	= "SELECT Id, CASE WHEN CarrierRef <=> '{$arrCDR['CarrierRef'}' THEN ".CDR_DUPLICATE." ELSE ".CDR_RECHARGE." END AS Status 
+				$strFindDuplicateSQL	= "SELECT Id, CASE WHEN CarrierRef <=> '{$arrCDR['CarrierRef']}' THEN ".CDR_DUPLICATE." ELSE ".CDR_RECHARGE." END AS Status 
 											FROM CDR 
-											WHERE Id != {$arrCDR['Id'} AND 
-											FNN = '{$arrCDR['FNN'}' AND 
-											Source <=> '{$arrCDR['Source'}' AND 
-											Destination <=> '{$arrCDR['Destination'}' AND 
-											StartDatetime <=> '{$arrCDR['StartDatetime'}' AND 
-											EndDatetime <=> '{$arrCDR['EndDatetime'}' AND 
-											Units = {$arrCDR['Units'} AND 
-											Cost = {$arrCDR['Cost'} AND 
-											RecordType = {$arrCDR['RecordType'} AND 
+											WHERE Id != {$arrCDR['Id']} AND 
+											FNN = '{$arrCDR['FNN']}' AND 
+											Source <=> '{$arrCDR['Source']}' AND 
+											Destination <=> '{$arrCDR['Destination']}' AND 
+											StartDatetime <=> '{$arrCDR['StartDatetime']}' AND 
+											EndDatetime <=> '{$arrCDR['EndDatetime']}' AND 
+											Units = {$arrCDR['Units']} AND 
+											Cost = {$arrCDR['Cost']} AND 
+											RecordType = {$arrCDR['RecordType']} AND 
 											RecordType NOT IN (10, 15, 33, 21) AND 
-											Credit = {$arrCDR['Credit'} AND 
-											Description <=> '{$arrCDR['Description'}' AND 
+											Credit = {$arrCDR['Credit']} AND 
+											Description <=> '{$arrCDR['Description']}' AND 
 											Status NOT IN (".CDR_DUPLICATE.", ".CDR_RECHARGE.")
 											ORDER BY Id DESC
 											LIMIT 1";
