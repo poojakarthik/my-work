bb886fbdf4ca87e3c80a40ae182cfd1ca0235330
Rich Davis <rdavis@yellowbilling.com.au>
2008-10-28 05:07:33 +0000

    added a script that will find the duplicate of a CDR

--- cli/normalisation/find_duplicate.php
+++ cli/normalisation/find_duplicate.php
@@ -14,20 +14,26 @@ $selCDR	= new StatementSelect("CDR", "*", "Id = {$intCDR}");
 $selCDR->Execute();
 $arrCDR	= $selCDR->Fetch();
 
-$strFindDuplicateSQL	= "SELECT Id, CASE WHEN CarrierRef <=> '{$arrCDR['CarrierRef']}' THEN ".CDR_DUPLICATE." ELSE ".CDR_RECHARGE." END AS Status 
+$strCarrierRef			= ($arrCDR['CarrierRef'] === NULL)		? $arrCDR['CarrierRef']		: "'{$arrCDR['CarrierRef']}'";
+$strSource				= ($arrCDR['Source'] === NULL)			? $arrCDR['Source']			: "'{$arrCDR['Source']}'";
+$strDestination			= ($arrCDR['Destination'] === NULL)		? $arrCDR['Destination']	: "'{$arrCDR['Destination']}'";
+$strStartDatetime		= ($arrCDR['StartDatetime'] === NULL)	? $arrCDR['StartDatetime']	: "'{$arrCDR['StartDatetime']}'";
+$strEndDatetime			= ($arrCDR['EndDatetime'] === NULL)		? $arrCDR['EndDatetime']	: "'{$arrCDR['EndDatetime']}'";
+$strDescription			= ($arrCDR['Description'] === NULL)		? $arrCDR['Description']	: "'{$arrCDR['Description']}'";
+$strFindDuplicateSQL	= "SELECT Id, CASE WHEN CarrierRef <=> {$strCarrierRef} THEN ".CDR_DUPLICATE." ELSE ".CDR_RECHARGE." END AS Status 
 											FROM CDR 
 											WHERE Id != {$arrCDR['Id']} AND 
 											FNN = '{$arrCDR['FNN']}' AND 
-											Source <=> '{$arrCDR['Source']}' AND 
-											Destination <=> '{$arrCDR['Destination']}' AND 
-											StartDatetime <=> '{$arrCDR['StartDatetime']}' AND 
-											EndDatetime <=> '{$arrCDR['EndDatetime']}' AND 
+											Source <=> {$strSource} AND 
+											Destination <=> {$strDestination} AND 
+											StartDatetime <=> {$strStartDatetime} AND 
+											EndDatetime <=> {$strEndDatetime} AND 
 											Units = {$arrCDR['Units']} AND 
 											Cost = {$arrCDR['Cost']} AND 
 											RecordType = {$arrCDR['RecordType']} AND 
 											RecordType NOT IN (10, 15, 33, 21) AND 
 											Credit = {$arrCDR['Credit']} AND 
-											Description <=> '{$arrCDR['Description']}' AND 
+											Description <=> {$strDescription} AND 
 											Status NOT IN (".CDR_DUPLICATE.", ".CDR_RECHARGE.")
 											ORDER BY Id DESC
 											LIMIT 1";
