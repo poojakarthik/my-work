13c5957622da2cc451459554daf3dd9a5a4558a6
Rich Davis <rdavis@yellowbilling.com.au>
2008-10-30 04:41:40 +0000

    fixed some contract management JS bugs

--- html/admin/javascript/contract_manage_expired.js
+++ html/admin/javascript/contract_manage_expired.js
@@ -332,7 +332,7 @@ var Contract_ManageExpired	= Class.create
 "		";
 		
 		// Create the Popup
-		Vixen.Popup.Create('ContractMonitor', strHtml, 'medium', 'centre', 'modal', strActioning + ' Contract Fees...', null, false);
+		Vixen.Popup.Create('ContractMonitor', strHtml, 'small', 'centre', 'modal', strActioning + ' Contract Fees...', null, false);
 	}
 });
 
