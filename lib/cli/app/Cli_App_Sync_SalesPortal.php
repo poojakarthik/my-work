bbb613b3797677a3d70d8022fb88aa616f584c5c
Rich Davis <rdavis@yellowbilling.com.au>
2008-11-04 06:48:39 +0000

    + Completed Flex >> Sales Portal Product Sync + Started work Sales Portal >> Flex Sync

--- lib/cli/app/Cli_App_Sync_SalesPortal.php
+++ lib/cli/app/Cli_App_Sync_SalesPortal.php
@@ -67,17 +67,126 @@ class Cli_App_Sync_SalesPortal extends Cli
 	// _pushVendors()	-- Synchronises the Flex.CustomerGroup table with SP Vendors 
 	protected function _pushVendors()
 	{
-		// TODO
+		$dsSalesPortal	= Data_Source::get('sales_portal');
+		$dacFlex		= DataAccess::getDataAccess();
+		
+		// Start a transaction for the Sales Portal
+		$dsSalesPortal->beginTransaction();
+		
+		try
+		{
+			// Get list of Customer Groups from Flex
+			$arrCustomerGroups	= Customer_Group::getAll();
+			foreach ($dsSalesPortal as $objCustomerGroup)
+			{
+				// Does this Customer Group exist in the Sales Portal?
+				$resVendors	= $dsSalesPortal->query("SELECT * FROM vendor WHERE reference = '{$objCustomerGroup->externalName}' LIMIT 1");
+				if (PEAR::isError($resVendors))
+				{
+					throw new Exception($resVendors->getError)." :: ".$resVendors->getUserInfo();
+				}
+				elseif (!($arrVendor = $resVendors->fetchRow(MDB2_FETCHMODE_ASSOC)))
+				{
+					// No -- add it
+					$resVendorInsert	= $dsSalesPortal->query("INSERT INTO vendor (name, description, reference) VALUES ('{$objCustomerGroup->externalName}', '{$objCustomerGroup->externalName}', '{$objCustomerGroup->id}');");
+					if (PEAR::isError($resVendorInsert))
+					{
+						throw new Exception($resVendorInsert->getError)." :: ".$resVendorInsert->getUserInfo();
+					}
+				}
+			}
+			
+			// All seems to have worked fine -- Commit the Transaction
+			$dsSalesPortal->commit();
+		}
+		catch (Exception $eException)
+		{
+			// Rollback the Transaction & passthru the Exception
+			$dsSalesPortal->rollback();
+			throw $eException;
+		}
 	}
 	
-	// _pushProducts()	-- Synchronises the Flex.CustomerGroup table with SP Vendors 
-	protected function _pushVendors()
+	// _pushProducts()	-- Synchronises the Flex Plans with SP Products 
+	protected function _pushProducts()
 	{
-		// TODO
+		$dsSalesPortal	= Data_Source::get('sales_portal');
+		$dacFlex		= DataAccess::getDataAccess();
+		
+		// Start a transaction for the Sales Portal
+		$dsSalesPortal->beginTransaction();
+		
+		try
+		{
+			$qryQuery	= new Query();
+			
+			//-------------------------- RATE PLANS --------------------------//
+			// Get list of Rate Plans from Flex
+			$resRatePlans	= $qryQuery->Execute("SELECT * FROM RatePlan WHERE Archived IN (0, 1);");
+			if ($resRatePlans === FALSE)
+			{
+				throw new Exception($qryQuery->Error());
+			}
+			else
+			{
+				while ($arrRatePlan = $resRatePlans->fetch_assoc())
+				{
+					// Determine the values
+					$intProductVendor		= $this->_convertFlexToSalesPortal('customer_group', $arrRatePlan['customer_group']);
+					$strProductName			= $arrRatePlan['Name'];
+					$strProductDescription	= $arrRatePlan['Description'];
+					$intProductType			= $this->_convertFlexToSalesPortal('service_type', $arrRatePlan['ServiceType']);
+					$intProductStatus		= $this->_convertFlexToSalesPortal('plan_archived', $arrRatePlan['Archived']);
+					
+					// Does it already exist in the SP?
+					$resProduct	= $dsSalesPortal->query("SELECT id FROM product WHERE reference = 'RatePlan.Id={$arrRatePlan['Id']}' LIMIT 1");
+					if (PEAR::isError($resProduct))
+					{
+						throw new Exception($resProduct->getError)." :: ".$resProduct->getUserInfo();
+					}
+					if ($resProduct->numRows())
+					{
+						// Already Exists -- do an UPDATE
+						$arrProduct			= $resProduct->fetchRow(MDB2_FETCHMODE_ASSOC);
+						$strUpdateSQL		= "UPDATE product SET vendor_id = {$intProductVendor}, name = '{$strProductName}', description = '{$strProductDescription}', product_type_id = {$intProductType}, product_status_id = {$intProductStatus} " .
+												"WHERE id = {$arrProduct['id']}";
+						$resProductUpdate	= $dsSalesPortal->query($strUpdateSQL);
+						if (PEAR::isError($resProductUpdate))
+						{
+							throw new Exception($resProductUpdate->getError)." :: ".$resProductUpdate->getUserInfo();
+						}
+					}
+					else
+					{
+						// Doesn't Exist -- do an INSERT
+						$strInsertSQL		= "INSERT INTO product (	vendor_id			, name					, description					, product_type_id	, product_status_id		, reference) VALUES " .
+																	"(	{$intProductVendor}	, '{$strProductName}'	, '{$strProductDescription}'	, {$intProductType}	, {$intProductStatus}	, 'RatePlan.Id={$arrRatePlan['Id']}')";
+						$resProductInsert	= $dsSalesPortal->query($strInsertSQL);
+						if (PEAR::isError($resProductInsert))
+						{
+							throw new Exception($resProductInsert->getError)." :: ".$resProductInsert->getUserInfo();
+						}
+					}
+				}
+			}
+			//----------------------------------------------------------------//
+			
+			// Other Product Definitions go here
+			// TODO
+			
+			// All seems to have worked fine -- Commit the Transaction
+			$dsSalesPortal->commit();
+		}
+		catch (Exception $eException)
+		{
+			// Rollback the Transaction & passthru the Exception
+			$dsSalesPortal->rollback();
+			throw $eException;
+		}
 	}
 	
-	// _pushDealers()	-- Synchronises the Flex.CustomerGroup table with SP Vendors 
-	protected function _pushVendors()
+	// _pushDealers()	-- Synchronises the Flex Dealers with SP Dealers 
+	protected function _pushDealers()
 	{
 		// TODO
 	}
@@ -96,6 +205,65 @@ class Cli_App_Sync_SalesPortal extends Cli
 		// TODO
 	}
 	//------------------------------------------------------------------------//
+	
+	// _convertFlexToSalesPortal()	-- Converts Values from Flex to Sales Portal Forms
+	protected function _convertFlexToSalesPortal($strType, $mixFlexValue)
+	{
+		static	$dsSalesPortal;
+		$dsSalesPortal	= (isset($dsSalesPortal)) ? $dsSalesPortal : Data_Source::get('sales_portal');
+		
+		switch (str_replace('_', '', strtolower($strType)))
+		{
+			case 'vendor':
+			case 'vendorid':
+			case 'customergroup':
+			case 'customergroupid':
+				static	$arrCustomerGroupVendor;
+				if (!isset($arrCustomerGroupVendor))
+				{
+					$resVendor	= $dsSalesPortal->query("SELECT id, reference FROM vendor");
+					if (PEAR::isError($resVendor))
+					{
+						throw new Exception($resVendor->getError)." :: ".$resVendor->getUserInfo();
+					}
+					$arrCustomerGroupVendor	= array();
+					while ($arrVendor = $resVendor->fetchRow(MDB2_FETCHMODE_ASSOC))
+					{
+						$arrReference	= explode('=', $arrVendor['reference']);
+						$arrCustomerGroupVendor[(int)$arrReference[1]]	= $arrVendor['id'];
+					}
+				}
+				return $arrCustomerGroupVendor[(int)$mixFlexValue];
+				break;
+				
+			case 'servicetype':
+			case 'servicetypeid':
+				// HACK: These should work, at least for now
+				$arrServiceTypeProductType	= array(
+														100 => 3,
+														101	=> 2,
+														102 => 1,
+														103 => 4
+													);
+				
+				return $arrServiceTypeProductType[(int)$mixFlexValue];
+				break;
+				
+			case 'planarchived':
+				// HACK: These should work, at least for now
+				$arrPlanArchivedProductStatus	= array(
+															0	=> 1,
+															1	=> 2
+														);
+				
+				return $arrPlanArchivedProductStatus[(int)$mixFlexValue];
+				break;
+			
+			default:
+				throw new Exception("Unknown Flex-to-SP Conversion Type: '{$strType}'!");
+				break;
+		}
+	}
 
 	function getCommandLineArguments()
 	{
