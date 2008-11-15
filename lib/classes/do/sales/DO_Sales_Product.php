05391f8a1c66e58bbd66f5aac93f01dab3a5778e
Hadrian Oliver <holiver@yellowbilling.com.au>
2008-11-15 03:36:22 +0000

    Added function removed in error

--- lib/classes/do/sales/DO_Sales_Product.php
+++ lib/classes/do/sales/DO_Sales_Product.php
@@ -3,6 +3,35 @@
 class DO_Sales_Product extends DO_Sales_Base_Product
 {
 
+	public static function listProductIdAndNameForDealerIdProductTypeIdAndVendorId($dealerId, $productTypeId, $vendorId)
+	{
+
+		$dataSource = self::getDataSource();
+
+
+
+		$strSQL = "
+		SELECT product.id as \"id\",product.name as \"name\"
+		FROM product, dealer_product
+		WHERE product.id = dealer_product.product_id 
+		AND dealer_product.dealer_id = '$dealerId' 
+		AND product.vendor_id='$vendorId'
+		AND dealer_product.dealer_id = '$dealerId'
+		AND product.product_type_id = '$productTypeId'
+		AND product.product_status_id = 1; "; // Active // WIP - this should be coded properly!
+
+		$result = $dataSource->query($strSQL);
+
+		if(PEAR::isError($result))
+		{
+			throw new Exception("Failed to build a list of products: " . $result->getMessage());
+		}
+
+		$arrProductList = $result->fetchAll(MDB2_FETCHMODE_ASSOC);
+
+		return $arrProductList;
+	}
+
 	public static function listProductIdAndNameForProductTypeIdAndVendorId($productTypeId, $vendorId)
 	{
 
