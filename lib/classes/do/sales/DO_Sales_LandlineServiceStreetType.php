200a352dbda898bb8cdfd8056d4b91c610816199
Joel Dawkins <jdawkins@yellowbilling.com.au>
2008-11-17 06:07:30 +0000

    made the listCommonTypes method which retrieves the most common Street Types. (Note that this is done in a static manner)

--- lib/classes/do/sales/DO_Sales_LandlineServiceStreetType.php
+++ lib/classes/do/sales/DO_Sales_LandlineServiceStreetType.php
@@ -8,6 +8,17 @@ class DO_Sales_LandlineServiceStreetType extends DO_Sales_Base_LandlineServiceSt
 		$ds = self::getPropertyDataSourceMappings();
 		return self::getFor(null, true, $ds['description']);
 	}
+
+	static function listCommonTypes()
+	{
+		// I'm using the codes instead of descriptions, so they don't get included multiple times because the description "Lane" relates to 3 unique codes
+		$ds			= self::getPropertyDataSourceMappings();
+		$strCode	= $ds['code'];
+		$strWhere	= "$strCode IN ('ST', 'RD', 'AVE', 'LANE', 'CT', 'NR')";
+		$strOrderBy	= "($strCode = 'ST') DESC, ($strCode = 'RD') DESC, ($strCode = 'AVE') DESC, ($strCode = 'LANE') DESC, ($strCode = 'CT') DESC";
+		
+		return self::getFor($strWhere, true, $strOrderBy);
+	}
 }
 
 ?>
