d2b7cade1b6e876a7e92349416f0d2bd19334844
Rich Davis <rdavis@yellowbilling.com.au>
2008-11-27 01:28:47 +0000

    debugging

--- html/admin/app_template/plan.php
+++ html/admin/app_template/plan.php
@@ -570,9 +570,9 @@ class AppTemplatePlan extends ApplicationTemplate
 		
 		// Included Data
 		DBO()->RatePlan->included_data	= max(0, (int)DBO()->RatePlan->included_data->Value);
+		DBO()->RatePlan->included_data	= (DBO()->RatePlan->included_data->Value > 0) ? DBO()->RatePlan->included_data / 1024 : 0;
 		$mixIncludedData	= DBO()->RatePlan->included_data->Value;
 		return "Included Data: '{$mixIncludedData}'";
-		DBO()->RatePlan->included_data	= (DBO()->RatePlan->included_data->Value > 0) ? DBO()->RatePlan->included_data / 1024 : 0;
 		
 		// V2: ServiceType
 		if (!DBO()->RatePlan->ServiceType->Value)
