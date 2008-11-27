5c581dc0096e5071e631263a13164b1bb4a3e14d
Rich Davis <rdavis@yellowbilling.com.au>
2008-11-27 01:15:16 +0000

    added frontend support for Included Data

--- html/admin/app_template/plan.php
+++ html/admin/app_template/plan.php
@@ -569,7 +569,8 @@ class AppTemplatePlan extends ApplicationTemplate
 		}
 		
 		// Included Data
-		DBO()->RatePlan->included_data	= max(0, (int)DBO()->RatePlan->included_data);
+		DBO()->RatePlan->included_data	= max(0, (int)DBO()->RatePlan->included_data->Value);
+		DBO()->RatePlan->included_data	= (DBO()->RatePlan->included_data->Value > 0) ? DBO()->RatePlan->included_data / 1024 : 0;
 		
 		// V2: ServiceType
 		if (!DBO()->RatePlan->ServiceType->Value)
