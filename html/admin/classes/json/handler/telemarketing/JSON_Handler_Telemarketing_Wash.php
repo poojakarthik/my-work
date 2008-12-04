f0c11618f03e58050ed1bee43769dfbebc0e2adb
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-04 06:31:51 +0000

    work on File Washing UI

--- html/admin/classes/json/handler/telemarketing/JSON_Handler_Telemarketing_Wash.php
+++ html/admin/classes/json/handler/telemarketing/JSON_Handler_Telemarketing_Wash.php
@@ -2,62 +2,21 @@
 
 class JSON_Handler_Telemarketing_Wash extends JSON_Handler
 {
-
-	// Waives the Contract Fees for a given ServiceRatePlan
-	public function buildProposedUploadPopup()
+	
+	public function getCallCentrePermissions()
 	{
 		// Check user permissions
 		AuthenticatedUser()->PermissionOrDie(PERMISSION_SUPER_ADMIN);
 		
 		try
 		{
-			$strDealerListHTML	= self::_renderCallCentreCombo();
-			$strVendorListHTML	= self::_renderVendorCombo();
-			
-			// Build the innerHTML
-			$strPopupContent	= "
-<form method='post' action='' enctype='multipart/form-data'>
-	<div class='GroupedContent'>
-		<table class='form-data' style='width:100%'>
-			<tbody>
-				<tr>
-					<td>Dealer:</td>
-					<td>
-						<select id='Telemarketing_Wash_Dealer' name='Telemarketing_Wash_Dealer'>
-							<option value='' selected='selected'>[None]</option>
-							{$strDealerListHTML}
-						</select>
-					</td>
-				</tr>
-				<tr>
-					<td>Vendor:</td>
-					<td>
-						<select id='Telemarketing_Wash_Vendor' name='Telemarketing_Wash_Vendor'>
-							<option value='' selected='selected'>[None]</option>
-							{$strVendorListHTML}
-						</select>
-					</td>
-				</tr>
-				<tr>
-					<td>File to wash:</td>
-					<td>
-						<input type='file' id='Telemarketing_Wash_File' name='Telemarketing_Wash_File' />
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-	<div style='width:100%; margin: 0 auto; text-align:center;'>
-		<input type='button' id='TelemarketingWashProposed_Submit' name='TelemarketingWashProposed_Submit' value='Submit' onclick='Flex.Telemarketing.ProposedList.submit()' style='margin-left:3px'></input> 
-		<input type='button' value='Cancel' onclick='Vixen.Popup.Close(this)' style='margin-left:3px'></input>
-	</div>
-</form>
-";
-			
+			// Get list of Call Centres
+			$arrCallCentres	= Dealer::getCallCentres();
+				
 			// If no exceptions were thrown, then everything worked
 			return array(
-							"Success"		=> TRUE,
-							"PopupContent"	=> $strPopupContent
+							"Success"					=> TRUE,
+							"arrCallCentrePermissions"	=> $arrCallCentres
 						);
 		}
 		catch (Exception $e)
@@ -70,38 +29,6 @@ class JSON_Handler_Telemarketing_Wash extends JSON_Handler
 							"ErrorMessage"	=> 'ERROR: '.$e->getMessage()
 						);
 		}
-	}
-
-	private static function _renderVendorCombo()
-	{
-		$arrVendors	= Customer_Group::getAll();
-		
-		// Render Vendor List
-		$strVendorListHTML	= '';
-		foreach ($arrVendors as $objCustomerGroup)
-		{
-			$strVendorListHTML	.= "<option value='{$objCustomerGroup->id}'>{$objCustomerGroup->externalName}</option>\n";
-		}
-		return trim($strVendorListHTML);
-	}
-
-	private static function _renderCallCentreCombo()
-	{
-		$arrCallCentres	= Dealer::getCallCentres();
-		
-		// Render Dealer List
-		$strDealerListHTML	= '';
-		foreach ($arrCallCentres as $objDealer)
-		{
-			$strDealerListHTML	.= "<option value='{$objDealer->id}'>{$objDealer->firstName} {$objDealer->lastName}</option>\n";
-		}
-		return trim($strDealerListHTML);
-	}
-	
-	public function getCallCentrePermissions()
-	{
-		// Get list of Call Centres
-		$arrCallCentres	= Dealer::getCallCentres();
 	}
 }
 
