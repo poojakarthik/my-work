7a8abdafde0a779515ebc8875d5093dd0b75bba4
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-04 03:18:32 +0000

    debugging

--- html/admin/javascript/telemarketing.js
+++ html/admin/javascript/telemarketing.js
@@ -6,51 +6,7 @@ var Telemarketing	= Class.create
 	// Prototype constructor
 	initialize	: function()
 	{
-		this._objPopups	= {
-							uploadProposed	: {
-												strId		: 'Telemarketing_Wash_Proposed',
-												strSize		: 'medium',
-												strAlign	: 'centre',
-												strNature	: 'modal',
-												strTitle	: 'Upload Proposed Dialling List'
-											}
-						};
-	},
-	
-	renderPopup	: function(strPopupName, objResponse)
-	{
-		Vixen.Popup.ClosePageLoadingSplash();
 		
-		if (!objResponse.PopupContent)
-		{
-			alert(objResponse.Debug);
-		}
-		
-		if (objResponse.Success)
-		{
-			// Render the popup
-			Vixen.Popup.Create(
-								this._objPopups[strPopupName].strId, 
-								objResponse.PopupContent, 
-								this._objPopups[strPopupName].strSize, 
-								this._objPopups[strPopupName].strAlign, 
-								this._objPopups[strPopupName].strNature, 
-								this._objPopups[strPopupName].strTitle
-							);
-		}
-		else
-		{
-			$Alert("Failed to open the '" + this._objPopups[strPopupName].strTitle + "' popup" + ((objResponse.ErrorMessage != undefined)? "<br />" + objResponse.ErrorMessage : ""));
-		}
-	},
-	
-	uploadProposedList	: function()
-	{
-		remoteClass		= 'Telemarketing_Wash';
-		remoteMethod	= 'buildProposedUploadPopup';
-		jsonFunc		= jQuery.json.jsonFunction(this.renderPopup.bind(this, 'uploadProposed'), null, remoteClass, remoteMethod);
-		Vixen.Popup.ShowPageLoadingSplash("Please Wait", null, null, null, 100);
-		jsonFunc();
 	}
 });
 
