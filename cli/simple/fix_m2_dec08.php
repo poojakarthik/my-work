7e00aa49e7702d49ff95dd5da724e963f1c32324
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-08 05:22:21 +0000

    added a script to correct the M2 CDR Error from dec 08

---
+++ cli/simple/fix_m2_dec08.php
@@ -0,0 +1,88 @@
+<?php
+// Load the Framework
+require_once("../../lib/classes/Flex.php");
+
+// Open the Import File
+$strImportPath	= '/home/rdavis/fix_m2_dec08.csv';
+$resImportFile	= fopen($strImportPath, 'r');
+if (!$resImportFile)
+{
+	throw new Exception("Unable to open File at '{$strImportPath}'");
+}
+
+// Start a new Transaction
+Data_Access::getDataAccess()->TransactionStart();
+
+$strProcessingDatetime	= date("Y-m-d H:i:s");
+try
+{
+	// Parse each line
+	while ($arrLine = fgetcsv($resImportFile))
+	{
+		$intAccount			= (int)$arrLine[0];
+		$fltOldTotal		= (float)$arrLine[1];
+		$fltNewTotal		= (float)$arrLine[2];
+		$fltAmountIncGST	= (float)$arrLine[3];
+		$fltAmount			= ($fltAmountIncGST / 11) * 10;
+		
+		$fltAmountIncGST	= round(abs($fltAmountIncGST), 2);
+		
+		// Get additional details
+		$objAccount	= new Account(array('Id'=>$intAccount), false, true);
+		
+		
+		// Add Charge
+		$objCharge	= new Charge();
+		$objCharge->AccountGroup		= $objAccount->AccountGroup;
+		$objCharge->Account				= $objAccount->Id;
+		$objCharge->CreatedBy			= 0;										// System User
+		$objCharge->CreatedOn			= $strProcessingDatetime;
+		$objCharge->ApprovedBy			= 0;										// System User
+		$objCharge->ChargedOn			= $strProcessingDatetime;
+		$objCharge->Status				= CHARGE_APPROVED;
+		$objCharge->global_tax_exempt	= 0;
+		$objCharge->Amount				= round(abs($fltAmount), 2);
+		if ($fltAmount > 0)
+		{
+			// Debit
+			$objCharge->Nature			= 'DR';
+			$objCharge->ChargeType		= 'UCHRG';
+			$objCharge->Description		= 'Undercharged from December 2008';
+			$objCharge->Notes			= "Account {$intAccount} has been debited \${$fltAmountIncGST} due to an M2 CDR issue affecting the December 2008 Invoice";
+		}
+		else
+		{
+			// Credit
+			$objCharge->Nature			= 'CR';
+			$objCharge->ChargeType		= 'OCHRG';
+			$objCharge->Description		= 'Overcharged from December 2008';
+			$objCharge->Notes			= "Account {$intAccount} has been credited \${$fltAmountIncGST} due to an M2 CDR issue affecting the December 2008 Invoice";
+		}
+		$objCharge->save();
+		
+		
+		// Add Note
+		$objNote	= new Note();
+		$objNote->Note			= $objCharge->Notes;
+		$objNote->AccountGroup	= $objAccount->AccountGroup;
+		$objNote->Account		= $objAccount->Id;
+		$objNote->Employee		= 0;											// System User
+		$objNote->Datetime		= $strProcessingDatetime;
+		$objNote->NoteType		= 7;
+		$objNote->save();
+	}
+	
+	// TEST MODE
+	throw new Exception("TEST MODE");
+}
+catch (Exception $eException)
+{
+	// Rollback the Transaction
+	Data_Access::getDataAccess()->TransactionRollback();
+	throw $eException;
+}
+
+// Commit the Transaction
+Data_Access::getDataAccess()->TransactionRollback();
+//Data_Access::getDataAccess()->TransactionCommit();
+?>
