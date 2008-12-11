819673a00ab90bd199e45a22060baf0bd545fdbb
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-11 03:18:33 +0000

    work on File Washing UI

--- html/admin/javascript/telemarketing.js
+++ html/admin/javascript/telemarketing.js
@@ -25,7 +25,7 @@ var Telemarketing	= Class.create
 		}
 		
 		// Add a target to the form
-		elmForm.setAttribute('target', strIframeId);
+		//elmForm.setAttribute('target', strIframeId);
 		
 		alert("Submitting '" + elmForm.id + "'");
 		
