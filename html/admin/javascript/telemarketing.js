efc22e009fb79482377f460f27652419485f962f
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-11 04:53:39 +0000

    work on File Washing UI

--- html/admin/javascript/telemarketing.js
+++ html/admin/javascript/telemarketing.js
@@ -14,7 +14,7 @@ var Telemarketing	= Class.create
 		// Create a hidden IFrame
 		var	strIframeId		= elmForm.id + "_submit_" + Math.floor(Math.random() * 99999);
 		var elmDiv			= document.createElement('div');
-		elmDiv.innerHTML	= "<iframe style='display:none' src='about:blank' id='" + strIframeId + "' name='" + strIframeId + "' onload='Flex.Telemarketing.iframeFormLoaded(this)'></iframe>";
+		elmDiv.innerHTML	= "<iframe style='display:none' id='" + strIframeId + "' name='" + strIframeId + "' onload='Flex.Telemarketing.iframeFormLoaded(this)' />";
 		elmDiv.id			= strIframeId + '_div';
 		document.body.appendChild(elmDiv);
 		
