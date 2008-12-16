d2e5aac81ee37b7bf6eacbf005ef58b8cedf9d93
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-16 01:03:46 +0000

    updated westpac collection config to include Aus Direct Entry Export Reports

---
+++ cli/collection/modules/config/telcoblue/westpac.cfg.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once("../../../../../flex.require.php");
+
+// Payments
+$arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_BPAY_WESTPAC]						['Regex']			= "/^[a-z]{3}\_\d{1,2}-\d{1,2}-\d{4}\_\d{1,10}\.CSV$/i";
+$arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_BPAY_WESTPAC]						['Uniqueness']		= "FileName = <FileName> AND SHA1 = <SHA1>";
+$arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_BPAY_WESTPAC]						['DownloadUnique']	= TRUE;
+$arrStructure[RESOURCE_TYPE_FILE_IMPORT_PAYMENT_BPAY_WESTPAC]						['Paths']			[]	= '/home/telcoblue/Incoming/bpay/';
+
+$arrStructure[RESOURCE_TYPE_FILE_EXPORT_DIRECT_DEBIT_AUSTRALIAN_DIRECT_ENTRY_FILE]	['Regex']			= "/^AUDES\d+\_\d+\_[A-Za-z]\_\d+\.csv$/i";
+$arrStructure[RESOURCE_TYPE_FILE_EXPORT_DIRECT_DEBIT_AUSTRALIAN_DIRECT_ENTRY_FILE]	['Uniqueness']		= "FileName = <FileName> AND SHA1 = <SHA1>";
+$arrStructure[RESOURCE_TYPE_FILE_EXPORT_DIRECT_DEBIT_AUSTRALIAN_DIRECT_ENTRY_FILE]	['DownloadUnique']	= TRUE;
+$arrStructure[RESOURCE_TYPE_FILE_EXPORT_DIRECT_DEBIT_AUSTRALIAN_DIRECT_ENTRY_FILE]	['Paths']			[]	= '/home/telcoblue/Incoming/westpac/ausde_export_reports/';
+
+// Config
+$arrModuleConfig['FileDefine']		['Value']		= $arrStructure;
+
+// Output
+$strOutputFile	= basename(__FILE__, '.cfg.php').'.serialised';
+@unlink($strOutputFile);
+
+if (file_put_contents($strOutputFile, serialize($arrStructure)))
+{
+	echo "\nSerialised Data successfully dumped to '$strOutputFile'.\n\n";
+}
+else
+{
+	echo "\nUnable to dump serialised data to '$strOutputFile'.\n\n";
+}
+
+?>
