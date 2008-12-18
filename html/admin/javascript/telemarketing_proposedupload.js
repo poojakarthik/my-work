9f92e826aec531fb71e49fc9d12ba32cf150c8a5
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-18 06:40:49 +0000

    Work on the File Washing UI

--- html/admin/javascript/telemarketing_proposedupload.js
+++ html/admin/javascript/telemarketing_proposedupload.js
@@ -99,7 +99,7 @@ var Telemarketing_ProposedUpload	= Class.create
 		Vixen.Popup.Close(this.objPopupUpload.strId);
 		
 		// Display confirmation popup
-		$Alert("<pre>"+objResponse.Message+"</pre>");
+		$Alert("<pre>"+objResponse.Message+"</pre>", null, null, 'modal');
 		return;
 	},
 	
