5bb5696ea2c12f3ca8144a606e8cca79d327d6a4
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-19 02:44:30 +0000

    Implemented the Telemarketing_FNN_Proposed class

---
+++ lib/classes/telemarketing/fnn/Telemarketing_FNN_Proposed.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Telemarketing_FNN_Proposed
+ *
+ * Models a record of the telemarketing_fnn_proposed table
+ *
+ * @class	Telemarketing_FNN_Proposed
+ */
+class Telemarketing_FNN_Proposed extends ORM
+{
+	protected	$_strTableName	= "telemarketing_fnn_proposed";
+	
+	//------------------------------------------------------------------------//
+	// __construct
+	//------------------------------------------------------------------------//
+	/**
+	 * __construct()
+	 *
+	 * constructor
+	 * 
+	 * constructor
+	 *
+	 * @param	array	$arrProperties 		[optional]	Associative array defining the class with keys for each field of the table
+	 * @param	boolean	$bolLoadById		[optional]	Automatically load the object with the passed Id
+	 * 
+	 * @return	void
+	 * 
+	 * @constructor
+	 */
+	public function __construct($arrProperties=Array(), $bolLoadById=FALSE)
+	{
+		// Parent constructor
+		parent::__construct($arrProperties, $bolLoadById);
+	}
+	
+	//------------------------------------------------------------------------//
+	// _preparedStatement
+	//------------------------------------------------------------------------//
+	/**
+	 * _preparedStatement()
+	 *
+	 * Access a Static Cache of Prepared Statements used by this Class
+	 *
+	 * Access a Static Cache of Prepared Statements used by this Class
+	 * 
+	 * @param	string		$strStatement						Name of the statement
+	 * 
+	 * @return	Statement										The requested Statement
+	 *
+	 * @method
+	 */
+	protected static function _preparedStatement($strStatement)
+	{
+		static	$arrPreparedStatements	= Array();
+		if (isset($arrPreparedStatements[$strStatement]))
+		{
+			return $arrPreparedStatements[$strStatement];
+		}
+		else
+		{
+			switch ($strStatement)
+			{
+				// SELECTS
+				case 'selById':
+					$arrPreparedStatements[$strStatement]	= new StatementSelect(	"telemarketing_fnn_proposed", "*", "id = <Id>", NULL, 1);
+					break;
+				
+				// INSERTS
+				case 'insSelf':
+					$arrPreparedStatements[$strStatement]	= new StatementInsert(	"telemarketing_fnn_proposed");
+					break;
+				
+				// UPDATE BY IDS
+				case 'ubiSelf':
+					$arrPreparedStatements[$strStatement]	= new StatementUpdateById(	"telemarketing_fnn_proposed");
+					break;
+				
+				// UPDATES
+				
+				default:
+					throw new Exception(__CLASS__."::{$strStatement} does not exist!");
+			}
+			return $arrPreparedStatements[$strStatement];
+		}
+	}
+}
+?>
