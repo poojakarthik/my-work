7af5320c7372e562875e32092e12b4b62759e89f
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-19 02:25:42 +0000

    Work on the File Washing UI

--- html/admin/classes/application/handler/Application_Handler_Telemarketing.php
+++ html/admin/classes/application/handler/Application_Handler_Telemarketing.php
@@ -88,9 +88,9 @@ class Application_Handler_Telemarketing extends Application_Handler
 				// Create a log dump
 				$strLogFileName	= FILES_BASE_PATH.'logs/telemarketing/proposed/'.date('YmdHis').'_'.AuthenticatedUser()->GetUserId().'.log';
 				@mkdir(dirname($strLogFileName));
-				@file_put_contents($strLogFileName, implode("\n", $arrErrors));
+				file_put_contents($strLogFileName, implode("\n", $arrErrors));
 				
-				throw new Exception("The uploaded file is invalid.  The were ".count($arrErrors)." error encountered while importing.\nPlease ensure that you have selected the correct file, and try again.\nIf this message appears more than once, please contact YBS.");
+				throw new Exception("The uploaded file is invalid.  The were ".count($arrErrors)." errors encountered while importing.\nPlease ensure that you have selected the correct file, and try again.\nIf this message appears more than once, please contact YBS.");
 			}
 			
 			// Update the FileImport Status to Imported
