08c022f7aac2a4fe7e813d602a2b169ac79ce924
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-22 02:04:06 +0000

    Work on the File Washing UI

--- html/admin/javascript/telemarketing_dncrdownload.js
+++ html/admin/javascript/telemarketing_dncrdownload.js
@@ -126,7 +126,7 @@ var Telemarketing_DNCRDownload	= Class.create
 		"	</div>\n" + 
 		"	<div style='width:100%; margin: 0 auto; text-align:center;'>\n" +
 		"		<input type='submit' id='Telemarketing_DNCRDownload_Download' value='Download' style='margin-left:3px' /> \n" +
-		"		<input type='button' id='Telemarketing_DNCRDownload_Cancel' value='Cancel' onclick='Vixen.Popup.Close(this)' style='margin-left:3px' /> \n" + 
+		"		<input type='button' id='Telemarketing_DNCRDownload_Cancel' value='Close' onclick='Vixen.Popup.Close(this)' style='margin-left:3px' /> \n" + 
 		"	</div>\n\n" +
 		"</form>\n\n"; 
 		
