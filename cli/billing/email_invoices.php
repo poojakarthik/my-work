24628b5164e40d3a9cb27b548ccdfa2312a7ba0a
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-30 04:45:13 +0000

    made some changes to the email invoice text

--- cli/billing/email_invoices.php
+++ cli/billing/email_invoices.php
@@ -162,15 +162,15 @@ function EmailInvoices($arrInvoiceRun, $bolIncludePDF=FALSE)
  			{
 				// PDF is included
 				$strContent	=	"Please find attached your Invoice for {$strBillingPeriod};\n\n" .
-								"For your convenience you can view your invoice at any time by going to {$arrCustomerGroups[$arrDetail['CustomerGroup']]['customer_exit_url']} .\n\n" .
-								"Additional account management benefits:\n\n" .
+								"For your convenience you can view your invoice at any time by going to {$arrCustomerGroups[$arrDetail['CustomerGroup']]['customer_exit_url']} and accessing your account.\n\n" .
+								"Additional benefits:\n\n" .
 								"- View unbilled charges.\n" .
 								"- Pay your bill online.\n" .
 								"- Make changes to your account or services.\n" .
 								"- Sign up to new services.\n" .
 								"- Report faults directly at your own convenience.\n\n" .
-								"If you are yet to setup your customer account go to: {$arrCustomerGroups[$arrDetail['CustomerGroup']]['customer_exit_url']} and click on “First Time User?” and follow the prompts. Should you have any difficulties accessing the customer portal please email {$arrCustomerGroups[$arrDetail['CustomerGroup']]['OutboundEmail']}.\n\n" .
-								"N.B. We don't want to clog your email box up each month, so as of January 2009 we will simply send you a link to download your latest invoice.\n\n" .
+								"If you are yet to setup your customer account go to: {$arrCustomerGroups[$arrDetail['CustomerGroup']]['customer_exit_url']} and click on “First Time User?” and follow the prompts. Should you have any difficulties accessing the customer portal please email {$arrCustomerGroups[$arrDetail['CustomerGroup']]['OutboundEmail']} or call our Customer Care Team for assistance.\n\n" .
+								"N.B. We don't want to clog your email box up each month, so as of February 2009 we will simply send you a link to download your latest invoice.\n\n" .
 								"Regards,\n\n" .
 								"The team at {$arrCustomerGroups[$arrDetail['CustomerGroup']]['ExternalName']}.";
  			}
