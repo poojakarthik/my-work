c864b5e6a7daa6c13dfed4c628de455ddca19dfd
Rich Davis <rdavis@yellowbilling.com.au>
2008-12-31 03:09:22 +0000

    Added a script which attempts to match up bad DestinationTranslation records with a valid Flex Destination

---
+++ cli/simple/destination_code_match.php
@@ -0,0 +1,61 @@
+<?php
+
+// Framework
+require_once("../../lib/classes/Flex.php");
+Flex::load();
+
+$arrWordFilter	= array('to', '-');
+
+// Cache Flex Destination Codes
+$resDestinations	= $qryQuery->Execute("SELECT * FROM Destination WHERE Context = 1");
+if ($resDestinations === false)
+{
+	throw new Exception($qryQuery->Error());
+}
+$arrDestinations	= array();
+while ($arrDestination = $resDestinations->fetch_assoc())
+{
+	$arrDestinations[$arrDestination['Code']]	= $arrDestination;
+}
+
+// Get all DestinationTranslation entries that have a Code of 0
+$qryQuery	= new Query();
+$resDestinationTranslation	= $qryQuery->Execute("SELECT * FROM DestinationTranslation WHERE Code = 0");
+if ($resDestinationTranslation === false)
+{
+	throw new Exception($qryQuery->Error());
+}
+
+while ($arrDestinationTranslation = $resDestinationTranslation->fetch_assoc())
+{
+	CliEcho("\t[+] ".GetConstantDescription($arrDestinationTranslation['Carrier'], 'Carrier').": ({$arrDestinationTranslation['CarrierCode']}) {$arrDestinationTranslation['Description']}...", false);
+	
+	// Filter out any useless words that will just give us junk matches
+	$arrCarrierDestination	= explode(' ', $arrWordFilter);
+	foreach ($arrCarrierDestination as $mixIndex=>$strWord)
+	{
+		if (in_array($strWord, $arrWordFilter))
+		{
+			unset($arrCarrierDestination[$mixIndex]);
+		}
+	}
+	
+	// See if there are any Flex Destinations that have a similar description
+	$intTotalWords	= count($arrCarrierDestination);
+	foreach ($arrDestinations as $intCode=>$arrDestination)
+	{
+		$intWordCount	= 0;
+		foreach ($arrCarrierDestination as $strWord)
+		{
+			$intWordCount	+= (stripos($arrDestination['Description'], $strWord) !== false) ? 1 : 0;
+		}
+		
+		if ($intWordCount)
+		{
+			CliEcho(" {$arrDestination['Description']}({$intWordCount}/{$intTotalWords})", false);
+		}
+	}
+	CliEcho();
+}
+
+?>
