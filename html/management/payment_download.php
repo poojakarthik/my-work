384bf5f036901a836754aea2a7fb6c6257317fd0
Rich Davis <rdavis@yellowbilling.com.au>
2009-01-28 01:09:37 +0000

    Payment Download should now be constrained by Customer Group, and no longer constrained by Employee

--- html/management/payment_download.php
+++ html/management/payment_download.php
@@ -69,7 +69,7 @@
 					if ($intCustomerGroup != $_POST['CustomerGroup'])
 					{
 						//throw new Exception("Trying to POP index {$mixIndex}");
-						$oblsamPayments->popItem($mixIndex);
+						throw new Exception(print_r($oblsamPayments->popItem($mixIndex), true));
 					}
 					elseif (!isset($arrAccounts[$intAccount]))
 					{
