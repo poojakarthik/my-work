8fba24d6b7193080b4b2cca2af2e48cecfda72b6
Rich Davis <rdavis@yellowbilling.com.au>
2009-01-28 03:04:24 +0000

    Payment Download should now be constrained by Customer Group, and no longer constrained by Employee

--- html/management/payment_download.php
+++ html/management/payment_download.php
@@ -60,11 +60,11 @@
 			//throw new Exception("'{$strCustomerGroups}'");
 			$intPaymentType		= (int)$_POST['PaymentType'];
 			$resPayments		= $qryQuery->Execute(	"SELECT Payment.AccountGroup, Payment.Account, Account.BusinessName, Account.TradingName, CustomerGroup.ExternalName AS CustomerGroup, Payment.TXNReference, Payment.PaidOn, Payment.Amount " .
-														"FROM Payment JOIN Account ON Payment.Account = Account.Id JOIN CustomerGroup ON CustomerGroup.Id = Account.CustomerGroup" .
+														"FROM Payment JOIN Account ON Payment.Account = Account.Id JOIN CustomerGroup ON CustomerGroup.Id = Account.CustomerGroup " .
 														"WHERE PaymentType = {$intPaymentType} AND PaidOn = '{$strPaidOn}' AND Account.CustomerGroup IN ({$strCustomerGroups})");
 			if ($resPayments === false)
 			{
-				throw new Exception($qryQuery->Error());
+				//throw new Exception($qryQuery->Error());
 			}
 			$arrPayments		= array();
 			while ($arrPayment = $resPayments->fetch_assoc())
