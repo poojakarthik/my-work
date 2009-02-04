70580c080709ef44461e9c2677d1579cdae931b5
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-04 23:33:17 +0000

    tried to add a menu-right icon

--- html/ui/classes/Page.php
+++ html/ui/classes/Page.php
@@ -821,10 +821,9 @@ class Page
 			{
 				// $mixValue is a submenu
 				$strClass	= ($intLevel === 0) ? "dropdown" : "dropright";
-				echo "$strIndent\t<li class='dropdown'><a>" . htmlspecialchars($strLabel, ENT_QUOTES) . "</a>\n";
+				echo "$strIndent\t<li class='{$strClass}'><a>" . htmlspecialchars($strLabel, ENT_QUOTES) . "</a>\n";
 				self::renderMenuLevel($mixValue, $intLevel + 1);
 				echo "$strIndent\t</li>\n";
-				echo "<script>alert('Dropright should work!  Label: ".htmlspecialchars($strLabel, ENT_QUOTES)."; Level: {$intLevel}');</script>";
 			}
 		}
 		echo "$strIndent</ul>\n";
