7f89517c4eb0d31e4b5b37de5f5f4cbe205665a3
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-10 00:27:43 +0000

    added a basic script to update binary fields with files

---
+++ cli/simple/update_binary_field.php
@@ -0,0 +1,27 @@
+<?php
+
+// Load Flex
+require_once("../../lib/classes/Flex.php");
+Flex::load();
+
+$strTable		= "file_type";
+$strField		= "icon_16x16";
+$strConstraint	= "name = 'PDF'";
+
+$strFilePath	= "../../html/admin/img/template/pdf_small.png";
+
+$strFileContents	= @file_get_contents($strFilePath);
+if ($strFileContents === false)
+{
+	throw new Exception("There was an error reading from '{$strFilePath}'.");
+}
+
+$arrColumns	= array($strField=>$strFileContents);
+$updUpdateBinary	= new StatetmentUpdate($strTable, $strConstraint, $arrColumns, 1);
+if ($updUpdateBinary->Execute() === false)
+{
+	throw new Exception($updUpdateBinary->Error());
+}
+
+CliEcho("Successfully updated {$strTable}.{$strField} where {$strConstraint} with {$strFilePath}");
+?>
