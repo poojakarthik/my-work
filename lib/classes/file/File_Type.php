427d7ff08ab013bc1d85ba2331a8b7fce71d6a11
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-10 01:16:46 +0000

    work on document management

---
+++ lib/classes/file/File_Type.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * File_Type
+ *
+ * Models a record of the file_type table
+ *
+ * @class	File_Type
+ */
+class File_Type extends ORM
+{
+	protected			$_strTableName				= "file_type";
+	protected static	$_strStaticTableName		= "file_type";
+	
+	/**
+	 * __construct()
+	 *
+	 * constructor
+	 *
+	 * @param	array	$arrProperties 		[optional]	Associative array defining the class with keys for each field of the table
+	 * @param	boolean	$bolLoadById		[optional]	Automatically load the object with the passed Id
+	 * 
+	 * @return	void
+	 * 
+	 * @constructor
+	 */
+	public function __construct($arrProperties=Array(), $bolLoadById=FALSE)
+	{
+		// Parent constructor
+		parent::__construct($arrProperties, $bolLoadById);
+	}
+	
+	/**
+	 * _preparedStatement()
+	 *
+	 * Access a Static Cache of Prepared Statements used by this Class
+	 * 
+	 * @param	string		$strStatement						Name of the statement
+	 * 
+	 * @return	Statement										The requested Statement
+	 *
+	 * @method
+	 */
+	protected static function _preparedStatement($strStatement)
+	{
+		static	$arrPreparedStatements	= Array();
+		if (isset($arrPreparedStatements[$strStatement]))
+		{
+			return $arrPreparedStatements[$strStatement];
+		}
+		else
+		{
+			switch ($strStatement)
+			{
+				// SELECTS
+				case 'selById':
+					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "id = <id>", NULL, 1);
+					break;
+				
+				// INSERTS
+				case 'insSelf':
+					$arrPreparedStatements[$strStatement]	= new StatementInsert(self::$_strStaticTableName);
+					break;
+				
+				// UPDATE BY IDS
+				case 'ubiSelf':
+					$arrPreparedStatements[$strStatement]	= new StatementUpdateById(self::$_strStaticTableName);
+					break;
+				
+				// UPDATES
+				
+				default:
+					throw new Exception(__CLASS__."::{$strStatement} does not exist!");
+			}
+			return $arrPreparedStatements[$strStatement];
+		}
+	}
+}
+?>
