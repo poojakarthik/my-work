8504410c2686bcc4c8f17cb9dfc9abbff3d2c411
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-12 04:22:08 +0000

    work on document management

--- html/admin/javascript/document.js
+++ html/admin/javascript/document.js
@@ -50,7 +50,7 @@ var Document	= Class.create
 				objEmail.name	= (objEmail.name == undefined) ? '' : objEmail.name;
 				
 				strPredefinedEmails	+= "			<tr>\n";
-				strPredefinedEmails	+= "				<td style='width:5%; text-align:right;'><input id='Document_Email_Checkbox_"+(i+1)+"' type='checkbox' /></td>\n";
+				strPredefinedEmails	+= "				<td style='width:5%; text-align:right;'><input id='Document_Email_Checkbox_"+(i+1)+"' type='checkbox' value='"+objEmail.address+"' onchange='Flex.Document.emailAddAddress(this.value);' /></td>\n";
 				strPredefinedEmails	+= "				<td style='width:30%'>"+objEmail.name+"</td>\n";
 				strPredefinedEmails	+= "				<td>"+objEmail.address+"</td>\n";
 				strPredefinedEmails	+= "			</tr>\n";
