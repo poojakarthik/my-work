a55fa4fb162d3d43bee10d13ac2bead49d2e3d88
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-16 05:17:06 +0000

    work on document management

--- html/admin/html_template/account/services_list.php
+++ html/admin/html_template/account/services_list.php
@@ -299,9 +299,9 @@ class HtmlTemplateAccountServicesList extends HtmlTemplate
 				if ($arrService['FuturePlan']['brochure_document_id'])
 				{
 					$objBrochureDocument		= new Document(array('id'=>$arrService['FuturePlan']['brochure_document_id']), true);
-					$objBrochureDocumentContent	= $objBrochureDocument->getContent();
+					$objBrochureDocumentContent	= $objBrochureDocument->getContentDetails();
 					
-					if ($objBrochureDocumentContent && $objBrochureDocumentContent->content)
+					if ($objBrochureDocumentContent && $objBrochureDocumentContent->bolHasContent)
 					{
 						$objBrochureIcon			= new File_Type(array('id'=>$objBrochureDocumentContent->file_type_id), true);
 						
