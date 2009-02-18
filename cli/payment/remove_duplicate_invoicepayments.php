0b4ca3183696000f8c0845b78cef8f4466c3b563
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-18 05:31:22 +0000

    added a script to correct InvoicePayment duplicates

---
+++ cli/payment/remove_duplicate_invoicepayments.php
@@ -0,0 +1,41 @@
+<?php
+
+// Framework
+require_once("../../lib/classes/Flex.php");
+Flex::load();
+
+try
+{
+	DataAccess::getDataAccess()->TransactionStart();
+	
+	$qryQuery	= new Query();
+	
+	$strSQL	= "	SELECT Payment. * , (Payment.Amount - Payment.Balance), SUM( InvoicePayment.Amount ) AS ActualAmount
+				FROM Payment JOIN InvoicePayment ON Payment.Id = InvoicePayment.Payment
+				GROUP BY Payment.Id
+				HAVING ActualAmount > ( Payment.Amount - Payment.Balance )
+				AND ActualAmount = ( Payment.Amount - Payment.Balance ) * 2";
+	$resPayments	= $qryQuery->Execute($strSQL);
+	if ($resPayments === false)
+	{
+		throw new Exception($qryQuery->Error());
+	}
+	while ($arrPayment = $resPayments->fetch_assoc())
+	{
+		// Delete one of the InvoicePayment duplicates
+		$resDelete	= $qryQuery->Execute("DELETE FROM InvoicePayment WHERE Payment = {$arrPayment['Id']} AND invoice_run_id = {$arrPayment['invoice_run_id']} AND Account = {$arrPayment['Account']} AND Amount = {$arrPayment['Amount']} LIMIT 1");
+		if ($resDelete === false)
+		{
+			throw new Exception($qryQuery->Error());
+		}
+	}
+	
+	throw new Exception("TEST MODE");
+}
+catch (Exception $eException)
+{
+	DataAccess::getDataAccess()->TransactionRollback();
+	throw $eException;
+}
+
+?>
