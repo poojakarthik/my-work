e89416ffaccb970557737bd0df45d16e9d47b1cd
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-23 06:04:11 +0000

    work on document management

--- lib/classes/Flex_Date.php
+++ lib/classes/Flex_Date.php
@@ -26,33 +26,19 @@ class Flex_Date
 	/**
 	 * difference()
 	 *
-	 * Calculates the difference between two dates
+	 * Calculates the difference between two dates.  All values are rounded down to the nearest unit.
 	 * 
 	 * @param	string	$strEarlierDate				Earlier Date 
 	 * @param	string	$strLaterDate				Later Date
 	 * @param	string	$strAccuracy				Difference units.  Accepts 'y', 'm', 'd', 'h', 'i', or 's'. (Default: 's')
-	 * @param	boolean	$bolCeil					TRUE	: Difference is rounded up to the nearest unit
-	 * 												FALSE	: Difference is rounded down to the nearest unit
-	 * 												NULL	: Difference is rounded to the nearest unit (Default)
 	 *
-	 * @return	integer								Units separating the two dates
+	 * @return	mixed								Integer for rounded result, otherwise float
 	 *
 	 * @method
 	 */
-	public static function difference($strEarlierDate, $strLaterDate, $strInterval='s', $bolCeil=null)
+	public static function difference($strEarlierDate, $strLaterDate, $strInterval='s')
 	{
-		if ($bolCeil === true)
-		{
-			$strRoundingFunction	= 'ceil';
-		}
-		elseif ($bolCeil === false)
-		{
-			$strRoundingFunction	= 'floor';
-		}
-		else
-		{
-			$strRoundingFunction	= 'round';
-		}
+		$strRoundingFunction	= 'floor';
 		
 		$intEarlierTime	= strtotime($strEarlierDate);
 		if ($intEarlierTime === false)
@@ -100,138 +86,25 @@ class Flex_Date
 		$intMonthEarlier		= (int)date("m", $intEarlierTime);
 		$intMonthLater			= (int)date("m", $intLaterTime);
 		
-		$intEarlierMonthDays	= (int)date('t', mktime(0, 0, 0, $intMonthEarlier, 1, $intYearEarlier));
-		$intLaterMonthDays		= (int)date('t', mktime(0, 0, 0, $intMonthLater, 1, $intYearLater));
-		
-		$intEarlierTimeOffset	= strtotime(date("1970-01-01 H:i:s", $intEarlierTime));
-		$intLaterTimeOffset		= strtotime(date("1970-01-01 H:i:s", $intLaterTime));
-		
 		// Months
-		$intMonths				= 0;
-		$intMonthsInDays		= 0;
-		$intEarlierMonthOffset	= ($intYearEarlier * 12)	+ $intMonthEarlier;
-		$intLaterMonthOffset	= ($intYearLater * 12)		+ $intMonthLater;
-		$arrMonthOffsets		= range($intEarlierMonthOffset, $intLaterMonthOffset);
-		foreach ($arrMonthOffsets as $intMonthOffset)
+		$intMonthDifference	= (($intYearLater - $intYearEarlier) * self::MONTHS_IN_YEAR);
+		$intMonthDifference	+= ($intMonthLater - $intMonthEarlier);
+		$intMonthDifference	= ((int)date("dHis", $intLaterTime) >= (int)date("dHis", $intEarlierTime)) ? $intMonthDifference : $intMonthDifference - 1;
+		if ($strInterval === 'm')
 		{
-			$intMonths++;
-			$intYear			= floor($intMonthOffset / 12);
-			$intMonth			= $intMonthOffset % 12;
-			$intDaysInMonth		= (int)date('t', mktime(0, 0, 0, $intMonth, 1, $intYear));
-			$intMonthsInDays	+= $intDaysInMonth;
+			return call_user_func($strRoundingFunction, $intMonthDifference);
 		}
-		$intTotalDifferenceInSeconds % self::SECONDS_IN_DAY;
-		$intMonthsInSeconds	= $intMonthsInDays * self::SECONDS_IN_DAY;
 		
-		
-		// Interval Conversion multi-dimensional array
-		//	$arrIntervalConversion['m']['y']	= 36
-		//	This means that the difference in years, converted to months, is 36 (ie. 3 years = 36 months)
-		$arrIntervalConversion	= array();
-		
-		// Seconds
-		$intSecondEarlier		= (int)date("s", $intEarlierTime);
-		$intSecondLater			= (int)date("s", $intLaterTime);
-		$intSecondDifference	= $intSecondLater - $intSecondEarlier;
-		$intSecondDifference	= ($intSecondDifference > 0) ? $intSecondDifference : self::SECONDS_IN_MINUTE + $intSecondDifference;
-		
-		// Minutes
-		$intMinuteEarlier		= (int)date("i", $intEarlierTime);
-		$intMinuteLater			= (int)date("i", $intLaterTime);
-		$intMinuteDifference	= $intMinuteLater - $intMinuteEarlier;
-		$intMinuteDifference	= ($intMinuteDifference > 0) ? $intMinuteDifference : self::MINUTES_IN_HOUR + $intMinuteDifference;
-		
-		// Hours
-		$intHourEarlier			= (int)date("H", $intEarlierTime);
-		$intHourLater			= (int)date("H", $intLaterTime);
-		$intHourDifference		= $intHourEarlier - $intHourEarlier;
-		$intHourDifference		= ($intHourDifference > 0) ? $intHourDifference : self::HOURS_IN_DAY + $intHourDifference;
-		
-		// Days
-		$intDayEarlier			= (int)date("d", $intEarlierTime);
-		$intDayLater			= (int)date("d", $intLaterTime);
-		$intDayDifference		= max(0, $intDayLater-$intDayEarlier);
-		
-		// Months
-		$intMonthEarlier		= (int)date("m", $intEarlierTime);
-		$intMonthLater			= (int)date("m", $intLaterTime);
-		$intMonthDifference		= max(0, $intMonthLater-$intMonthEarlier);
-				
 		// Years
-		$intYearEarlier			= (int)date("Y", $intEarlierTime);
-		$intYearLater			= (int)date("Y", $intLaterTime);
-		$intYearDifference		= max(0, $intYearLater-$intYearEarlier);
-		
-		// Months in Days
-		$intMonthsInDays		= 0;
-		$intEarlierMonthOffset	= ($intYearEarlier * 12) + $intMonthEarlier;
-		$intLaterMonthOffset	= ($intYearLater * 12) + $intMonthLater;
-		$arrMonthOffsets		= range($intEarlierMonthOffset, $intLaterMonthOffset);
-		foreach ($arrMonthOffsets as $intMonthOffset)
+		$intYearDifference	= $intYearLater - $intYearEarlier;
+		if ((int)date("mdHis", $intLaterTime) >= (int)date("mdHis", $intEarlierTime))
 		{
-			$intYear			= floor($intMonthOffset / 12);
-			$intMonth			= $intMonthOffset % 12;
-			$intDaysInMonth		= (int)date('t', mktime(0, 0, 0, $intMonth, 1, $intYear));
-			$intMonthsInDays	+= $intDaysInMonth;
+			$intYearDifference++;
 		}
-		
-		// Years in Days
-		$arrIntervalConversion['d']['y']	= 0;
-		$arrYears		= range($intYearEarlier, $intYearLater);
-		foreach ($arrYears as $intYear)
+		if ($strInterval === 'y')
 		{
-			if ($intYear % 4 === 0 && ($intYear % 100 || $intYear % 400 === 0))
-			{
-				$arrIntervalConversion['d']['y']	+= 365;
-			}
-			else
-			{
-				$arrIntervalConversion['d']['y']	+= 366;
-			}
+			return call_user_func($strRoundingFunction, $intYearDifference);
 		}
-		
-		
-		
-		/*
-		
-		$arrIntervalConversion['y']['s']	= $intSecondDifference;
-		$arrIntervalConversion['y']['i']	= $intSecondDifference;
-		$arrIntervalConversion['y']['h']	= $intSecondDifference;
-		$arrIntervalConversion['y']['d']	= $intSecondDifference;
-		$arrIntervalConversion['y']['m']	= $intSecondDifference;
-		$arrIntervalConversion['y']['y']	= $intSecondDifference;
-		
-		
-		
-		
-		
-		$arrIntervalConversion['y']['y']	= $intYearDifference;
-		$arrIntervalConversion['m']['y']	= $intYearDifference * self::MONTHS_IN_YEAR;
-		$arrIntervalConversion['h']['y']	= $arrIntervalConversion['d']['y'] * self::HOURS_IN_DAY;
-		$arrIntervalConversion['i']['y']	= $arrIntervalConversion['d']['y'] * self::MINUTES_IN_DAY;
-		$arrIntervalConversion['s']['y']	= $arrIntervalConversion['d']['y'] * self::SECONDS_IN_DAY;
-		
-		
-		$arrIntervalConversion['y']['m']	= ($bolCeil) ? self::MONTHS_IN_YEAR : (($bolCeil === false) ? 0 : round($intMonthDifference / 100) * 100);
-		$arrIntervalConversion['m']['m']	= $intMonthDifference;
-		$arrIntervalConversion['d']['m']	= $intMonthsInDays;
-		$arrIntervalConversion['h']['m']	= $intMonthsInDays * self::HOURS_IN_DAY;
-		$arrIntervalConversion['i']['m']	= $intMonthsInDays * self::MINUTES_IN_DAY;
-		$arrIntervalConversion['s']['m']	= $intMonthsInDays * self::SECONDS_IN_DAY;
-		
-		// Minutes
-		$intMinuteEarlier		= (int)date("i", $intEarlierTime);
-		$intMinuteLater			= (int)date("i", $intLaterTime);
-		$intMinuteDifference	= max(0, $intMinuteLater-$intMinuteEarlier);
-		
-		$intSecondsInMinutes	= ($bolCeil) ? self::SECONDS_IN_MINUTE : (($bolCeil === false) ? 0 : round($intSecondDifference / 100) * 100);
-		*/
-		return	$arrIntervalConversion[strtolower($strInterval)]['y'] + 
-				$arrIntervalConversion[strtolower($strInterval)]['m'] + 
-				$arrIntervalConversion[strtolower($strInterval)]['d'] + 
-				$arrIntervalConversion[strtolower($strInterval)]['h'] + 
-				$arrIntervalConversion[strtolower($strInterval)]['i'] + 
-				$arrIntervalConversion[strtolower($strInterval)]['s'];
 	}
 	
 	/**
