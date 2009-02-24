aa4590df2ce9d964cb9e86b4c7ae93635b99f687
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-24 03:24:27 +0000

    work on document management

--- html/admin/javascript/plan_change.js
+++ html/admin/javascript/plan_change.js
@@ -118,7 +118,7 @@ function VixenPlanChangeClass()
 		
 		// Call AJAX function to return Script HTML
 		var fncJsonFunc	= jQuery.json.jsonFunction(renderAuthScript.bind(this), null, 'Plan', 'renderAuthScript');
-		fncJsonFunc($ID('Service.Id').value, $ID('Combo_NewPlan.Id').value $ID('Combo_NewPlan.StartTime').value);
+		fncJsonFunc($ID('Service.Id').value, $ID('Combo_NewPlan.Id').value, $ID('Combo_NewPlan.StartTime').value);
 	}
 }
 
