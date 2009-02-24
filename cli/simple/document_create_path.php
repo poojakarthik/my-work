a18e19b57debf4cd7ae10e994da4be54d6b688eb
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-24 01:46:32 +0000

    work on document management

--- cli/simple/document_create_path.php
+++ cli/simple/document_create_path.php
@@ -4,6 +4,12 @@
 require_once("../../lib/classes/Flex.php");
 Flex::load();
 
+$resFileInfoDB		= new finfo(FILEINFO_MIME);
+if (!$resFileInfoDB)
+{
+	throw new Exception("Unable to load MIME Database");
+}
+
 $objDocument		= new Document();
 $objDocumentContent	= new Document_Content();
 
@@ -15,6 +21,8 @@ $objDocumentContent	= new Document_Content();
 do
 {
 	$strPath		= trim(getUserInput("Please enter the Document path for the Document you want to create: "));
+	
+	// Check the directory
 	$strParentPath	= dirname($strPath);
 	if ($strParentPath)
 	{
@@ -28,8 +36,15 @@ do
 			$objDocumentContent->parent_brochure_id	= $objParentDocument->id;
 		}
 	}
+	
+	// Check the file Name (can't contain /'s, new lines, or tabs)
+	$strName	= basename($strPath);
+	if (preg_match("/^[^\r\n\t\/]+$/i", $strName))
+	{
+		$objDocumentContent->name	= $strName;
+	}
 }
-while (!$strPath);
+while (!$objDocumentContent->name);
 
 // Document Nature (File/Folder)
 do
@@ -42,31 +57,77 @@ do
 			break;
 			
 		case 'FOLDER':
-			$objDocument->document_nature_id	= DOCUMENT_NATURE_FILE;
-			break;
-		
-		default:
-			$strNature	= null;
+			$objDocument->document_nature_id	= DOCUMENT_NATURE_FOLDER;
 			break;
 	}
 }
-while (!$strNature);
-
-// Name
-// TODO
+while (!$objDocument->document_nature_id);
 
 // Description (optional)
-// TODO
+$strDescription		= trim(getUserInput("Please enter the Description for the Documents (optional): "));
+if ($strDescription)
+{
+	$objDocumentContent->description;
+}
 
-// File Type (files only)
-// TODO
-
-// Content File Path (files only)(optional)
-// TODO
-
-
+// Files Only
+if ($objDocument->document_nature_id === DOCUMENT_NATURE_FILE)
+{
+	// Content File Path (optional)
+	do
+	{
+		$strContentPath		= trim(getUserInput("Please enter the path to the file to import: "));
+		if (is_file($strContentPath))
+		{
+			// File Extension
+			$strBaseName	= basename($strContentPath);
+			$strExtension	= substr($strBaseName, strripos($strBaseName, '.') + 1);
+			
+			// Mime Type
+			$strMimeContentType	= $resFileInfoDB->file($strContentPath);
+			
+			// Find File/Mime Type Combination in Flex
+			$objFileType	= File_Type::getForExtensionAndMimeType($strExtension, $strMimeContentType);
+			if ($objFileType)
+			{
+				$objDocumentContent->file_type_id	= $objFileType->id;
+				$objDocumentContent->mime_type_id	= $objFileType->mime_type_id;
+			}
+			else
+			{
+				throw new Exception("'{$strBaseName}' has an extension of '{$strExtension}' and MIME Content type of '{$strMimeContentType}', which are not supported in Flex");
+			}
+			
+			// Set Document Content
+			$objDocumentContent->content	= file_get_contents($strContentPath);
+		}
+	}
+	while (!$objDocumentContent->file_type_id || !$objDocumentContent->mime_type_id);
+}
 
 //----------------------------------------------------------------------------//
+
+// Save to the DB
+DataAccess::getDataAccess()->TransactionStart();
+try
+{
+	$objDocument->employee_id	= Employee::SYSTEM_EMPLOYEE_ID;
+	$objDocument->save();
+	
+	$objDocumentContent->document_id	= $objDocument->id;
+	$objDocumentContent->employee_id	= Employee::SYSTEM_EMPLOYEE_ID;
+	$objDocumentContent->status_id		= STATUS_ACTIVE;
+	$objDocumentContent->save();
+	
+	throw new Exception("TEST MODE");
+	
+	DataAccess::getDataAccess()->TransactionCommit();
+}
+catch (Exception $eException)
+{
+	DataAccess::getDataAccess()->TransactionRollback();
+	throw $eException;
+}
 
 exit(0);
 
