2264c95279816a800b4bd9519f9886b492ab8ef7
Rich Davis <rdavis@yellowbilling.com.au>
2009-02-25 06:04:54 +0000

    Added some new Rollout Scripts

--- lib/rollout/version/Flex_Rollout_Version_000143.php
+++ lib/rollout/version/Flex_Rollout_Version_000143.php
@@ -19,11 +19,11 @@ class Flex_Rollout_Version_000143 extends Flex_Rollout_Version
 		// 1:	Rename AccountLetterLog and its Fields to underscored naming convention
 		$strSQL =	"ALTER TABLE AccountLetterLog " .
 					"RENAME account_letter_log, " .
-					"CHANGE Id			id							BIGINT(20)	NOT NULL	UNSIGNED	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
-					"CHANGE Account		account_id					BIGINT(20)	NOT NULL	UNSIGNED					COMMENT '(FK) Account this log entry belongs to', " .
-					"CHANGE Invoice		invoice_id					BIGINT(20)	NULL		UNSIGNED					COMMENT '(FK) Invoice this log relates to', " .
-					"CHANGE LetterType	document_template_type_id	BIGINT(20)	NOT NULL	UNSIGNED					COMMENT '(FK) Document Template Type this log refers to', " .
-					"CHANGE CreatedOn	created_on					TIMESTAMP	NOT NULL								COMMENT 'Creation Timestamp', " .
+					"CHANGE Id			id							BIGINT(20)	UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
+					"CHANGE Account		account_id					BIGINT(20)	UNSIGNED	NOT NULL					COMMENT '(FK) Account this log entry belongs to', " .
+					"CHANGE Invoice		invoice_id					BIGINT(20)	UNSIGNED	NULL						COMMENT '(FK) Invoice this log relates to', " .
+					"CHANGE LetterType	document_template_type_id	BIGINT(20)	UNSIGNED	NOT NULL					COMMENT '(FK) Document Template Type this log refers to', " .
+					"CHANGE CreatedOn	created_on					TIMESTAMP				NOT NULL					COMMENT 'Creation Timestamp', " .
 					"\n" .
 					"ADD CONSTRAINT fk_account_letter_log_account_id				FOREIGN KEY	(account_id) 				REFERENCES Account(Id)				ON UPDATE CASCADE ON DELETE RESTRICT, " .
 					"ADD CONSTRAINT fk_account_letter_log_invoice_id				FOREIGN KEY	(invoice_id) 				REFERENCES Invoice(Id)				ON UPDATE CASCADE ON DELETE RESTRICT, " .
@@ -39,20 +39,20 @@ class Flex_Rollout_Version_000143 extends Flex_Rollout_Version
 								"DROP FOREIGN KEY fk_account_letter_log_document_template_type_id, " .
 								"\n" .
 								"RENAME AccountLetterLog, " .
-								"CHANGE id							Id			BIGINT(20)	NOT NULL	UNSIGNED	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
-								"CHANGE account_id					Account		BIGINT(20)	NOT NULL	UNSIGNED					COMMENT '(FK) Account this log entry belongs to', " .
-								"CHANGE invoice_id					Invoice		BIGINT(20)	NULL		UNSIGNED					COMMENT '(FK) Invoice this log relates to', " .
-								"CHANGE document_template_type_id	LetterType	BIGINT(20)	NOT NULL	UNSIGNED					COMMENT '(FK) Document Template Type this log refers to', " .
-								"CHANGE created_on					CreatedOn	DATETIME	NOT NULL								COMMENT 'Creation Timestamp';";
+								"CHANGE id							Id			BIGINT(20)	UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
+								"CHANGE account_id					Account		BIGINT(20)	UNSIGNED	NOT NULL					COMMENT '(FK) Account this log entry belongs to', " .
+								"CHANGE invoice_id					Invoice		BIGINT(20)	UNSIGNED	NULL						COMMENT '(FK) Invoice this log relates to', " .
+								"CHANGE document_template_type_id	LetterType	BIGINT(20)	UNSIGNED	NOT NULL					COMMENT '(FK) Document Template Type this log refers to', " .
+								"CHANGE created_on					CreatedOn	DATETIME				NOT NULL					COMMENT 'Creation Timestamp';";
 		
 		// 2:	Rename DestinationTranslation and its Fields to underscored naming convention
 		$strSQL =	"ALTER TABLE DestinationTranslation " .
 					"RENAME cdr_call_type_translation, " .
-					"CHANGE Id			id				BIGINT(20)		NOT NULL	UNSIGNED	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
-					"CHANGE Code		code			BIGINT(20)		NOT NULL	UNSIGNED					COMMENT 'Flex Call Type Code', " .
-					"CHANGE Carrier		carrier_id		BIGINT(20)		NOT NULL								COMMENT '(FK) Carrier', " .
-					"CHANGE CarrierCode	carrier_code	VARCHAR(255)	NOT NULL								COMMENT 'Carrier Call Type Code', " .
-					"CHANGE Description	description		VARCHAR(255)	NOT NULL								COMMENT 'Carrier Call Type Description', " .
+					"CHANGE Id			id				BIGINT(20)		UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
+					"CHANGE Code		code			BIGINT(20)		UNSIGNED	NOT NULL					COMMENT 'Flex Call Type Code', " .
+					"CHANGE Carrier		carrier_id		BIGINT(20)					NOT NULL					COMMENT '(FK) Carrier', " .
+					"CHANGE CarrierCode	carrier_code	VARCHAR(255)				NOT NULL					COMMENT 'Carrier Call Type Code', " .
+					"CHANGE Description	description		VARCHAR(255)				NOT NULL					COMMENT 'Carrier Call Type Description', " .
 					"\n" .
 					"ADD CONSTRAINT fk_cdr_call_type_translation_carrier_id			FOREIGN KEY	(carrier_id) 				REFERENCES Carrier(Id)				ON UPDATE CASCADE ON DELETE RESTRICT;";
 		$result = $dbAdmin->query($strSQL);
@@ -64,18 +64,18 @@ class Flex_Rollout_Version_000143 extends Flex_Rollout_Version
 								"DROP FOREIGN KEY fk_cdr_call_type_translation_carrier_id, " .
 								"\n" .
 								"RENAME DestinationTranslation, " .
-								"CHANGE id				Id			BIGINT(20)		NOT NULL	UNSIGNED	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
-								"CHANGE code			Code		BIGINT(20)		NOT NULL	UNSIGNED					COMMENT 'Flex Call Type Code', " .
-								"CHANGE carrier_id		Carrier		BIGINT(20)		NOT NULL								COMMENT '(FK) Carrier', " .
-								"CHANGE carrier_code	CarrierCode	VARCHAR(255)	NOT NULL								COMMENT 'Carrier Call Type Code', " .
-								"CHANGE description		Description	VARCHAR(255)	NOT NULL								COMMENT 'Carrier Call Type Description';";
+								"CHANGE id				Id			BIGINT(20)		UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
+								"CHANGE code			Code		BIGINT(20)		UNSIGNED	NOT NULL					COMMENT 'Flex Call Type Code', " .
+								"CHANGE carrier_id		Carrier		BIGINT(20)					NOT NULL					COMMENT '(FK) Carrier', " .
+								"CHANGE carrier_code	CarrierCode	VARCHAR(255)				NOT NULL					COMMENT 'Carrier Call Type Code', " .
+								"CHANGE description		Description	VARCHAR(255)				NOT NULL					COMMENT 'Carrier Call Type Description';";
 		
 		// 3:	Rename DocumentResourceTypeFileType and its Fields to underscored naming convention
 		$strSQL =	"ALTER TABLE DocumentResourceTypeFileType " .
 					"RENAME document_resource_type_file_type, " .
-					"CHANGE Id				id							BIGINT(20)	NOT NULL	UNSIGNED	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
-					"CHANGE ResourceType	document_resource_type_id	BIGINT(20)	NOT NULL	UNSIGNED					COMMENT '(FK) Document Resource Type', " .
-					"CHANGE FileType		file_type_id				BIGINT(20)	NOT NULL	UNSIGNED					COMMENT '(FK) File Type', " .
+					"CHANGE Id				id							BIGINT(20)	UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
+					"CHANGE ResourceType	document_resource_type_id	BIGINT(20)	UNSIGNED	NOT NULL					COMMENT '(FK) Document Resource Type', " .
+					"CHANGE FileType		file_type_id				BIGINT(20)	UNSIGNED	NOT NULL					COMMENT '(FK) File Type', " .
 					"\n" .
 					"ADD CONSTRAINT fk_document_resource_type_file_type_document_resource_type_id	FOREIGN KEY	(document_resource_type_id) REFERENCES DocumentResourceType(Id)	ON UPDATE CASCADE ON DELETE CASCADE, " .
 					"ADD CONSTRAINT fk_document_resource_type_file_type_file_type_id				FOREIGN KEY	(file_type_id) 				REFERENCES FileType(Id)				ON UPDATE CASCADE ON DELETE RESTRICT, " .
@@ -89,9 +89,9 @@ class Flex_Rollout_Version_000143 extends Flex_Rollout_Version
 								"DROP FOREIGN KEY fk_document_resource_type_file_type_file_type_id, " .
 								"\n" .
 								"RENAME DocumentResourceTypeFileType, " .
-								"CHANGE id							Id				BIGINT(20)	NOT NULL	UNSIGNED	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
-								"CHANGE document_resource_type_id	ResourceType	BIGINT(20)	NOT NULL	UNSIGNED					COMMENT '(FK) Document Resource Type', " .
-								"CHANGE file_type_id				FileType		BIGINT(20)	NOT NULL	UNSIGNED					COMMENT '(FK) File Type';";
+								"CHANGE id							Id				BIGINT(20)	UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
+								"CHANGE document_resource_type_id	ResourceType	BIGINT(20)	UNSIGNED	NOT NULL					COMMENT '(FK) Document Resource Type', " .
+								"CHANGE file_type_id				FileType		BIGINT(20)	UNSIGNED	NOT NULL					COMMENT '(FK) File Type';";
 			
 	}
 	
