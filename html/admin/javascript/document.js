6d07925680ea73e9d627c1de738e1ad0bcdd97cc
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-02 02:07:18 +0000

    work on document management

--- html/admin/javascript/document.js
+++ html/admin/javascript/document.js
@@ -408,7 +408,7 @@ var Document	= Class.create
 					{
 						strIcon	= '../admin/reflex.php/File/Image/FileTypeIcon/'+objChild.file_type_id+'/16x16';
 					}
-					strDocumentListing	+=	"				<tr onmouseover='this.className=\"alt\"' onmouseout='this.className=\"\"' onclick='"+strLink+"'>\n" +
+					strDocumentListing	+=	"				<tr onmouseover='this.className=\"alt-hover\"' onmouseout='this.className=\"\"' onclick='"+strLink+"'>\n" +
 											"					<td><img src='"+strIcon+"' />&nbsp;<span>"+objChild.friendly_name+"</span></td>\n" +
 											"					<td><em>"+(objChild.description ? objChild.description : '')+"</em></td>\n" +
 											"					<td>&nbsp;</td>\n" +
