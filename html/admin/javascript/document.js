0e7dc25c22aa1c95a7201b1a7f6d4efb8b66176f
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-02 05:30:07 +0000

    work on document management

--- html/admin/javascript/document.js
+++ html/admin/javascript/document.js
@@ -382,7 +382,7 @@ var Document	= Class.create
 				var strIcon	= '';
 				if (i != 0)
 				{
-					strBreadcrumbMenu	+= '&nbsp;<img src="../admin/img/template/menu_open_right.png" />&nbsp;';
+					strBreadcrumbMenu	+= '<img class="document-explorer-address-separator" src="../admin/img/template/menu_open_right.png" />';
 					strIcon				= '../admin/img/template/folder.png';
 				}
 				else
