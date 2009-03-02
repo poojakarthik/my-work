5000e307a9cc946ca07d46ed570ef7b5ce28b6fb
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-02 05:21:03 +0000

    work on document management

--- html/admin/javascript/document.js
+++ html/admin/javascript/document.js
@@ -391,7 +391,7 @@ var Document	= Class.create
 				}
 				
 				var strOnClick	= (i < objResponse.objDocument.arrPath.length - 1) ? "onclick='Flex.Document.updateExplorerPopup("+objResponse.objDocument.arrPath[i].document_id+");'" : '';
-				strBreadcrumbMenu	+= "<span "+strOnClick+"><img class='document-explorer-icon' src='"+strIcon+"' />"+objResponse.objDocument.arrPath[i].friendly_name+"</span>";
+				strBreadcrumbMenu	+= "<span class='document-explorer-address-node' "+strOnClick+"><img class='document-explorer-icon' src='"+strIcon+"' />"+objResponse.objDocument.arrPath[i].friendly_name+"</span>";
 			}
 			
 			// Build Document Listing
