e3a4599ea736f8096d7e96d0dde5a3b943776269
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-02 06:28:58 +0000

    work on document management

--- html/admin/javascript/document.js
+++ html/admin/javascript/document.js
@@ -361,8 +361,8 @@ var Document	= Class.create
 		// Render the Explorer popup at the Root Directory
 		this.updateExplorerPopup(null);
 
-		this.pupExplorer.recentre();
 		this.pupExplorer.display();
+		this.pupExplorer.recentre();
 	},
 	
 	updateExplorerPopup	: function(intDocumentId)
