fcf1d639d4d3d260fbb0a53848e96ed136e8dfb7
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-03 00:23:07 +0000

    work on document management

--- html/admin/javascript/document.js
+++ html/admin/javascript/document.js
@@ -426,7 +426,7 @@ var Document	= Class.create
 					}
 					
 					strDocumentListing	+=	"				<tr valign='top' onclick='"+strLink+"'>\n" +
-											"					<td class='field-name' title='"+(objChild.description ? objChild.description : objChild.friendly_name)+"'>"+strIcon+"&nbsp;<span>"+strFriendlyName+"</span></td>\n" +
+											"					<td class='field-name' title='"+(objChild.description ? objChild.description : objChild.friendly_name)+"'>"+strIcon+"<span>"+strFriendlyName+"</span></td>\n" +
 											"					<td class='field-size'>"+(objChild.nature == 'DOCUMENT_NATURE_FILE' ? objChild.file_size+' KB' : '')+"</td>\n" +
 											"					<td class='field-date'>"+objChild.date_modified+"</td>\n" +
 											"					<td class='field-user'>"+objChild.modified_by+"</td>\n" +
