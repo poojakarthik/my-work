a97fd975562d6afa809775d36917ba4afca2d6fa
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-03 22:25:38 +0000

    work on document management

--- html/admin/javascript/document_explorer.js
+++ html/admin/javascript/document_explorer.js
@@ -26,7 +26,7 @@ var Document_Explorer	= Class.create
 		
 		this.elmDocumentExplorerDIV				= document.createElement('div');
 		this.elmDocumentExplorerDIV.className	= "document-explorer";
-		this.elmDocumentExplorerDIV.onClick		= "Flex.Document.Explorer.canvasClick()";
+		this.elmDocumentExplorerDIV.setAttribute('onclick', "Flex.Document.Explorer.canvasClick()");
 		this.elmEncapsulator.appendChild(this.elmDocumentExplorerDIV);
 		
 		this.elmHeaderTable				= document.createElement('table');
