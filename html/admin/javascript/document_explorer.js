c58877b4c6c837bef44dc427c704aeb8606f0954
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-04 06:09:53 +0000

    work on document management

--- html/admin/javascript/document_explorer.js
+++ html/admin/javascript/document_explorer.js
@@ -481,11 +481,9 @@ var Document_Explorer	= Class.create
 			
 			if (objChild.editable)
 			{
+				var strEditCall	= "(jQuery.json.jsonFunction(Flex.Document.Explorer.renderEditPopup.bind(Flex.Document.Explorer), null, \"Document\", \"getDetails\"))("+objChild.id+")";
+				
 				arrActions.push("<span onclick='alert(\"Edit a doc!\")'><img class='icon' src='../admin/img/template/page_white_edit.png' />&nbsp;Edit</span>");
-			}
-			
-			if (objChild.editable)
-			{
 				arrActions.push("<span onclick='alert(\"Delete a doc!\")'><img class='icon' src='../admin/img/template/delete.png' />&nbsp;Delete</span>");
 			}
 		}
