aee7b13a358abd34f590ced9855a6b54c6e62d89
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-05 23:57:10 +0000

    work on document management

---
+++ cli/simple/fix_brochure_parents.php
@@ -0,0 +1,55 @@
+<?php
+
+// Framework
+require_once("../../lib/classes/Flex.php");
+Flex::load();
+
+try
+{
+	if (!DataAccess::getDataAccess()->TransactionStart())
+	{
+		throw new Exception("Unable to start a Transaction");
+	}
+	$qryQuery	= new Query();
+	
+	$arrCustomerGroupDocuments	= array();
+	
+	// Select all Plans with a Plan Brochure which has no parent
+	$resPlansWithBrochures	= $qryQuery->Execute("SELECT RatePlan.* FROM RatePlan JOIN document_content ON RatePlan.brochure_document_id = document_content.document_id WHERE parent_document_id IS NULL");
+	if (!$resPlansWithBrochures)
+	{
+		throw new Exception($qryQuery->Error());
+	}
+	while ($arrPlan = $resPlansWithBrochures->fetch_assoc())
+	{
+		Log::getLog()->log(" [+] ".GetConstantDescription($arrPlan['customer_group'])."");
+		
+		$objRatePlan			= new Rate_Plan($arrPlan);
+		$objBrochureDocument	= new Document(array('id'=>$objRatePlan->brochure_document_id), true);
+		$objBrochureContent		= $objBrochureDocument->getContent();
+		
+		$objCustomerGroupDir	= Document::getByPath("/Plan Brochures/{$this->customer_group}/");
+		if (!$objCustomerGroupDir)
+		{
+			throw new Exception("'/Plan Brochures/{$this->customer_group}/' doesn't exist yet!");
+		}
+		
+		// Replace the Brochure with itself
+		$objBrochureContent->id					= null;
+		$objBrochureContent->parent_document_id	= $objCustomerGroupDir->id;
+		$objBrochureContent->save();
+	}
+	
+	if (!DataAccess::getDataAccess()->TransactionCommit())
+	{
+		throw new Exception("Unable to commit the Transaction");
+	}
+}
+catch (Exception $eException)
+{
+	DataAccess::getDataAccess()->TransactionRollback();
+	throw $eException;
+}
+
+
+?>
---
+++ cli/simple/update_plan_brochures.php
@@ -0,0 +1,9 @@
+<?php
+
+// Load Flex
+require_once("../../lib/classes/Flex.php");
+Flex::load();
+
+// Config
+$strPDFBaseDir	= "/home/rdavis/telcoblue/document_management/Brochures/";
+?>
