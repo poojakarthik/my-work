cef5cc8217e966fed664e83992e7080e0153f6b0
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-06 05:17:12 +0000

    Work on Rate Plan attributes

--- html/admin/html_template/plan/list.php
+++ html/admin/html_template/plan/list.php
@@ -153,13 +153,13 @@ window.location				= \"$strAvailablePlansLink?RatePlan.ServiceType=\"+ elmServic
 			switch ($arrRatePlan['Archived'])
 			{
 				case RATE_STATUS_ARCHIVED:
-					$strStatusCell	.= "src='../admin/img/template/indiactor_inactive.png' alt='".GetConstantDescription($arrRatePlan['Archived'], "RateStatus")."'";
+					$strStatusCell	.= "src='../admin/img/template/indicator_inactive.png' alt='".GetConstantDescription($arrRatePlan['Archived'], "RateStatus")."'";
 					break;
 				case RATE_STATUS_DRAFT:
-					$strStatusCell	.= "src='../admin/img/template/indiactor_draft.png' alt='".GetConstantDescription($arrRatePlan['Archived'], "RateStatus")."'";
+					$strStatusCell	.= "src='../admin/img/template/indicator_draft.png' alt='".GetConstantDescription($arrRatePlan['Archived'], "RateStatus")."'";
 					break;
 				case RATE_STATUS_ACTIVE:
-					$strStatusCell	.= "src='../admin/img/template/indiactor_active.png' alt='".GetConstantDescription($arrRatePlan['Archived'], "RateStatus")."'";
+					$strStatusCell	.= "src='../admin/img/template/indicator_active.png' alt='".GetConstantDescription($arrRatePlan['Archived'], "RateStatus")."'";
 					break;
 			}
 			
