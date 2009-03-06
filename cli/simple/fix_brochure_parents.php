83381fedc99d2dacb99ba346f3822890f8b3c601
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-06 00:03:32 +0000

    work on document management

--- cli/simple/fix_brochure_parents.php
+++ cli/simple/fix_brochure_parents.php
@@ -22,7 +22,7 @@ try
 	}
 	while ($arrPlan = $resPlansWithBrochures->fetch_assoc())
 	{
-		Log::getLog()->log(" [+] ".GetConstantDescription($arrPlan['customer_group'])."");
+		Log::getLog()->log(" [+] ".GetConstantDescription($arrPlan['customer_group'], 'CustomerGroup')."::{$arrPlan['']}...");
 		
 		$objRatePlan			= new Rate_Plan($arrPlan);
 		$objBrochureDocument	= new Document(array('id'=>$objRatePlan->brochure_document_id), true);
