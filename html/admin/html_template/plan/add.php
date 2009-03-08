d5f96d1f813946fad5d1dcbcd57ccc1030381fb7
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-08 23:29:48 +0000

    Work on Adding RatePlan.cdr_required and locked to Plan pages

--- html/admin/html_template/plan/add.php
+++ html/admin/html_template/plan/add.php
@@ -225,7 +225,7 @@ class HtmlTemplatePlanAdd extends HtmlTemplate
 		echo "<div id='PlanDetailsColumn2' style='width:50%;float:left'>";
 		
 		// Plan Change Locking
-		$strChecked	= (DBO()->RatePlan->cdr_required->Value) ? "checked='checked'" : '';
+		$strChecked	= (DBO()->RatePlan->cdr_required->Value == 0) ? '' : "checked='checked'";
 		echo "
 <div class='DefaultElement' style='margin-bottom:4px;'>
 	<input type='checkbox' id='RatePlan.locked' name='RatePlan.locked' value='1' $strChecked class='DefaultInputCheckBox2 Default' />
@@ -233,7 +233,7 @@ class HtmlTemplatePlanAdd extends HtmlTemplate
 </div>";
 		
 		// CDR Required
-		$strChecked	= (isset(DBO()->RatePlan->cdr_required) || DBO()->RatePlan->cdr_required->Value) ? "checked='checked'" : '';
+		$strChecked	= (DBO()->RatePlan->cdr_required->Isset || DBO()->RatePlan->cdr_required->Value) ? "checked='checked'" : '';
 		echo "
 <div class='DefaultElement' style='margin-bottom:4px;'>
 	<input type='checkbox' id='RatePlan.cdr_required' name='RatePlan.cdr_required' value='1' $strChecked class='DefaultInputCheckBox2 Default' />
