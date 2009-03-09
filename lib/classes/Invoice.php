188d11dd2909c733eb488070ed91f5e0fce4122c
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-09 00:36:21 +0000

    added some prorata debugging

--- lib/classes/Invoice.php
+++ lib/classes/Invoice.php
@@ -875,6 +875,9 @@ class Invoice extends ORM
 		if ($intBillingPeriod)
 		{
 			$fltProratedAmount			= ($fltAmount / $intBillingPeriod) * $intProratePeriod;
+			Log()->getLog()->log("{$fltProratedAmount}\t= ({$fltAmount} / {$intBillingPeriod}) * {$intProratePeriod};");
+			Log()->getLog()->log("{$fltProratedAmount}\t= ({$fltAmount} / ".($intBillingPeriod / Flex_Date::SECONDS_IN_DAY).") * ".($intBillingPeriod / Flex_Date::SECONDS_IN_DAY).";");
+			
 			$fltProratedAmount			= round($fltProratedAmount, $intDecimalPlaces);
 			return $fltProratedAmount;
 		}
