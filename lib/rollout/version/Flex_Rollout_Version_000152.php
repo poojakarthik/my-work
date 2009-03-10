03348ab6f018497d34d1377b7a4eeac38fede764
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-10 00:47:45 +0000

    Fixed Rollout v152 bug

--- lib/rollout/version/Flex_Rollout_Version_000152.php
+++ lib/rollout/version/Flex_Rollout_Version_000152.php
@@ -21,11 +21,11 @@ class Flex_Rollout_Version_000152 extends Flex_Rollout_Version
 		// 1:	Add the delivery_method Table
 		$strSQL =	"CREATE TABLE delivery_method " .
 					"(" .
-					"	id				BIGINT(20)		UNSIGNED	NOT NULL	COMMENT 'Unique Identifier', " .
-					"	name			VARCHAR(255)				NOT NULL	COMMENT 'Name of the Delivery Method', " .
-					"	description		VARCHAR(1024)				NOT NULL	COMMENT 'Description of the Delivery Method', " .
-					"	const_name		VARCHAR(512)				NOT NULL	COMMENT 'Constant Name for this Delivery Method', " .
-					"	account_setting	TINYINT(1)					NOT NULL	COMMENT '1: Can be used as an Account setting; 0: System-only setting', " .
+					"	id				BIGINT(20)		UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT 'Unique Identifier', " .
+					"	name			VARCHAR(255)				NOT NULL					COMMENT 'Name of the Delivery Method', " .
+					"	description		VARCHAR(1024)				NOT NULL					COMMENT 'Description of the Delivery Method', " .
+					"	const_name		VARCHAR(512)				NOT NULL					COMMENT 'Constant Name for this Delivery Method', " .
+					"	account_setting	TINYINT(1)					NOT NULL					COMMENT '1: Can be used as an Account setting; 0: System-only setting', " .
 					"	" .
 					"	CONSTRAINT	pk_delivery_method_id	PRIMARY KEY (id)" .
 					") ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
@@ -52,12 +52,12 @@ class Flex_Rollout_Version_000152 extends Flex_Rollout_Version
 		// 3:	Add the customer_group_delivery_method Table
 		$strSQL =	"CREATE TABLE customer_group_delivery_method " .
 					"(" .
-					"	id						BIGINT(20)		UNSIGNED	NOT NULL							COMMENT 'Unique Identifier', " .
-					"	customer_group_id		BIGINT(20)					NOT NULL							COMMENT '(FK) Customer Group', " .
-					"	delivery_method_id		BIGINT(20)		UNSIGNED	NOT NULL							COMMENT '(FK) Delivery Method', " .
-					"	minimum_invoice_value	DECIMAL(13, 4)				NOT NULL							COMMENT 'Minimum Invoice value for this Delivery Method to apply', " .
-					"	employee_id				BIGINT(20)		UNSIGNED	NOT NULL							COMMENT '(FK) Employee who defined this setting', " .
-					"	created_on				TIMESTAMP					NOT NULL DEFAULT CURRENT_TIMESTAMP	COMMENT 'Creation Timestamp', " .
+					"	id						BIGINT(20)		UNSIGNED	NOT NULL	AUTO_INCREMENT				COMMENT 'Unique Identifier', " .
+					"	customer_group_id		BIGINT(20)					NOT NULL								COMMENT '(FK) Customer Group', " .
+					"	delivery_method_id		BIGINT(20)		UNSIGNED	NOT NULL								COMMENT '(FK) Delivery Method', " .
+					"	minimum_invoice_value	DECIMAL(13, 4)				NOT NULL								COMMENT 'Minimum Invoice value for this Delivery Method to apply', " .
+					"	employee_id				BIGINT(20)		UNSIGNED	NOT NULL								COMMENT '(FK) Employee who defined this setting', " .
+					"	created_on				TIMESTAMP					NOT NULL	DEFAULT CURRENT_TIMESTAMP	COMMENT 'Creation Timestamp', " .
 					"	" .
 					"	CONSTRAINT	pk_customer_group_delivery_method_id					PRIMARY KEY (id), " .
 					"	CONSTRAINT	fk_customer_group_delivery_method_customer_group_id		FOREIGN KEY (customer_group_id)		REFERENCES CustomerGroup(Id)	ON UPDATE CASCADE ON DELETE CASCADE, " .
