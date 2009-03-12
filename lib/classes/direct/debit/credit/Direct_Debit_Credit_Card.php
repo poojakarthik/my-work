ee0ce7a71d47a0e9937d6b0e7a1c8a7be8453c5f
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-12 06:24:01 +0000

    Added direct_debit* Classes

---
+++ lib/classes/direct/debit/credit/Direct_Debit_Credit_Card.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Direct_Debit_Credit_Card
+ *
+ * Models a record of the direct_debit_credit_card table
+ *
+ * @class	Direct_Debit_Credit_Card
+ */
+class Direct_Debit_Credit_Card extends ORM
+{
+	protected			$_strTableName				= "direct_debit_credit_card";
+	protected static	$_strStaticTableName		= "direct_debit_credit_card";
+	
+	/**
+	 * __construct()
+	 *
+	 * constructor
+	 *
+	 * @param	array	$arrProperties 		[optional]	Associative array defining the class with keys for each field of the table
+	 * @param	boolean	$bolLoadById		[optional]	Automatically load the object with the passed Id
+	 * 
+	 * @return	void
+	 * 
+	 * @constructor
+	 */
+	public function __construct($arrProperties=Array(), $bolLoadById=FALSE)
+	{
+		// Parent constructor
+		parent::__construct($arrProperties, $bolLoadById);
+	}
+	
+	/**
+	 * _preparedStatement()
+	 *
+	 * Access a Static Cache of Prepared Statements used by this Class
+	 * 
+	 * @param	string		$strStatement						Name of the statement
+	 * 
+	 * @return	Statement										The requested Statement
+	 *
+	 * @method
+	 */
+	protected static function _preparedStatement($strStatement)
+	{
+		static	$arrPreparedStatements	= Array();
+		if (isset($arrPreparedStatements[$strStatement]))
+		{
+			return $arrPreparedStatements[$strStatement];
+		}
+		else
+		{
+			switch ($strStatement)
+			{
+				// SELECTS
+				case 'selById':
+					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "id = <Id>", NULL, 1);
+					break;
+				
+				// INSERTS
+				case 'insSelf':
+					$arrPreparedStatements[$strStatement]	= new StatementInsert(self::$_strStaticTableName);
+					break;
+				
+				// UPDATE BY IDS
+				case 'ubiSelf':
+					$arrPreparedStatements[$strStatement]	= new StatementUpdateById(self::$_strStaticTableName);
+					break;
+				
+				// UPDATES
+				
+				default:
+					throw new Exception(__CLASS__."::{$strStatement} does not exist!");
+			}
+			return $arrPreparedStatements[$strStatement];
+		}
+	}
+}
+?>
