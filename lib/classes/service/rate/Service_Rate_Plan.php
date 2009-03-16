22ffce2e999f9342c06fd8c6eb7c63e1c1aa6c84
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-16 07:53:01 +0000

    Locked Plan script now supports actual calcuation of the contract payout

--- lib/classes/service/rate/Service_Rate_Plan.php
+++ lib/classes/service/rate/Service_Rate_Plan.php
@@ -87,6 +87,7 @@ class Service_Rate_Plan extends ORM
 		$fltTotalPayout		= 0.0;
 		
 		$intInvoiceCount	= $this->getInvoiceCount();
+		throw new Exception("Invoice Count: {$intInvoiceCount}");
 		
 		$intContractMonthsRemaining	= $this->contractMonthsRemaining($bolInclusive);
 		
