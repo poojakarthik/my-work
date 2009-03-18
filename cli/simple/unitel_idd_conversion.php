78031aed894c18d87cf2144efcef609d643719c2
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-18 06:14:50 +0000

    added a script which does best matching on Unitel Rate Ids

---
+++ cli/simple/unitel_idd_conversion.php
@@ -0,0 +1,75 @@
+<?php
+
+// Framework
+require_once("../../flex.require.php");
+
+$arrWordFilter	= array('to', 'and', '&', '-', 'is', 'offnet', 'onnet', 'off-net', 'on-net', 'off', 'on', 'net', 'telstra', 'mobile');
+
+$strInFile		= "/home/rdavis/unitel_rate_ids.csv";
+$strOutFile		= "/home/rdavis/unitel_rate_ids_suggestions.csv";
+
+$resInputFile	= fopen($strInFile, 'r');
+$resOutputFile	= fopen($strOutFile, 'w');
+
+// Get Destinations
+$resDestinations	= $qryQuery->Execute("SELECT * FROM Destination WHERE Context = 1");
+if ($resDestinations === false)
+{
+	throw new Exception($qryQuery->Error());
+}
+$arrDestinations	= array();
+while ($arrDestination = $resDestinations->fetch_assoc())
+{
+	$arrDestination['fixed_description']		= trim(strtolower($arrDestination['Description']));
+	$arrDestinations[$arrDestination['Code']]	= $arrDestination;
+}
+
+// Parse the Input File
+while ($arrLine = fgetcsv($resInputFile))
+{
+	if (!(int)$arrLine[0])
+	{
+		// Non-Data Row
+		continue;
+	}
+	
+	// Check for an exact match
+	if ($mixFlexCode = array_search(trim(strtolower($arrLine[1])), $arrDestinations))
+	{
+		// Found an exact match
+		$arrLine[]		= $mixFlexCode.':'.$arrDestinations[$mixFlexCode]['Description'];
+	}
+	else
+	{
+		// Filter out any useless words that will just give us junk matches
+		$arrKeywords	= explode(' ', $arrLine[1]);
+		
+		// Attempt to match
+		$arrMatches	= array();
+		foreach ($arrDestinations as $mixFlexCode=>$arrFlexDestination)
+		{
+			foreach ($arrKeywords as $mixIndex=>$strWord)
+			{
+				if (!in_array(strtolower($strWord), $arrWordFilter) && stripos($arrDestination['fixed_description'], $strWord) >= 0)
+				{
+					$arrMatches[$mixFlexCode]	= (array_key_exists($strWord, $arrMatches)) ? $arrMatches[$strWord] + 1 : 1;
+				}
+			}
+		}
+		
+		// Order Matches by match count, then add to output file
+		sort($arrMatches, SORT_NUMERIC);
+		foreach ($arrMatches as $mixFlexCode=>$intMatchCount)
+		{
+			$arrLine[]		= $mixFlexCode.':'.$arrDestinations[$mixFlexCode]['Description'];
+		}
+	}
+	
+	// Write the modified line to the Output File
+	fwrite($resOutputFile, implode(',', $arrLine)."\n");
+}
+
+// Cleanup
+fclose($resInputFile);
+fclose($resOutputFile);
+?>
