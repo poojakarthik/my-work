f915e2c4fbe8e2ca7ca811aca5a0aa49aa71fb76
Rich Davis <rdavis@yellowbilling.com.au>
2009-03-31 08:17:07 +0000

    Work on Action Type interface

--- html/ui/javascript/flex_constant.js
+++ html/ui/javascript/flex_constant.js
@@ -12,7 +12,8 @@ var Flex_Constant	= Class.create
 	// Loads a Constant Group (or array of Constant Groups) from Flex using AJAX
 	loadConstantGroup	: function(mixConstantGroup, fncCallback)
 	{
-		alert(mixConstantGroup);
+		alert(mixConstantGroup.toSource());
+		alert(mixConstantGroup.toArray().toSource());
 		
 		var arrConstantGroups	= new Array();
 		if (typeof mixConstantGroup == 'array')
