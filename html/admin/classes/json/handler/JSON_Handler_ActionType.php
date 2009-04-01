8fbabba5d775085c1b71df660117d0e30528f4d3
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-01 01:25:17 +0000

    Work on Action Type interface

--- html/admin/classes/json/handler/JSON_Handler_ActionType.php
+++ html/admin/classes/json/handler/JSON_Handler_ActionType.php
@@ -39,5 +39,64 @@ class JSON_Handler_ActionType extends JSON_Handler
 						);
 		}
 	}
+	
+	public function save($objActionTypeStdClass)
+	{
+		try
+		{
+			if ((int)$objActionTypeStdClass->id)
+			{
+				// Edit
+				$objActionType										= Action_Type::getForId((int)$objActionTypeStdClass->id);
+				$objActionType->description							= $objActionTypeStdClass->description;
+				$objActionType->action_type_detail_requirement_id	= $objActionTypeStdClass->action_type_detail_requirement_id;
+				$objActionType->active_status_id					= $objActionTypeStdClass->active_status_id;
+				$objActionType->save();
+			}
+			elseif (Action_Type::getForName($objActionTypeStdClass->name))
+			{
+				// Name is not unique
+				throw new Exception("The Name '{$objActionTypeStdClass->name}' is already in use.  Please use another.");
+			}
+			else
+			{
+				// New
+				$objActionType										= new Action_Type();
+				$objActionType->name								= $objActionTypeStdClass->name;
+				$objActionType->description							= $objActionTypeStdClass->description;
+				$objActionType->action_type_detail_requirement_id	= $objActionTypeStdClass->action_type_detail_requirement_id;
+				$objActionType->is_automatic_only					= 0;
+				$objActionType->is_system							= 0;
+				$objActionType->active_status_id					= ACTIVE_STATUS_ACTIVE;
+				$objActionType->save();
+				
+				foreach ($objActionType->arrAssociationTypes as $intAssociationType)
+				{
+					$objActionTypeActionAssociationType								= new Action_TypeActionAssociationType();
+					$objActionTypeActionAssociationType->action_type_id				= $objActionType->id;
+					$objActionTypeActionAssociationType->action_association_type_id	= (int)$intAssociationType;
+					$objActionTypeActionAssociationType->save();
+				}
+			}
+			
+			// If no exceptions were thrown, then everything worked
+			return array(
+							"Success"			=> true,
+							"intActionTypeId"	=> $objActionType->id,
+							"strDebug"			=> (AuthenticatedUser()->UserHasPerm(PERMISSION_PROPER_GOD)) ? $this->_JSONDebug : ''
+						);
+		}
+		catch (Exception $e)
+		{
+			// Send an Email to Devs
+			//SendEmail("rdavis@yellowbilling.com.au", "Exception in ".__CLASS__, $e->__toString(), CUSTOMER_URL_NAME.'.errors@yellowbilling.com.au');
+			
+			return array(
+							"Success"	=> false,
+							"Message"	=> 'ERROR: '.$e->getMessage(),
+							"strDebug"	=> (AuthenticatedUser()->UserHasPerm(PERMISSION_PROPER_GOD)) ? $this->_JSONDebug : ''
+						);
+		}
+	}
 }
 ?>
