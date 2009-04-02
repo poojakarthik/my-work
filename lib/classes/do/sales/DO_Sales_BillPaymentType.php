1af76a18a4eec798516088c191ab7f8c0974a58d
Joel Dawkins <jdawkins@yellowbilling.com.au>
2009-04-02 05:26:03 +0000

    * Added constants to this class, relating to the records of the bill_payment_type

--- lib/classes/do/sales/DO_Sales_BillPaymentType.php
+++ lib/classes/do/sales/DO_Sales_BillPaymentType.php
@@ -2,6 +2,10 @@
 
 class DO_Sales_BillPaymentType extends DO_Sales_Base_BillPaymentType
 {
+	// These constants should directly relate to the records of this table
+	const ACCOUNT		= 1;
+	const DIRECT_DEBIT	= 2;
+	
 	static function listAll()
 	{
 		$ds = self::getPropertyDataSourceMappings();
