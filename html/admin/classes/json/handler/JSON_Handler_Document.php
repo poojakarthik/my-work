55e63436823f8e65a2808e58a8c3b999f482199f
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-08 05:15:37 +0000

    Fixed Document compression bugs

--- html/admin/classes/json/handler/JSON_Handler_Document.php
+++ html/admin/classes/json/handler/JSON_Handler_Document.php
@@ -155,6 +155,7 @@ class JSON_Handler_Document extends JSON_Handler
 			{
 				$objChild			= new Document($arrChild);
 				$objChildContent	= $objChild->getContentDetails();
+				throw new Exception($objChildContent->toArray());
 				throw new Exception("Trying to add '{$objChildContent->name}'...");
 				
 				// Hide system documents from general users
