7c74d62990b4210fd1745fbc5c37a42dd6dae366
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-08 05:23:08 +0000

    Fixed Document compression bugs

--- lib/classes/document/Document_Content.php
+++ lib/classes/document/Document_Content.php
@@ -44,6 +44,8 @@ class Document_Content extends ORM
 		
 		$this->_arrProperties['uncompressed_file_size']	= ($this->content === null) ? null : strlen($this->content);
 		$this->intContentSize	= $this->uncompressed_file_size;
+		
+		throw new Exception($this->_arrProperties);
 	}
 	
 	/**
