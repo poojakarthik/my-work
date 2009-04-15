473e34b5e26e905a1057f65e3c221c65c4750537
Mark Sergeant <msergeant@yellowbilling.com.au>
2009-04-15 08:14:26 +0000

    * Updates for MOTD.

--- html/admin/classes/htmltemplate/HtmlTemplate_Console.php
+++ html/admin/classes/htmltemplate/HtmlTemplate_Console.php
@@ -14,31 +14,31 @@ class HtmlTemplate_Console extends FlexHtmlTemplate
 		{
 			// There is a daily message to display
 			$strTimestamp	= date("M jS, Y H:i:s", strtotime($arrMessage['Timestamp']));
-			$strMessage		= str_replace("\n", "", $arrMessage['Message']);
+			$strMessage		= $arrMessage['Message'];
 			
-			$strDailyMessageSection = "
-<div id='DailyMessageContainer'>
-	<table class='reflex'>
+			$strDailyMessageSection = '
+<div id="DailyMessageContainer">
+	<table class="reflex">
 		<tbody>
 			<thead>
-				<th>Message of the day.</th>
+				<th>Message of the day</th>
 			</thead>
 			<tfoot>
-				<th>Last updated: $strTimestamp</th>
+				<th>Last updated: ' . $strTimestamp . '</th>
 			</tfoot>
-			<tr class='alt'>
+			<tr class="alt">
 				<td>&nbsp;</td>
 			</tr>
-			<tr class='alt'>
-				<td>$strMessage</td>
+			<tr class="alt">
+				<td>' . $strMessage . '</td>
 			</tr>
-			<tr class='alt'>
+			<tr class="alt">
 				<td>&nbsp;</td>
 			</tr>
 		</tbody>
 	</table>
-</form>
-";
+</div>
+';
 		}
 		else
 		{
