3560dddb0c7a5edb93b5d35c78b1f3fe4ea40b4b
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-17 03:50:12 +0000

    Removed 'Add Associated Accounts' menu item

--- html/admin/html_template/contact/options.php
+++ html/admin/html_template/contact/options.php
@@ -77,7 +77,7 @@ class HtmlTemplateContactOptions extends HtmlTemplate
 		$strViewContactNotesLink = Href()->ViewContactNotes(DBO()->Contact->Id->Value);
 		echo "<li><a href='$strViewContactNotesLink'>View Contact Notes</a></li>\n";
 		$strAddAssociateAccountLink = Href()->AddAssociatedAccount(DBO()->Contact->Account->Value);
-		echo "<li><a href='$strAddAssociateAccountLink'>Add Associated Account</a></li>\n";
+		//echo "<li><a href='$strAddAssociateAccountLink'>Add Associated Account</a></li>\n";
 
 		echo "</div>\n";
 		echo "<div class='Seperator'></div>\n";
