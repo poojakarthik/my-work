4d531f5eadaa03d3a228e91279d5953c200598e1
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-17 07:31:32 +0000

    Added some classes which make RPC calls easier

--- cli/simple/rpc_class_test.php
+++ cli/simple/rpc_class_test.php
@@ -7,7 +7,7 @@ Flex::load();
 $objRPCSalesPortal	= new RPC_Client_SalesPortal('https://sp.telcoblue.yellowbilling.com.au/sales/format:json/portal/');
 
 // Login
-$objResponse	= $objRPCSalesPortal->call('jsonLogin', array('rich', 'rich'), 'json');
+$objResponse	= $objRPCSalesPortal->call('jsonLogin', array('rich', 'rich'), 'login');
 if ($objResponse !== true)
 {
 	throw new Exception("Login failed: \n\n".print_r($objResponse, true));
