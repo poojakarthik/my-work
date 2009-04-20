3ac38a1f17bbe1d9bf8c8f736fb5f804400035a7
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-20 02:38:57 +0000

    Added Rollout v169 which changes how address details are stored in Flex

--- lib/rollout/version/Flex_Rollout_Version_000169.php
+++ lib/rollout/version/Flex_Rollout_Version_000169.php
@@ -30,13 +30,13 @@ class Flex_Rollout_Version_000169 extends Flex_Rollout_Version
 		{
 			throw new Exception(__CLASS__ . ' Failed to remove country.description, change code to code_3_char, and add code_2_char Fields. ' . $result->getMessage() . " (DB Error: " . $result->getUserInfo() . ")");
 		}
+		$this->rollbackSQL[] =	"	UPDATE	country
+									SET		description = name
+									WHERE	1;";
 		$this->rollbackSQL[] =	"	ALTER TABLE	country
 										ADD		description				VARCHAR(255)	NOT NULL	COMMENT 'Description of the Country',
 										CHANGE	code_3_char	code		VARCHAR(255)	NOT NULL	COMMENT 'Abbreviation of the Country\'s name',
 										DROP	code_2_char;";
-		$this->rollbackSQL[] =	"	UPDATE	country
-									SET		description = name
-									WHERE	1;";
 		
 		//	2:	Populate country.code_2_char Field
 		$strSQL = "	UPDATE	country
