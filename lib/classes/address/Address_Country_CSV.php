89985eda067e292e17fc5be274458a06202ad056
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-21 08:04:16 +0000

    work on country CSV importing

---
+++ lib/classes/address/Address_Country_CSV.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Address_Country_CSV
+ *
+ * Models a Locality CSV Database Record
+ *
+ * @class	Address_Country_CSV
+ */
+class Address_Country_CSV
+{
+	const	GET_MODE_BEFORE_REVISION	= 1;
+	const	GET_MODE_TO_REVISION		= 2;
+	const	GET_MODE_FOR_REVISION		= 3;
+	const	GET_MODE_AFTER_REVISION		= 4;
+	
+	const	DATABASE_FLEX_PATH			= 'files/rollout/import/country/';
+	
+	protected static	$_arrDatabaseCache;
+	
+	protected static	$_arrImportColumns	=	array
+												(
+													'name'					=> 0,
+													'code_2_char'			=> 1,
+													'code_3_char'			=> 2,
+													'has_postcode'			=> 3,
+													'Flex_Rollout_Version'	=> 4
+												);
+	
+	protected	$_strName;
+	protected	$_strCode2Char;
+	protected	$_strCode3Char;
+	protected	$_intHasPostcode;
+	
+	/**
+	 * __construct()
+	 *
+	 * Constructor
+	 * 
+	 * @constructor
+	 */
+	protected function __construct($strName, $strCode2Char, $strCode3Char, $intHasPostcode)
+	{
+		$this->_strName			= $strName;
+		$this->_strCode2Char	= $strCode2Char;
+		$this->_strCode3Char	= $strCode3Char;
+		$this->_intHasPostcode	= (int)$intHasPostcode;
+	}
+	
+	/**
+	 * getAll()
+	 *
+	 * constructor
+	 *
+	 * @param	array	$arrProperties 		[optional]	Associative array defining the class with keys for each field of the table
+	 * @param	boolean	$bolLoadById		[optional]	Automatically load the object with the passed Id
+	 * 
+	 * @return	void
+	 * 
+	 * @constructor
+	 */
+	public static function getAll($intGetMode=null, $intRolloutVersion=null)
+	{
+		$this->_load();
+		
+		$arrCountries	= array();
+		foreach (self::$_arrDatabaseCache as $intCacheRollout=>$arrCountries)
+		{
+			if ((!$intGetMode || !$intRolloutVersion) || 
+				($intGetMode == self::GET_MODE_BEFORE_REVISION && $intCacheRollout < $intRolloutVersion) || 
+				($intGetMode == self::GET_MODE_TO_REVISION && $intCacheRollout <= $intRolloutVersion) || 
+				($intGetMode == self::GET_MODE_FOR_REVISION && $intCacheRollout == $intRolloutVersion) || 
+				($intGetMode == self::GET_MODE_AFTER_REVISION && $intCacheRollout > $intRolloutVersion))
+			{
+				foreach ($arrCountries as $arrCountry)
+				{
+					$arrCountries[]	= $arrCountry;
+				}
+			}
+		}
+		return $arrCountries;
+	}
+	
+	/**
+	 * load()
+	 *
+	 * Loads the Country CSV Database
+	 * 
+	 * @return	void
+	 * 
+	 * @constructor
+	 */
+	protected static function _load()
+	{
+		// Cache the CSV Database in memory if possible
+		if (!self::$_arrDatabaseCache)
+		{
+			$strDatabasePath	= Flex::getBase().self::DATABASE_FLEX_PATH.'country.csv';
+			$strErrorTail		= "for Country '{$objAddressCountryCSV->strName}' at path '{$strDatabasePath}'";
+			
+			// Load the CSV Database
+			if (!$resInputFile = @fopen($strDatabasePath, 'r'))
+			{
+				throw new Exception("Unable to load Countries {$strErrorTail}: \n\n".print_r(error_get_last(), true));
+			}
+			
+			// Parse Header
+			if (!$arrHeader = fgetcsv($resInputFile))
+			{
+				throw new Exception("Unable to parse File Header {$strErrorTail}");
+				
+				foreach (self::$_arrImportColumns as $strAlias=>$intColumn)
+				{
+					if (!array_key_exists($intColumn, $arrHeader))
+					{
+						throw new Exception("Header column at Index {$intColumn} does not exist {$strErrorTail}");
+					}
+					elseif ($arrHeader[$intColumn] !== $strAlias)
+					{
+						throw new Exception("Header column at Index {$intColumn} expected '{$strAlias}'; found '{$arrHeader[$intColumn]}' {$strErrorTail}");
+					}
+				}
+			}
+			
+			// Parse Data
+			while ($arrData = fgetcsv($resInputFile))
+			{
+				$intRolloutVersion		= (int)self::$_arrImportColumns['Flex_Rollout_Version'];
+				$objAddressLocalityCSV	= new Address_Country_CSV(
+																	$arrData[self::$_arrImportColumns['name']],
+																	$arrData[self::$_arrImportColumns['code_2_char']],
+																	$arrData[self::$_arrImportColumns['code_3_char']],
+																	$arrData[self::$_arrImportColumns['has_postcode']],
+																	$arrData[$intRolloutVersion]
+																);
+				
+				self::$_arrDatabaseCache[$intRolloutVersion]	= (!self::$_arrDatabaseCache[$intRolloutVersion]) ? array() : self::$_arrDatabaseCache[$intRolloutVersion];
+				self::$_arrDatabaseCache[$intRolloutVersion][]	= $objAddressLocalityCSV;
+			}
+		}
+	}
+}
+?>
--- lib/classes/address/Address_Locality_CSV.php
+++ lib/classes/address/Address_Locality_CSV.php
@@ -39,27 +39,13 @@ class Address_Locality_CSV
 	 * 
 	 * @constructor
 	 */
-	protected function __construct($strName, $strPostcode, $mixState)
+	protected function __construct($strName, $strPostcode, Address_State_CSV $objState)
 	{
 		$this->_strName		= $strName;
 		$this->_strPostcode	= $strPostcode;
 		
-		
-		if ($mixState instanceof Address_State_CSV)
-		{
-			// CSV State object
-			$this->_objAddressStateCSV	= $mixState;
-		}
-		elseif ($mixState instanceof State)
-		{
-			// Flex State object
-			$this->_objAddressStateCSV	= $mixState;
-		}
-		else
-		{
-			// Assume Flex State Id -- load from the DB
-			$objState	= State::getForId((int)$mixState);
-		}
+		// CSV State object
+		$this->_objAddressStateCSV	= $objState;
 	}
 	
 	/**
@@ -111,52 +97,6 @@ class Address_Locality_CSV
 			{
 				$objAddressLocalityCSV	= new Address_Locality_CSV();
 			}
-		}
-	}
-	
-	/**
-	 * _preparedStatement()
-	 *
-	 * Access a Static Cache of Prepared Statements used by this Class
-	 * 
-	 * @param	string		$strStatement						Name of the statement
-	 * 
-	 * @return	Statement										The requested Statement
-	 *
-	 * @method
-	 */
-	protected static function _preparedStatement($strStatement)
-	{
-		static	$arrPreparedStatements	= Array();
-		if (isset($arrPreparedStatements[$strStatement]))
-		{
-			return $arrPreparedStatements[$strStatement];
-		}
-		else
-		{
-			switch ($strStatement)
-			{
-				// SELECTS
-				case 'selById':
-					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "id = <Id>", NULL, 1);
-					break;
-				
-				// INSERTS
-				case 'insSelf':
-					$arrPreparedStatements[$strStatement]	= new StatementInsert(self::$_strStaticTableName);
-					break;
-				
-				// UPDATE BY IDS
-				case 'ubiSelf':
-					$arrPreparedStatements[$strStatement]	= new StatementUpdateById(self::$_strStaticTableName);
-					break;
-				
-				// UPDATES
-				
-				default:
-					throw new Exception(__CLASS__."::{$strStatement} does not exist!");
-			}
-			return $arrPreparedStatements[$strStatement];
 		}
 	}
 }
