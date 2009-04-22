e283263a886e36df0943ac2f96ed7a543e80c23a
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-22 00:33:34 +0000

    Added some tests for Country CSV import

--- cli/simple/country_csv_test.php
+++ cli/simple/country_csv_test.php
@@ -5,9 +5,21 @@ require_once('../../lib/classes/Flex.php');
 Flex::load();
 
 // Get all Countrys
-$arrCountries	= Address_Country_CSV::getAll();
-
-echo "\n>>>";
-print_r($arrCountries);
+echo "\n>>ALL COUNTRIES>>>";
+print_r(Address_Country_CSV::getAll());
 echo "<<<\n";
+
+echo "\n>>BEFORE ROLLOUT 80>>>";
+print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_BEFORE_REVISION, 80));
+echo "<<<\n";
+
+echo "\n>>BEFORE ROLLOUT 100>>>";
+print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_BEFORE_REVISION, 100));
+echo "<<<\n";
+
+echo "\n>>BEFORE ROLLOUT 171>>>";
+print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_BEFORE_REVISION, 171));
+echo "<<<\n";
+
+?>
 ?>
