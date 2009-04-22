410ce8adb75cad7e66fb270b45d6fc3e358ae948
Rich Davis <rdavis@yellowbilling.com.au>
2009-04-22 01:05:20 +0000

    Added some tests for Country CSV import

--- cli/simple/country_csv_test.php
+++ cli/simple/country_csv_test.php
@@ -8,7 +8,7 @@ Flex::load();
 echo "\n>>ALL COUNTRIES>>>";
 print_r(Address_Country_CSV::getAll());
 echo "<<<\n";
-
+/*
 echo "\n>>BEFORE ROLLOUT 80>>>";
 print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_BEFORE_REVISION, 80));
 echo "<<<\n";
@@ -21,5 +21,26 @@ echo "\n>>BEFORE ROLLOUT 171>>>";
 print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_BEFORE_REVISION, 171));
 echo "<<<\n";
 
-?>
+echo "\n>>BEFORE ROLLOUT 200>>>";
+print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_BEFORE_REVISION, 200));
+echo "<<<\n";
+*/
+
+echo "\n>>TO ROLLOUT 80>>>";
+print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_TO_REVISION, 80));
+echo "<<<\n";
+
+echo "\n>>TO ROLLOUT 100>>>";
+print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_TO_REVISION, 100));
+echo "<<<\n";
+
+echo "\n>>TO ROLLOUT 171>>>";
+print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_TO_REVISION, 171));
+echo "<<<\n";
+
+echo "\n>>TO ROLLOUT 200>>>";
+print_r(Address_Country_CSV::getAll(Address_Country_CSV::GET_MODE_TO_REVISION, 200));
+echo "<<<\n";
+
+
 ?>
