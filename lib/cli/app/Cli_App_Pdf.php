55c2692fb087efdb5b1b3dbe98b9fba132096765
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-01 08:49:47 +0000

    Cli_App_Pdf can now ignore a list of account numbers

--- lib/cli/app/Cli_App_Pdf.php
+++ lib/cli/app/Cli_App_Pdf.php
@@ -572,10 +572,11 @@ class Cli_App_Pdf extends Cli
 				self::ARG_VALIDATION 	=> 'Cli::_validIsSet()'
 			),
 			
-			SWITCH_IGNORE_ACCOUNTS => array(
-				self::ARG_REQUIRED 	=> FALSE,
-				self::ARG_DESCRIPTION => " if set, ignores a space-delimited list of Accounts (encapsulated in quotes for multiple Accounts)",
-				self::ARG_DEFAULT 	=> FALSE,
+			self::SWITCH_IGNORE_ACCOUNTS => array(
+				self::ARG_LABEL 		=> "IGNORE_ACCOUNTS", 
+				self::ARG_REQUIRED 		=> FALSE,
+				self::ARG_DESCRIPTION 	=> " if set, ignores a space-delimited list of Accounts (encapsulated in quotes for multiple Accounts)",
+				self::ARG_DEFAULT 		=> FALSE,
 				self::ARG_VALIDATION 	=> 'Cli::_validIsSet()'
 			),
 		);
