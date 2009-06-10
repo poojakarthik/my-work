964042ed8880173846be636c01d88599bdbea5d1
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-10 03:02:27 +0000

    updated test suite for Operation-based Permissions

--- html/admin/javascript/developer_operationpermission.js
+++ html/admin/javascript/developer_operationpermission.js
@@ -69,7 +69,7 @@ var Developer_OperationPermission	= Class.create
 // Static Methods
 Developer_OperationPermission._handleResponse	= function(objResponse, fncCallback)
 {
-	alert(arguments);
+	alert(Array.apply(arguments));
 	if (objResponse)
 	{
 		if (objResponse.Success)
