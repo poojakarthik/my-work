91124ffc3aaeec12fb0fe65f129ad383b9c0de0e
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-15 01:41:32 +0000

    Updated JSON pagination stuff

--- html/admin/javascript/developer_datasetpagination.js
+++ html/admin/javascript/developer_datasetpagination.js
@@ -3,7 +3,7 @@ var Developer_DatasetPagination	= Class.create
 	initialize	: function()
 	{
 		// Init Dataset & Pagination
-		this.objDataset		= new Dataset_Ajax(Dataset_Ajax.CACHE_MODE_FULL_CACHING, {strObject: 'JSON_Handler_Employee', strMethod: 'getRecords'});
+		this.objDataset		= new Dataset_Ajax(Dataset_Ajax.CACHE_MODE_FULL_CACHING, {strObject: 'Employee', strMethod: 'getRecords'});
 		this.objPagination	= new Pagination(this._updateTable.bind(this), 30, this.objDataset);
 		
 		// Init Popup	
