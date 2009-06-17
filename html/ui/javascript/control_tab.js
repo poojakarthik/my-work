bd53515b9f2f1f373e0397a5413ffd58ea8488fe
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-17 04:15:52 +0000

    added JS Tab Groups and tests

---
+++ html/ui/javascript/control_tab.js
@@ -0,0 +1,23 @@
+var Control_Tab	= Class.create
+({
+	initialize	: function(strName, objContentDIV)
+	{
+		this.setName(strName);
+		this.setContent(objContentDIV);
+	},
+	
+	getName	: function()
+	{
+		return this.strName;
+	}
+	
+	getContent	: function()
+	{
+		return this.objContentDIV;
+	},
+	
+	setContent	: function(objContentDIV)
+	{
+		this.objContentDIV	= objContentDIV;
+	}
+})
---
+++ html/ui/javascript/control_tab_group.js
@@ -0,0 +1,101 @@
+var Control_Tab_Group	= Class.create
+({
+	initialize	: function(objDIVContainer)
+	{
+		this._arrTabs		= [];
+		
+		// Build
+		this.objDIVContainer			= objDIVContainer;
+		this.objDIVContainer.innerHTML	= '';
+		
+		// Container
+		this.objContainer						= {};
+		this.objContainer.domElement			= objDIVContainer;
+		this.objContainer.domElement.innerHTML	= '';
+		
+		// Tab Row
+		this.objContainer.objTabRow							= {};
+		this.objContainer.objTabRow.domElement				= document.createElement('div');
+		this.objContainer.objTabRow.domElement.className	= '';
+		
+		// Page Container
+		this.objContainer.objPageContainer						= {};
+		this.objContainer.objPageContainer.domElement			= document.createElement('div');
+		this.objContainer.objPageContainer.domElement.className	= '';
+	},
+	
+	addTab		: function(strAlias, objControlTab)
+	{
+		// Check the Alias is valid and unique
+		if (!strAlias.match("/^[a-z\_]\w*$/i") || this.tabExists(strAlias))
+		{
+			throw "A tab with the alias '" + strAlias + "' already exists!";
+		}
+		
+		// Add the Tab to this Group
+		var objPage				= document.createElement('div');
+		objPage.className		= 'page';
+		objPage.style.display	= 'none';
+		this.objContainer.objPageContainer.domElement.appendChild(objPage);
+		
+		var domTabButton		= document.createElement('div');
+		domTabButton.className	= 'tab';
+		domTabButton.innerHTML	= objControlTab.getName().replace("/&/gmi", '&amp;')replace('/"/gmi', '&quot;')replace("/>/gmi", '&gt;')replace("/</gmi", '&lt;');
+		
+		this._arrTabs.push({strAlias: strAlias, domTabButton: domTabButton, objPage: objTabPage, objControlTab: objControlTab});
+		
+		// If this is the first tab, then select it
+		this.switchToTab(strAlias);
+	},
+	
+	switchToTab	: function(mixTab)
+	{
+		var objControlTab	= this.getTab(mixTab);
+		if (objControlTab)
+		{
+			// Set Visibility
+			for (var i = 0; i < this._arrTabs.length; i++)
+			{
+				if (this._arrTabs[i].objControlTab == objControlTab)
+				{
+					// Selected Tab
+					this._arrTabs[i].objPage.style.display	= 'block';
+				}
+				else
+				{
+					// Non-selected Tab
+					this._arrTabs[i].objPage.style.display	= 'none';
+				}
+			}
+		}
+	},
+	
+	getTab	: function(mixTab)
+	{
+		if (mixTab instanceof Control_Tab)
+		{
+			// Control_Tab object
+			return mixTab;
+		}
+		
+		// Is mixTab an alias?
+		for (var i = 0; i < this._arrTabs.length; i++)
+		{
+			if (this._arrTabs[i].strAlias == mixTab)
+			{
+				// Yes -- return the Control_Tab object
+				return this._arrTabs[i].objControlTab;
+			}
+		}
+		
+		// Is mixTab an Index?
+		var intTabIndex	= Number(mixTab);
+		if (intTabIndex >= 0 && intTabIndex < this._arrTabs.length)
+		{
+			// Yes -- return the Control_Tab object
+			return this._arrTabs[intTabIndex].objControlTab;
+		}
+		
+		return false;
+	}
+})
