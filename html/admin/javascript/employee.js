e9b954b05cd9eccaca99efa638a1b5bcd5a0f107
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-21 23:53:38 +0000

    JS Tab Groups can now be embedded in page, or the sole element in a popup

--- html/admin/javascript/employee.js
+++ html/admin/javascript/employee.js
@@ -25,7 +25,7 @@ var Employee	= Class.create
 		// Set properties
 		this.objProperties	= objResponse.objEmployee;
 		
-		alert(Object.inspect(objResponse));
+		alert(Object.inspect(objResponse.objEmployee));
 		
 		// Callback
 		if (fncCallback)
