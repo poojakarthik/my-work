638190467baaa1951a11c2a11cef0f74ae177abb
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-22 23:02:49 +0000

    Crazy ElementGroup-like stuff

--- html/ui/javascript/reflex_validation.js
+++ html/ui/javascript/reflex_validation.js
@@ -14,11 +14,28 @@ var Reflex_Validation	=
 	fnn		: function(strTest)
 	{
 		// Allows for FNNs in the following forms:
-		//	* 0733534816
-		//	* 134585
-		//	* 1300154845
-		//	* 1800648432
-		return (strTest.search(/(13\d{4}|(0[123456789]\d{2}|1[38])\d{6})/) != -1) ? true : false;
+		//	* 0733534816	-- Fixed Line (Land Line)
+		//	* 0406818784	-- Mobile MSN
+		//	* 134585		-- Inbound 13
+		//	* 1300154845	-- Inbound 1300
+		//	* 1800648432	-- Inbound 1800
+		return (Reflex_Validation.fnnFixedLine(strTest) || Reflex_Validation.fnnMobile(strTest) || Reflex_Validation.fnnInbound(strTest));
+		//return (strTest.search(/(13\d{4}|(0[123456789]\d{2}|1[38]00)\d{6})/) != -1) ? true : false;
+	},
+	
+	fnnFixedLine	: function(strTest)
+	{
+		return (strTest.search(/0[12356789]\d{8}/) != -1) ? true : false;
+	},
+	
+	fnnMobile		: function(strTest)
+	{
+		return (strTest.search(/04\d{8}/) != -1) ? true : false;
+	},
+	
+	fnnInbound		: function(strTest)
+	{
+		return (strTest.search(/(13\d{4}|1[38]00\d{6})/) != -1) ? true : false;
 	},
 	
 	digits	: function(strTest)
