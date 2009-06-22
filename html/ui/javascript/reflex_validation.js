1e285ae9e0842241120344a6ee9e1441206eb528
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-22 22:50:27 +0000

    Crazy ElementGroup-like stuff

--- html/ui/javascript/reflex_validation.js
+++ html/ui/javascript/reflex_validation.js
@@ -6,23 +6,23 @@ var Reflex_Validation	=
 		return (strValue.length > 0);
 	},
 	
-	email	: function(strEmail)
+	email	: function(strTest)
 	{
-		return (strEmail.search(/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i) != -1) ? true : false;
+		return (strTest.search(/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i) != -1) ? true : false;
 	},
 	
-	fnn		: function(strFNN)
+	fnn		: function(strTest)
 	{
 		// Allows for FNNs in the following forms:
 		//	* 0733534816
 		//	* 134585
 		//	* 1300154845
 		//	* 1800648432
-		return (strFNN.search(/(13\d{4}|(0[12356789]\d{2}|1[38])\d{6})/) != -1) ? true : false;
+		return (strTest.search(/(13\d{4}|(0[12356789]\d{2}|1[38])\d{6})/) != -1) ? true : false;
 	},
 	
-	digits	: function(strDigits)
+	digits	: function(strTest)
 	{
-		return (strFNN.search(/^\d*$/) != -1) ? true : false;
+		return (strTest.search(/^\d*$/) != -1) ? true : false;
 	}
 };
