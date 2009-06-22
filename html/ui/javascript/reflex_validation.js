82a26a23796724540c58bd593f3decf66f887b38
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-22 23:07:24 +0000

    Crazy ElementGroup-like stuff

--- html/ui/javascript/reflex_validation.js
+++ html/ui/javascript/reflex_validation.js
@@ -8,7 +8,7 @@ var Reflex_Validation	=
 	
 	email	: function(strTest)
 	{
-		return (strTest.search(/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i) != -1) ? true : false;
+		return (strTest.search(/^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i) != -1) ? true : false;
 	},
 	
 	fnn		: function(strTest)
@@ -20,22 +20,22 @@ var Reflex_Validation	=
 		//	* 1300154845	-- Inbound 1300
 		//	* 1800648432	-- Inbound 1800
 		return (Reflex_Validation.fnnFixedLine(strTest) || Reflex_Validation.fnnMobile(strTest) || Reflex_Validation.fnnInbound(strTest));
-		//return (strTest.search(/(13\d{4}|(0[123456789]\d{2}|1[38]00)\d{6})/) != -1) ? true : false;
+		//return (strTest.search(/^(13\d{4}|(0[123456789]\d{2}|1[38]00)\d{6})$/) != -1) ? true : false;
 	},
 	
 	fnnFixedLine	: function(strTest)
 	{
-		return (strTest.search(/0[12356789]\d{8}/) != -1) ? true : false;
+		return (strTest.search(/^0[12356789]\d{8}$/) != -1) ? true : false;
 	},
 	
 	fnnMobile		: function(strTest)
 	{
-		return (strTest.search(/04\d{8}/) != -1) ? true : false;
+		return (strTest.search(/^04\d{8}$/) != -1) ? true : false;
 	},
 	
 	fnnInbound		: function(strTest)
 	{
-		return (strTest.search(/(13\d{4}|1[38]00\d{6})/) != -1) ? true : false;
+		return (strTest.search(/^(13\d{4}|1[38]00\d{6})$/) != -1) ? true : false;
 	},
 	
 	digits	: function(strTest)
