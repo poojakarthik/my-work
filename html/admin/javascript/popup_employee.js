b51b909d08d2d31fe4089a569a974015ababe463
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-22 07:33:26 +0000

    Crazy ElementGroup-like stuff

--- html/admin/javascript/popup_employee.js
+++ html/admin/javascript/popup_employee.js
@@ -217,11 +217,11 @@ var Popup_Employee	= Class.create(Reflex_Popup,
 				break;
 		}
 		
-		for (i in this._objPage.table.tbody)
+		for (i in this.objControlTabGroup.getTab('Details').table.tbody)
 		{
 			if (i != 'domElement')
 			{
-				this._objPage.table.tbody[i].objControl.setRenderMode(bolControlMode);
+				this.objControlTabGroup.getTab('Details').table.tbody[i].objControl.setRenderMode(bolControlMode);
 			}
 		}
 	},
