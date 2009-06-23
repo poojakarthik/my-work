bacbcf9d4af9811fd88b9e181813ca3031502ed1
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-23 03:08:13 +0000

    Crazy ElementGroup-like stuff

--- html/ui/javascript/control_field_date_picker.js
+++ html/ui/javascript/control_field_date_picker.js
@@ -28,7 +28,7 @@ var Control_Field_Date_Picker	= Class.create(/* extends */ Control_Field,
 		this.objControlOutput.domEdit.appendChild(this.objControlOutput.domIcon);
 		
 		var objDate	= new Date();
-		this.objDatePicker	= DateChooser.factory(this.domHidden.id, Control_Field_Date_Picker.YEAR_START, Control_Field_Date_Picker.YEAR_END, Control_Field_Date_Picker.DATE_FORMAT, false, true, true, objDate.getFullYear(), objDate.getMonth(), objDate.getDay());
+		this.objDatePicker	= DateChooser.factory(this.objControlOutput.domHidden.id, Control_Field_Date_Picker.YEAR_START, Control_Field_Date_Picker.YEAR_END, Control_Field_Date_Picker.DATE_FORMAT, false, true, true, objDate.getFullYear(), objDate.getMonth(), objDate.getDay());
 		
 		this.objControlOutput.domView		= document.createElement('span');
 		this.objControlOutput.domElement.appendChild(this.objControlOutput.domView);
