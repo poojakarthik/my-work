1c282347a817341d08d762d506e7c69394e2afd1
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-24 23:14:15 +0000

    transaction test

--- html/admin/classes/json/handler/JSON_Handler_ActionsAndNotes.php
+++ html/admin/classes/json/handler/JSON_Handler_ActionsAndNotes.php
@@ -134,7 +134,8 @@ class JSON_Handler_ActionsAndNotes extends JSON_Handler
 		{
 			TransactionRollback();
 			return array(	"success"		=> false,
-							"errorMessage"	=> $e->getMessage()
+							/*"errorMessage"	=> $e->getMessage()*/
+							"errorMessage"	=> $e->getMessage($this->_JSONDebug)
 						);
 		}
 	}
