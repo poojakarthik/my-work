b0e65f72d55c58f4182973a1bc884de16a7d35d0
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-25 05:00:32 +0000

    Added ability to get a list of User Roles via AJAX

---
+++ html/ui/javascript/control_treeview_multicolumn.js
@@ -0,0 +1,20 @@
+var Control_Treeview_Multicolumn	= Class.create
+({
+	initialize	: function(oColumnDefinition)
+	{
+		this.oTable				= {};
+		this.oTable.domElement	= document.createElement('table');
+		this.oTable.domElement.addClassName('treeview');
+		this.domElement.appendChild(this.oContainer.oTable.domElement);
+	},
+	
+	getElement	: function()
+	{
+		return this.oTable.domElement;
+	}
+});
+
+Control_Treeview_Multicolumn.factory	= function(oColumnDefinition)
+{
+	return new Control_Treeview_Multicolumn(oColumnDefinition);
+};
