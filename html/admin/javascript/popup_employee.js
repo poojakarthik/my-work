7ffa551d0c7dc93be2e31cb1f8598ebf35e5f838
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-25 05:04:19 +0000

    Added a Multicolumn Treeview Control

--- html/admin/javascript/popup_employee.js
+++ html/admin/javascript/popup_employee.js
@@ -205,6 +205,15 @@ var Popup_Employee	= Class.create(Reflex_Popup,
 		objTabPage.domElement	= document.createElement('div');
 		objTabPage.domElement.innerHTML	= '[ Permissions ]';
 		
+		// Available Profiles
+		objTabPage.objAvailableProfiles				= {};
+		objTabPage.objAvailableProfiles.domElement	= document.createElement('div');
+		objTabPage.objAvailableProfiles.domElement.addClassName('treeview');
+		objTabPage.domElement.appendChild(objTabPage.objAvailableProfiles.domElement);
+		
+		objTabPage.objAvailableProfiles.objTreeview.objControl	= new Control_Treeview_Multicolumn();
+		objTabPage.objAvailableProfiles.domElement.appendChild(objTabPage.objAvailableProfiles.objTreeview.objControl.getElement());
+		
 		return objTabPage;
 	},
 	
