dd4cb90bfa90632039dbdfe503b55c1281b19b16
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-25 05:00:19 +0000

    Added ability to get a list of User Roles via AJAX

--- html/admin/html_template/employee/view.php
+++ html/admin/html_template/employee/view.php
@@ -63,6 +63,9 @@ class HtmlTemplateEmployeeView extends HtmlTemplate
 		$this->LoadJavascript("control_tab_group");
 		$this->LoadJavascript("control_tab");
 		
+		$this->LoadJavascript("control_treeview_multicolumn");
+		$this->LoadJavascript("control_treeview_multicolumn_node");
+		
 		$this->LoadJavascript("date_time_picker_dynamic");
 		
 		$this->LoadJavascript("control_field");
