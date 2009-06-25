ef2261dcc74a468c6e611255d3711a3c9be6a395
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-25 00:57:15 +0000

    Added ability to get a list of User Roles via AJAX

---
+++ html/admin/classes/json/handler/user/JSON_Handler_User_Role.php
@@ -0,0 +1,90 @@
+<?php
+
+class JSON_Handler_User_Role extends JSON_Handler
+{
+	protected	$_JSONDebug	= '';
+		
+	public function __construct()
+	{
+		// Send Log output to a debug string
+		Log::registerLog('JSON_Handler_Debug', Log::LOG_TYPE_STRING, $this->_JSONDebug);
+		Log::setDefaultLog('JSON_Handler_Debug');
+	}
+	
+	public function getDataset($bolCountOnly=false, $intLimit=0, $intOffset=0)
+	{
+		try
+		{
+			if ($bolCountOnly)
+			{
+				// Count Only
+				return array(
+								"Success"			=> true,
+								"intRecordCount"	=> self::_getDatasetLength(),
+								"strDebug"			=> (AuthenticatedUser()->UserHasPerm(PERMISSION_PROPER_GOD)) ? $this->_JSONDebug : ''
+							);
+			}
+			else
+			{
+				// Include Data
+				$intLimit	= (max($intLimit, 0) == 0) ? null : (int)$intLimit;
+				$intOffset	= ($intLimit === null) ? null : max((int)$intOffset, 0);
+				
+				$qryQuery	= new Query();
+				
+				// Retrieve list of Employees
+				$strEmployeeSQL	= "SELECT * FROM Employee WHERE 1";
+				$strEmployeeSQL	.= ($intLimit !== null) ? " LIMIT {$intLimit} OFFSET {$intOffset}" : '';
+				$resEmployees	= $qryQuery->Execute($strEmployeeSQL);
+				if ($resEmployees === false)
+				{
+					throw new Exception($qryQuery->Error());
+				}
+				$arrEmployees	= array();
+				$intCount		= 0;
+				while ($arrEmployee = $resEmployees->fetch_assoc())
+				{
+					$arrEmployees[$intCount+$intOffset]	= $arrEmployee;
+					$intCount++;
+				}
+				
+				// If no exceptions were thrown, then everything worked
+				return array(
+								"Success"			=> true,
+								"arrRecords"		=> $arrEmployees,
+								"intRecordCount"	=> ($intLimit === null) ? count($arrEmployees) : self::_getDatasetLength(),
+								"strDebug"			=> (AuthenticatedUser()->UserHasPerm(PERMISSION_PROPER_GOD)) ? $this->_JSONDebug : ''
+							);
+			}
+		}
+		catch (Exception $e)
+		{
+			// Send an Email to Devs
+			//SendEmail("rdavis@yellowbilling.com.au", "Exception in ".__CLASS__, $e->__toString(), CUSTOMER_URL_NAME.'.errors@yellowbilling.com.au');
+			
+			return array(
+							"Success"	=> false,
+							"Message"	=> 'ERROR: '.$e->getMessage(),
+							"strDebug"	=> (AuthenticatedUser()->UserHasPerm(PERMISSION_PROPER_GOD)) ? $this->_JSONDebug : ''
+						);
+		}
+	}
+	
+	private static function _getDatasetLength()
+	{
+		$qryQuery	= new Query();
+		
+		// Retrieve COUNT() of Employees
+		$strCountSQL	= "SELECT COUNT(Id) AS employee_count FROM Employee WHERE 1";
+		$resCount		= $qryQuery->Execute($strCountSQL);
+		if ($resCount === false)
+		{
+			throw new Exception($qryQuery->Error());
+		}
+		if ($arrCount = $resCount->fetch_assoc())
+		{
+			return $arrCount['employee_count'];
+		}
+	}
+}
+?>
---
+++ html/admin/javascript/user_role.js
@@ -0,0 +1,74 @@
+var User_Role	= Class.create
+({
+	initialize	: function(iId, fCallback)
+	{
+		if (iId)
+		{
+			// Load via JSON
+			this.iId	= iId;
+			User_Role._oDataset.getRecords(this._load.bind(this, fCallback));
+		}
+		else
+		{
+			// New Object -- this should never happen
+			this.oProperties	= {};
+		}
+	},
+	
+	_load	: function(fCallback, iRecordCount, aResultSet)
+	{
+		// Set properties
+		this.oProperties	= aResultSet[this.iId];
+		
+		// Callback
+		if (fCallback)
+		{
+			fCallback(this);
+		}
+	}
+});
+
+User_Role._oDataset	= new Dataset_Ajax(Dataset_Ajax.CACHE_MODE_FULL_CACHING, {strObject: 'User_Role', strMethod: 'getDataset'});
+
+User_Role.getForId	= function(iId, fCallback)
+{
+	return new User_Role(iId, fCallback);
+}
+
+User_Role.getAll	= function(fCallback, iRecordCount, aResultSet)
+{
+	if (iRecordCount === undefined || aResultSet === undefined)
+	{
+		// Make Request
+		this._oDataset.getRecords(this.getAll.bind(this, fCallback));
+	}
+	else
+	{
+		// Pass Response to Callback
+		fCallback(aResultSet);
+	}
+};
+
+User_Role.getAllAsSelectOptions	= function(fCallback, oResponse)
+{
+	if (!oResponse)
+	{
+		// Make Request
+		this.getAll(this.getAllAsSelectOptions.bind(this, fCallback));
+	}
+	else
+	{
+		// Create an Array of SELECT DOM Elements
+		var aOptions	= {};
+		for (i in oResponse)
+		{
+			var domOption		= document.createElement('option');
+			domOption.value		= oResponse[i].id;
+			domOption.innerHTML	= oResponse[i].name;
+			aSelectOptions.push(domOption);
+		}
+		
+		// Pass to Callback
+		fCallback(aOptions);
+	}
+};
