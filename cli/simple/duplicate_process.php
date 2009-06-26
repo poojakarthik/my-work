351adbf86fd029f5bf8de62c410e0d1b64b5b9d1
Rich Davis <rdavis@yellowbilling.com.au>
2009-06-26 04:35:39 +0000

    starting adding single-instance execution for CLI scripts

---
+++ cli/simple/duplicate_process.php
@@ -0,0 +1,23 @@
+<?php
+
+// Load Framework
+require_once(dirname(__FILE__).'../../lib/classes/Flex.php');
+Flex::load();
+
+echo "\n\n";
+
+// Wait a while
+$intStartTime	= time();
+$intTime		= $intStartTime;
+$intMaxSeconds	= 60;
+while ($intTime < ($intStartTime + $intMaxSeconds))
+{
+	$intTime	= time();
+	echo "\\033[K";
+	echo (($intStartTime + $intMaxSeconds) - $intTime)." seconds remaining";
+}
+
+echo "\n\n";
+exit(0);
+
+?>
