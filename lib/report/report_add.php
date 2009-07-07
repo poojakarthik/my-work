d6fa2cc8ee114bc8c072f9edac03140d0a3d7469
Joel Dawkins <jdawkins@yellowbilling.com.au>
2009-07-07 09:05:08 +0000

    * All 6 Adjustment related data reports are now defined in report_add.php

--- lib/report/report_add.php
+++ lib/report/report_add.php
@@ -284,7 +284,7 @@ $arrSQLSelect['Amount ($ Ex GST)']			['Value']	= "Charge.Amount";
 $arrSQLSelect['Created On']					['Value']	= "DATE_FORMAT(Charge.CreatedOn, '%d/%m/%Y')";
 $arrSQLSelect['Created By']					['Value']	= "CONCAT(Creator.FirstName, ' ', Creator.LastName)";
 $arrSQLSelect['Charged On']					['Value']	= "DATE_FORMAT(Charge.ChargedOn, '%d/%m/%Y')";
-$arrSQLSelect['Rejected By']				['Value']	= "CONCAT(Rejector.FirstName, ' ', Rejector.LastName)";
+$arrSQLSelect['Rejected By']				['Value']	= "IF((Rejector.Id IS NOT NULL), CONCAT(Rejector.FirstName, ' ', Rejector.LastName), 'UNKNOWN')";
 
 $arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
 
@@ -346,6 +346,315 @@ $arrDataReport['SQLFields'] = serialize($arrSQLFields);
 $arrDataReports[] = $arrDataReport;
 
 
+
+//---------------------------------------------------------------------------//
+// RECURRING ADJUSTMENT REQUESTS AWAITING APPROVAL
+//---------------------------------------------------------------------------//
+
+$arrDataReport	= array();
+$arrDocReq		= array();
+$arrSQLSelect	= array();
+$arrSQLFields	= array();
+
+
+$arrDataReport['Name']			= "Recurring Adjustment Requests Awaiting Approval";
+$arrDataReport['Summary']		= "Lists all Recurring Adjustment Requests Currently Awaiting Approval";
+$arrDataReport['RenderMode']	= REPORT_RENDER_INSTANT;
+$arrDataReport['Priviledges']	= 64;											// Credit Management
+//$arrDataReport['Priviledges']	= 1;											// Live
+$arrDataReport['CreatedOn']		= date("Y-m-d");
+$arrDataReport['SQLTable']		= "	RecurringCharge
+									INNER JOIN Account ON RecurringCharge.Account = Account.Id
+									LEFT JOIN Service ON RecurringCharge.Service = Service.Id
+									INNER JOIN CustomerGroup ON Account.CustomerGroup = CustomerGroup.Id
+									INNER JOIN Employee AS Creator ON RecurringCharge.CreatedBy = Creator.Id";
+$arrDataReport['SQLWhere']		= "	(<ChargeType> = 'ANY' OR CONCAT(RecurringCharge.Nature, ': ', RecurringCharge.ChargeType, ' - ', RecurringCharge.Description) = <ChargeType>)
+									AND (<CustomerGroupId> = 0 OR Account.CustomerGroup = <CustomerGroupId>)
+									AND (RecurringCharge.CreatedOn BETWEEN <EarliestDate> AND <LatestDate>)
+									AND RecurringCharge.recurring_charge_status_id = (SELECT id FROM recurring_charge_status WHERE system_name = 'AWAITING_APPROVAL')
+									ORDER BY RecurringCharge.CreatedOn ASC, RecurringCharge.Id ASC";
+$arrDataReport['SQLGroupBy']	= "";
+
+// Documentation Reqs
+$arrDocReq[]	= "DataReport";
+$arrDataReport['Documentation']	= serialize($arrDocReq);
+
+// SQL Select
+$arrSQLSelect['Recurring Adjustment Id']		['Value']	= "RecurringCharge.Id";
+$arrSQLSelect['Customer Group']					['Value']	= "CustomerGroup.internal_name";
+$arrSQLSelect['Account']						['Value']	= "RecurringCharge.Account";
+$arrSQLSelect['Service FNN']					['Value']	= "Service.FNN";
+$arrSQLSelect['Nature']							['Value']	= "RecurringCharge.Nature";
+$arrSQLSelect['Type']							['Value']	= "RecurringCharge.ChargeType";
+$arrSQLSelect['Description']					['Value']	= "RecurringCharge.Description";
+$arrSQLSelect['Minimum Charge ($ Ex GST)']		['Value']	= "RecurringCharge.MinCharge";
+$arrSQLSelect['Recurring Charge ($ Ex GST)']	['Value']	= "RecurringCharge.RecursionCharge";
+$arrSQLSelect['Created On']						['Value']	= "DATE_FORMAT(RecurringCharge.CreatedOn, '%d/%m/%Y')";
+$arrSQLSelect['Created By']						['Value']	= "CONCAT(Creator.FirstName, ' ', Creator.LastName)";
+$arrSQLSelect['Proposed Start Date']			['Value']	= "DATE_FORMAT(RecurringCharge.StartedOn, '%d/%m/%Y')";
+
+$arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
+
+// SQL Fields
+$arrChargeTypeQuery = array('Query'			=> "SELECT DISTINCT CONCAT(Nature, ': ', ChargeType, ' - ', Description) AS Value, 
+													IF(Archived = 0, CONCAT('[ACTIVE] ', Nature, ': ', ChargeType, ' - ', Description), CONCAT('[ARCHVIED] ', Nature, ': ', ChargeType, ' - ', Description)) AS Label
+												FROM RecurringChargeType
+												ORDER BY Label ASC;",
+							'ValueType'		=> "dataString",
+							'IgnoreField'	=> array(	'Value'	=> 'ANY',
+														'Label'	=> 'Any')
+						);
+
+
+$arrCustomerGroupQuery = array(	'Query'			=> "SELECT Id AS Value, internal_name AS Label
+													FROM CustomerGroup
+													ORDER BY internal_name ASC;",
+								'ValueType'		=> "dataInteger",
+								'IgnoreField'	=> array(	'Value'	=> 0,
+															'Label'	=> 'Any')
+								);
+
+
+
+$arrSQLFields['ChargeType']			= Array('Type'					=> "Query",
+											'DBQuery'				=> $arrChargeTypeQuery,
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Adjustment Type",
+											);
+
+$arrSQLFields['CustomerGroupId']	= Array('Type'					=> "Query",
+											'DBQuery'				=> $arrCustomerGroupQuery,
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Customer Group",
+											);
+
+$arrSQLFields['EarliestDate']		= Array(
+											'Type'					=> "dataDate",
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Earliest Request Date",
+											);
+$arrSQLFields['LatestDate']			= Array(
+											'Type'					=> "dataDate",
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Latest Request Date",
+											);
+
+$arrDataReport['SQLFields'] = serialize($arrSQLFields);
+
+// Add the report to the array of reports to add to the database
+$arrDataReports[] = $arrDataReport;
+
+
+//---------------------------------------------------------------------------//
+// RECURRING ADJUSTMENT REQUESTS THAT HAVE BEEN APPROVED
+//---------------------------------------------------------------------------//
+
+$arrDataReport	= array();
+$arrDocReq		= array();
+$arrSQLSelect	= array();
+$arrSQLFields	= array();
+
+
+$arrDataReport['Name']			= "Recurring Adjustment Requests That Have Been Approved";
+$arrDataReport['Summary']		= "Lists all Recurring Adjustment Requests That Have Been Approved";
+$arrDataReport['RenderMode']	= REPORT_RENDER_INSTANT;
+$arrDataReport['Priviledges']	= 64;											// Credit Management
+//$arrDataReport['Priviledges']	= 1;											// Live
+$arrDataReport['CreatedOn']		= date("Y-m-d");
+$arrDataReport['SQLTable']		= "	RecurringCharge
+									INNER JOIN Account ON RecurringCharge.Account = Account.Id
+									LEFT JOIN Service ON RecurringCharge.Service = Service.Id
+									INNER JOIN CustomerGroup ON Account.CustomerGroup = CustomerGroup.Id
+									INNER JOIN Employee AS Creator ON RecurringCharge.CreatedBy = Creator.Id
+									INNER JOIN Employee AS Approver ON RecurringCharge.ApprovedBy = Approver.Id
+									INNER JOIN recurring_charge_status ON RecurringCharge.recurring_charge_status_id = recurring_charge_status.id";
+$arrDataReport['SQLWhere']		= "	(<ChargeType> = 'ANY' OR CONCAT(RecurringCharge.Nature, ': ', RecurringCharge.ChargeType, ' - ', RecurringCharge.Description) = <ChargeType>)
+									AND (<CustomerGroupId> = 0 OR Account.CustomerGroup = <CustomerGroupId>)
+									AND (RecurringCharge.CreatedOn BETWEEN <EarliestDate> AND <LatestDate>)
+									AND RecurringCharge.ApprovedBy IS NOT NULL
+									AND RecurringCharge.recurring_charge_status_id != (SELECT id FROM recurring_charge_status WHERE system_name = 'DECLINED')
+									AND (<IncludeAutoApproved> = 1 OR (<IncludeAutoApproved> = 0 AND RecurringCharge.ApprovedBy != 0))
+									ORDER BY RecurringCharge.CreatedOn ASC, RecurringCharge.Id ASC";
+$arrDataReport['SQLGroupBy']	= "";
+
+// Documentation Reqs
+$arrDocReq[]	= "DataReport";
+$arrDataReport['Documentation']	= serialize($arrDocReq);
+
+// SQL Select
+$arrSQLSelect['Recurring Adjustment Id']		['Value']	= "RecurringCharge.Id";
+$arrSQLSelect['Customer Group']					['Value']	= "CustomerGroup.internal_name";
+$arrSQLSelect['Account']						['Value']	= "RecurringCharge.Account";
+$arrSQLSelect['Service FNN']					['Value']	= "Service.FNN";
+$arrSQLSelect['Nature']							['Value']	= "RecurringCharge.Nature";
+$arrSQLSelect['Type']							['Value']	= "RecurringCharge.ChargeType";
+$arrSQLSelect['Description']					['Value']	= "RecurringCharge.Description";
+$arrSQLSelect['Minimum Charge ($ Ex GST)']		['Value']	= "RecurringCharge.MinCharge";
+$arrSQLSelect['Recurring Charge ($ Ex GST)']	['Value']	= "RecurringCharge.RecursionCharge";
+$arrSQLSelect['Total Charged ($ Ex GST)']		['Value']	= "RecurringCharge.TotalCharged";
+$arrSQLSelect['Created On']						['Value']	= "DATE_FORMAT(RecurringCharge.CreatedOn, '%d/%m/%Y')";
+$arrSQLSelect['Created By']						['Value']	= "CONCAT(Creator.FirstName, ' ', Creator.LastName)";
+$arrSQLSelect['Start Date']						['Value']	= "DATE_FORMAT(RecurringCharge.StartedOn, '%d/%m/%Y')";
+$arrSQLSelect['Approved By']					['Value']	= "CONCAT(Approver.FirstName, ' ', Approver.LastName)";
+$arrSQLSelect['Current Status']					['Value']	= "recurring_charge_status.name";
+
+$arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
+
+// SQL Fields
+$arrChargeTypeQuery = array('Query'			=> "SELECT DISTINCT CONCAT(Nature, ': ', ChargeType, ' - ', Description) AS Value, 
+													IF(Archived = 0, CONCAT('[ACTIVE] ', Nature, ': ', ChargeType, ' - ', Description), CONCAT('[ARCHVIED] ', Nature, ': ', ChargeType, ' - ', Description)) AS Label
+												FROM RecurringChargeType
+												ORDER BY Label ASC;",
+							'ValueType'		=> "dataString",
+							'IgnoreField'	=> array(	'Value'	=> 'ANY',
+														'Label'	=> 'Any')
+						);
+
+
+$arrCustomerGroupQuery = array(	'Query'			=> "SELECT Id AS Value, internal_name AS Label
+													FROM CustomerGroup
+													ORDER BY internal_name ASC;",
+								'ValueType'		=> "dataInteger",
+								'IgnoreField'	=> array(	'Value'	=> 0,
+															'Label'	=> 'Any')
+								);
+
+
+
+$arrSQLFields['ChargeType']			= Array('Type'					=> "Query",
+											'DBQuery'				=> $arrChargeTypeQuery,
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Adjustment Type",
+											);
+
+$arrSQLFields['CustomerGroupId']	= Array('Type'					=> "Query",
+											'DBQuery'				=> $arrCustomerGroupQuery,
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Customer Group",
+											);
+
+$arrSQLFields['EarliestDate']		= Array(
+											'Type'					=> "dataDate",
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Earliest Request Date",
+											);
+$arrSQLFields['LatestDate']			= Array(
+											'Type'					=> "dataDate",
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Latest Request Date",
+											);
+
+$arrSQLFields['IncludeAutoApproved']	= Array(
+											'Type'					=> "dataBoolean",
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Include Automatically Approved",
+											);
+
+
+$arrDataReport['SQLFields'] = serialize($arrSQLFields);
+
+// Add the report to the array of reports to add to the database
+$arrDataReports[] = $arrDataReport;
+
+//---------------------------------------------------------------------------//
+// RECURRING ADJUSTMENT REQUESTS THAT HAVE BEEN REJECTED
+//---------------------------------------------------------------------------//
+
+$arrDataReport	= array();
+$arrDocReq		= array();
+$arrSQLSelect	= array();
+$arrSQLFields	= array();
+
+
+$arrDataReport['Name']			= "Recurring Adjustment Requests That Have Been Rejected";
+$arrDataReport['Summary']		= "Lists all Recurring Adjustment Requests That Have Been Rejected";
+$arrDataReport['RenderMode']	= REPORT_RENDER_INSTANT;
+$arrDataReport['Priviledges']	= 64;											// Credit Management
+//$arrDataReport['Priviledges']	= 1;											// Live
+$arrDataReport['CreatedOn']		= date("Y-m-d");
+$arrDataReport['SQLTable']		= "	RecurringCharge
+									INNER JOIN Account ON RecurringCharge.Account = Account.Id
+									LEFT JOIN Service ON RecurringCharge.Service = Service.Id
+									INNER JOIN CustomerGroup ON Account.CustomerGroup = CustomerGroup.Id
+									INNER JOIN Employee AS Creator ON RecurringCharge.CreatedBy = Creator.Id
+									LEFT JOIN Employee AS Rejector ON RecurringCharge.ApprovedBy = Rejector.Id";
+$arrDataReport['SQLWhere']		= "	(<ChargeType> = 'ANY' OR CONCAT(RecurringCharge.Nature, ': ', RecurringCharge.ChargeType, ' - ', RecurringCharge.Description) = <ChargeType>)
+									AND (<CustomerGroupId> = 0 OR Account.CustomerGroup = <CustomerGroupId>)
+									AND (RecurringCharge.CreatedOn BETWEEN <EarliestDate> AND <LatestDate>)
+									AND RecurringCharge.recurring_charge_status_id = (SELECT id FROM recurring_charge_status WHERE system_name = 'DECLINED')
+									ORDER BY RecurringCharge.CreatedOn ASC, RecurringCharge.Id ASC";
+$arrDataReport['SQLGroupBy']	= "";
+
+// Documentation Reqs
+$arrDocReq[]	= "DataReport";
+$arrDataReport['Documentation']	= serialize($arrDocReq);
+
+// SQL Select
+$arrSQLSelect['Recurring Adjustment Id']		['Value']	= "RecurringCharge.Id";
+$arrSQLSelect['Customer Group']					['Value']	= "CustomerGroup.internal_name";
+$arrSQLSelect['Account']						['Value']	= "RecurringCharge.Account";
+$arrSQLSelect['Service FNN']					['Value']	= "Service.FNN";
+$arrSQLSelect['Nature']							['Value']	= "RecurringCharge.Nature";
+$arrSQLSelect['Type']							['Value']	= "RecurringCharge.ChargeType";
+$arrSQLSelect['Description']					['Value']	= "RecurringCharge.Description";
+$arrSQLSelect['Minimum Charge ($ Ex GST)']		['Value']	= "RecurringCharge.MinCharge";
+$arrSQLSelect['Recurring Charge ($ Ex GST)']	['Value']	= "RecurringCharge.RecursionCharge";
+$arrSQLSelect['Created On']						['Value']	= "DATE_FORMAT(RecurringCharge.CreatedOn, '%d/%m/%Y')";
+$arrSQLSelect['Created By']						['Value']	= "CONCAT(Creator.FirstName, ' ', Creator.LastName)";
+$arrSQLSelect['Proposed Start Date']			['Value']	= "DATE_FORMAT(RecurringCharge.StartedOn, '%d/%m/%Y')";
+$arrSQLSelect['Rejected By']					['Value']	= "IF((Rejector.Id IS NOT NULL), CONCAT(Rejector.FirstName, ' ', Rejector.LastName), 'UNKNOWN')";
+
+$arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
+
+// SQL Fields
+$arrChargeTypeQuery = array('Query'			=> "SELECT DISTINCT CONCAT(Nature, ': ', ChargeType, ' - ', Description) AS Value, 
+													IF(Archived = 0, CONCAT('[ACTIVE] ', Nature, ': ', ChargeType, ' - ', Description), CONCAT('[ARCHVIED] ', Nature, ': ', ChargeType, ' - ', Description)) AS Label
+												FROM RecurringChargeType
+												ORDER BY Label ASC;",
+							'ValueType'		=> "dataString",
+							'IgnoreField'	=> array(	'Value'	=> 'ANY',
+														'Label'	=> 'Any')
+						);
+
+
+$arrCustomerGroupQuery = array(	'Query'			=> "SELECT Id AS Value, internal_name AS Label
+													FROM CustomerGroup
+													ORDER BY internal_name ASC;",
+								'ValueType'		=> "dataInteger",
+								'IgnoreField'	=> array(	'Value'	=> 0,
+															'Label'	=> 'Any')
+								);
+
+
+
+$arrSQLFields['ChargeType']			= Array('Type'					=> "Query",
+											'DBQuery'				=> $arrChargeTypeQuery,
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Adjustment Type",
+											);
+
+$arrSQLFields['CustomerGroupId']	= Array('Type'					=> "Query",
+											'DBQuery'				=> $arrCustomerGroupQuery,
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Customer Group",
+											);
+
+$arrSQLFields['EarliestDate']		= Array(
+											'Type'					=> "dataDate",
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Earliest Request Date",
+											);
+$arrSQLFields['LatestDate']			= Array(
+											'Type'					=> "dataDate",
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "Latest Request Date",
+											);
+
+
+$arrDataReport['SQLFields'] = serialize($arrSQLFields);
+
+// Add the report to the array of reports to add to the database
+$arrDataReports[] = $arrDataReport;
 
 
 //----------------------------------------------------------------------------//
