f9abe2bd0699d781a11cdbd84e264df436e7a36b
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-08 05:08:08 +0000

    added multicolumn select

---
+++ html/ui/javascript/control_select_multicolumn.js
@@ -0,0 +1,94 @@
+var Control_Select_Multicolumn	= Class.create
+({
+	initialize	: function(bSelectMode)
+	{
+		// DOM Elements
+		this.oTable				= {};
+		this.oTable.domElement	= document.createElement('table');
+		this.oTable.domElement.addClassName('select');
+		
+		// Properties
+		this.aOptions		= [];
+		this.iSelectedIndex	= null;
+		this.oColumns		= {};
+	},
+	
+	add	: function(oOption)
+	{
+		// Validate parameters
+		if (!(oOption instanceof Control_Select_Multicolumn_Option))
+		{
+			throw "oOption is not a 'Control_Select_Multicolumn_Option' object!";
+		}
+		else
+		{
+			// Add to array and return the index
+			return this.aOptions.push(oOption) - 1;
+		}
+	},
+	
+	remove	: function(oOption)
+	{
+		// Validate parameters
+		if (!(oOption instanceof Control_Select_Multicolumn_Option))
+		{
+			throw "oOption is not a 'Control_Select_Multicolumn_Option' object!";
+		}
+		else if ((var iIndex = this.aOptions.indexOf(oOption)) > -1)
+		{
+			if (iIndex === this.iSelectedIndex)
+			{
+				this.iSelectedIndex	= null;
+			}
+			
+			this.aOptions.splice(iIndex, 1);
+			return this.aOptions.length;
+		}
+		else
+		{
+			return false;
+		}
+	},
+	
+	getSelectedOption	: function()
+	{
+		if (this.iSelectedIndex === null)
+		{
+			return null;
+		}
+		else
+		{
+			return this.aOptions[this.iSelectedIndex];
+		}
+	},
+	
+	setColumns	: function(oDefinition)
+	{
+		this.oColumns	= {};
+		for (sColumnAlias in oDefinition)
+		{
+			this.oColumns[sColumnAlias]	= {};
+			this.oColumns[sColumnAlias].sWidth	= (oDefinition[sColumnAlias].sWidth) ? oDefinition[sColumnAlias].sWidth : 
+		}
+		this._render();
+	},
+	
+	_render	: function()
+	{
+		// Render the Children
+		for (i in this.oColumns)
+		{
+			this.oColumns[i].render();
+		}
+	}
+});
+
+// Static Methods
+Control_Select_Multicolumn.factory	= function()
+{
+	return new Control_Select_Multicolumn();
+};
+
+// Class Constants
+Control_Select_Multicolumn.SELECT_MODE_SINGLE	= false;
+Control_Select_Multicolumn.SELECT_MODE_MULTIPLE	= true;
---
+++ html/ui/javascript/control_select_multicolumn_option.js
@@ -0,0 +1,57 @@
+var Control_Select_Multicolumn_Option	= Class.create
+({
+	initialize	: function(oContent, mValue)
+	{
+		// DOM Elements
+		this._oTR				= {}
+		this._oTR.domElement	= document.createElement('tr');
+		
+		// Properties
+		this.setContent(oContent);
+		this.setValue(mValue);
+		this.setSelected(false);
+	},
+	
+	attachTo	: function(oControlSelectMulticolumn)
+	{
+		oControlSelectMulticolumn.domElement.appendChild();
+	},
+	
+	setContent	: function(oContent)
+	{
+		this._oContent	= oContent;
+	},
+	
+	setValue	: function(mValue)
+	{
+		this._mValue	= mValue;
+	},
+	
+	getValue	: function()
+	{
+		return this._mValue;
+	},
+	
+	setSelected	: function(bSelected)
+	{
+		this._bSelected	= (bSelected) ? true : false;
+	},
+	
+	isSelected	: function()
+	{
+		return this._bSelected;
+	},
+	
+	render	: function(oVisibleColumns)
+	{
+		// Remove all existing columns
+		this._oTR.domElement.innerHTML	= '';
+		
+		// Add all visible columns
+		for (sField in oVisibleColumns)
+		{
+			var domTD		= document.createElement('td');
+			domTD.innerHTML	= (this._oContent && this._oContent[sField]) ? this._oContent[sField] : '';
+		}
+	}
+});
