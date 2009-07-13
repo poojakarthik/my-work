f063b97978c71519f99849b25c7bdabbbbc7c44b
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-13 02:16:19 +0000

    work on new employee popup

--- html/ui/javascript/control_tree_grid_node_data.js
+++ html/ui/javascript/control_tree_grid_node_data.js
@@ -15,7 +15,7 @@ var Control_Tree_Grid_Node_Data	= Class.create(/* extends */ Control_Tree_Grid_N
 		
 		this._oElement.oExpandIcon				= {};
 		this._oElement.oExpandIcon.domElement	= document.createElement('img');
-		this._oElement.oExpandIcon.domElement.addEventListener('click', this.toggleExpanded.bind(this), false);
+		this._oElement.oExpandIcon.onClick		= this.toggleExpanded.bind(this);
 		
 		// Properties
 		this.setContent(oContent);
@@ -121,11 +121,13 @@ var Control_Tree_Grid_Node_Data	= Class.create(/* extends */ Control_Tree_Grid_N
 					{
 						this._oElement.oExpandIcon.domElement.src	= '../admin/img/template/' + (this.isExpanded() ? 'tree_open.png' : 'tree_closed.png');
 						this._oElement.oExpandIcon.domElement.addClassName('clickable');
+						this._oElement.oExpandIcon.domElement.addEventListener('click', this._oElement.oExpandIcon.onClick, false);
 					}
 					else
 					{
 						this._oElement.oExpandIcon.domElement.src	= '../admin/img/template/1px-transparent.png';
 						this._oElement.oExpandIcon.domElement.removeClassName('clickable');
+						this._oElement.oExpandIcon.domElement.removeEventListener('click', this._oElement.oExpandIcon.onClick, false);
 					}
 					this._oElement.oExpandIcon.domElement.style.marginLeft	= (iDepth * Control_Tree_Grid_Node_Data.TREE_DEPTH_SCALE)+'px';
 					domTD.appendChild(this._oElement.oExpandIcon.domElement);
