aea2a7e6c7c9474632958a8d5a75447cb00cb516
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-13 07:30:56 +0000

    work on new employee popup

--- html/ui/javascript/control_tree_grid_node_data.js
+++ html/ui/javascript/control_tree_grid_node_data.js
@@ -160,7 +160,7 @@ var Control_Tree_Grid_Node_Data	= Class.create(/* extends */ Control_Tree_Grid_N
 		this._bSelected	= (bSelected) ? true : false;
 		
 		// Update Icon
-		this._oElement.oSelectIcon.domElement.src	= '../admin/img/template/' + (this.isSelected() ? 'checkbox_checked.png' : 'checkbox_unchecked.png');
+		this._oElement.oSelectIcon.domElement.src	= '../admin/img/template/checkbox' + (this.isSelected() ? '-checked' : '') + '.png';
 		
 		if (this._oVisibleColumns)
 		{
