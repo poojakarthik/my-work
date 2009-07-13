6f31cfc9951a042c4e8e20975d210cd1821e2f0b
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-13 01:36:12 +0000

    work on new employee popup

--- html/admin/javascript/popup_employee.js
+++ html/admin/javascript/popup_employee.js
@@ -219,23 +219,23 @@ var Popup_Employee	= Class.create(Reflex_Popup,
 		oCSR.oContent[Control_Tree_Grid.COLUMN_VALUE]	= 7;
 		oCSR.oControl									= new Control_Tree_Grid_Node_Data(oCSR.oContent);
 		oTabPage.oProfiles.oControl.appendChild(oCSR.oControl);
-		/*
+		/**/
 		var oFlexAdmin										= {}
 		oFlexAdmin.oContent									= {};
 		oFlexAdmin.oContent[Control_Tree_Grid.COLUMN_LABEL]	= 'Flex Admin';
 		oFlexAdmin.oContent[Control_Tree_Grid.COLUMN_VALUE]	= 2;
 		oFlexAdmin.oControl									= new Control_Tree_Grid_Node_Data(oFlexAdmin.oContent);
 		oTabPage.oProfiles.oControl.appendChild(oFlexAdmin.oControl);
-		/*
+		/**/
 		var oCSM										= {}
 		oCSM.oContent									= {};
 		oCSM.oContent[Control_Tree_Grid.COLUMN_LABEL]	= 'Customer Service Manager';
 		oCSM.oContent[Control_Tree_Grid.COLUMN_VALUE]	= 4;
 		oCSM.oControl									= new Control_Tree_Grid_Node_Data(oCSM.oContent);
 		oTabPage.oProfiles.oControl.appendChild(oCSM.oControl);
-		/*
+		/**/
 		oCSM.oControl.appendChild(new Control_Tree_Grid_Node_Data(oCSR.oContent));
-		*/
+		/**/
 		// Set Columns
 		var oProfileColumns	= {};
 		oProfileColumns[Control_Tree_Grid.COLUMN_EXPAND]	= {};
