a79c1086215ac1d8e6d40f01a3a1d860e2ac5d92
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-14 23:03:47 +0000

    work on new employee popup

--- html/ui/javascript/control_tree_grid_node_data.js
+++ html/ui/javascript/control_tree_grid_node_data.js
@@ -54,10 +54,12 @@ var Control_Tree_Grid_Node_Data	= Class.create(/* extends */ Control_Tree_Grid_N
 			{
 				if (this._oParent.getLastChild())
 				{
+					alert(this._oContent['label'].oData.sLabel + " has siblings!");
 					this.getRootNode().getElement().insertBefore(this.getElement(), this._oParent.getLastChild().getElement().nextSibling);
 				}
 				else
 				{
+					alert(this._oContent['label'].oData.sLabel + " has no siblings!");
 					this.getRootNode().getElement().insertBefore(this.getElement(), this._oParent.getElement().nextSibling);
 				}
 				
