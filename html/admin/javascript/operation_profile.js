d53fba41afe3cf09022116a92701a75224aef3fc
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-16 23:18:22 +0000

    work on new employee popup

--- html/admin/javascript/operation_profile.js
+++ html/admin/javascript/operation_profile.js
@@ -17,52 +17,11 @@ Operation_Profile.buildTree	= function(oOperationProfiles)
 	
 	for (iOperationProfileId in oOperationProfiles)
 	{
-		oOperationProfiles[iOperationProfileId].aChildren	= [];
-	}
-	
-	// Convert Child-Prerequisites to Parent-Dependants
-	for (iOperationId in oOperations)
-	{
-		// Convert Child-Prerequisites to Parent-Dependants
-		for (var i = 0; i < oOperations[iOperationId].aPrerequisites.length; i++)
-		{
-			if (!oOperations[oOperations[iOperationId].aPrerequisites[i]])
-			{
-				throw "Invalid prerequisite reference: "+oOperations[iOperationId].aPrerequisites[i];
-			}
-			
-			// Add as a dependant
-			oOperations[oOperations[iOperationId].aPrerequisites[i]].aDependants.push(iOperationId);
-		}
-		
-		oOperations[iOperationId].aInstances	= [];
-	}
-	
-	// Create the Tree
-	for (iOperationId in oOperations)
-	{
-		if (!oOperations[iOperationId].aPrerequisites || !oOperations[iOperationId].aPrerequisites.length)
-		{
-			// Add each top-level Operation (i.e. no prerequsities) to the Tree
-			oDependencyTree[iOperationId]	= Operation.buildTreeNode(oOperations, iOperationId);
-		}
+		oOperationProfiles[iOperationProfileId].aInstances	= [];
 	}
 	
 	Reflex_Debug.asHTMLPopup(oProfileTree);
 	Reflex_Debug.asHTMLPopup(oOperations);
 	
-	return oDependencyTree;
-};
-
-Operation_Profile.buildTreeNode	= function(oOperations, iPrerequisiteOperationId)
-{
-	var oDependencyTree	= {};
-	
-	for (var i = 0; i < oOperations[iPrerequisiteOperationId].aDependants.length; i++)
-	{
-		// Add the dependants to the tree
-		oDependencyTree[oOperations[iPrerequisiteOperationId].aDependants[i]]	= Operation.buildDependencyTreeNode(oOperations, oOperations[iPrerequisiteOperationId].aDependants[i]);
-	}
-	
-	return oDependencyTree;
+	return oProfileTree;
 };
