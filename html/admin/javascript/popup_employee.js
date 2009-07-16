fb2775b2ac187c45b4c40aec9b2d35fc7a5cf94e
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-16 03:28:05 +0000

    added a JS Class that does very pretty debugging of JS constructs

--- html/admin/javascript/popup_employee.js
+++ html/admin/javascript/popup_employee.js
@@ -61,6 +61,10 @@ var Popup_Employee	= Class.create(Reflex_Popup,
 	
 	buildContent	: function()
 	{
+		// DEBUG TEST
+		Reflex_Debug.asHTMLPopup(this.objEmployee);
+		// DEBUG TEST
+		
 		// Build Content
 		this._objPage			= {};
 		this._objPage.domElement	= document.createElement('div');
