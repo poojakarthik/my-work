da94990e7365a1b2401df3830baadad9cb234605
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-16 03:20:14 +0000

    added a JS Class that does very pretty debugging of JS constructs

--- html/ui/classes/Page.php
+++ html/ui/classes/Page.php
@@ -280,6 +280,7 @@ class Page
 		echo "\t\t<script type='text/javascript' src='{$strFrameworkDir}javascript/sha1.js' ></script>\n";
 		echo "\t\t<script type='text/javascript' src='{$strFrameworkDir}javascript/reflex_popup.js' ></script>\n";
 		echo "\t\t<script type='text/javascript' src='{$strFrameworkDir}javascript/flex_constant.js' ></script>\n";
+		echo "\t\t<script type='text/javascript' src='{$strFrameworkDir}javascript/reflex_debug.js' ></script>\n";
 		
 		// Include reference to all other javascript files required of the page
 		if (!array_key_exists('*arrJavaScript', $GLOBALS) || !is_array($GLOBALS['*arrJavaScript']))
