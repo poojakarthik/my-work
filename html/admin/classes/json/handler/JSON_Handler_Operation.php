14349679c56d4cefcadddd9d697fe124ef3c678e
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-16 07:51:05 +0000

    work on new employee popup

--- html/admin/classes/json/handler/JSON_Handler_Operation.php
+++ html/admin/classes/json/handler/JSON_Handler_Operation.php
@@ -61,7 +61,7 @@ class JSON_Handler_Operation extends JSON_Handler
 				$qQuery	= new Query();
 				
 				// Retrieve list of Employees
-				$sGetAllSQL	= "SELECT * FROM Operation WHERE 1";
+				$sGetAllSQL	= "SELECT * FROM operation WHERE 1";
 				$sGetAllSQL	.= ($iLimit !== null) ? " LIMIT {$iLimit} OFFSET {$iOffset}" : '';
 				$rGetAll	= $qQuery->Execute($sGetAllSQL);
 				if ($rGetAll === false)
@@ -113,87 +113,6 @@ class JSON_Handler_Operation extends JSON_Handler
 		{
 			return $aCount['record_count'];
 		}
-	}
-	
-	public function getOperationTree()
-	{
-		try
-		{
-			$qQuery	= new Query();
-			
-			// Get Operations Tree
-			$aOperationsTree	= self::buildOperationTree();
-			
-			// If no exceptions were thrown, then everything worked
-			return array(
-							"Success"			=> true,
-							"oPermissions"		=> $aOperationsTree,
-							"strDebug"			=> (AuthenticatedUser()->UserHasPerm(PERMISSION_PROPER_GOD)) ? $this->_JSONDebug : ''
-						);
-		}
-		catch (Exception $e)
-		{
-			// Send an Email to Devs
-			//SendEmail("rdavis@yellowbilling.com.au", "Exception in ".__CLASS__, $e->__toString(), CUSTOMER_URL_NAME.'.errors@yellowbilling.com.au');
-			
-			return array(
-							"Success"	=> false,
-							"Message"	=> 'ERROR: '.$e->getMessage(),
-							"strDebug"	=> (AuthenticatedUser()->UserHasPerm(PERMISSION_PROPER_GOD)) ? $this->_JSONDebug : ''
-						);
-		}
-	}
-	
-	public static function buildOperationTree($iOperationId=null)
-	{
-		static $qQuery;
-		$qQuery	= ($qQuery) ? $qQuery : new Query();
-		
-		$aOperations	= array();
-		
-		if ($iOperationId === null)
-		{
-			$sOperations	= "	SELECT		o.id		AS operation_id
-								
-								FROM		`operation` o
-											LEFT JOIN operation_prerequesite op_parent ON (o.id = op_parent.operation_id)
-											LEFT JOIN operation_prerequesite op_child ON (o.id = op_child.prerequesite_operation_id)
-								
-								WHERE		1
-								
-								GROUP BY	o.id
-								
-								HAVING		COUNT(op_parent.id) = 0";
-		}
-		else
-		{
-			$sOperations	= "	SELECT		op_child.id		AS operation_id
-								
-								FROM		`operation` o
-											JOIN operation_prerequesite op_parent ON (o.id = op_parent.operation_id)
-											LEFT JOIN operation_prerequesite op_child ON (o.id = op_child.prerequesite_operation_id)
-								
-								WHERE		o.id = {$iOperationId}";
-		}
-		
-		$Operations	= $qQuery->Execute($sOperations);
-		if ($Operations === false)
-		{
-			throw new Exception($qQuery->Error());
-		}
-		while ($aOperationId = $Operations->fetch_assoc())
-		{
-			$iOperationId	= $aOperationId['operation_id'];
-			
-			$oOperationEntity	= Operation::getForId($iOperationId);
-			$oOperation			= $oOperationEntity->toStdClass();
-			
-			$oOperation->oOperations	= self::buildOperationTree($iOperationId);
-			
-			$aOperations[$iOperationId]	= $oOperation;
-		}
-		
-		return $aOperations;
 	}
 	
 	public static function getOperations($bIncludeDependencyReferences=false)
