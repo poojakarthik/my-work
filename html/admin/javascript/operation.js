0d473ad4ffeaa8c527482c86d25896ed4134d021
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-23 04:27:34 +0000

    work on new employee popup

--- html/admin/javascript/operation.js
+++ html/admin/javascript/operation.js
@@ -68,8 +68,8 @@ Operation.getAllIndexed	= function(fCallback, aResultSet)
 		}
 		
 		// Pass to Callback
-		Reflex_Debug.asHTMLPopup(oResultSet);
-		Reflex_Debug.asHTMLPopup(aResultSet);
+		//Reflex_Debug.asHTMLPopup(oResultSet);
+		//Reflex_Debug.asHTMLPopup(aResultSet);
 		fCallback(oResultSet);
 	}
 };
