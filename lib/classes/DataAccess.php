8aa6d9cd50e4337895869ce77a2dfe72fd0384d4
Rich Davis <rdavis@yellowbilling.com.au>
2009-07-30 03:34:17 +0000

    Query Classes now have basic profiling information

--- lib/classes/DataAccess.php
+++ lib/classes/DataAccess.php
@@ -466,11 +466,11 @@ class DataAccess
 	
 	function __destruct()
 	{
-		$sTouchfile	= date("Ymdhis+u");
-		touch("/home/rdavis/touch_{$sTouchfile}.destruct.touch");
+		$sTouchfile	= FILES_BASE_PATH.'touch_'.date("Ymdhis+u");
+		touch("{$sTouchfile}.destruct.touch");
 		if ($this->getProfilingEnabled())
 		{
-			touch("/home/rdavis/touch_{$sTouchfile}.save.touch");
+			touch("{$sTouchfile}.save.touch");
 			// Write out Profiling Data to log file
 			$this->exportProfilingToXML();
 		}
