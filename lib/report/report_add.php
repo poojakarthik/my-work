600f077b28000263b63f5f56a2c44b2f875a8e86
Rich Davis <rdavis@yellowbilling.com.au>
2009-08-10 02:22:43 +0000

    added the Service on a Service Status report

--- lib/report/report_add.php
+++ lib/report/report_add.php
@@ -25,17 +25,32 @@ $arrSQLFields	= array();
 $arrDataReports = array();
 
 //---------------------------------------------------------------------------//
-// ACCOUNTS CREATED IN A DATE PERIOD
+// SERVICES OF A SERVICE TYPE ON A STATUS ON A GIVEN DATE
 //---------------------------------------------------------------------------//
 
-$arrDataReport['Name']			= "Accounts Created in a Date Period";
-$arrDataReport['Summary']		= "Show a list of Accounts which were created in the specified Date Period";
+$arrDataReport['Name']			= "Services on a Service Status";
+$arrDataReport['Summary']		= "Shows a list of Services for a given Service Type, Service Status, and Date combination";
 $arrDataReport['RenderMode']	= REPORT_RENDER_INSTANT;
 $arrDataReport['Priviledges']	= 2147483648;									// Debug
 //$arrDataReport['Priviledges']	= 1;											// Live
 $arrDataReport['CreatedOn']		= date("Y-m-d");
-$arrDataReport['SQLTable']		= "Account JOIN CustomerGroup ON CustomerGroup.Id = Account.CustomerGroup JOIN Contact ON Contact.Id = Account.PrimaryContact";
-$arrDataReport['SQLWhere']		= "Account.CreatedOn BETWEEN <StartDate> AND <EndDate> AND Account.Archived = 0";
+$arrDataReport['SQLTable']		= "	Service s
+									JOIN Account a ON (s.Account = a.Id)
+									JOIN CustomerGroup cg ON (a.CustomerGroup = cg.Id)
+									JOIN Contact c ON (a.PrimaryContact = c.Id)
+									JOIN ServiceRatePlan srp ON (s.Id = srp.Service)";
+$arrDataReport['SQLWhere']		= "	<EffectiveDate> BETWEEN CAST(srp.StartDatetime AS DATE) AND CAST(srp.EndDatetime AS DATE)
+									AND srp.Id =	(
+														SELECT		Id
+														FROM		ServiceRatePlan
+														WHERE		Service = srp.Service
+																	AND <EffectiveDate> BETWEEN CAST(StartDatetime AS DATE) AND CAST(EndDatetime AS DATE)
+														ORDER BY	CreatedOn DESC
+														LIMIT		1
+													)
+									AND s.ServiceType = <ServiceType>
+									AND s.Status = <ServiceStatus>
+									AND IF(s.ClosedOn IS NULL, <EffectiveDate> > s.CreatedOn, <EffectiveDate> BETWEEN s.CreatedOn AND s.ClosedOn)";
 $arrDataReport['SQLGroupBy']	= "";
 
 // Documentation Reqs
@@ -43,32 +58,65 @@ $arrDocReq[]	= "DataReport";
 $arrDataReport['Documentation']	= serialize($arrDocReq);
 
 // SQL Select
-$arrSQLSelect['Account #']			['Value']	= "Account.Id";
+$arrSQLSelect['FNN']				['Value']	= "s.FNN";
+$arrSQLSelect['FNN']				['Type']	= EXCEL_TYPE_FNN;
+
+$arrSQLSelect['Account #']			['Value']	= "a.Id";
 $arrSQLSelect['Account #']			['Type']	= EXCEL_TYPE_INTEGER;
 
-$arrSQLSelect['Account Name']		['Value']	= "Account.BusinessName";
+$arrSQLSelect['Account Name']		['Value']	= "a.BusinessName";
 
-$arrSQLSelect['Customer Group']		['Value']	= "CustomerGroup.external_name";
+$arrSQLSelect['Customer Group']		['Value']	= "cg.external_name";
 
-$arrSQLSelect['Contact']			['Value']	= "CONCAT(Contact.FirstName, ' ', Contact.LastName)";
+$arrSQLSelect['Contact']			['Value']	= "CONCAT(c.FirstName, ' ', c.LastName)";
 
-$arrSQLSelect['Contact Phone']		['Value']	= "CASE WHEN Contact.Phone != '' THEN Contact.Phone ELSE Contact.Mobile END";
+$arrSQLSelect['Contact Phone']		['Value']	= "IF(c.Phone != '', c.Phone, c.Mobile)";
 $arrSQLSelect['Contact Phone']		['Type']	= EXCEL_TYPE_FNN;
 
-$arrSQLSelect['Date Created']		['Value']	= "Account.CreatedOn";
+$arrSQLSelect['Rate Plan']			['Value']	= "rp.Name";
+
+$arrSQLSelect['Has Tolled']			['Value']	= "IF(s.EarliestCDR, 'Yes', 'No')";
 
 $arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
 
 // SQL Fields
-$arrSQLFields['StartDate']	= Array(
+$arrServiceTypeQuery =	array
+						(
+							'Query'			=> "	SELECT id AS Value, name AS Label
+													FROM service_type
+													ORDER BY name ASC;",
+							'ValueType'		=> "dataInteger"
+						);
+
+$arrServiceStatusQuery =	array
+							(
+								'Query'			=> "	SELECT id AS Value, name AS Label
+														FROM service_status
+														ORDER BY name ASC;",
+								'ValueType'		=> "dataInteger"
+							);
+
+
+
+$arrSQLFields['ServiceType']	= Array(
+											'Type'					=> "Query",
+											'DBQuery'				=> $arrServiceTypeQuery,
+											'Documentation-Entity'	=> "DataReport",
+											'Documentation-Field'	=> "ServiceType",
+										);
+
+
+$arrSQLFields['ServiceStatus']	= Array(
+											'Type'					=> "Query",
+											'DBQuery'				=> $arrServiceStatusQuery,
+											'Documentation-Entity'	=> "Service",
+											'Documentation-Field'	=> "Status",
+										);
+
+$arrSQLFields['EffectiveDate']	= Array(
 										'Type'					=> "dataDate",
 										'Documentation-Entity'	=> "DataReport",
 										'Documentation-Field'	=> "StartDateRange",
-									);
-$arrSQLFields['EndDate']	= Array(
-										'Type'					=> "dataDate",
-										'Documentation-Entity'	=> "DataReport",
-										'Documentation-Field'	=> "EndDateRange",
 									);
 $arrDataReport['SQLFields'] = serialize($arrSQLFields);
 
--- lib/report/report_source.php
+++ lib/report/report_source.php
@@ -2799,4 +2799,55 @@ $arrDataReports[] = $arrDataReport;
 
 
 
+//---------------------------------------------------------------------------//
+// ACCOUNTS CREATED IN A DATE PERIOD
+//---------------------------------------------------------------------------//
+
+$arrDataReport['Name']			= "Accounts Created in a Date Period";
+$arrDataReport['Summary']		= "Show a list of Accounts which were created in the specified Date Period";
+$arrDataReport['RenderMode']	= REPORT_RENDER_INSTANT;
+$arrDataReport['Priviledges']	= 2147483648;									// Debug
+//$arrDataReport['Priviledges']	= 1;											// Live
+$arrDataReport['CreatedOn']		= date("Y-m-d");
+$arrDataReport['SQLTable']		= "Account JOIN CustomerGroup ON CustomerGroup.Id = Account.CustomerGroup JOIN Contact ON Contact.Id = Account.PrimaryContact";
+$arrDataReport['SQLWhere']		= "Account.CreatedOn BETWEEN <StartDate> AND <EndDate> AND Account.Archived = 0";
+$arrDataReport['SQLGroupBy']	= "";
+
+// Documentation Reqs
+$arrDocReq[]	= "DataReport";
+$arrDataReport['Documentation']	= serialize($arrDocReq);
+
+// SQL Select
+$arrSQLSelect['Account #']			['Value']	= "Account.Id";
+$arrSQLSelect['Account #']			['Type']	= EXCEL_TYPE_INTEGER;
+
+$arrSQLSelect['Account Name']		['Value']	= "Account.BusinessName";
+
+$arrSQLSelect['Customer Group']		['Value']	= "CustomerGroup.external_name";
+
+$arrSQLSelect['Contact']			['Value']	= "CONCAT(Contact.FirstName, ' ', Contact.LastName)";
+
+$arrSQLSelect['Contact Phone']		['Value']	= "CASE WHEN Contact.Phone != '' THEN Contact.Phone ELSE Contact.Mobile END";
+$arrSQLSelect['Contact Phone']		['Type']	= EXCEL_TYPE_FNN;
+
+$arrSQLSelect['Date Created']		['Value']	= "Account.CreatedOn";
+
+$arrDataReport['SQLSelect'] = serialize($arrSQLSelect);
+
+// SQL Fields
+$arrSQLFields['StartDate']	= Array(
+										'Type'					=> "dataDate",
+										'Documentation-Entity'	=> "DataReport",
+										'Documentation-Field'	=> "StartDateRange",
+									);
+$arrSQLFields['EndDate']	= Array(
+										'Type'					=> "dataDate",
+										'Documentation-Entity'	=> "DataReport",
+										'Documentation-Field'	=> "EndDateRange",
+									);
+$arrDataReport['SQLFields'] = serialize($arrSQLFields);
+
+
+// Add the report to the array of reports to add to the database
+$arrDataReports[] = $arrDataReport;
 ?>
