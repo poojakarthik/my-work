284ab6fbfc25d2979f5aa55736e42ad51d428e02
Rich Davis <rdavis@yellowbilling.com.au>
2009-09-16 05:13:07 +0000

    if the last Invoice was Interim/Final, then you can only change a Plan effective of the next Invoice Run

--- html/admin/html_template/service/plan_change.php
+++ html/admin/html_template/service/plan_change.php
@@ -151,8 +151,8 @@ class HtmlTemplateServicePlanChange extends HtmlTemplate
 		echo "   <div class='DefaultOutput'>\n";
 		echo "      <select id='Combo_NewPlan.StartTime' name='NewPlan.StartTime' style='width:100%'>\n";
 		
-		echo "<option value='0'>".print_r(DBO()->NewPlan->PermittedStartTimes, true)."</option>\n";
-		if (in_array(0, DBO()->NewPlan->PermittedStartTimes))
+		echo "<option value='3'>".print_r(DBO()->NewPlan->PermittedStartTimes->Value, true)."</option>\n";
+		if (in_array(0, DBO()->NewPlan->PermittedStartTimes->Value))
 		{
 			if (DBO()->NewPlan->StartTime->Value == 0)
 			{
