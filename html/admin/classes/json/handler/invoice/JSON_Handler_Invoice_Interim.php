210806560bb09cfcdbf9cbc99d7ccfbfc581cc2a
Rich Davis <rdavis@yellowbilling.com.au>
2009-09-16 04:14:09 +0000

    You are no longer able to generate an Interim/Final Invoice if the last Invoice was Interim/Final

--- html/admin/classes/json/handler/invoice/JSON_Handler_Invoice_Interim.php
+++ html/admin/classes/json/handler/invoice/JSON_Handler_Invoice_Interim.php
@@ -40,7 +40,7 @@ class JSON_Handler_Invoice_Interim extends JSON_Handler
 				$intInvoiceRunType	= $objAccount->getInterimInvoiceType();
 				
 				// Check the last Invoice Type
-				$rLastInvoiceType	= $qryQuery->Execute("SELECT ir.Id, ir.BillingDate, ir.invoice_run_type_id FROM Invoice i JOIN InvoiceRun ir ON (i.invoice_run_id = ir.Id) WHERE i.Account = {$objAccount->Id} AND  AND i.Status != ".INVOICE_TEMP);
+				$rLastInvoiceType	= $qryQuery->Execute("SELECT ir.Id, ir.BillingDate, ir.invoice_run_type_id FROM Invoice i JOIN InvoiceRun ir ON (i.invoice_run_id = ir.Id) WHERE i.Account = {$objAccount->Id} AND i.Status != ".INVOICE_TEMP." ORDER BY BillingDate DESC");
 				$aLastInvoiceType	= $rLastInvoiceType->fetch_assoc();
 				if ($aLastInvoiceType && $aLastInvoiceType['invoice_run_type_id'])
 				{
