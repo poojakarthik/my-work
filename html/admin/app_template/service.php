299a469424f2e346b74409030e7ff4113f1a5b84
Rich Davis <rdavis@yellowbilling.com.au>
2009-09-16 05:03:52 +0000

    if the last Invoice was Interim/Final, then you can only change a Plan effective of the next Invoice Run

--- html/admin/app_template/service.php
+++ html/admin/app_template/service.php
@@ -2738,7 +2738,7 @@ class AppTemplateService extends ApplicationTemplate
 		}
 		$aPermittedStartTimes	= array(1);
 		$aLastInvoiceType		= $rLastInvoiceType->fetch_assoc();
-		if (!($aLastInvoiceType && !in_array($aLastInvoiceType['invoice_run_type_id'], array(INVOICE_RUN_TYPE_INTERIM, INVOICE_RUN_TYPE_FINAL))))
+		if (!($aLastInvoiceType && in_array($aLastInvoiceType['invoice_run_type_id'], array(INVOICE_RUN_TYPE_INTERIM, INVOICE_RUN_TYPE_FINAL))))
 		{
 			$aPermittedStartTimes[]	= 0;
 			/*
