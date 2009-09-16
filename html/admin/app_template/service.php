240257e1e01178174c84a2bd00824f5ecfa47e40
Rich Davis <rdavis@yellowbilling.com.au>
2009-09-16 04:38:43 +0000

    You are no longer able to change a Service's Plan if the last Invoice was Interim/Final

--- html/admin/app_template/service.php
+++ html/admin/app_template/service.php
@@ -2730,6 +2730,7 @@ class AppTemplateService extends ApplicationTemplate
 			return TRUE;
 		}
 		
+		$qryQuery			= new Query();
 		$rLastInvoiceType	= $qryQuery->Execute("SELECT ir.Id, ir.BillingDate, ir.invoice_run_type_id FROM Invoice i JOIN InvoiceRun ir ON (i.invoice_run_id = ir.Id) WHERE i.Account = ".DBO()->Account->Id->Value." AND i.Status != ".INVOICE_TEMP." ORDER BY BillingDate DESC");
 		if ($rLastInvoiceType === false)
 		{
