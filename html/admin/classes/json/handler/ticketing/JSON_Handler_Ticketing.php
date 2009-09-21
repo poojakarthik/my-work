1b2aa7f0c6248d74afb5bf17f9cfb7af2e970284
Joel Dawkins <jdawkins@yellowbilling.com.au>
2009-09-21 00:53:08 +0000

    * Fixed a bug in the SaveContact functionality, so that it checks that the associated account exists, before trying to associate the contact with the account

--- html/admin/classes/json/handler/ticketing/JSON_Handler_Ticketing.php
+++ html/admin/classes/json/handler/ticketing/JSON_Handler_Ticketing.php
@@ -249,8 +249,15 @@ class JSON_Handler_Ticketing extends JSON_Handler
 
 		$contact = Ticketing_Contact::getWithProperties($properties);
 
-		// Need to associate with an account!!
-		Ticketing_Contact_Account::associate($contact, $accountId);
+		// If a valid account was passed, then associate this contact with the account
+		$intAccountId = intval($accountId);
+		$objAccount = Account::getForId($intAccountId);
+
+		if ($objAccount !== NULL)
+		{
+			// Associate with the account
+			Ticketing_Contact_Account::associate($contact, $objAccount->id);
+		}
 
 		return $this->contactProps($contact);
 	}
