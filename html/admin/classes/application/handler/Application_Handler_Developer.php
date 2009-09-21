9f1100dcabbda92e9172adba11d93da8497f041c
Rich Davis <rdavis@yellowbilling.com.au>
2009-09-21 04:29:53 +0000

    made progress with Reflex JS Datepicker

--- html/admin/classes/application/handler/Application_Handler_Developer.php
+++ html/admin/classes/application/handler/Application_Handler_Developer.php
@@ -46,7 +46,7 @@ class Application_Handler_Developer extends Application_Handler
 														array	(
 																	'strName'	=> 'Datepicker',
 																	'strType'	=> self::URL_TYPE_JS,
-																	'strURL'	=> 'JsAutoLoader.loadScript(["js/reflex_date_format.js", "js/reflex_date_picker.js"], function(){new Reflex_Date_Picker()});'
+																	'strURL'	=> 'JsAutoLoader.loadScript(["js/reflex_date_format.js", "js/reflex_date_picker.js"], function(){new Reflex_Date_Picker()}, true);'
 																)
 													);
 			
