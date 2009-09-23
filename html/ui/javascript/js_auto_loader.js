875b5693c1960bc475e2e38af30141611c11ee6c
Rich Davis <rdavis@yellowbilling.com.au>
2009-09-23 03:49:37 +0000

    work on Datepicker

--- html/ui/javascript/js_auto_loader.js
+++ html/ui/javascript/js_auto_loader.js
@@ -13,7 +13,7 @@ var JsAutoLoader = {
 		var	strScriptName	= arrScripts.shift();
 		var fncCallback		= (arrScripts.length > 0) ? JsAutoLoader.loadScript.bind(JsAutoLoader, arrScripts, funcOnLoadEventHandler, bolUseJavascriptPhp) : funcOnLoadEventHandler;
 		
-		alert("Dynamically Loading JS File '" + strScriptName + "' (" + arrScripts.length + " JS files to load in total: " + arrScripts + ")");
+		alert("Dynamically Loading JS File '" + strScriptName + "' (" + arrScripts.length + " JS files left to load: " + arrScripts + ")");
 		
 		// Retrieve the timestamp of when the user started their session
 		// This is used as a work around, to stop the browser from using a cached, old version of the script you want
