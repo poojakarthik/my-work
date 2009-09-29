a04db90bd0880dda5ad15a710a1242c100845c89
Rich Davis <rdavis@yellowbilling.com.au>
2009-09-29 01:42:54 +0000

    testing

--- html/ui/javascript/date.js
+++ html/ui/javascript/date.js
@@ -38,7 +38,7 @@ Date.prototype.getDaysInMonth	= function()
  *	e.g.	(new Date()).shift(1, 'days');		or	(new Date()).shift(1, Date.DATE_INTERVAL_DAY);
  *			(new Date()).shift(-6, 'months');	or	(new Date()).shift(-6, Date.DATE_INTERVAL_MONTH);
  */
-Date.prototype.shift	= function(iInterval, sIntervalType)
+Date.prototype.shiftInterval	= function(iInterval, sIntervalType)
 {
 	iInterval	= parseInt(iInterval);
 	switch (sIntervalType)
