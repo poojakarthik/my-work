b97514037f4bc984d936c3a6ca77d733a08e346f
Rich Davis <rdavis@yellowbilling.com.au>
2009-10-26 02:16:46 +0000

    work on Dialler Report Import

--- html/admin/classes/json/handler/telemarketing/JSON_Handler_Telemarketing_Wash.php
+++ html/admin/classes/json/handler/telemarketing/JSON_Handler_Telemarketing_Wash.php
@@ -207,6 +207,58 @@ class JSON_Handler_Telemarketing_Wash extends JSON_Handler
 						);
 		}
 	}
+	
+	public function getRecentDiallerReports()
+	{
+		$bolVerboseErrors	= AuthenticatedUser()->UserHasPerm(PERMISSION_GOD);
+		
+		try
+		{
+			// Check user permissions
+			if (!AuthenticatedUser()->UserHasPerm(PERMISSION_PROPER_ADMIN))
+			{
+				return array(
+								'Success'			=> false,
+								'ErrorMessage'		=> "Insufficient privileges",
+								'HasPermissions'	=> false,
+							);
+			}
+			
+			$qryQuery	= new Query();
+			
+			// Get list of Imported Files
+			$resResult	= $qryQuery->Execute("SELECT FileImport.Id as file_import_id, FileImport.FileName AS file_name, FileImport.ImportedOn as file_imported_on, tfp.dealer_id, dealer.first_name AS dealer_first_name, dealer.last_name AS dealer_last_name, tfp.customer_group_id, CustomerGroup.external_name AS customer_group_name " .
+												"FROM (( FileImport JOIN telemarketing_fnn_dialled tfd ON tfd.file_import_id = FileImport.Id) JOIN CustomerGroup ON CustomerGroup.Id = tfd.customer_group_id) JOIN dealer ON dealer.id = tfd.dealer_id " .
+												"WHERE FileImport.ImportedOn >= SUBDATE(CURDATE(), INTERVAL 7 DAYS) " .
+												"GROUP BY FileImport.Id " .
+												"ORDER BY file_imported_on DESC, file_name ASC");
+			if ($resResult === false)
+			{
+				throw new Exception("There was an error retrieving the required data for this page.  If this problem continues to occur, please notify YBS." . (($bolVerboseErrors) ? "\n".$qryQuery->Error() : ''));
+			}
+			$arrImportedFiles	= array();
+			while ($arrImportedFile = $resResult->fetch_assoc())
+			{
+				$arrImportedFiles[$arrImportedFile['file_import_id']]	= $arrImportedFile;
+			}
+			
+			// If no exceptions were thrown, then everything worked
+			return array(
+							"Success"			=> TRUE,
+							"arrImportedFiles"	=> $arrImportedFiles
+						);
+		}
+		catch (Exception $e)
+		{
+			// Send an Email to Devs
+			//SendEmail("rdavis@yellowbilling.com.au", "Exception in ".__CLASS__, $e->__toString(), CUSTOMER_URL_NAME.'.errors@yellowbilling.com.au');
+			
+			return array(
+							"Success"		=> FALSE,
+							"ErrorMessage"	=> 'ERROR: '.$e->getMessage()
+						);
+		}
+	}
 }
 
 ?>
