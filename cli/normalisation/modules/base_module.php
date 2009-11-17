a7b3ac4467ed80358fce7c7cceaf4734aa4ecb9d
Rich Davis <rdavis@yellowbilling.com.au>
2009-11-17 01:31:09 +0000

    Update support for LinxOnline Usage files

--- cli/normalisation/modules/base_module.php
+++ cli/normalisation/modules/base_module.php
@@ -949,6 +949,8 @@ abstract class NormalisationModule extends CarrierModule
 	 {
 	 	static	$selUnknownDestination;
 	 	
+		CliEcho("Finding Destination Translation for Carrier {$this->intBaseCarrier} with Code '{$mixCarrierCode}' in Context {$this->_intContext}");
+	 	
 	 	// See if we have translation data for this destination
 	 	$arrData = Array("Carrier" => $this->intBaseCarrier, "CarrierCode" => $mixCarrierCode, "Context" => $this->_intContext);
 		$intResult = $this->_selFindDestination->Execute($arrData);
