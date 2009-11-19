13bd664f7c0816def619f51cead55b41a78b6f8d
Rich Davis <rdavis@yellowbilling.com.au>
2009-11-19 06:05:59 +0000

    reporting changes

--- html/ui/javascript/control_tab.js
+++ html/ui/javascript/control_tab.js
@@ -2,17 +2,24 @@ var Control_Tab	= Class.create
 ({
 	initialize	: function(strName, objContentDIV, strIconURL)
 	{
+		this.oControlTabGroup	= null;
+		
+		this.oTabPage				= document.createElement('div');
+		this.oTabPage.className		= 'tab-page';
+		this.oTabPage.hide();
+		
+		this.oTabButton				= document.createElement('div');
+		this.oTabButton.className	= 'tab';
+		
+		this.setName(strName);
+		this.setIcon(strIconURL);
+		this.setContent(objContentDIV);
+	},
+	
+	setName	: function(strName)
+	{
 		this.strName	= strName;
-		
-		if (objContentDIV)
-		{
-			this.setContent(objContentDIV);
-		}
-		
-		if (strIconURL)
-		{
-			this.setIcon(strIconURL);
-		}
+		this._paintTabButton();
 	},
 	
 	getName	: function()
@@ -27,16 +34,44 @@ var Control_Tab	= Class.create
 	
 	setContent	: function(objContentDIV)
 	{
-		this.objContentDIV	= objContentDIV;
+		this.oTabPage.removeChild(this.objContentDIV);
+		this.objContentDIV	= objContentDIV ? objContentDIV : null;
+		
+		if (this.objContentDIV)
+		{
+			this.oTabPage.appendChild(this.objContentDIV);
+		}
 	},
 	
 	setIcon	: function(strIconURL)
 	{
-		this.strIconURL	= strIconURL;
+		this.strIconURL	= strIconURL ? strIconURL : null;
+		this._paintTabButton();
 	},
 	
 	getIcon	: function(strIconURL)
 	{
 		return this.strIconURL;
+	},
+	
+	_paintTabButton	: function()
+	{
+		// Update Tab Button
+		var strTabButtonHTML	= "<span>" + this.getName().replace(/&/gmi, '&amp;').replace(/"/gmi, '&quot;').replace(/>/gmi, '&gt;').replace(/</gmi, '&lt;') + "</span>";
+		if (objControlTab.getIcon())
+		{
+			strTabButtonHTML	= "<img class='icon' alt='' title='" + objControlTab.getName() + "' src='" + objControlTab.getIcon() + "' />" + strTabButtonHTML;
+		}
+		this.oTabButton.innerHTML	= strTabButtonHTML;
+	},
+	
+	setParentGroup	: function(oControlTabGroup)
+	{
+		this.oControlTabGroup	= oControlTabGroup ? oControlTabGroup : null;
+		
+		if (this.oControlTabGroup)
+		{
+			this.oTabButton.addEventListener('click', this.oControlTabGroup.switchToTab.bind(this.oControlTabGroup, strAlias), false);
+		}
 	}
 });
--- html/ui/javascript/control_tab_group.js
+++ html/ui/javascript/control_tab_group.js
@@ -12,15 +12,13 @@ var Control_Tab_Group	= Class.create
 	 *	@param	boolean	bolEmbedded					TRUE	: Embedded in page
 	 *												FALSE	: Sole element in a Popup
 	 */
-	initialize	: function(objDIVContainer, bolEmbedded, bolFadeFX)
+	initialize	: function(objDIVContainer, bolEmbedded)
 	{
 		// Parameter defaults 
 		bolEmbedded		= (bolEmbedded || bolEmbedded == undefined || bolEmbedded == null) ? true : false;
-		//this.bolFadeFX	= (bolFadeFX) ? true : false;
-		this.bolFadeFX		= false;
 		this.oSelectedTab	= null;
 		
-		this._arrTabs		= [];
+		this._aTabOrder		= [];
 		
 		// Container
 		this.objContainer						= {};
@@ -33,13 +31,7 @@ var Control_Tab_Group	= Class.create
 		this.objContainer.objTabRow.domElement				= document.createElement('div');
 		this.objContainer.objTabRow.domElement.className	= 'tab-row';
 		this.objContainer.domElement.appendChild(this.objContainer.objTabRow.domElement);
-		/*
-		// Tab Row Clearing
-		this.objContainer.objTabRow.objClearing							= {};
-		this.objContainer.objTabRow.objClearing.domElement				= document.createElement('div');
-		this.objContainer.objTabRow.objClearing.domElement.className	= 'clearing';
-		this.objContainer.objTabRow.domElement.appendChild(this.objContainer.objTabRow.objClearing.domElement);
-		*/
+		
 		// Page Container
 		this.objContainer.objPageContainer						= {};
 		this.objContainer.objPageContainer.domElement			= document.createElement('div');
@@ -49,7 +41,7 @@ var Control_Tab_Group	= Class.create
 	
 	addTab		: function(strAlias, objControlTab)
 	{
-		var bolFirstTab	= (this._arrTabs.length == 0) ? true : false;
+		var bolFirstTab	= (this._aTabOrder.length == 0) ? true : false;
 		
 		// Check the Alias is valid and unique
 		if (strAlias.search(/^[a-z\_][\w]*$/i) == -1)
@@ -62,36 +54,14 @@ var Control_Tab_Group	= Class.create
 		}
 		
 		// Add the Tab to this Group
-		var objPage				= document.createElement('div');
-		//objPage.style.opacity	= 0;
-		objPage.style.display	= 'none';
-		objPage.className		= 'tab-page';
-		objPage.appendChild(objControlTab.getContent());
-		
-		var strTabButtonHTML	= "<span>" + objControlTab.getName().replace(/&/gmi, '&amp;').replace(/"/gmi, '&quot;').replace(/>/gmi, '&gt;').replace(/</gmi, '&lt;') + "</span>";
-		if (objControlTab.getIcon())
-		{
-			strTabButtonHTML	= "<img class='icon' alt='' title='" + objControlTab.getName() + "' src='" + objControlTab.getIcon() + "' />" + strTabButtonHTML;
-		}
-		
-		var domTabButton		= document.createElement('div');
-		domTabButton.className	= 'tab';
-		domTabButton.innerHTML	= strTabButtonHTML;
-		domTabButton.addEventListener('click', this.switchToTab.bind(this, strAlias), false);
-		
-		var objTab	= {strAlias: strAlias, domTabButton: domTabButton, objPage: objPage, objControlTab: objControlTab};
-		
-		// Fade FX
-		/*if (this.bolFadeFX)
-		{
-			objTab.objFXFade	= new FX_Fade(this.setPageOpacity.bind(this, strAlias), bolFirstTab, 10, 1);
-		}*/
+		objControlTab.setParentGroup(this);
 		
 		// Add to list of tabs
-		this._arrTabs.push(objTab);
+		this._aTabOrder.push(strAlias);
+		this._oTabs[strAlias]	= objControlTab;
 		
 		// Render
-		this._render();
+		this._paint();
 	},
 	
 	removeTab	: function(mixTab)
@@ -99,29 +69,39 @@ var Control_Tab_Group	= Class.create
 		var objControlTab	= this.getTab(mixTab);
 		if (objControlTab)
 		{
-			this._arrTabs.splice(this._arrTabs.indexOf(objControlTab), 1);
+			objControlTab.setParentGroup(null);
 			
-			this._render();
+			for (sAlias in this._oTabs)
+			{
+				if (this._oTabs[sAlias] === objControlTab)
+				{
+					delete this._oTabs[sAlias];
+					this._aTabOrder.splice(this._aTabOrder.indexOf(sAlias), 1);
+					break;
+				}
+			}
+			
+			this._paint();
 		}
 	},
 	
-	_render	: function()
+	_paint	: function()
 	{
 		// Purge existing Tabs
 		this.objContainer.objTabRow.domElement.childElements().each(this.objContainer.objTabRow.domElement.removeChild, this.objContainer.objTabRow.domElement);
 		this.objContainer.objPageContainer.domElement.childElements().each(this.objContainer.objPageContainer.domElement.removeChild, this.objContainer.objPageContainer.domElement);
 		
 		// Render Tabs
-		for (var i = 0; i < this._arrTabs.length; i++)
+		for (var i = 0; i < this._aTabOrder.length; i++)
 		{
-			this.objContainer.objPageContainer.domElement.appendChild(this._arrTabs[i].objPage);
-			this.objContainer.objTabRow.domElement.appendChild(this._arrTabs[i].domTabButton);
+			this.objContainer.objPageContainer.domElement.appendChild(this._oTabs[this._aTabOrder[i]].getPage());
+			this.objContainer.objTabRow.domElement.appendChild(this._oTabs[this._aTabOrder[i]].getButton());
 		}
 		
 		// Switch to the last selected tab (or first tab available)
 		if (!this.switchToTab(this.oSelectedTab))
 		{
-			this.switchToTab(this._arrTabs.first());
+			this.switchToTab(this._aTabOrder.first());
 		}
 	},
 	
@@ -132,21 +112,13 @@ var Control_Tab_Group	= Class.create
 		if (objControlTab)
 		{
 			// Set Visibility
-			for (var i = 0; i < this._arrTabs.length; i++)
+			for (var i = 0; i < this._aTabOrder.length; i++)
 			{
-				if (this._arrTabs[i].objControlTab == objControlTab)
+				if (this._oTabs[this._aTabOrder[i]] == objControlTab)
 				{
 					// Selected Tab
-					if (this.bolFadeFX)
-					{
-						this._arrTabs[i].objFXFade.show();
-					}
-					else
-					{
-						//this._arrTabs[i].objPage.style.opacity	= 1;
-						this._arrTabs[i].objPage.style.display	= 'block';
-					}
-					this._arrTabs[i].domTabButton.className	= 'tab selected';
+					objControlTab.getPage().show();
+					objControlTab.getButton().addClassName('selected');
 					
 					this.oSelectedTab	= objControlTab;
 					bSwitched			= true;
@@ -154,16 +126,8 @@ var Control_Tab_Group	= Class.create
 				else
 				{
 					// Non-selected Tab
-					if (this.bolFadeFX)
-					{
-						this._arrTabs[i].objFXFade.hide();
-					}
-					else
-					{
-						//this._arrTabs[i].objPage.style.opacity	= 0;
-						this._arrTabs[i].objPage.style.display	= 'none';
-					}
-					this._arrTabs[i].domTabButton.className	= 'tab';
+					this._oTabs[this._aTabOrder[i]].getPage().hide();
+					this._oTabs[this._aTabOrder[i]].getButton().removeClassName('selected');
 				}
 			}
 		}
@@ -171,51 +135,52 @@ var Control_Tab_Group	= Class.create
 		return bSwitched;
 	},
 	
-	getTab	: function(mixTab, bolAsControlTab)
+	showTab	: function(mTab)
 	{
-		bolAsControlTab	= (bolAsControlTab == undefined || bolAsControlTab == null) ? true : false;
+		// Ensure that the Tab is visible and selectable
 		
+	},
+	
+	hideTab	: function()
+	{
+		// Ensure that the Tab is invisible and not selectable
+		
+	},
+	
+	getTab	: function(mixTab)
+	{
 		if (mixTab instanceof Control_Tab)
 		{
 			// Control_Tab object
-			for (var i = 0; i < this._arrTabs.length; i++)
+			for (var i = 0; i < this._aTabOrder.length; i++)
 			{
-				if (this._arrTabs[i].objControlTab == mixTab)
+				if (this._oTabs[this._aTabOrder[i]] == mixTab)
 				{
 					// Yes -- return the Control_Tab object
-					return (bolAsControlTab) ? this._arrTabs[i].objControlTab : this._arrTabs[i];
+					return this._oTabs[this._aTabOrder[i]];
 				}
 			}
 		}
 		
 		// Is mixTab an alias?
-		for (var i = 0; i < this._arrTabs.length; i++)
+		for (var i = 0; i < this._aTabOrder.length; i++)
 		{
-			if (this._arrTabs[i].strAlias == mixTab)
+			if (this._aTabOrder[i] == mixTab)
 			{
 				// Yes -- return the Control_Tab object
-				return (bolAsControlTab) ? this._arrTabs[i].objControlTab : this._arrTabs[i];
+				return this._oTabs[this._aTabOrder[i]];
 			}
 		}
 		
 		// Is mixTab an Index?
 		var intTabIndex	= Number(mixTab);
-		if (intTabIndex >= 0 && intTabIndex < this._arrTabs.length)
+		if (intTabIndex >= 0 && intTabIndex < this._aTabOrder.length)
 		{
 			// Yes -- return the Control_Tab object
-			return (bolAsControlTab) ? this._arrTabs[intTabIndex].objControlTab : this._arrTabs[intTabIndex];
+			return this._oTabs[this._aTabOrder[intTabIndex]];
 		}
 		
 		//alert("Unable to find tab '" + mixTab + "'");
 		return false;
-	},
-	
-	setPageOpacity	: function(strAlias, fltOpacity)
-	{
-		var objTab	= this.getTab(strAlias, false);
-		if (objTab)
-		{
-			objTab.objPage.style.opacity	= fltOpacity;
-		}
 	}
 });
