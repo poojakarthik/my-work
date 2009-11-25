3d5b2d91c5d640129d7019c4cae1d03b8396b6d0
Rich Davis <rdavis@yellowbilling.com.au>
2009-11-25 02:10:59 +0000

    work on Rate Plan Add UI

--- html/admin/app_template/plan.php
+++ html/admin/app_template/plan.php
@@ -1131,7 +1131,7 @@ class AppTemplatePlan extends ApplicationTemplate
 				DBL()->discount_record_type->discount_id	= $dboRatePlanDiscount->id->Value;
 				DBL()->discount_record_type->Load();
 				
-				throw new Exception(DBL()->discount_record_type);
+				//throw new Exception(DBL()->discount_record_type);
 				
 				
 				foreach (DBL()->discount_record_type as $dboDiscountRecordType)
