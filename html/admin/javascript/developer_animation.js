1098ff69d7a78b2f790a9f70d365591c6b9eea75
Rich Davis <rdavis@yellowbilling.com.au>
2009-12-14 04:59:40 +0000

    work on Permissions UI

--- html/admin/javascript/developer_animation.js
+++ html/admin/javascript/developer_animation.js
@@ -9,30 +9,5 @@ var Developer_Animation	= Class.create(/* extends */Reflex_Popup,
 						hide	: null
 					};
 		this.container.style.opacity	= 0;
-	},
-	
-	display	: function($super)
-	{
-		if (this.fx.hide)
-		{
-			this.fx.hide.cancel();
-		}
-		$super();
-		
-		// Animate
-		this.fx.display	= new Reflex_FX_Morph(this.container, {opacity: 1}, 0.1, 'ease-out');
-		this.fx.display.start();
-	},
-	
-	hide	: function($super)
-	{
-		if (this.fx.display)
-		{
-			this.fx.display.cancel();
-		}
-		
-		// Animate
-		this.fx.hide	= new Reflex_FX_Morph(this.container, {opacity: 0}, 0.1, null, $super.bind(this));
-		this.fx.hide.start();
 	}
 });
