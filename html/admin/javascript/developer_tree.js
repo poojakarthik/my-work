b137cf2c6d64246050a90fd3ec9cdf3a2c4dedf4
Rich Davis <rdavis@yellowbilling.com.au>
2009-12-17 22:36:54 +0000

    work on a new Tree control

---
+++ html/admin/javascript/developer_tree.js
@@ -0,0 +1,30 @@
+
+Developer_Tree	= Class.create(/* extends */Reflex_Popup,
+{
+	initialize	: function($super)
+	{
+		$super(20);
+		
+		// Init Tree
+		this.oTreePanel	= new Control_Tree();
+		this.oTreePanel.setColumns(	{
+										'label'			:	{
+																sTitle	: 'File',
+																fWidth	: 50
+															},
+										'size'			:	{
+																sTitle	: 'Size',
+																fWidth	: 25
+															},
+										'modified-on'	:	{
+																sTitle	: 'Modified On',
+																fWidth	: 25
+															}
+									});
+		
+		var oContent	= document.createElement('div');
+		oContent.appendChild(this.oTreePanel.getElement());
+		
+		this.setContent(oContent);
+	}
+});
