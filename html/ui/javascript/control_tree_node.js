1ba3a9c38cccd34e369b66c3327d7a15dcc8ff17
Rich Davis <rdavis@yellowbilling.com.au>
2010-01-04 04:09:05 +0000

    Work on Tree View

--- html/ui/javascript/control_tree_node.js
+++ html/ui/javascript/control_tree_node.js
@@ -127,7 +127,7 @@ Control_Tree_Node	= Class.create
 			this.oElement.addClassName('reflex-tree-node-expanded');
 			
 			// Open
-			alert(oDimensions);
+			alert(oDimensions.toSource());
 			//this.oSlideFX	= new Reflex_FX_Shift(this.oChildrenListContainer, null, null, {height: this.oChildrenList.getHeight()+'px'}, 1.0, Control_Tree_Node.SLIDE_ANIMATION_DURATION * oPercentComplete, 'ease', (function(){this.oChildrenList.setStyle({position: 'static'});}).bind(this));
 		}
 		else
