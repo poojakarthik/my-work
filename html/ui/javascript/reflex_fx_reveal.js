0bbd3d6df2aec2f4a03068e7f8b02ee50ef328b2
Rich Davis <rdavis@yellowbilling.com.au>
2010-01-05 21:37:39 +0000

    work on the JS "Reveal" effect

--- html/ui/javascript/reflex_fx_reveal.js
+++ html/ui/javascript/reflex_fx_reveal.js
@@ -93,16 +93,16 @@ Reflex_FX_Reveal	= Class.create(/* extends */Reflex_FX,
 		switch (sDisplayMode.toLowerCase().strip())
 		{
 			case Reflex_FX_Reveal.DISPLAY_MODE_ROLLOUT:
-				this.oContainedElement.style.top	= oDirection.top	? '0px' : null;
-				this.oContainedElement.style.bottom	= oDirection.bottom	? '0px' : null;
-				this.oContainedElement.style.left	= oDirection.left	? '0px' : null;
-				this.oContainedElement.style.right	= oDirection.right	? '0px' : null;
+				this.oContainedElement.style.top	= oDirection.down	? '0px' : null;
+				this.oContainedElement.style.bottom	= oDirection.up		? '0px' : null;
+				this.oContainedElement.style.left	= oDirection.right	? '0px' : null;
+				this.oContainedElement.style.right	= oDirection.left	? '0px' : null;
 				break;
 			
 			default:
 			case Reflex_FX_Reveal.DISPLAY_MODE_SLIDE:
-				this.oContainedElement.style.top	= oDirection.top	? null : '0px';
-				this.oContainedElement.style.bottom	= oDirection.bottom	? null : '0px';
+				this.oContainedElement.style.top	= oDirection.up		? null : '0px';
+				this.oContainedElement.style.bottom	= oDirection.down	? null : '0px';
 				this.oContainedElement.style.left	= oDirection.left	? null : '0px';
 				this.oContainedElement.style.right	= oDirection.right	? null : '0px';
 				break;
