dda9d4e8a091d5fd06aa5db187381026914bd8e9
Rich Davis <rdavis@yellowbilling.com.au>
2010-01-05 06:43:13 +0000

    work on the JS "Reveal" effect

--- html/ui/javascript/reflex_fx_reveal.js
+++ html/ui/javascript/reflex_fx_reveal.js
@@ -114,9 +114,10 @@ Reflex_FX_Reveal	= Class.create(/* extends */Reflex_FX,
 	
 	_destruct	: function($super)
 	{
-		var fPercentComplete		= this.getPercentComplete(false);
-		this.oElement.style.height	= fPercentComplete < 1.0 ? this.oElement.style.height	: null;
-		this.oElement.style.width	= fPercentComplete < 1.0 ? this.oElement.style.width	: null;
+		var fPercentComplete			= this.getPercentComplete(false);
+		this.oElement.style.height		= fPercentComplete < 1.0 ? this.oElement.style.height	: null;
+		this.oElement.style.width		= fPercentComplete < 1.0 ? this.oElement.style.width	: null;
+		this.oElement.style.position	= fPercentComplete < 1.0 ? this.oElement.style.position	: null;
 		$super();
 	},
 	
