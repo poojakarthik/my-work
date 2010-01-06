45469ff0c6c72d4e7486ac4939166f6d2c8f2c17
Rich Davis <rdavis@yellowbilling.com.au>
2010-01-06 00:52:31 +0000

    work on Treeview

--- html/ui/javascript/control_tree_node.js
+++ html/ui/javascript/control_tree_node.js
@@ -223,5 +223,6 @@ Control_Tree_Node	= Class.create
 	}
 });
 
-Control_Tree_Node.SLIDE_ANIMATION_DURATION	= 0.1;
+//Control_Tree_Node.SLIDE_ANIMATION_DURATION	= 0.1;
+Control_Tree_Node.SLIDE_ANIMATION_DURATION	= 1.0;
 Control_Tree_Node.NODE_INDENT_STEPPING_EM	= 2;
