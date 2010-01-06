f0efaa85ea3977e01c1ff2496a1b56a988866f77
Rich Davis <rdavis@yellowbilling.com.au>
2010-01-06 01:04:44 +0000

    work on Treeview

--- html/ui/javascript/control_tree_node.js
+++ html/ui/javascript/control_tree_node.js
@@ -225,4 +225,4 @@ Control_Tree_Node	= Class.create
 
 Control_Tree_Node.SLIDE_ANIMATION_DURATION	= 0.1;
 //Control_Tree_Node.SLIDE_ANIMATION_DURATION	= 1.0;
-Control_Tree_Node.NODE_INDENT_STEPPING_EM	= 2;
+Control_Tree_Node.NODE_INDENT_STEPPING_EM	= 1.5;
