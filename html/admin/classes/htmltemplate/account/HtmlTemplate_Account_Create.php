fc835dbd6d9e6838d8889bfef9e37e31559f2745
Ryan Forrester <rforrester@yellowbilling.com.au>
2010-02-10 06:26:59 +0000

    New create account page updates (now 100% valid xhtml)

--- html/admin/classes/htmltemplate/account/HtmlTemplate_Account_Create.php
+++ html/admin/classes/htmltemplate/account/HtmlTemplate_Account_Create.php
@@ -13,13 +13,18 @@ class HtmlTemplate_Account_Create extends FlexHtmlTemplate
 		$strAccountCreateLink = MenuItems::ManageSales();
 		// $_GET['Associated']=1000167166
 		
-		echo "<div class='page-reset'><form method='POST' action='{$strAccountCreateLink}'>\n";
+		echo "
+		<div class='page-reset'>
+		<form method='post' action='{$strAccountCreateLink}'>\n";
 		
 		// Message Notice
 		if(array_key_exists("Associated", $_GET))
 		{
-			echo "<h1>Add Associated Account</h1><input type='hidden' name='Associated' value='{$_GET['Associated']}'>";
+			echo "
+			<h1>Add Associated Account</h1>
+			<input type='hidden' name='Associated' value='{$_GET['Associated']}' />";
 		}
+		
 		if(!array_key_exists("Associated", $_GET))
 		{
 			echo "
@@ -30,65 +35,71 @@ class HtmlTemplate_Account_Create extends FlexHtmlTemplate
 				you will need to use the &quot;Add Associated Account&quot; link from the existing Account.
 			</div>\n";
 		}
-		echo "<div class='Seperator'></div>\n";
-		
-		
-		
+
 		// Account Details
 		echo "
+		<div class='Seperator'></div>
 		<h2 class='Account'>Account Details</h2>
 		<table class='form-layout'>
 		<tr>
 			<th>Business Name:</th>
-			<td><input type='text' name='Account[BusinessName]' class='input-string' maxlength='255'></td>
+			<td><input type='text' name='Account[BusinessName]' class='input-string' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Trading Name:</th>
-			<td><input type='text' name='Account[TradingName]' class='input-string' maxlength='255'></td>
+			<td><input type='text' name='Account[TradingName]' class='input-string' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>ABN:</th>
-			<td><input type='text' name='Account[ABN]' class='input-string' maxlength='255'></td>
+			<td><input type='text' name='Account[ABN]' class='input-string' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>ACN:</th>
-			<td><input type='text' name='Account[ACN]' class='input-string' maxlength='255'></td>
+			<td><input type='text' name='Account[ACN]' class='input-string' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Address (Line 1):</th>
-			<td><input type='text' name='Account[Address1]' class='input-string' maxlength='255'></td>
+			<td><input type='text' name='Account[Address1]' class='input-string' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Address (Line 2):</th>
-			<td><input type='text' name='Account[Address2]' class='input-string' maxlength='255'></td>
+			<td><input type='text' name='Account[Address2]' class='input-string' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Suburb:</th>
-			<td><input type='text' name='Account[Suburb]' class='input-string' maxlength='255'></td>
+			<td><input type='text' name='Account[Suburb]' class='input-string' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Postcode:</th>
-			<td><input type='text' name='Account[Postcode]' class='input-string' maxlength='255'></td>
+			<td><input type='text' name='Account[Postcode]' class='input-string' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>State:</th>
-			<td><select name='Account[State]'><option value=''></option></select></td>
+			<td>
+				<select name='Account[State]'>
+					<option value=''></option>
+				</select>
+			</td>
 		</tr>
 		<tr>
 			<th>Customer Group:</th>
-			<td><select name='Account[CustomerGroup]'><option value=''></option></select></td>
+			<td>
+				<select name='Account[CustomerGroup]'>
+					<option value=''></option>
+				</select>
+			</td>
 		</tr>
 		</table>\n";
-		
 	
 		// Billing Details
 		echo "
+		<div class='Seperator'></div>
 		<h2 class='Account'>Billing Details</h2>
 		<table class='form-layout'>
 		<tr>
 			<th>NDD Fee:</th>
 			<td>
-				<input type='checkbox' name='Account[DisableDDR]' value='1'>
+				<input type='checkbox' name='Account[DisableDDR]' value='1' />
 				Do NOT charge an admin fee (non direct debit fee)
 			</td>
 		</tr>
@@ -96,9 +107,9 @@ class HtmlTemplate_Account_Create extends FlexHtmlTemplate
 			<th>Late Payments:</th>
 			<td>
 				<ul>
-					<li><input type='radio' name='Account[DisableLatePayment]' value='0'> Charge a late payment fee</li>
-					<li><input type='radio' name='Account[DisableLatePayment]' value='-1'> Don't charge a late payment fee on the next invoice</li>
-					<li><input type='radio' name='Account[DisableLatePayment]' value='1'> Never charge a late payment fee</li>
+					<li><input type='radio' name='Account[DisableLatePayment]' value='0' /> Charge a late payment fee</li>
+					<li><input type='radio' name='Account[DisableLatePayment]' value='-1' /> Don't charge a late payment fee on the next invoice</li>
+					<li><input type='radio' name='Account[DisableLatePayment]' value='1' /> Never charge a late payment fee</li>
 				</ul>
 			</td>
 		</tr>
@@ -114,30 +125,30 @@ class HtmlTemplate_Account_Create extends FlexHtmlTemplate
 			<th>Payment Method:</th>
 			<td>
 				<ul>
-					<li><input type='radio' name='Account[BillingType]' value='3'> Invoice</li>
+					<li><input type='radio' name='Account[BillingType]' value='3' /> Invoice</li>
 					<li>
-						<input type='radio' name='Account[BillingType]' value='1'> Direct Debit - from Bank Account
+						<input type='radio' name='Account[BillingType]' value='1' /> Direct Debit - from Bank Account
 						<table>
 						<tr>
 							<th>Bank Name:</th>
-							<td><input name='DDR[BankName]' maxlength='255' value='' type='text'></td>
+							<td><input name='DDR[BankName]' maxlength='255' value='' type='text' /></td>
 						</tr>
 						<tr>
 							<th>BSB #:</th>
-							<td><input name='DDR[BSB]' maxlength='6' value='' type='text'></td>
+							<td><input name='DDR[BSB]' maxlength='6' value='' type='text' /></td>
 						</tr>
 						<tr>
 							<th>Account #:</th>
-							<td><input name='DDR[AccountNumber]' maxlength='9' value='' type='text'></td>
+							<td><input name='DDR[AccountNumber]' maxlength='9' value='' type='text' /></td>
 						</tr>
 						<tr>
 							<th>Account Name:</th>
-							<td><input name='DDR[AccountName]' maxlength='255' value='' type='text'></td>
+							<td><input name='DDR[AccountName]' maxlength='255' value='' type='text' /></td>
 						</tr>
 						</table>
 					</li>
 					<li>
-						<input type='radio' name='Account[BillingType]' value='2'> Direct Debit - from Credit Card
+						<input type='radio' name='Account[BillingType]' value='2' /> Direct Debit - from Credit Card
 						<table>
 						<tr>
 							<th>Card Type:</th>
@@ -152,11 +163,11 @@ class HtmlTemplate_Account_Create extends FlexHtmlTemplate
 						</tr>
 						<tr>
 							<th>Card Holder Name:</th>
-							<td><input name='CC[Name]' maxlength='255' value='' type='text'></td>
+							<td><input name='CC[Name]' maxlength='255' value='' type='text' /></td>
 						</tr>
 						<tr>
 							<th>Credit Card Number:</th>
-							<td><input name='CC[CardNumber]' maxlength='20' value='' type='text'></td>
+							<td><input name='CC[CardNumber]' maxlength='20' value='' type='text' /></td>
 						</tr>
 						<tr>
 							<th>Expiration Date:</th>
@@ -189,7 +200,7 @@ class HtmlTemplate_Account_Create extends FlexHtmlTemplate
 						</tr>
 						<tr>
 							<th>CVV #:</th>
-							<td><input name='CC[CVV]' maxlength='255' value='' type='text'></td>
+							<td><input name='CC[CVV]' maxlength='255' value='' type='text' /></td>
 						</tr>
 						</table>
 					</li>
@@ -197,70 +208,80 @@ class HtmlTemplate_Account_Create extends FlexHtmlTemplate
 			</td>
 		</tr>
 		</table>\n";
-		
 
 		// Primary Contact Details
 		echo "
+		<div class='Seperator'></div>
 		<h2 class='Account'>Primary Contact Details</h2>
 		<table class='form-layout'>
 		<tr>
 			<th></th>
 			<td>
 				<ul>
-					<li><input type='radio' name='Contact[USE]' value='1'> Select an existing contact from the list below:</li>
-					<li><input type='radio' name='Contact[USE]' value='0'> Create a new Contact using the following details:</li>
+					<li><input type='radio' name='Contact[USE]' value='1' /> Select an existing contact from the list below:</li>
+					<li><input type='radio' name='Contact[USE]' value='0' /> Create a new Contact using the following details:</li>
 				</ul>
 			</td>
 		</tr>
 		<tr>
 			<th>Title:</th>
-			<td><select name='Contact[Title]'><option value=''></option></select></td>
+			<td>
+				<select name='Contact[Title]'>
+					<option value=''></option>
+				</select>
+			</td>
 		</tr>
 		<tr>
 			<th>First Name:</th>
-			<td><input type='text' name='Contact[FirstName]' maxlength='255'></td>
+			<td><input type='text' name='Contact[FirstName]' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Last Name:</th>
-			<td><input type='text' name='Contact[LastName]' maxlength='255'></td>
+			<td><input type='text' name='Contact[LastName]' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Date of Birth:</th>
 			<td>
-				<select name='Contact[DOB][day]'></select>
-				<select name='Contact[DOB][month]'></select>
-				<select name='Contact[DOB][year]'></select>
+				<select name='Contact[DOB][day]'>
+					<option></option>
+				</select>
+				<select name='Contact[DOB][month]'>
+					<option></option>
+				</select>
+				<select name='Contact[DOB][year]'>
+					<option></option>
+				</select>
 			</td>
 		</tr>
 		<tr>
 			<th>Job Title:</th>
-			<td><input type='text' name='Contact[JobTitle]' maxlength='255'></td>
+			<td><input type='text' name='Contact[JobTitle]' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Email Address:</th>
-			<td><input type='text' name='Contact[Email]' maxlength='255'></td>
+			<td><input type='text' name='Contact[Email]' maxlength='255' /></td>
 		</tr>
 		<tr>
 			<th>Phone Number:</th>
-			<td><input type='text' name='Contact[Phone]' maxlength='25'></td>
+			<td><input type='text' name='Contact[Phone]' maxlength='25' /></td>
 		</tr>
 		<tr>
 			<th>Mobile Number:</th>
-			<td><input type='text' name='Contact[Mobile]' maxlength='25'></td>
+			<td><input type='text' name='Contact[Mobile]' maxlength='25' /></td>
 		</tr>
 		<tr>
 			<th>Fax Number:</th>
-			<td><input type='text' name='Contact[Fax]' maxlength='25'></td>
+			<td><input type='text' name='Contact[Fax]' maxlength='25' /></td>
 		</tr>
 		<tr>
 			<th>Password:</th>
-			<td><input type='text' name='Contact[Password]' maxlength='255'></td>
+			<td><input type='text' name='Contact[Password]' maxlength='255' /></td>
 		</tr>
 		</table>\n";
-		
-		
+
 		// Legend
 		echo "
+		<div class='Seperator'></div>
 		<h2 class='Account'>Legend</h2>
 		<table class='form-layout'>
 		<tr>
@@ -275,8 +296,19 @@ class HtmlTemplate_Account_Create extends FlexHtmlTemplate
 			</td>
 		</tr>
 		</table>\n";
+
+		// Add account or Close page
+		echo "
+		<div class='Seperator'></div>
+		<div style='text-align: center;'>
+			<input type='button' name='Add_Account_Submit' value='Save' />
+			<input type='button' name='Add_Account_Cancel' value='Cancel' />
+		</div>";
 		
-		echo "</div>";
+		// End page reset wrapper
+		echo "
+		</form>
+		</div>";
 		
 	}
 	
