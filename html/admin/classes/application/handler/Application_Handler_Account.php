f40fc647a87ec7d0c6e42a711622608b2fbad005
Ryan Forrester <rforrester@yellowbilling.com.au>
2010-02-15 04:51:25 +0000

    Create account updates, added method to save the form data

--- html/admin/classes/application/handler/Application_Handler_Account.php
+++ html/admin/classes/application/handler/Application_Handler_Account.php
@@ -2,54 +2,11 @@
 
 class Application_Handler_Account extends Application_Handler
 {
+	
 	// Renders the page for Account Creation
 	public function Create($subPath)
 	{
-		/*
-		 * POSSIBLE ERRORS IN A SUBMISSION
-		Please select a valid Customer Group.
-		Please select a valid Billing Method.
-		Please select a valid Credit Card Type.
-		Please select a valid State.
-		Please enter a Business Name.
-		Please enter an ABN or ACN.
-		Please enter a valid ABN.
-		Please enter a valid ACN.
-		Please enter an Address.
-		Please enter a Suburb.
-		Please enter a Postcode.
-		
-		Please enter a Direct Debit Bank Name.
-		Please enter a Direct Debit BSB #.
-		Please enter a valid Direct Debit BSB #.
-		Please enter a Direct Debit Account #.
-		Please enter a valid Direct Debit Account #.
-		Please enter a Direct Debit Account Name.
-		
-		Please enter a Credit Card Holder Name.
-		Please enter a Credit Card #.
-		Please enter a valid Credit Card Number.
-		Please enter a Credit Card Expiry Month.
-		Please enter a Credit Card Expiry Year.
-		Please enter a valid Credit Card Expiration Date.
-		Please enter a valid Credit Card CVV.
-		
-		
-		Please enter a Title.
-		Please enter a First Name.
-		Please enter a Last Name.
-		Please enter a valid Date of Birth.
-		Please enter an Email Address.
-		Please enter a Contact Number.
-		Please enter a valid Phone Number.
-		Please enter a valid Mobile Number.
-		Please enter a valid Fax Number.
-		Please enter a Username.
-		The Username you entered already exists. Please enter a unique Username.
-		The Email you entered is already in use by an active contact. Please use this contact, or enter a different email address.
-		Please enter a Password.
-		*/
-		
+			
 		// Check user permissions
 		AuthenticatedUser()->PermissionOrDie(PERMISSION_PROPER_ADMIN);
 		
@@ -112,10 +69,6 @@ class Application_Handler_Account extends Application_Handler
 				
 			}
 			
-			
-			// Set the final breadcrumb
-			// BreadCrumb()->SetCurrentPage("Add Account");
-			
 			// Merge the PHP with the HTML template
 			$this->LoadPage('account_create', HTML_CONTEXT_DEFAULT, $arrDetailsToRender);
 			
@@ -127,5 +80,29 @@ class Application_Handler_Account extends Application_Handler
 			$this->LoadPage('error_page', HTML_CONTEXT_DEFAULT, $arrDetailsToRender);
 		}
 	}
+	
+	public function Save()
+	{
+	
+		// Check user permissions
+		AuthenticatedUser()->PermissionOrDie(PERMISSION_PROPER_ADMIN);
+		
+		try
+		{
+			
+			// Merge the PHP with the HTML template
+			$this->LoadPage('account', HTML_CONTEXT_DEFAULT, $arrDetailsToRender);
+				
+		}
+		
+		catch (Exception $eException)
+		{
+			$arrDetailsToRender['Message']		= "An error occured";
+			$arrDetailsToRender['ErrorMessage']	= $eException->getMessage();
+			$this->LoadPage('error_page', HTML_CONTEXT_DEFAULT, $arrDetailsToRender);
+		}
+			
+	}
+	
 }
 ?>
