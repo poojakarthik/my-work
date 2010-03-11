aff04c0fbf1ccf47893562b2a3f0f797e1fecadf
Ryan Forrester <rforrester@yellowbilling.com.au>
2010-03-11 23:26:33 +0000

    Wrong place

--- html/admin/classes/MenuItems.php
+++ html/admin/classes/MenuItems.php
@@ -3625,26 +3625,6 @@ else
 				break;
 		}
 	}
-	
-	//------------------------------------------------------------------------//
-	// AddAccount
-	//------------------------------------------------------------------------//
-	/**
-	 * AddAccount()
-	 *
-	 * Compiles the Href to be executed when the AddAccount menu item is triggered
-	 *
-	 * Compiles the Href to be executed when the AddAccount menu item is triggered
-	 * 
-	 * @return	string					Href to trigger the functionality
-	 * @method
-	 */
-	function SaveAccount()
-	{
-		$this->strContextMenuLabel = "Add New Account";
-		$this->strLabel = "Add New Account";
-		return self :: NEW_FRAMEWORK . "reflex.php/Account/Save/";
-	}
 
 }
 ?>
