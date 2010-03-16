f4a73ee47bbe15b097f59a1cd704f8d21f88e996
Rich Davis <rdavis@yellowbilling.com.au>
2010-03-16 01:30:56 +0000

    fixed caching in framework 1 pages

--- html/management/config/application.php
+++ html/management/config/application.php
@@ -31,13 +31,14 @@
 
 // Never Cache
 // FIXME: We should probably look into a better solution for this...
+/*
 header( 'Expires: Mon, 20 Oct 1985 10:00:00 GMT' );
 header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
 header( 'Cache-Control: no-store, no-cache, must-revalidate' );
 header( 'Cache-Control: post-check=0, pre-check=0', false );
 header( 'Cache-Control: max-age=0', false );
 header( 'Pragma: no-cache' );
-
+*/
 // sanitize page details
 
 if (is_array($arrPage['Permission']))
