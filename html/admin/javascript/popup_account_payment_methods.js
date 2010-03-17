ad9470fe77a943e4be47e9df7eaddf8b5d5b0527
Ryan Forrester <rforrester@yellowbilling.com.au>
2010-03-17 05:00:04 +0000

    popup account payment method class

---
+++ html/admin/javascript/popup_account_payment_methods.js
@@ -0,0 +1,30 @@
+var Popup_Account_Payment_Methods	= Class.create(Reflex_Popup,
+{
+	
+	initiliaze	: function()
+	{
+	
+	},
+	
+	addPaymentMethod	: function()
+	{
+		
+	},
+	
+	showPaymentMethods	: function()
+	{
+		
+	}
+
+	display	: function()
+	{
+		
+	},
+	
+	hide	: function()
+	{
+		
+	}
+	
+});
+
