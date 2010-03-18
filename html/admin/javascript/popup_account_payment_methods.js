0f87b5146f245393ba48636f19b0f5bdca0aac77
Ryan Forrester <rforrester@yellowbilling.com.au>
2010-03-18 00:45:31 +0000

    popup account payment method class

--- html/admin/javascript/popup_account_payment_methods.js
+++ html/admin/javascript/popup_account_payment_methods.js
@@ -1,30 +1,36 @@
 var Popup_Account_Payment_Methods	= Class.create(Reflex_Popup,
 {
 	
-	initiliaze	: function()
+	initialize	: function($super, iAccountId)
 	{
-	
+		// Popup Settings
+		$super(60);
+		this.setTitle("Change Payment Method");
+		this.addCloseButton();
+		this.setIcon("../admin/img/template/user_edit.png");
+		
+		// Set Content
+		this.setContent('Test: '+iAccountId);
+		
+		// Setup Buttons
+		this.domChangePaymentMethodButton				= document.createElement('button');
+		this.domChangePaymentMethodButton.innerHTML		= "Change Payment Method";
+		this.domCancelButton							= document.createElement('button');
+		this.domCancelButton.innerHTML					= "Cancel";
+		this.setFooterButtons([this.domChangePaymentMethodButton, this.domCancelButton], false);
+		
+		// Display Popup
+		this.display();
+		
+		
+		this._addEventListeners();
+		
 	},
 	
 	addPaymentMethod	: function()
 	{
 		
 	},
-	
-	showPaymentMethods	: function()
-	{
-		
-	}
-
-	display	: function()
-	{
-		
-	},
-	
-	hide	: function()
-	{
-		
-	}
 	
 });
 
