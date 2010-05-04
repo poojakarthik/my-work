dfc05b0673e8e0abf115c58a8c18957324d23493
Rich Davis <rdavis@yellowbilling.com.au>
2010-05-04 05:43:17 +0000

    Added Rollout v211 which adds some extra details for Motorpass customers

---
+++ lib/rollout/version/Flex_Rollout_Version_000211.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Version 211 of database update.
+ * This version: -
+ *
+ *	1:	Add the rebill_motorpass.account_name and card_expiry fields
+ *
+ */
+
+class Flex_Rollout_Version_000211 extends Flex_Rollout_Version
+{
+	private $rollbackSQL = array();
+	
+	public function rollout()
+	{
+		// Define operations
+		$aOperations	=	array
+							(
+								array
+								(
+									'sDescription'		=>	"Add the rebill_motorpass.account_name and card_expiry fields",
+									'sAlterSQL'			=>	"	ALTER TABLE	rebill_motorpass
+																ADD COLUMN	account_name		VARCHAR(256)	NOT NULL	COMMENT 'Motorpass Account Name',
+																ADD COLUMN	card_expiry_date	DATE			NOT NULL	COMMENT 'Expiry date for the Motorpass Card (day-of-month should be the last day in the month)';;",
+									'sRollbackSQL'		=>	"	ALTER TABLE	rebill_motorpass
+																DROP COLUMN	account_name,
+																DROP COLUMN	card_expiry_date;",
+									'sDataSourceName'	=> FLEX_DATABASE_CONNECTION_ADMIN
+								)
+							);
+		
+		// Perform Batch Rollout
+		$iRolloutVersionNumber	= self::getRolloutVersionNumber(__CLASS__);
+		$iStepNumber			= 0;
+		foreach ($aOperations as $aOperation)
+		{
+			$iStepNumber++;
+			
+			$this->outputMessage("Applying {$iRolloutVersionNumber}.{$iStepNumber}: {$aOperation['sDescription']}...\n");
+			
+			// Attempt to apply changes
+			$oResult	= Data_Source::get($aOperation['sDataSourceName'])->query($aOperation['sAlterSQL']);
+			if (PEAR::isError($oResult))
+			{
+				throw new Exception(__CLASS__ . " Failed to {$aOperation['sDescription']}. " . $oResult->getMessage() . " (DB Error: " . $oResult->getUserInfo() . ")");
+			}
+			
+			// Append to Rollback Scripts (if one or more are provided)
+			if (array_key_exists('sRollbackSQL', $aOperation))
+			{
+				$aRollbackSQL	= (is_array($aOperation['sRollbackSQL'])) ? $aOperation['sRollbackSQL'] : array($aOperation['sRollbackSQL']);
+				
+				foreach ($aRollbackSQL as $sRollbackQuery)
+				{
+					if (trim($sRollbackQuery))
+					{
+						$this->rollbackSQL[] =	$sRollbackQuery;
+					}
+				}
+			}
+		}
+	}
+
+	function rollback()
+	{
+		$dbAdmin = Data_Source::get(FLEX_DATABASE_CONNECTION_ADMIN);
+
+		if (count($this->rollbackSQL))
+		{
+			for ($l = count($this->rollbackSQL) - 1; $l >= 0; $l--)
+			{
+				$result = $dbAdmin->query($this->rollbackSQL[$l]);
+				if (PEAR::isError($result))
+				{
+					throw new Exception(__CLASS__ . ' Failed to rollback: ' . $this->rollbackSQL[$l] . '. ' . $result->getMessage());
+				}
+			}
+		}
+	}
+}
+
+?>
