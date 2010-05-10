16b94bfb7bc21c625a72b5347b105c369416d629
Rich Davis <rdavis@yellowbilling.com.au>
2010-05-10 23:28:45 +0000

    Added some debugging to Invoice_Run#export()

--- lib/classes/carrier/module/Carrier_Module.php
+++ lib/classes/carrier/module/Carrier_Module.php
@@ -124,8 +124,8 @@ class Carrier_Module extends ORM_Cached
 				case 'selAll':
 					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "1");
 					break;
-				case 'selForCarrierModuleType':
-					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "Type = <carrier_module_type_id> AND (ISNULL(<customer_group_id>) OR <customer_group_id> = customer_group) AND (<include_inactive> = 0 OR Active = 1)");
+				case 'selForCarrierModuleTypeAndCustomerGroup':
+					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "Type = <carrier_module_type_id> AND (ISNULL(<customer_group_id>) OR ISNULL(customer_group) OR <customer_group_id> = customer_group) AND (<include_inactive> = 0 OR Active = 1)");
 					break;
 				
 				// INSERTS
