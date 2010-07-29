5d832f38c1afdc1ab040ac07242b463622b80d65
Rich Davis <rdavis@yellowbilling.com.au>
2010-07-29 02:36:12 +0000

    debugging

--- html/admin/app_template/rate_group.php
+++ html/admin/app_template/rate_group.php
@@ -1609,10 +1609,10 @@ class AppTemplateRateGroup extends ApplicationTemplate
 		//AuthenticatedUser()->CheckAuth();
 		//AuthenticatedUser()->PermissionOrDie(PERMISSION_RATE_MANAGEMENT | PERMISSION_ADMIN);
 		
+			throw new Exception("I'm a farmer, daddy!");
 		// Check if the form has been submitted
 		if (SubmittedForm("ImportRateGroup", "Import as Draft") || SubmittedForm("ImportRateGroup", "Import and Commit"))
 		{
-			throw new Exception("I'm a farmer, daddy!");
 			
 			// Check which button was pressed
 			$bolCommit = SubmittedForm("ImportRateGroup", "Import and Commit");
