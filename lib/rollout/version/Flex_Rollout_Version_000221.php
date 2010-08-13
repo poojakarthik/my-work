3f4d4a38d8b4f40effee9a7e41432ef6fc583870
Rich Davis <rdavis@yellowbilling.com.au>
2010-08-13 05:34:42 +0000

    fixed a bug in Rollout v221

--- lib/rollout/version/Flex_Rollout_Version_000221.php
+++ lib/rollout/version/Flex_Rollout_Version_000221.php
@@ -4,8 +4,7 @@
  * Version 221 of database update.
  * This version: -
  *
- *	1:	Add the new AAPT Provisioning Resource Types
- *	2:	Add the new ReD Motorpass Provisioning Resource Types
+ *	1:	Add the new ReD Motorpass Provisioning Resource Types
  *
  */
 
@@ -18,19 +17,6 @@ class Flex_Rollout_Version_000221 extends Flex_Rollout_Version
 		// Define operations
 		$aOperations	=	array
 							(
-								array
-								(
-									'sDescription'		=>	"Add the new AAPT Provisioning Resource Types",
-									'sAlterSQL'			=>	"	INSERT INTO	resource_type
-																	(name								, description						, const_name		, resource_type_nature)
-																VALUES
-																	('AAPT E-Systems Preselection File'			, 'AAPT E-Systems Preselection File'		, 'RESOURCE_TYPE_FILE_EXPORT_PROVISIONING_AAPT_ESYSTEMS_PRESELECTION'		, (SELECT id FROM resource_type_nature WHERE const_name = 'RESOURCE_TYPE_NATURE_EXPORT_FILE' LIMIT 1)),
-																	('AAPT E-Systems Full Service Rebill File'	, 'AAPT E-Systems Full Service Rebill File'	, 'RESOURCE_TYPE_FILE_EXPORT_PROVISIONING_AAPT_ESYSTEMS_FULLSERVICEREBILL'	, (SELECT id FROM resource_type_nature WHERE const_name = 'RESOURCE_TYPE_NATURE_EXPORT_FILE' LIMIT 1)),
-																	('AAPT E-Systems Deactivations File'		, 'AAPT E-Systems Deactivations File'		, 'RESOURCE_TYPE_FILE_EXPORT_PROVISIONING_AAPT_ESYSTEMS_DEACTIVATIONS'		, (SELECT id FROM resource_type_nature WHERE const_name = 'RESOURCE_TYPE_NATURE_EXPORT_FILE' LIMIT 1)),
-																	('AAPT E-Systems Daily Event File'			, 'AAPT E-Systems Daily Event File'			, 'RESOURCE_TYPE_FILE_IMPORT_PROVISIONING_AAPT_ESYSTEMS_DAILYEVENT'			, (SELECT id FROM resource_type_nature WHERE const_name = 'RESOURCE_TYPE_NATURE_IMPORT_FILE' LIMIT 1));",
-									'sRollbackSQL'		=>	"	DELETE FROM	resource_type	WHERE const_name IN ('RESOURCE_TYPE_FILE_EXPORT_PROVISIONING_AAPT_ESYSTEMS_PRESELECTION', 'RESOURCE_TYPE_FILE_EXPORT_PROVISIONING_AAPT_ESYSTEMS_FULLSERVICEREBILL', 'RESOURCE_TYPE_FILE_EXPORT_PROVISIONING_AAPT_ESYSTEMS_DEACTIVATIONS', 'RESOURCE_TYPE_FILE_IMPORT_PROVISIONING_AAPT_ESYSTEMS_DAILYEVENT');",
-									'sDataSourceName'	=> FLEX_DATABASE_CONNECTION_ADMIN
-								),
 								array
 								(
 									'sDescription'		=>	"Add the new ReD Motorpass Provisioning Resource Types",
