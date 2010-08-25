70adf1a5715380c747ee031f18d51318c93139b0
Rod McTainsh <rmctainsh@yellowbilling.com.au>
2010-08-25 05:17:32 +0000

    new rollout version,
    - add new invoice_run_type 'Interim First Invoice'

---
+++ lib/rollout/version/Flex_Rollout_Version_000224.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Version 224 of database update.
+ * This version: -
+ *
+ *	1:	Add the new invoice_run_type 'Interim First Invoice'
+ *
+ */
+
+class Flex_Rollout_Version_000224 extends Flex_Rollout_Version
+{
+	private $rollbackSQL = array();
+	
+	public function rollout()
+	{
+		// Define operations
+		$aOperations	=	array
+							(
+								array
+								(
+									'sDescription'		=>	"Add the new invoice_run_type 'Interim First Invoice'",
+									'sAlterSQL'			=>	"	INSERT INTO	invoice_run_type (name, description, const_name)
+																VALUES		('Interim First Invoice', 'Interim First Invoice', 'INVOICE_RUN_TYPE_INTERIM_FIRST');",
+									'sRollbackSQL'		=>	"	DELETE FROM	invoice_run_type
+																WHERE 		const_name = 'INVOICE_RUN_TYPE_INTERIM_FIRST';",
+									'sDataSourceName'	=> FLEX_DATABASE_CONNECTION_ADMIN
+								)
+							);
+		
+		// Perform Batch Rollout
+		$iRolloutVersionNumber	= self::getRolloutVersionNumber(__CLASS__);
+		$iStepNumber			= 0;
+		foreach ($aOperations as $aOperation)
+		{
+			$iStepNumber++;
+			
+			$this->outputMessage("Applying {$iRolloutVersionNumber}.{$iStepNumber}: {$aOperation['sDescription']}...\n");
+			
+			// Attempt to apply changes
+			$oResult	= Data_Source::get($aOperation['sDataSourceName'])->query($aOperation['sAlterSQL']);
+			if (PEAR::isError($oResult))
+			{
+				throw new Exception(__CLASS__ . " Failed to {$aOperation['sDescription']}. " . $oResult->getMessage() . " (DB Error: " . $oResult->getUserInfo() . ")");
+			}
+			
+			// Append to Rollback Scripts (if one or more are provided)
+			if (array_key_exists('sRollbackSQL', $aOperation))
+			{
+				$aRollbackSQL	= (is_array($aOperation['sRollbackSQL'])) ? $aOperation['sRollbackSQL'] : array($aOperation['sRollbackSQL']);
+				
+				foreach ($aRollbackSQL as $sRollbackQuery)
+				{
+					if (trim($sRollbackQuery))
+					{
+						$this->rollbackSQL[] =	$sRollbackQuery;
+					}
+				}
+			}
+		}
+	}
+
+	function rollback()
+	{
+		$dbAdmin = Data_Source::get(FLEX_DATABASE_CONNECTION_ADMIN);
+
+		if (count($this->rollbackSQL))
+		{
+			for ($l = count($this->rollbackSQL) - 1; $l >= 0; $l--)
+			{
+				$result = $dbAdmin->query($this->rollbackSQL[$l]);
+				if (PEAR::isError($result))
+				{
+					throw new Exception(__CLASS__ . ' Failed to rollback: ' . $this->rollbackSQL[$l] . '. ' . $result->getMessage());
+				}
+			}
+		}
+	}
+}
+
+?>
