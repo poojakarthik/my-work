6125e51280a563125a2dab6db7a70556001bde73
Rich Davis <rdavis@yellowbilling.com.au>
2010-09-01 04:59:07 +0000

    test for customer portal itemistaion

--- html/customer/app_template/service.php
+++ html/customer/app_template/service.php
@@ -116,6 +116,8 @@ class AppTemplateService extends ApplicationTemplate
 		$aFNNInstances		= Service::getFNNInstances(DBO()->Service->FNN->Value, DBO()->Service->Account->Value);
 		$aRelatedServiceIds	= array_keys($aFNNInstances);
 		
+		echo "\n<!-- SERVICES: \n".print_r($aRelatedServiceIds, true)." -->\n";
+		
 		// build the where clause and array for retrieving the relevant CDRs
 		$strCDRWhereClause = "Service = <Service> OR Service IN (".implode(', ', $aRelatedServiceIds).") AND (Status = <CDRRated> OR Status = <CDRTempInvoice>)$strFilter AND Credit != 1";
 		$arrCDRWhereClause = Array("Service"=> DBO()->Service->Id->Value, "CDRRated"=> CDR_RATED, "CDRTempInvoice"=> CDR_TEMP_INVOICE);
