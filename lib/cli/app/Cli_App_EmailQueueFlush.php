b79fd8976cad3533128cd689e74dbc83ffb024dc
Rod McTainsh <rmctainsh@yellowbilling.com.au>
2010-10-11 04:52:03 +0000

    added cli app email queue flush

---
+++ lib/cli/app/Cli_App_EmailQueueFlush.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once dirname(__FILE__) . '/' . '../../../' . 'flex.require.php';
+require_once dirname(__FILE__) . '/' . '../../pdf/Flex_Pdf.php';
+
+class Cli_App_EmailQueueFlush extends Cli
+{
+	const SWITCH_TEST_RUN				= "t";
+	const SWITCH_QUEUE_ID				= "q";
+	const SWITCH_DEBUG_EMAIL_ADDRESS	= "e";
+	
+	function run()
+	{
+		try
+		{
+			// The arguments are present and in a valid format if we get past this point.
+			$arrArgs	= $this->getValidatedArguments();
+			
+			// Args
+			$bTestRun		= (bool)$arrArgs[self::SWITCH_TEST_RUN];
+			$iQueueId		= (int)$arrArgs[self::SWITCH_QUEUE_ID];
+			$sDebugAddress	= $arrArgs[self::SWITCH_DEBUG_EMAIL_ADDRESS];
+			$sDebugAddress	= !!$sDebugAddress ? $sDebugAddress	: null;
+			
+			$aEmailQueues	= array();
+			if($iQueueId && is_numeric($iQueueId))
+			{
+				// Deliver the single queue
+				$aEmailQueues[]	= Email_Queue::getForId($iQueueId);
+			}
+			else
+			{
+				// Deliver all waiting email_queue records
+				$aEmailQueues	= Email_Queue::getWaitingQueues();
+			}
+			
+			$iQueueCount	= count($aEmailQueues);
+			Log::getLog()->log("{$iQueueCount} queue".($iQueueCount == 1 ? '' : 's')." to deliver");
+			if (count($aEmailQueues) > 0)
+			{
+				// Create and save an Email_Queue_Batch
+				$oEmailQueueBatch					= new Email_Queue_Batch();
+				$oEmailQueueBatch->created_datetime	= date('Y-m-d H:i:s');
+				$oEmailQueueBatch->save();
+				
+				foreach ($aEmailQueues as $oEmailQueue)
+				{
+					// Deliver the email queue, only commits (actually sends) if NOT in test mode
+					$oEmailQueue->deliver($oEmailQueueBatch, $bTestRun, $sDebugAddress);
+				}
+				
+				Log::getLog()->log("All queues delivered");
+			}
+			
+			return 0;
+		}
+		catch(Exception $oException)
+		{
+			$this->showUsage('Error: '.$oException->getMessage());
+			return 1;
+		}
+	}
+
+	function getCommandLineArguments()
+	{
+		return array(
+
+			self::SWITCH_TEST_RUN => array(
+				self::ARG_REQUIRED		=> FALSE,
+				self::ARG_DESCRIPTION	=> "for testing script outcome [fully functional EXCEPT emails will not be sent]",
+				self::ARG_DEFAULT		=> FALSE,
+				self::ARG_VALIDATION	=> 'Cli::_validIsSet()'
+			),
+
+			self::SWITCH_QUEUE_ID => array(
+				self::ARG_REQUIRED		=> FALSE,
+				self::ARG_DESCRIPTION	=> "id of the specific email_queue to deliver",
+				self::ARG_DEFAULT		=> FALSE,
+				self::ARG_VALIDATION	=> 'Cli::_validInteger("%1$s");'
+			),
+
+			self::SWITCH_DEBUG_EMAIL_ADDRESS => array(
+				self::ARG_REQUIRED		=> FALSE,
+				self::ARG_DESCRIPTION	=> "an email address to redirect all of the sent emails to [optional, effective only if -t option supplied]",
+				self::ARG_DEFAULT		=> FALSE,
+				self::ARG_VALIDATION	=> 'Cli::_validString("%1$s");'
+			),
+		);
+	}
+
+}
+
+
+?>
