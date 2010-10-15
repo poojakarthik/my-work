498615d5e4a37f18a548c444c65583fccf9c6415
Rich Davis <rdavis@yellowbilling.com.au>
2010-10-15 06:20:31 +0000

    updated AAPT collection config

--- cli/collection/modules/config/telcoblue/aapt.cfg.php
+++ cli/collection/modules/config/telcoblue/aapt.cfg.php
@@ -5,10 +5,20 @@ require_once("../../../../../flex.require.php");
 // File Types
 $aFileTypes	= array(
 	// CDRs
+	/* Old, uncompressed location
 	RESOURCE_TYPE_FILE_IMPORT_CDR_AAPT_ESYSTEMS_CTOP				=> array(
 		'Regex'				=> '/^CTOP\_[a-z0-9]+\_\d+\_\d{14}\.txt$/i',
 		'Uniqueness'		=> "FileName = <FileName> AND SHA1 = <SHA1>",
 		'DownloadUnique'	=> true
+	),*/
+	RESOURCE_TYPE_FILE_IMPORT_CDR_AAPT_ESYSTEMS_CTOP				=> array(
+		'Regex'					=> '/^CTOP\_[a-z0-9]+\_\d+\_\d{14}\.txt\.gz$/i',
+		'Uniqueness'			=> "FileName = <FileName> AND SHA1 = <SHA1>",
+		'DownloadUnique'		=> true,
+		'Compression'			=> array(
+			'StreamWrapper'		=> 'compress.zlib',
+			'FileExtensions'	=> array('gz'=>'')	// Remove .gz (if it's the last extension)
+		)
 	),
 	// Provisioning Responses
 	RESOURCE_TYPE_FILE_IMPORT_PROVISIONING_AAPT_ESYSTEMS_DAILYEVENT	=> array(
@@ -31,9 +41,16 @@ $aDirectories	=	array(
 // PROPOSED FORMAT
 $aPaths	=	array
 			(
-				'/download/{/^CTOP\_[a-z0-9]+\_\d+\_\d{14}\.txt$/i}'	=> &$aFileTypes[RESOURCE_TYPE_FILE_IMPORT_CDR_AAPT_ESYSTEMS_CTOP],
-				'/download/{/^(D)([a-z0-9]{3})(E)(\d{14})$/i}'			=> &$aFileTypes[RESOURCE_TYPE_FILE_IMPORT_PROVISIONING_AAPT_ESYSTEMS_DAILYEVENT],
+				//'/download/{/^CTOP\_[a-z0-9]+\_\d+\_\d{14}\.txt$/i}'	=> &$aFileTypes[RESOURCE_TYPE_FILE_IMPORT_CDR_AAPT_ESYSTEMS_CTOP],
+				'/download/{/^CTOP\_[a-z0-9]+\_\d+\_\d{14}\.txt\.gz$/i}'	=> &$aFileTypes[RESOURCE_TYPE_FILE_IMPORT_CDR_AAPT_ESYSTEMS_CTOP],
+				'/download/{/^(D)([a-z0-9]{3})(E)(\d{14})$/i}'				=> &$aFileTypes[RESOURCE_TYPE_FILE_IMPORT_PROVISIONING_AAPT_ESYSTEMS_DAILYEVENT],
 			);
+
+// Config
+$aModuleConfig['Host']			['Value']		= 'ftp.powertel.com.au';
+$aModuleConfig['Username']		['Value']		= 'telcoxfr';
+$aModuleConfig['Password']		['Value']		= 'T3LCO408';
+$aModuleConfig['FileDefine']		['Value']		= $aDirectories;
 
 // Output
 $strOutputFile	= basename(__FILE__, '.cfg.php').'.serialised';
