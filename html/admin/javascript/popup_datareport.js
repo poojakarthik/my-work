46f333350c9b45dfaf8ff0376dbb26be4c4ccfe2
Rich Davis <rdavis@yellowbilling.com.au>
2011-01-04 01:25:11 +0000

    Data Report popup should now use the current year as the maximum year

--- html/admin/javascript/popup_datareport.js
+++ html/admin/javascript/popup_datareport.js
@@ -450,7 +450,7 @@ Popup_DataReport._createDateTimeSelect	= function(oInputInfo)
 						'/',
 						Popup_DataReport._createNumberSelect(1, 12, oDate.getMonth() + 1),
 						'/',
-						Popup_DataReport._createNumberSelect(2000, Date().getFullYear(), oDate.getFullYear()),
+						Popup_DataReport._createNumberSelect(2000, (new Date()).getFullYear(), oDate.getFullYear()),
 						' '
 					);
 	
