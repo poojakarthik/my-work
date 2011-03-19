64cc94e6b130432eb9c4de6db7155983b9df4ad7
Rich Davis <rdavis@yellowbilling.com.au>
2011-03-19 06:46:34 +0000

    Rollout v240 now rounds to the Rating standard when calculating Migration Balance Adjustments

--- lib/rollout/version/Flex_Rollout_Version_000240.php
+++ lib/rollout/version/Flex_Rollout_Version_000240.php
@@ -926,7 +926,7 @@ class Flex_Rollout_Version_000240 extends Flex_Rollout_Version
 		$aAdjustmentInsertData = array();
 		while ($aRow = $mResult->fetchRow(MDB2_FETCHMODE_ASSOC))
 		{
-			$fAmount = Flex::roundToRatingStandard(Flex::roundToRatingStandard($aRow['balance'], 4) - Flex::roundToRatingStandard($aRow['old_balance'], 4), 4);
+			$fAmount = Rate::roundToRatingStandard(Rate::roundToRatingStandard($aRow['balance'], 4) - Rate::roundToRatingStandard($aRow['old_balance'], 4), 4);
 			if ($fAmount < 0)
 			{
 				// Debit
