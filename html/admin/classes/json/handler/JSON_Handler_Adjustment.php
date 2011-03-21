d101875f071f31aa240f0efde24cdf72a77f52ca
Jan van der Breggen <jvanderbreggen@yellowbilling.com.au>
2011-03-21 02:43:23 +0000

    fixed reversed payment and adjustment problem and other things

--- html/admin/classes/json/handler/JSON_Handler_Adjustment.php
+++ html/admin/classes/json/handler/JSON_Handler_Adjustment.php
@@ -305,7 +305,13 @@ class JSON_Handler_Adjustment extends JSON_Handler
 				
 				// Process the adjustment
 				$oLogicAccount = Logic_Account::getInstance($oAdjustment->account_id);
-				$oLogicAccount->processDistributable(new Logic_Adjustment($oAdjustment));
+				//$oLogicAccount->processDistributable(new Logic_Adjustment($oAdjustment));
+				//simply distributing the balance of the new adjustment is not enough
+				//because there might be other distributables that could previously not distribute their balance
+				//which after distributing this one can distribute theirs. 
+				//Example: if this adjustemtn is a debit adjustment, and before distributing it the sum(collectable.balance) === 0, and there is a payment with remaining balance, that payment's balance must be distributed after distributing the current adjustment's balance.
+				//@TODO: in order to optimise this, implement functionality that allows for just distributing balances of any distributables that currently have a balance remaining, instead of a full redistribution.
+				$oLogicAccount->redistributeBalances();
 			}
 			
 			if (!$oDataAccess->TransactionCommit())
