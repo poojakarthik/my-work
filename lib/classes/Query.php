0e8b906d5fe1714d60f0e4685e1e64a7a81248d7
Rich Davis <rdavis@yellowbilling.com.au>
2011-07-26 01:00:41 +0000

    Query::run() now only returns a Query_Result if the original return value was a mysqli_result

--- lib/classes/Query.php
+++ lib/classes/Query.php
@@ -140,7 +140,7 @@
 		if (false === ($mResult = $oQuery->Execute($sQuery)) && !$bSilentFail) {
 			throw new Exception_Database($oQuery->Error());
 		}
-		return new Query_Result($mResult);
+		return ($mResult instanceof mysqli_result) ? new Query_Result($mResult) : $mResult;
 	 }
 
 	 public static function prepareByPHPType($mValue, $sConnectionType=FLEX_DATABASE_CONNECTION_DEFAULT) {
