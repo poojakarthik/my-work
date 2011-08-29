f2d4efbb33bd691b5fc26f736b2bd8b44852afcd
Rich Davis <rdavis@yellowbilling.com.au>
2011-08-29 00:44:03 +0000

    Added some debugging

--- lib/cli/app/Cli_App_Contracts.php
+++ lib/cli/app/Cli_App_Contracts.php
@@ -198,6 +198,8 @@ class Cli_App_Contracts extends Cli
 				$arrServiceRatePlan['contract_breach_reason_description']	= GetConstantDescription($arrServiceRatePlan['contract_breach_reason_id'], 'contract_breach_reason');
 			}
 
+			$this->log("Saving: ".print_r($arrServiceRatePlan, true));
+
 			// Update the ServiceRatePlan record
 			if ($ubiServiceRatePlan->Execute($arrServiceRatePlan) === FALSE)
 			{
