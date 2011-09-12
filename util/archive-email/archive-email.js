ef57e56c069e3268ece76e64f78fc6479463e4ef
Rich Davis <rdavis@yellowbilling.com.au>
2011-09-12 00:07:04 +0000

    Work on PDF cache killer app

--- util/archive-email/archive-email.js
+++ util/archive-email/archive-email.js
@@ -1,2 +1,20 @@
 
-// 
+// Arguments
+var	ARGUMENTS	= _parseArguments(process.argv, {
+	'flex-config'	: {
+		'short'		: 'c',
+		'values'	: ['sPath']
+	},
+	'list-mode'	: {
+		'short'		: 'l'
+	}
+});
+
+// TODO: Move this into a module (fw/cli/arguments)
+var	_parseArguments	= function (aArguments, oConfig) {
+	var	oArguments	= {};
+	
+	
+	
+	return oArguments;
+};
