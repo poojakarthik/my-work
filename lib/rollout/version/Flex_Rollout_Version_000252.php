4f7fc68941053d68584bb17bd43a490521dd2796
Rich Davis <rdavis@yellowbilling.com.au>
2011-11-22 02:44:59 +0000

    Added Rollout v252 which adds the "Techoni" Carrier

---
+++ lib/rollout/version/Flex_Rollout_Version_000252.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Version 252 of database update.
+ */
+
+class Flex_Rollout_Version_000252 extends Flex_Rollout_Version
+{
+	private $rollbackSQL = array();
+
+	public function rollout()
+	{
+		// Define operations
+		$aOperations = array(
+			array(
+				'sDescription'		=> "Adds the 'Techoni' Carrier",
+				'sAlterSQL'			=> "
+					INSERT INTO	Carrier
+						(Name		, carrier_type	, description	, const_name)
+					VALUES
+						('Techoni'	, (SELECT id FROM carrier_type WHERE const_name = 'CARRIER_TYPE_INVENTORY_SUPPLIER')	, 'Techoni'	, 'CARRIER_TECHONI');
+				",
+				'sRollbackSQL'		=> "
+					DELETE FROM	Carrier
+					WHERE		const_name = 'CARRIER_TECHONI';
+				",
+				'sDataSourceName'	=> FLEX_DATABASE_CONNECTION_ADMIN
+			)
+		);
+
+		// Perform Batch Rollout
+		$iRolloutVersionNumber	= self::getRolloutVersionNumber(__CLASS__);
+		$iStepNumber			= 0;
+		foreach ($aOperations as $aOperation)
+		{
+			$iStepNumber++;
+
+			$this->outputMessage("Applying {$iRolloutVersionNumber}.{$iStepNumber}: {$aOperation['sDescription']}...\n");
+
+			// Attempt to apply changes
+			$oResult	= Data_Source::get($aOperation['sDataSourceName'])->query($aOperation['sAlterSQL']);
+			if (PEAR::isError($oResult))
+			{
+				throw new Exception(__CLASS__ . " Failed to {$aOperation['sDescription']}. " . $oResult->getMessage() . " (DB Error: " . $oResult->getUserInfo() . ")");
+			}
+
+			// Append to Rollback Scripts (if one or more are provided)
+			if (array_key_exists('sRollbackSQL', $aOperation))
+			{
+				$aRollbackSQL	= (is_array($aOperation['sRollbackSQL'])) ? $aOperation['sRollbackSQL'] : array($aOperation['sRollbackSQL']);
+
+				foreach ($aRollbackSQL as $sRollbackQuery)
+				{
+					if (trim($sRollbackQuery))
+					{
+						$this->rollbackSQL[] =	$sRollbackQuery;
+					}
+				}
+			}
+		}
+	}
+
+	function rollback()
+	{
+		$dbAdmin = Data_Source::get(FLEX_DATABASE_CONNECTION_ADMIN);
+
+		if (count($this->rollbackSQL))
+		{
+			for ($l = count($this->rollbackSQL) - 1; $l >= 0; $l--)
+			{
+				$result = $dbAdmin->query($this->rollbackSQL[$l]);
+				if (PEAR::isError($result))
+				{
+					throw new Exception(__CLASS__ . ' Failed to rollback: ' . $this->rollbackSQL[$l] . '. ' . $result->getMessage());
+				}
+			}
+		}
+	}
+}
+
+?>
