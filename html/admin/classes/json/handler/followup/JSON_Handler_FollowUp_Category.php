eefcc0d1e48cb9724e4c4ca9f7b388fc507be3f1
Rich Davis <rdavis@yellowbilling.com.au>
2012-06-25 00:50:40 +0000

    + Adding a new Follow-Up should now correctly load Categories

--- html/admin/classes/json/handler/followup/JSON_Handler_FollowUp_Category.php
+++ html/admin/classes/json/handler/followup/JSON_Handler_FollowUp_Category.php
@@ -1,245 +1,194 @@
 <?php
-
-class JSON_Handler_FollowUp_Category extends JSON_Handler implements JSON_Handler_Loggable
-{
-	public function getDataSet($bCountOnly=false, $iLimit=0, $iOffset=0, $oFieldsToSort=null, $oFilter=null)
-	{
-		try
-		{
+class JSON_Handler_FollowUp_Category extends JSON_Handler implements JSON_Handler_Loggable {
+	public function getDataSet($bCountOnly=false, $iLimit=0, $iOffset=0, $oFieldsToSort=null, $oFilter=null) {
+		try {
 			// Check permissions
-			if (!AuthenticatedUser()->UserHasPerm(array(PERMISSION_OPERATOR, PERMISSION_OPERATOR_EXTERNAL)))
-			{
+			if (!AuthenticatedUser()->UserHasPerm(array(PERMISSION_OPERATOR, PERMISSION_OPERATOR_EXTERNAL))) {
 				throw new JSON_Handler_FollowUp_Category_Exception('You do not have permission to view Follow-Up Category.');
 			}
 			
-			$aSort		= get_object_vars($oFieldsToSort);
-			$aFilter	= get_object_vars($oFilter);
+			$aSort = $oFieldsToSort ? get_object_vars($oFieldsToSort) : null;
+			$aFilter = $oFilter ? get_object_vars($oFilter): null;
 			
-			if ($bCountOnly)
-			{
+			if ($bCountOnly) {
 				// Count Only
 				return 	array(
-							"Success"		=> true,
-							"iRecordCount"	=> FollowUp_Category::searchFor(null, null, $aSort, $aFilter, true)
-						);
-			}
-			else
-			{
-				$iLimit			= (max($iLimit, 0) == 0) ? null : (int)$iLimit;
-				$iOffset		= ($iLimit === null) ? null : max((int)$iOffset, 0);
-				$aCategories	= FollowUp_Category::searchFor($iLimit, $iOffset, $aSort, $aFilter);
-				$aResults		= array();
-				$iCount			= 0;		
-				foreach ($aCategories as $oCategory)
-				{
+					"Success" => true,
+					"iRecordCount" => FollowUp_Category::searchFor(null, null, $aSort, $aFilter, true)
+				);
+			} else {
+				$iLimit = (max($iLimit, 0) == 0) ? null : (int)$iLimit;
+				$iOffset = ($iLimit === null) ? null : max((int)$iOffset, 0);
+				$aCategories = FollowUp_Category::searchFor($iLimit, $iOffset, $aSort, $aFilter);
+				$aResults = array();
+				$iCount = 0;		
+				foreach ($aCategories as $oCategory) {
 					// Add to Result Set
-					$aResults[$iCount+$iOffset]	= $oCategory->toStdClass();
+					$aResults[$iCount+$iOffset] = $oCategory->toStdClass();
 					$iCount++;
 				}
 				
 				// If no exceptions were thrown, then everything worked
 				return 	array(
-							"Success"		=> true,
-							"aRecords"		=> $aResults,
-							"iRecordCount"	=> FollowUp_Category::searchFor(null, null, $aSort, $aFilter, true)
-						);
+					"Success" => true,
+					"aRecords" => $aResults,
+					"iRecordCount" => FollowUp_Category::searchFor(null, null, $aSort, $aFilter, true)
+				);
 			}
-		}
-		catch (JSON_Handler_FollowUp_Category_Exception $oException)
-		{
+		} catch (JSON_Handler_FollowUp_Category_Exception $oException) {
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> $oException->getMessage()
-					);
-		}
-		catch (Exception $e)
-		{
+				"Success" => false,
+				"Message" => $oException->getMessage()
+			);
+		} catch (Exception $e) {
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> AuthenticatedUser()->UserHasPerm(PERMISSION_GOD) ? $e->getMessage() : 'There was an error getting the dataset'
-					);
+				"Success" => false,
+				"Message" => AuthenticatedUser()->UserHasPerm(PERMISSION_GOD) ? $e->getMessage() : 'There was an error getting the dataset'
+			);
 		}
 	}
 	
-	public function getForId($iCategoryId)
-	{
-		try
-		{
+	public function getForId($iCategoryId) {
+		try {
 			// Check permissions
-			if (!AuthenticatedUser()->UserHasPerm(array(PERMISSION_OPERATOR, PERMISSION_OPERATOR_EXTERNAL)))
-			{
+			if (!AuthenticatedUser()->UserHasPerm(array(PERMISSION_OPERATOR, PERMISSION_OPERATOR_EXTERNAL))) {
 				throw new JSON_Handler_FollowUp_Category_Exception('You do not have permission to view Follow-Up Categories.');
 			}
 			
 			// If no exceptions were thrown, then everything worked
 			return 	array(
-						"Success"	=> true,
-						"oRecord"	=> FollowUp_Category::getForId($iCategoryId)->toStdClass()
-					);
-		}
-		catch (JSON_Handler_FollowUp_Category_Exception $oException)
-		{
+				"Success" => true,
+				"oRecord" => FollowUp_Category::getForId($iCategoryId)->toStdClass()
+			);
+		} catch (JSON_Handler_FollowUp_Category_Exception $oException) {
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> $oException->getMessage()
-					);
-		}
-		catch (Exception $e)
-		{
+				"Success" => false,
+				"Message" => $oException->getMessage()
+			);
+		} catch (Exception $e) {
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> AuthenticatedUser()->UserHasPerm(PERMISSION_GOD) ? $e->getMessage() : 'There was an error getting the category details'
-					);
+				"Success" => false,
+				"Message" => AuthenticatedUser()->UserHasPerm(PERMISSION_GOD) ? $e->getMessage() : 'There was an error getting the category details'
+			);
 		}
 	}
 	
-	public function deactivate($iCategoryId)
-	{
+	public function deactivate($iCategoryId) {
 		// Start a new database transaction
-		$oDataAccess	= DataAccess::getDataAccess();
-		if (!$oDataAccess->TransactionStart())
-		{
+		$oDataAccess = DataAccess::getDataAccess();
+		if (!$oDataAccess->TransactionStart()) {
 			// Failure!
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> Employee::getForId(Flex::getUserId())->isGod() ? 'There was an error accessing the database' : ''
-					);
+				"Success" => false,
+				"Message" => Employee::getForId(Flex::getUserId())->isGod() ? 'There was an error accessing the database' : ''
+			);
 		}
 		
-		try
-		{
+		try {
 			// Check permissions
-			if (!AuthenticatedUser()->UserHasPerm(array(PERMISSION_OPERATOR, PERMISSION_OPERATOR_EXTERNAL)))
-			{
+			if (!AuthenticatedUser()->UserHasPerm(array(PERMISSION_OPERATOR, PERMISSION_OPERATOR_EXTERNAL))) {
 				throw new JSON_Handler_FollowUp_Category_Exception('You do not have permission to deactivate Follow-Up Categories.');
 			}
 			
 			// Change status and save
-			$oCategory				= FollowUp_Category::getForId($iCategoryId);
-			$oCategory->status_id	= STATUS_INACTIVE;
+			$oCategory = FollowUp_Category::getForId($iCategoryId);
+			$oCategory->status_id = STATUS_INACTIVE;
 			$oCategory->save();
 			
 			$oDataAccess->TransactionCommit();
 			
 			// If no exceptions were thrown, then everything worked
 			return 	array(
-						"Success"	=> true
+						"Success" => true
 					);
-		}
-		catch (JSON_Handler_FollowUp_Category_Exception $oException)
-		{
+		} catch (JSON_Handler_FollowUp_Category_Exception $oException) {
 			$oDataAccess->TransactionRollback();
 			
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> $oException->getMessage()
+						"Success" => false,
+						"Message" => $oException->getMessage()
 					);
-		}
-		catch (Exception $e)
-		{
+		} catch (Exception $e) {
 			$oDataAccess->TransactionRollback();
 			
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> AuthenticatedUser()->UserHasPerm(PERMISSION_GOD) ? $e->getMessage() : 'There was an error deactivating the category'
+						"Success" => false,
+						"Message" => AuthenticatedUser()->UserHasPerm(PERMISSION_GOD) ? $e->getMessage() : 'There was an error deactivating the category'
 					);
 		}
 	}
 	
-	public function save($iCategoryId, $oDetails)
-	{
+	public function save($iCategoryId, $oDetails) {
 		// Start a new database transaction
-		$oDataAccess	= DataAccess::getDataAccess();
-		if (!$oDataAccess->TransactionStart())
-		{
+		$oDataAccess = DataAccess::getDataAccess();
+		if (!$oDataAccess->TransactionStart()) {
 			// Failure!
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> Employee::getForId(Flex::getUserId())->isGod() ? 'There was an error accessing the database' : ''
-					);
+				"Success" => false,
+				"Message" => Employee::getForId(Flex::getUserId())->isGod() ? 'There was an error accessing the database' : ''
+			);
 		}
 		
-		try
-		{
+		try {
 			// Check permissions
-			if (!AuthenticatedUser()->UserHasPerm(array(PERMISSION_OPERATOR, PERMISSION_OPERATOR_EXTERNAL)))
-			{
+			if (!AuthenticatedUser()->UserHasPerm(array(PERMISSION_OPERATOR, PERMISSION_OPERATOR_EXTERNAL))) {
 				throw new JSON_Handler_FollowUp_Category_Exception('You do not have permission to view Follow-Up Categories.');
 			}
 			
-			if ($iCategoryId)
-			{
-				$oCategory	= FollowUp_Category::getForId($iCategoryId);
-			}
-			else
-			{
-				$oCategory	= new FollowUp_Category();
+			if ($iCategoryId) {
+				$oCategory = FollowUp_Category::getForId($iCategoryId);
+			} else {
+				$oCategory = new FollowUp_Category();
 			}
 			
 			// Validate input
-			$iNameLength	= 128;
-			$iDescLength	= 256;
-			$aErrors		= array();
-			if (is_null($oDetails->name) || strlen($oDetails->name) == 0)
-			{
-				$aErrors[]	= 'Name missing.';
-			}
-			else if (strlen($oDetails->description) > $iNameLength)
-			{
-				$aErrors[]	= "Name is too long, maximum {$iNameLength} characters.";
+			$iNameLength = 128;
+			$iDescLength = 256;
+			$aErrors = array();
+			if (is_null($oDetails->name) || strlen($oDetails->name) == 0) {
+				$aErrors[] = 'Name missing.';
+			} else if (strlen($oDetails->description) > $iNameLength) {
+				$aErrors[] = "Name is too long, maximum {$iNameLength} characters.";
 			}
 			
-			if (is_null($oDetails->description) || strlen($oDetails->description) == 0)
-			{
-				$aErrors[]	= 'Description missing.';
-			}
-			else if (strlen($oDetails->description) > $iDescLength)
-			{
-				$aErrors[]	= "Description is too long, maximum {$iDescLength} characters.";
+			if (is_null($oDetails->description) || strlen($oDetails->description) == 0) {
+				$aErrors[] = 'Description missing.';
+			} else if (strlen($oDetails->description) > $iDescLength) {
+				$aErrors[] = "Description is too long, maximum {$iDescLength} characters.";
 			}
 			
-			if (($oDetails->status_id != STATUS_ACTIVE) && ($oDetails->status_id != STATUS_INACTIVE))
-			{
-				$aErrors[]	= 'Invalid status';
+			if (($oDetails->status_id != STATUS_ACTIVE) && ($oDetails->status_id != STATUS_INACTIVE)) {
+				$aErrors[] = 'Invalid status';
 			}
 			
-			$oCategory->name		= $oDetails->name;
-			$oCategory->description	= $oDetails->description;
-			$oCategory->status_id	= $oDetails->status_id;
+			$oCategory->name = $oDetails->name;
+			$oCategory->description = $oDetails->description;
+			$oCategory->status_id = $oDetails->status_id;
 			$oCategory->save();
 			
 			$oDataAccess->TransactionCommit();
 			
 			// If no exceptions were thrown, then everything worked
 			return 	array(
-						"Success"	=> true,
-						"iRecordId"	=> $oCategory->id
-					);
-		}
-		catch (JSON_Handler_FollowUp_Category_Exception $oException)
-		{
+				"Success" => true,
+				"iRecordId" => $oCategory->id
+			);
+		} catch (JSON_Handler_FollowUp_Category_Exception $oException) {
 			$oDataAccess->TransactionRollback();
 			
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> $oException->getMessage()
-					);
-		}
-		catch (Exception $e)
-		{
+				"Success" => false,
+				"Message" => $oException->getMessage()
+			);
+		} catch (Exception $e) {
 			$oDataAccess->TransactionRollback();
 			
 			return 	array(
-						"Success"	=> false,
-						"Message"	=> AuthenticatedUser()->UserHasPerm(PERMISSION_GOD) ? $e->getMessage() : 'There was an error getting the category details'
-					);
+				"Success" => false,
+				"Message" => AuthenticatedUser()->UserHasPerm(PERMISSION_GOD) ? $e->getMessage() : 'There was an error getting the category details'
+			);
 		}
 	}
 }
 
-class JSON_Handler_FollowUp_Category_Exception extends Exception
-{
-	// No changes
-}
-
-?>
+class JSON_Handler_FollowUp_Category_Exception extends Exception {/* No Changes */}
