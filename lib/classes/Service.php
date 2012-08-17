445d6459e22d6b47adb69150508f64826c42ddd4
Rich Davis <richjddavis@gmail.com>
2012-08-17 10:02:39 +1000

    Changing a Service's Plan will now only provision if the Service is not PENDING or ARCHIVED

--- lib/classes/Service.php
+++ lib/classes/Service.php
@@ -254,54 +254,57 @@ class Service extends ORM
 		
 		// Perform Automatic provisioning
 		if ($this->ServiceType == SERVICE_TYPE_LAND_LINE) {
-			$sNow = DataAccess::getDataAccess()->getNow();
+			// Don't provision Pending or Archived Services
+			if ($this->Status != SERVICE_PENDING && $this->Status != SERVICE_ARCHIVED) {
+				$sNow = DataAccess::getDataAccess()->getNow();
 
-			// Cancel any waiting requests for this service
-			$mResult = Query::run("	SELECT	*
-									FROM	ProvisioningRequest
-									WHERE	Service = <service_id>
-									AND		Status = <provisioning_request_status_id>;",
-									array(
-										'service_id' 						=> $this->Id,
-										'provisioning_request_status_id'	=> REQUEST_STATUS_WAITING
-									));
-			while ($aRow = $mResult->fetch_assoc()) {
-				$oRequest = new Provisioning_Request($aRow);
-				$oRequest->cancel();
+				// Cancel any waiting requests for this service
+				$mResult = Query::run("	SELECT	*
+										FROM	ProvisioningRequest
+										WHERE	Service = <service_id>
+										AND		Status = <provisioning_request_status_id>;",
+										array(
+											'service_id' 						=> $this->Id,
+											'provisioning_request_status_id'	=> REQUEST_STATUS_WAITING
+										));
+				while ($aRow = $mResult->fetch_assoc()) {
+					$oRequest = new Provisioning_Request($aRow);
+					$oRequest->cancel();
+				}
+
+				// Request data (scheduled for when the plan change becomes effective)
+				$aRequestData = array(
+					"AccountGroup"			=> $this->AccountGroup,
+					"Account"				=> $this->Account,
+					"Service"				=> $this->Id,
+					"FNN"					=> $this->FNN,
+					"Employee"				=> $intUserId,
+					"Carrier"				=> NULL,
+					"Type"					=> NULL,
+					"RequestedOn"			=> $sNow,
+					"AuthorisationDate"		=> $sNow, 
+					'scheduled_datetime'	=> $strStartDatetime,
+					"Status"				=> REQUEST_STATUS_WAITING
+				);
+				
+				// Full Service Request
+				$aRequestData['Carrier']	= $objNewRatePlan->CarrierFullService;
+				$aRequestData['Type']		= PROVISIONING_TYPE_FULL_SERVICE_PLAN_CHANGE;
+				$oFullServiceRequest 		= new Provisioning_Request($aRequestData);
+				$oFullServiceRequest->save();
+				
+				// Preselection Request
+				$aRequestData['Carrier'] 	= $objNewRatePlan->CarrierPreselection;
+				$aRequestData['Type']		= PROVISIONING_TYPE_PRESELECTION_PLAN_CHANGE;
+				$oPreSelectionRequest 		= new Provisioning_Request($aRequestData);
+				$oPreSelectionRequest->save();
+
+				// Update the ServiceRatePlan with the provisioning_request_ids
+				$oNewServiceRatePlan->fullservice_provisioning_request_id = $oFullServiceRequest->id;
+				$oNewServiceRatePlan->preselection_provisioning_request_id = $oPreSelectionRequest->id;
+				
+				$oNewServiceRatePlan->save();
 			}
-
-			// Request data (scheduled for when the plan change becomes effective)
-			$aRequestData = array(
-				"AccountGroup"			=> $this->AccountGroup,
-				"Account"				=> $this->Account,
-				"Service"				=> $this->Id,
-				"FNN"					=> $this->FNN,
-				"Employee"				=> $intUserId,
-				"Carrier"				=> NULL,
-				"Type"					=> NULL,
-				"RequestedOn"			=> $sNow,
-				"AuthorisationDate"		=> $sNow, 
-				'scheduled_datetime'	=> $strStartDatetime,
-				"Status"				=> REQUEST_STATUS_WAITING
-			);
-			/*
-			// Full Service Request
-			$aRequestData['Carrier']	= $objNewRatePlan->CarrierFullService;
-			$aRequestData['Type']		= PROVISIONING_TYPE_FULL_SERVICE_PLAN_CHANGE;
-			$oFullServiceRequest 		= new Provisioning_Request($aRequestData);
-			$oFullServiceRequest->save();
-			
-			// Preselection Request
-			$aRequestData['Carrier'] 	= $objNewRatePlan->CarrierPreselection;
-			$aRequestData['Type']		= PROVISIONING_TYPE_PRESELECTION_PLAN_CHANGE;
-			$oPreSelectionRequest 		= new Provisioning_Request($aRequestData);
-			$oPreSelectionRequest->save();
-
-			// Update the ServiceRatePlan with the provisioning_request_ids
-			$oNewServiceRatePlan->fullservice_provisioning_request_id = $oFullServiceRequest->id;
-			$oNewServiceRatePlan->preselection_provisioning_request_id = $oPreSelectionRequest->id;
-			*/
-			$oNewServiceRatePlan->save();
 		}
 		
 		// Add a system note describing the change of plan
