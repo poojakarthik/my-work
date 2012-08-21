b1f5609884372755969c8c1b820b3eb64d6325e5
Rich Davis <richjddavis@gmail.com>
2012-08-21 17:25:07 +1000

    Exception_Assertion will now include the message in the subject if no title is provided

--- lib/classes/exception/Exception_Assertion.php
+++ lib/classes/exception/Exception_Assertion.php
@@ -1,57 +1,32 @@
 <?php
-/**
- * Exception_Assertion
- *
- * This Exception should be thrown when an assertion fails
- * 
+/*
  * This Exception should be thrown when an assertion fails
  * That is to say, if something happens that should NEVER happen (indicating a bug in the system), this Exception should be used.
  * Instanciating an Exception_Assertion, will automatically send an EMAIL_NOTIFICATION_ALERT email to all predefined recipients of this email notification type
- *
- * @class	Exception_Assertion
  */
-class Exception_Assertion extends Exception
-{
-	/**
-	 * __construct()
-	 *
-	 * Constructor
-	 * 
-	 * Constructor
-	 * This will automatically send an EMAIL_NOTIFICATION_ALERT email to all predefined recipients of this email notification type
-	 *
-	 * @param	string	[ $strMessage ]			Exception Message. This will be include in the body of the EMAIL_NOTIFICATION_ALERT email, Defaults to NULL
-	 * @param	string	[ $strExtraDetails ]	Defaults to NULL.  If defined then these will also be included in the body of the EMAIL_NOTIFICATION_ALERT email
-	 * @param	string	[ $strAssertionName ]	Defaults to NULL.  If included, then this will form part of the subject for the EMAIL_NOTIFICATION_ALERT email
-	 * 
-	 * @return	void
-	 * 
-	 * @method
-	 */
-	public function __construct($strMessage=NULL, $strExtraDetails=NULL, $strAssertionName=NULL)
+class Exception_Assertion extends Exception {
+	public function __construct($sMessage=null, $sExtraDetails=null, $sAssertionName=null)
 	{
-		parent::__construct($strMessage);
+		parent::__construct($sMessage);
 		
 		// Send an EMAIL_NOTIFICATION_ALERT email
-		if (strlen($strMessage) == 0)
-		{
-			$strMessage = "[ No Message ]";
+		if (!strlen($sMessage)) {
+			$sMessage = "[ No Message ]";
 		}
 		
-		$strDetails = $strMessage;
-		if ($strExtraDetails)
-		{
-			$strDetails .= "\n\nFurther Details:\n"
-						. (is_string($strExtraDetails) ? $strExtraDetails : print_r($strExtraDetails, true));
+		$sDetails = $sMessage;
+		if ($sExtraDetails) {
+			$sDetails .= "\n\nFurther Details:\n"
+						. (is_string($sExtraDetails) ? $sExtraDetails : print_r($sExtraDetails, true));
 		}
 		
-		$strEmailSubject = "Assertion Failed";
-		if (strlen($strAssertionName))
-		{
-			$strEmailSubject .= " - $strAssertionName";
+		$sEmailSubject = "Assertion Failed";
+		if (strlen($sAssertionName)) {
+			$sEmailSubject .= " - {$sAssertionName}";
+		} elseif () {
+			$sEmailSubject .= " - {$sMessage}";
 		}
 		
-		Flex::sendEmailNotificationAlert($strEmailSubject, $strDetails, FALSE, TRUE);
+		Flex::sendEmailNotificationAlert($sEmailSubject, $sDetails, false, true);
 	}
 }
-?>
