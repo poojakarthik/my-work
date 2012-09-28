005608d5c050231f250c29255157b88ceba8969c
Rich Davis <richjddavis@gmail.com>
2012-09-28 11:00:31 +1000

    Customer Group "Base URLs" should now not require a protocol

--- html/customer/app_template/user.php
+++ html/customer/app_template/user.php
@@ -1,86 +1,86 @@
 <?php
 class AppTemplateUser extends ApplicationTemplate {
 
-	const	DEBUG	= false;
+	const DEBUG = false;
 
-	const	ACCOUNT_NAME_LEVENSHTEIN_ENABLED					= true;
-	const	ACCOUNT_NAME_LEVENSHTEIN_DISTANCE_MAX_PERCENTAGE	= 0.2;	// Distance can be a maximum of 20% of the string length
-	const	ACCOUNT_NAME_LEVENSHTEIN_MIN_LENGTH					= 8;	// Minimum length of the real Account Name for Levenshtein allowance is enabled
+	const ACCOUNT_NAME_LEVENSHTEIN_ENABLED = true;
+	const ACCOUNT_NAME_LEVENSHTEIN_DISTANCE_MAX_PERCENTAGE = 0.2; // Distance can be a maximum of 20% of the string length
+	const ACCOUNT_NAME_LEVENSHTEIN_MIN_LENGTH = 8; // Minimum length of the real Account Name for Levenshtein allowance is enabled
 
-	const	USER_PASSWORD_LENGTH_MIN	= 6;
-	const	USER_PASSWORD_LENGTH_MAX	= 40;
+	const USER_PASSWORD_LENGTH_MIN = 6;
+	const USER_PASSWORD_LENGTH_MAX = 40;
 
-	const	USER_USERNAME_LENGTH_MIN	= 3;
-	const	USER_USERNAME_LENGTH_MAX	= 30;
+	const USER_USERNAME_LENGTH_MIN = 3;
+	const USER_USERNAME_LENGTH_MAX = 30;
 
 	public function Register() {
 		try {
-			$oCustomerGroup	= Customer_Group::getForBaseURL(($_SERVER['HTTPS'] ? 'https://' : 'http://').$_SERVER['HTTP_HOST']);
+			$oCustomerGroup = Customer_Group::getForBaseURL($_SERVER['HTTP_HOST']);
 
 			BreadCrumb()->SetCurrentPage("User Registration");
 
-			$bRegistrationSuccessful	= false;
+			$bRegistrationSuccessful = false;
 
 			// Were we POSTed to?
 			if (strtoupper($_SERVER['REQUEST_METHOD']) == 'POST') {
 				//throw new Exception("Registration is not yet supported");
 
-				$aValidationErrors	= array();
+				$aValidationErrors = array();
 
-				$oAccountUser	= new Account_User();
+				$oAccountUser = new Account_User();
 				
 				// Validation
 				//---------------------------------------------------------------//
 				// User: Username
 				try {
-					$sUsername	= self::_validateUsername($_POST['user-username']);
+					$sUsername = self::_validateUsername($_POST['user-username']);
 				} catch (AppTemplateUserExceptionValidation $oUsernameException) {
-					$aValidationErrors['user-username']	= $oUsernameException->getRelevantMessage();
+					$aValidationErrors['user-username'] = $oUsernameException->getRelevantMessage();
 				}
-				$oAccountUser->username	= $sUsername;
+				$oAccountUser->username = $sUsername;
 
 				// User: Password
 				try {
-					$sPassword	= self::_validatePassword($_POST['user-password'], $_POST['user-password-confirm']);
+					$sPassword = self::_validatePassword($_POST['user-password'], $_POST['user-password-confirm']);
 				} catch (AppTemplateUserExceptionValidation $oPasswordException) {
-					$aValidationErrors['user-password']	= $oPasswordException->getRelevantMessage();
+					$aValidationErrors['user-password'] = $oPasswordException->getRelevantMessage();
 				}
-				$oAccountUser->password	= sha1($sPassword);
+				$oAccountUser->password = sha1($sPassword);
 
 				// User: Actual Name
-				$oAccountUser->given_name	= $_POST['user-givenname'];
-				$oAccountUser->family_name	= $_POST['user-familyname'];
+				$oAccountUser->given_name = $_POST['user-givenname'];
+				$oAccountUser->family_name = $_POST['user-familyname'];
 				if (!strlen($oAccountUser->given_name)) {
-					$aValidationErrors['user-givenname']	= 'Please supply your Given Name';
+					$aValidationErrors['user-givenname'] = 'Please supply your Given Name';
 				}
 				if (!strlen($oAccountUser->family_name)) {
-					$aValidationErrors['user-familyname']	= 'Please supply your Family Name';
+					$aValidationErrors['user-familyname'] = 'Please supply your Family Name';
 				}
 
 				// User: Email
-				$oAccountUser->email	= trim($_POST['user-email']);
+				$oAccountUser->email = trim($_POST['user-email']);
 				if (!preg_match('/^[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i', $oAccountUser->email)) {
-					$aValidationErrors['user-email']	= "The supplied email address is invalid.";
+					$aValidationErrors['user-email'] = "The supplied email address is invalid.";
 				}
 
 				// Account
 				try {
-					$oAccount	= self::_validateAccount($_POST['account-number'], $_POST['account-name'], $_POST['account-invoice-number'], $oCustomerGroup);
+					$oAccount = self::_validateAccount($_POST['account-number'], $_POST['account-name'], $_POST['account-invoice-number'], $oCustomerGroup);
 				} catch (AppTemplateUserExceptionValidation $oAccountException) {
-					$aValidationErrors['account']	= $oAccountException->getRelevantMessage();
+					$aValidationErrors['account'] = $oAccountException->getRelevantMessage();
 				}
-				$oAccountUser->account_id	= $oAccount->Id;
+				$oAccountUser->account_id = $oAccount->Id;
 				//---------------------------------------------------------------//
 				
 				if (!$aValidationErrors) {
 					// Save to DB
 					//---------------------------------------------------------------//
-					$oDataAccess	= DataAccess::getDataAccess();
+					$oDataAccess = DataAccess::getDataAccess();
 					
 					// Start Transaction
 					$oDataAccess->TransactionStart();
 					try {
-						$oAccountUser->status_id	= STATUS_ACTIVE;
+						$oAccountUser->status_id = STATUS_ACTIVE;
 						$oAccountUser->save();
 
 						//throw new Exception("DEBUG LOL! Saving: ".print_r($oAccountUser->toArray(), true));
@@ -97,23 +97,23 @@ class AppTemplateUser extends ApplicationTemplate {
 					// Log the user in automatically and redirect to the console
 					//---------------------------------------------------------------//
 					// HACKHACKHACK: Lovely hacks.  Fake authentication then redirect to the console.
-					$_POST['VixenUserName']	= $sUsername;
-					$_POST['VixenPassword']	= $sPassword;
+					$_POST['VixenUserName'] = $sUsername;
+					$_POST['VixenPassword'] = $sPassword;
 					AuthenticatedUser()->CheckClientAuth();
 
 					header("Location: ".Href()->Home());
 					exit;
 					//---------------------------------------------------------------//
 				} else {
-					DBO()->GeneralError->Message	= 'There were errors with some of the information you supplied.  Please look below for more details.';
-					DBO()->ValidationErrors->Fields	= $aValidationErrors;
+					DBO()->GeneralError->Message = 'There were errors with some of the information you supplied.  Please look below for more details.';
+					DBO()->ValidationErrors->Fields = $aValidationErrors;
 				}
 			}
 		} catch (Exception $oException) {
 			// TODO: Somehow pass a "general" 500-style error through to user_register
 			// FIXME: This error text should probably be handled in the renderer.
-			//$sGeneralError	= "An internal error caused your registration to fail.  If you continue to see this message, contact us using one of the methods listed on [CUSTOMER_GROUP_URL]";
-			DBO()->GeneralError->Message	= (self::DEBUG) ? $oException->getMessage() : "An internal error caused your registration to fail — please try again.";
+			//$sGeneralError = "An internal error caused your registration to fail.  If you continue to see this message, contact us using one of the methods listed on [CUSTOMER_GROUP_URL]";
+			DBO()->GeneralError->Message = (self::DEBUG) ? $oException->getMessage() : "An internal error caused your registration to fail — please try again.";
 		}
 
 		$this->LoadPage('user_register');
@@ -121,7 +121,7 @@ class AppTemplateUser extends ApplicationTemplate {
 	}
 
 	private static function _validateUsername($sUsername) {
-		$sCleanedUsername	= trim($sUsername);
+		$sCleanedUsername = trim($sUsername);
 
 		// Validate
 		if (!strlen($sCleanedUsername)) {
@@ -133,9 +133,9 @@ class AppTemplateUser extends ApplicationTemplate {
 
 		// Ensure there are no other users with this username
 		if (Query::run("
-			SELECT		*
-			FROM		account_user au
-			WHERE		au.username = <sUsername> /* Case-insensitive match */
+			SELECT *
+			FROM account_user au
+			WHERE au.username = <sUsername> /* Case-insensitive match */
 		", array('sUsername'=>$sCleanedUsername))->num_rows) {
 			throw new AppTemplateUserExceptionValidation("{$sUsername} is already in use.  Please try another username.");
 		}
@@ -168,23 +168,23 @@ class AppTemplateUser extends ApplicationTemplate {
 		// Account Number
 		//-------------------------------------------------------------------//
 		// Validate format
-		$iAccountNumber	= (int)$sAccountNumber;
+		$iAccountNumber = (int)$sAccountNumber;
 		if (!preg_match('/^[0-9]+$/', trim($sAccountNumber))) {
 			throw new AppTemplateUserExceptionValidation("{$sAccountNumber} is not a valid Account Number", "We couldn't verify your Account details.  Please double-check them and try again.");
 		}
 		// Find the relevant Account
 		try {
-			$oAccount	= Account::getForId((int)$iAccountNumber);
+			$oAccount = Account::getForId((int)$iAccountNumber);
 		} catch (Exception_ORM $oAccountNotFoundException) {
 			throw new AppTemplateUserExceptionValidation("Account '{$iAccountNumber}' doesn't exist", "We couldn't verify your Account details.  Please double-check them and try again.");
 		}
 
 		// Ensure that the Account is from the correct Customer Group
-		$oAccountCustomerGroup	= Customer_Group::getForId($oAccount->CustomerGroup);
+		$oAccountCustomerGroup = Customer_Group::getForId($oAccount->CustomerGroup);
 		if (!in_array($oAccountCustomerGroup->id, array_keys($aAllowableCustomerGroups))) {
-			$aCustomerGroupNames	= array();
+			$aCustomerGroupNames = array();
 			foreach ($aAllowableCustomerGroups as $oAllowableCustomerGroup) {
-				$aCustomerGroupNames[]	= $oAllowableCustomerGroup->internal_name;
+				$aCustomerGroupNames[] = $oAllowableCustomerGroup->internal_name;
 			}
 			throw new AppTemplateUserExceptionValidation("Account {$iAccountNumber} is a {$oAccountCustomerGroup->internal_name} Account, but URL restricts to [".implode(', ', $aCustomerGroupNames)."]", "We couldn't verify your Account details.  Please double-check them and try again.");
 		}
@@ -192,13 +192,13 @@ class AppTemplateUser extends ApplicationTemplate {
 
 		// Account Name
 		//-------------------------------------------------------------------//
-		$sMinifiedSuppliedName	= preg_replace('/[^a-z0-9]/', '', strtolower($sAccountName));
-		$sMinifiedActualName	= preg_replace('/[^a-z0-9]/', '', strtolower($oAccount->BusinessName));
+		$sMinifiedSuppliedName = preg_replace('/[^a-z0-9]/', '', strtolower($sAccountName));
+		$sMinifiedActualName = preg_replace('/[^a-z0-9]/', '', strtolower($oAccount->BusinessName));
 
 		if (self::ACCOUNT_NAME_LEVENSHTEIN_ENABLED && strlen($sMinifiedActualName) >= self::ACCOUNT_NAME_LEVENSHTEIN_MIN_LENGTH) {
 			// Calculate Levenshtein distance (allow close matches)
-			$iAccountNameLevenshtein		= levenshtein($sMinifiedSuppliedName, $sMinifiedActualName);
-			$iPermittedLevenshteinDistance	= floor(strlen($sMinifiedActualName) * self::ACCOUNT_NAME_LEVENSHTEIN_DISTANCE_MAX_PERCENTAGE);
+			$iAccountNameLevenshtein = levenshtein($sMinifiedSuppliedName, $sMinifiedActualName);
+			$iPermittedLevenshteinDistance = floor(strlen($sMinifiedActualName) * self::ACCOUNT_NAME_LEVENSHTEIN_DISTANCE_MAX_PERCENTAGE);
 			if ($iAccountNameLevenshtein === -1) {
 				// Algortithm only works up to 255 characters
 				throw new AppTemplateUserExceptionValidation("Account Name exceeds limits of Levenshtein algorithm (Supplied: '{$sMinifiedSuppliedName}'; Actual: '{$sMinifiedActualName}'; Distance: {$iAccountNameLevenshtein}; Permitted Distance: {$iPermittedLevenshteinDistance})", "We couldn't verify your Account details.  Please double-check them and try again.");
@@ -215,13 +215,13 @@ class AppTemplateUser extends ApplicationTemplate {
 		// Recent Invoice
 		//-------------------------------------------------------------------//
 		// Validate format
-		$iInvoiceNumber	= (int)$sRecentInvoice;
+		$iInvoiceNumber = (int)$sRecentInvoice;
 		if (!preg_match('/^[0-9]+$/', trim($sRecentInvoice))) {
 			throw new AppTemplateUserExceptionValidation("{$sRecentInvoice} is not a valid Invoice Number", "We couldn't verify your Account details.  Please double-check them and try again.");
 		}
 		// Find the relevant Invoice
 		try {
-			$oInvoice	= Invoice::getForId((int)$iInvoiceNumber);
+			$oInvoice = Invoice::getForId((int)$iInvoiceNumber);
 		} catch (Exception_ORM $oInvoiceNotFoundException) {
 			throw new AppTemplateUserExceptionValidation("Invoice '{$iAccountNumber}' doesn't exist", "We couldn't verify your Account details.  Please double-check them and try again.");
 		}
@@ -232,24 +232,24 @@ class AppTemplateUser extends ApplicationTemplate {
 
 		// Is this Invoice one of the 3 most recent?
 		if (!Query::run("
-			SELECT		i_recent.*
-			FROM		Account a
+			SELECT i_recent.*
+			FROM Account a
 						JOIN (
-							SELECT		i_recent_i.*
-							FROM		Invoice i_recent_i
+							SELECT i_recent_i.*
+							FROM Invoice i_recent_i
 										JOIN InvoiceRun i_recent_ir ON (i_recent_ir.Id = i_recent_i.invoice_run_id)
-							WHERE		i_recent_i.Account = <iAccountId>
+							WHERE i_recent_i.Account = <iAccountId>
 										AND i_recent_ir.invoice_run_type_id IN (
 											".INVOICE_RUN_TYPE_LIVE.",
 											".INVOICE_RUN_TYPE_INTERIM.",
 											".INVOICE_RUN_TYPE_FINAL.",
 											".INVOICE_RUN_TYPE_INTERIM_FIRST."
 										)
-							ORDER BY	i_recent_i.CreatedOn DESC,
+							ORDER BY i_recent_i.CreatedOn DESC,
 										i_recent_i.Id DESC
-							LIMIT		3
+							LIMIT 3
 						) i_recent
-			WHERE		a.Id = <iAccountId>
+			WHERE a.Id = <iAccountId>
 						AND i_recent.Id = <iInvoiceId>;
 		", array('iAccountId'=>$iAccountNumber, 'iInvoiceId'=>$iInvoiceNumber))->num_rows) {
 			// No match
@@ -263,11 +263,11 @@ class AppTemplateUser extends ApplicationTemplate {
 };
 
 class AppTemplateUserExceptionValidation extends Exception {
-	public static $bForceDebugMessages	= false;	// DEBUG
+	public static $bForceDebugMessages = false; // DEBUG
 
 	public function __construct($sDebugMessage, $sUserMessage=null) {
-		$this->_sDebugMessage	= $sDebugMessage;
-		$this->_sUserMessage	= ($sUserMessage) ? $sUserMessage : $sDebugMessage;
+		$this->_sDebugMessage = $sDebugMessage;
+		$this->_sUserMessage = ($sUserMessage) ? $sUserMessage : $sDebugMessage;
 		parent::__construct($this->getRelevantMessage());
 	}
 
