86479ce09d8f20f6edbb3318d6f1410ae5a9df34
Rich Davis <richjddavis@gmail.com>
2012-10-31 08:19:56 +1000

    Added an assertion to assure there is always a contract_terms record (and code cleanup)

--- html/admin/classes/json/handler/service/JSON_Handler_Service_Contract.php
+++ html/admin/classes/json/handler/service/JSON_Handler_Service_Contract.php
@@ -1,132 +1,129 @@
 <?php
-
 class JSON_Handler_Service_Contract extends JSON_Handler implements JSON_Handler_Loggable, JSON_Handler_Catchable {
 	
 	public function getDetails($iServiceRatePlanId) {
-		$oServiceRatePlan		= Service_Rate_Plan::getForId((int)$iServiceRatePlanId);
-		$oService				= Service::getForId($oServiceRatePlan->Service);
-		$oServiceType			= Service_Type::getForId($oService->ServiceType);
-		$oRatePlan				= Rate_Plan::getForId($oServiceRatePlan->RatePlan);
-		$oContractStatus		= Contract_Status::getForId($oServiceRatePlan->contract_status_id);
-		$oAccount				= Account::getForId($oService->Account);
-		$oCustomerGroup			= Customer_Group::getForId($oAccount->CustomerGroup);
+		$oServiceRatePlan = Service_Rate_Plan::getForId((int)$iServiceRatePlanId);
+		$oService = Service::getForId($oServiceRatePlan->Service);
+		$oServiceType = Service_Type::getForId($oService->ServiceType);
+		$oRatePlan = Rate_Plan::getForId($oServiceRatePlan->RatePlan);
+		$oContractStatus = Contract_Status::getForId($oServiceRatePlan->contract_status_id);
+		$oAccount = Account::getForId($oService->Account);
+		$oCustomerGroup = Customer_Group::getForId($oAccount->CustomerGroup);
 
-		$oContractBreachReason			= ($oServiceRatePlan->contract_breach_reason_id) ? Contract_Breach_Reason::getForId($oServiceRatePlan->contract_breach_reason_id) : null;
-		$oContractBreachFeesEmployee	= ($oServiceRatePlan->contract_breach_fees_employee_id) ? Employee::getForId($oServiceRatePlan->contract_breach_fees_employee_id) : null;
+		$oContractBreachReason = ($oServiceRatePlan->contract_breach_reason_id) ? Contract_Breach_Reason::getForId($oServiceRatePlan->contract_breach_reason_id) : null;
+		$oContractBreachFeesEmployee = ($oServiceRatePlan->contract_breach_fees_employee_id) ? Employee::getForId($oServiceRatePlan->contract_breach_fees_employee_id) : null;
 
-		$oContractPayoutCharge		= ($oServiceRatePlan->contract_payout_charge_id) ? Charge::getForId($oServiceRatePlan->contract_payout_charge_id) : null;
-		$oContractPayoutChargeType	= ($oContractPayoutCharge && $oContractPayoutCharge->charge_type_id) ? Charge_Type::getForId($oContractPayoutCharge->charge_type_id) : null;
+		$oContractPayoutCharge = ($oServiceRatePlan->contract_payout_charge_id) ? Charge::getForId($oServiceRatePlan->contract_payout_charge_id) : null;
+		$oContractPayoutChargeType = ($oContractPayoutCharge && $oContractPayoutCharge->charge_type_id) ? Charge_Type::getForId($oContractPayoutCharge->charge_type_id) : null;
 
-		$oExitFeeCharge		= ($oServiceRatePlan->exit_fee_charge_id) ? Charge::getForId($oServiceRatePlan->exit_fee_charge_id) : null;
-		$oExitFeeChargeType	= ($oExitFeeCharge && $oExitFeeCharge->charge_type_id) ? Charge_Type::getForId($oExitFeeCharge->charge_type_id) : null;
+		$oExitFeeCharge = ($oServiceRatePlan->exit_fee_charge_id) ? Charge::getForId($oServiceRatePlan->exit_fee_charge_id) : null;
+		$oExitFeeChargeType = ($oExitFeeCharge && $oExitFeeCharge->charge_type_id) ? Charge_Type::getForId($oExitFeeCharge->charge_type_id) : null;
 
 		// Return relevant data
 		return array(
-			'id'			=> $oServiceRatePlan->Id,
+			'id' => $oServiceRatePlan->Id,
 			// Account
-			'account_id'	=> $oService->Account,
-			'account'		=> array(
-				'id'				=> $oAccount->Id,
-				'account_name'		=> $oAccount->BusinessName,
-				'customer_group_id'	=> $oAccount->CustomerGroup,
-				'customer_group'	=> array(
-					'id'				=> $oCustomerGroup->Id,
-					'internal_name'		=> $oCustomerGroup->internal_name,
-					'primary_colour'	=> $oCustomerGroup->customer_primary_color
+			'account_id' => $oService->Account,
+			'account' => array(
+				'id' => $oAccount->Id,
+				'account_name' => $oAccount->BusinessName,
+				'customer_group_id' => $oAccount->CustomerGroup,
+				'customer_group' => array(
+					'id' => $oCustomerGroup->Id,
+					'internal_name' => $oCustomerGroup->internal_name,
+					'primary_colour' => $oCustomerGroup->customer_primary_color
 				)
 			),
 			// Service
-			'service_id'	=> $oServiceRatePlan->Service,
-			'service'		=> array(
-				'id'					=> $oService->Id,
-				'service_identifier'	=> $oService->FNN,
-				'service_type_id'		=> $oService->ServiceType,
-				'service_type'			=> array(
-					'id'			=> $oServiceType->id,
-					'name'			=> $oServiceType->name,
-					'description'	=> $oServiceType->description,
-					'constant'		=> $oServiceType->const_name
+			'service_id' => $oServiceRatePlan->Service,
+			'service' => array(
+				'id' => $oService->Id,
+				'service_identifier' => $oService->FNN,
+				'service_type_id' => $oService->ServiceType,
+				'service_type' => array(
+					'id' => $oServiceType->id,
+					'name' => $oServiceType->name,
+					'description' => $oServiceType->description,
+					'constant' => $oServiceType->const_name
 				)
 			),
 			// Plan
-			'rate_plan_id'	=> $oServiceRatePlan->RatePlan,
-			'rate_plan'		=> array(
-				'id'							=> $oRatePlan->Id,
-				'name'							=> $oRatePlan->Name,
-				'contract_term'					=> $oRatePlan->ContractTerm,
-				'minimum_charge'				=> $oRatePlan->MinMonthly,
-				'contract_exit_fee'				=> $oRatePlan->contract_exit_fee,
-				'contract_payout_percentage'	=> $oRatePlan->contract_payout_percentage
+			'rate_plan_id' => $oServiceRatePlan->RatePlan,
+			'rate_plan' => array(
+				'id' => $oRatePlan->Id,
+				'name' => $oRatePlan->Name,
+				'contract_term' => $oRatePlan->ContractTerm,
+				'minimum_charge' => $oRatePlan->MinMonthly,
+				'contract_exit_fee' => $oRatePlan->contract_exit_fee,
+				'contract_payout_percentage' => $oRatePlan->contract_payout_percentage
 			),
 			// Contract
-			'contract_start_datetime'			=> $oServiceRatePlan->StartDatetime,
-			'contract_end_datetime'				=> $oServiceRatePlan->EndDatetime,
-			'contract_scheduled_end_datetime'	=> $oServiceRatePlan->contract_scheduled_end_datetime,
-			'contract_effective_end_datetime'	=> $oServiceRatePlan->contract_effective_end_datetime,
-			'contract_term_remaining'			=> $oServiceRatePlan->contractMonthsRemaining(),
-			'contract_status_id'				=> $oServiceRatePlan->contract_status_id,
-			'contract_status'					=> array(
-				'id'		=> $oContractStatus->id,
-				'name'		=> $oContractStatus->name,
-				'constant'	=> $oContractStatus->const_name
+			'contract_start_datetime' => $oServiceRatePlan->StartDatetime,
+			'contract_end_datetime' => $oServiceRatePlan->EndDatetime,
+			'contract_scheduled_end_datetime' => $oServiceRatePlan->contract_scheduled_end_datetime,
+			'contract_effective_end_datetime' => $oServiceRatePlan->contract_effective_end_datetime,
+			'contract_term_remaining' => $oServiceRatePlan->contractMonthsRemaining(),
+			'contract_status_id' => $oServiceRatePlan->contract_status_id,
+			'contract_status' => array(
+				'id' => $oContractStatus->id,
+				'name' => $oContractStatus->name,
+				'constant' => $oContractStatus->const_name
 			),
 			// Contract Breach
-			'contract_breach_reason_id'	=> $oServiceRatePlan->contract_breach_reason_id,
-			'contract_breach_reason'	=> ((!$oContractBreachReason) ? null : array(
-				'id'		=> $oContractBreachReason->id,
-				'name'		=> $oContractBreachReason->name,
-				'constant'	=> $oContractBreachReason->const_name
+			'contract_breach_reason_id' => $oServiceRatePlan->contract_breach_reason_id,
+			'contract_breach_reason' => ((!$oContractBreachReason) ? null : array(
+				'id' => $oContractBreachReason->id,
+				'name' => $oContractBreachReason->name,
+				'constant' => $oContractBreachReason->const_name
 			)),
-			'contract_breach_reason_description'	=> $oServiceRatePlan->contract_breach_reason_description,
+			'contract_breach_reason_description' => $oServiceRatePlan->contract_breach_reason_description,
 			// Contract Payout
-			'contract_payout_recommended'			=> $oServiceRatePlan->calculateContractPayout(),
-			'contract_payout_percentage'			=> $oServiceRatePlan->contract_payout_percentage,
-			'contract_payout_charge_id'				=> $oServiceRatePlan->contract_payout_charge_id,
-			'contract_payout_charge'				=> ((!$oContractPayoutCharge) ? null : array(
-				'id'				=> $oContractPayoutCharge->id,
-				'charge_type_id'	=> $oContractPayoutCharge->charge_type_id,
-				'charge_type'		=> array(
-					'id'			=> $oContractPayoutChargeType->Id,
-					'charge_type'	=> $oContractPayoutChargeType->ChargeType
+			'contract_payout_recommended' => $oServiceRatePlan->calculateContractPayout(),
+			'contract_payout_percentage' => $oServiceRatePlan->contract_payout_percentage,
+			'contract_payout_charge_id' => $oServiceRatePlan->contract_payout_charge_id,
+			'contract_payout_charge' => ((!$oContractPayoutCharge) ? null : array(
+				'id' => $oContractPayoutCharge->id,
+				'charge_type_id' => $oContractPayoutCharge->charge_type_id,
+				'charge_type' => array(
+					'id' => $oContractPayoutChargeType->Id,
+					'charge_type' => $oContractPayoutChargeType->ChargeType
 				),
-				'amount'			=> Rate::roundtoCurrencyStandard($oContractPayoutCharge->Amount),
-				'charge_status_id'	=> $oContractPayoutCharge->Status,
-				'charge_status'		=> array(
-					'id'		=> $oContractPayoutCharge->Status,
-					'name'		=> GetConstantDescription($oContractPayoutCharge->Status, 'ChargeStatus'),
-					'constant'	=> GetConstantName($oContractPayoutCharge->Status, 'ChargeStatus')
+				'amount' => Rate::roundtoCurrencyStandard($oContractPayoutCharge->Amount),
+				'charge_status_id' => $oContractPayoutCharge->Status,
+				'charge_status' => array(
+					'id' => $oContractPayoutCharge->Status,
+					'name' => GetConstantDescription($oContractPayoutCharge->Status, 'ChargeStatus'),
+					'constant' => GetConstantName($oContractPayoutCharge->Status, 'ChargeStatus')
 				)
 			)),
 			// Contract Exit Fee
-			'exit_fee_recommended'	=> $oServiceRatePlan->calculateContractExitFee(),
-			'exit_fee_charge_id'	=> $oServiceRatePlan->exit_fee_charge_id,
-			'exit_fee_charge'		=> ((!$oExitFeeCharge) ? null : array(
-				'id'				=> $oExitFeeCharge->id,
-				'charge_type_id'	=> $oExitFeeCharge->charge_type_id,
-				'charge_type'		=> array(
-					'id'			=> $oExitFeeChargeType->Id,
-					'charge_type'	=> $oExitFeeChargeType->ChargeType
+			'exit_fee_recommended' => $oServiceRatePlan->calculateContractExitFee(),
+			'exit_fee_charge_id' => $oServiceRatePlan->exit_fee_charge_id,
+			'exit_fee_charge' => ((!$oExitFeeCharge) ? null : array(
+				'id' => $oExitFeeCharge->id,
+				'charge_type_id' => $oExitFeeCharge->charge_type_id,
+				'charge_type' => array(
+					'id' => $oExitFeeChargeType->Id,
+					'charge_type' => $oExitFeeChargeType->ChargeType
 				),
-				'amount'			=> Rate::roundtoCurrencyStandard($oExitFeeCharge->Amount),
-				'charge_status_id'	=> $oExitFeeCharge->Status,
-				'charge_status'		=> array(
-					'id'		=> $oExitFeeCharge->Status,
-					'name'		=> GetConstantDescription($oExitFeeCharge->Status, 'ChargeStatus'),
-					'constant'	=> GetConstantName($oExitFeeCharge->Status, 'ChargeStatus')
+				'amount' => Rate::roundtoCurrencyStandard($oExitFeeCharge->Amount),
+				'charge_status_id' => $oExitFeeCharge->Status,
+				'charge_status' => array(
+					'id' => $oExitFeeCharge->Status,
+					'name' => GetConstantDescription($oExitFeeCharge->Status, 'ChargeStatus'),
+					'constant' => GetConstantName($oExitFeeCharge->Status, 'ChargeStatus')
 				)
 			)),
 			// Contract Breach Fees Audit
-			'contract_breach_fees_datetime'		=> $oServiceRatePlan->contract_breach_fees_charged_on,
-			'contract_breach_fees_employee_id'	=> $oServiceRatePlan->contract_breach_fees_employee_id,
-			'contract_breach_fees_employee'		=> ((!$oContractBreachFeesEmployee) ? null : array(
-				'id'			=> $oContractBreachFeesEmployee->Id,
-				'first_name'	=> $oContractBreachFeesEmployee->FirstName,
-				'last_name'		=> $oContractBreachFeesEmployee->LastName
+			'contract_breach_fees_datetime' => $oServiceRatePlan->contract_breach_fees_charged_on,
+			'contract_breach_fees_employee_id' => $oServiceRatePlan->contract_breach_fees_employee_id,
+			'contract_breach_fees_employee' => ((!$oContractBreachFeesEmployee) ? null : array(
+				'id' => $oContractBreachFeesEmployee->Id,
+				'first_name' => $oContractBreachFeesEmployee->FirstName,
+				'last_name' => $oContractBreachFeesEmployee->LastName
 			)),
-			'contract_breach_fees_reason'	=> $oServiceRatePlan->contract_breach_fees_reason
+			'contract_breach_fees_reason' => $oServiceRatePlan->contract_breach_fees_reason
 		);
 	}
 
 }
-
-?>
--- lib/classes/service/rate/Service_Rate_Plan.php
+++ lib/classes/service/rate/Service_Rate_Plan.php
@@ -1,73 +1,51 @@
 <?php
-//----------------------------------------------------------------------------//
-// Service_Rate_Plan
-//----------------------------------------------------------------------------//
-/**
- * Service
- *
- * Models a record of the ServiceRatePlan table
- *
- * Models a record of the ServiceRatePlan table
- *
- * @class	Service
- */
 class Service_Rate_Plan extends ORM_Cached
-{	
-	protected			$_strTableName			= "ServiceRatePlan";
-	protected static	$_strStaticTableName	= "ServiceRatePlan";
+{ protected $_strTableName = "ServiceRatePlan";
+	protected static $_strStaticTableName = "ServiceRatePlan";
 
-	protected static function getCacheName()
-	{
+	protected static function getCacheName() {
 		// It's safest to keep the cache name the same as the class name, to ensure uniqueness
 		static $strCacheName;
-		if (!isset($strCacheName))
-		{
+		if (!isset($strCacheName)) {
 			$strCacheName = __CLASS__;
 		}
 		return $strCacheName;
 	}
 
-	protected static function getMaxCacheSize()
-	{
+	protected static function getMaxCacheSize() {
 		return 100;
 	}
 
 	//---------------------------------------------------------------------------------------------------------------------------------//
-	//				START - FUNCTIONS REQUIRED WHEN INHERITING FROM ORM_Cached UNTIL WE START USING PHP 5.3 - START
+	// START - FUNCTIONS REQUIRED WHEN INHERITING FROM ORM_Cached UNTIL WE START USING PHP 5.3 - START
 	//---------------------------------------------------------------------------------------------------------------------------------//
 
-	public static function clearCache()
-	{
+	public static function clearCache() {
 		parent::clearCache(__CLASS__);
 	}
 
-	protected static function getCachedObjects()
-	{
+	protected static function getCachedObjects() {
 		return parent::getCachedObjects(__CLASS__);
 	}
 
-	protected static function addToCache($mixObjects)
-	{
+	protected static function addToCache($mixObjects) {
 		parent::addToCache($mixObjects, __CLASS__);
 	}
 
-	public static function getForId($intId, $bolSilentFail=false)
-	{
+	public static function getForId($intId, $bolSilentFail=false) {
 		return parent::getForId($intId, $bolSilentFail, __CLASS__);
 	}
 
-	public static function getAll($bolForceReload=false)
-	{
+	public static function getAll($bolForceReload=false) {
 		return parent::getAll($bolForceReload, __CLASS__);
 	}
 
-	public static function importResult($aResultSet)
-	{
+	public static function importResult($aResultSet) {
 		return parent::importResult($aResultSet, __CLASS__);
 	}
 
 	//---------------------------------------------------------------------------------------------------------------------------------//
-	//				END - FUNCTIONS REQUIRED WHEN INHERITING FROM ORM_Cached UNTIL WE START USING PHP 5.3 - END
+	// END - FUNCTIONS REQUIRED WHEN INHERITING FROM ORM_Cached UNTIL WE START USING PHP 5.3 - END
 	//---------------------------------------------------------------------------------------------------------------------------------//
 
 	
@@ -76,31 +54,30 @@ class Service_Rate_Plan extends ORM_Cached
 	 *
 	 * Calculates the number of months remaining on this Contract
 	 *
-	 * @param	[boolean	$bInclusive		]	TRUE	: The months remaining is rounded up (includes this month) (Default)
-	 * 											FALSE	: The months remaining is rounded down (excludes this month)
+	 * @param [boolean $bInclusive ] TRUE : The months remaining is rounded up (includes this month) (Default)
+	 * FALSE : The months remaining is rounded down (excludes this month)
 	 * 
-	 * @return	integer
+	 * @return integer
 	 * 
 	 * @method
 	 */
-	public function contractMonthsRemaining($bInclusive=true)
-	{
-		$iScheduledEndDatetime		= strtotime($this->contract_scheduled_end_datetime);
-		$iEffectiveEndDatetime		= $this->contract_effective_end_datetime ? strtotime($this->contract_effective_end_datetime) : null;
-		$iStartDatetime				= strtotime($this->StartDatetime);
+	public function contractMonthsRemaining($bInclusive=true) {
+		$iScheduledEndDatetime = strtotime($this->contract_scheduled_end_datetime);
+		$iEffectiveEndDatetime = $this->contract_effective_end_datetime ? strtotime($this->contract_effective_end_datetime) : null;
+		$iStartDatetime = strtotime($this->StartDatetime);
 		//throw new Exception("Scheduled Contract End: {$this->contract_scheduled_end_datetime}");
 		
 		if ($iScheduledEndDatetime) {
-			$iCurrentDate		= time();
+			$iCurrentDate = time();
 			
 			// End Date
-			$sEndDate			= date("Y-m-d", $iScheduledEndDatetime + 1);
+			$sEndDate = date("Y-m-d", $iScheduledEndDatetime + 1);
 
 			// From Date:
-			//	Never earlier than the Contract Start
-			//	Never later than the Contract End (effective or scheduled)
-			$iFromDate			= max($iCurrentDate, $iStartDatetime);
-			$iFromDate			= min($iFromDate, $iScheduledEndDatetime, coalesce($iEffectiveEndDatetime, PHP_INT_MAX));
+			// Never earlier than the Contract Start
+			// Never later than the Contract End (effective or scheduled)
+			$iFromDate = max($iCurrentDate, $iStartDatetime);
+			$iFromDate = min($iFromDate, $iScheduledEndDatetime, coalesce($iEffectiveEndDatetime, PHP_INT_MAX));
 			
 			return max(0, min(Flex_Date::difference(date('Y-m-d', $iFromDate), $sEndDate, 'm', 'ceil'), Rate_Plan::getForId($this->RatePlan)->ContractTerm));
 		} else {
@@ -113,41 +90,39 @@ class Service_Rate_Plan extends ORM_Cached
 	 *
 	 * Calculates the Payout figure for this Contract (excluding global tax)
 	 *
-	 * @param	[boolean	$bInclusive		]	TRUE	: The months remaining is rounded up (includes this month) (Default)
-	 * 											FALSE	: The months remaining is rounded down (excludes this month)
+	 * @param [boolean $bInclusive ] TRUE : The months remaining is rounded up (includes this month) (Default)
+	 * FALSE : The months remaining is rounded down (excludes this month)
 	 * 
-	 * @return	integer
+	 * @return integer
 	 * 
 	 * @method
 	 */
-	public function calculatePayout($bInclusive=true)
-	{
-		$oContractTerms	= Contract_Terms::getCurrent();
-		$oRatePlan		= Rate_Plan::getForId($this->RatePlan);
+	public function calculatePayout($bInclusive=true) {
+		$oContractTerms = Contract_Terms::getCurrent();
+		$oRatePlan = Rate_Plan::getForId($this->RatePlan);
 		
-		$fTotalPayout		= 0.0;
+		$fTotalPayout = 0.0;
 		
-		$iContractMonthsRemaining	= $this->contractMonthsRemaining($bInclusive);
+		$iContractMonthsRemaining = $this->contractMonthsRemaining($bInclusive);
 		//throw new Exception("Months Remaining: {$iContractMonthsRemaining}; Invoice Count: {$iInvoiceCount}");
 		
-		if ($iContractMonthsRemaining)
-		{
+		if ($iContractMonthsRemaining) {
 			// Payout
-			$fTotalPayout	+= coalesce($this->calculateContractPayout($bInclusive), 0.0);
+			$fTotalPayout += coalesce($this->calculateContractPayout($bInclusive), 0.0);
 			
 			// Early Exit Fee
-			$fTotalPayout	+= coalesce($this->calculateContractExitFee($bInclusive), 0.0);
+			$fTotalPayout += coalesce($this->calculateContractExitFee($bInclusive), 0.0);
 		}
 		return Rate::roundToCurrencyStandard($fTotalPayout, 2);
 	}
 
 	public function calculateContractPayout($bInclusive=true) {
-		$oContractTerms				= Contract_Terms::getCurrent();
-		$iInvoiceCount				= $this->getInvoiceCount();
-		$oRatePlan					= Rate_Plan::getForId($this->RatePlan);
-		$iContractMonthsRemaining	= $this->contractMonthsRemaining($bInclusive);
+		$oContractTerms = Contract_Terms::getCurrent();
+		$iInvoiceCount = $this->getInvoiceCount();
+		$oRatePlan = Rate_Plan::getForId($this->RatePlan);
+		$iContractMonthsRemaining = $this->contractMonthsRemaining($bInclusive);
 
-		$fPayout	= 0.0;
+		$fPayout = 0.0;
 		if ($iContractMonthsRemaining) {
 			if ($iInvoiceCount >= $oContractTerms->contract_payout_minimum_invoices) {
 				return Rate::roundToCurrencyStandard(($iContractMonthsRemaining * $oRatePlan->MinMonthly) * ($oRatePlan->contract_payout_percentage / 100), 2);
@@ -157,10 +132,10 @@ class Service_Rate_Plan extends ORM_Cached
 	}
 
 	public function calculateContractExitFee($bInclusive=true) {
-		$oContractTerms				= Contract_Terms::getCurrent();
-		$iInvoiceCount				= $this->getInvoiceCount();
-		$oRatePlan					= Rate_Plan::getForId($this->RatePlan);
-		$iContractMonthsRemaining	= $this->contractMonthsRemaining($bInclusive);
+		$oContractTerms = Contract_Terms::getCurrent();
+		$iInvoiceCount = $this->getInvoiceCount();
+		$oRatePlan = Rate_Plan::getForId($this->RatePlan);
+		$iContractMonthsRemaining = $this->contractMonthsRemaining($bInclusive);
 
 		if ($iContractMonthsRemaining) {
 			if ($iInvoiceCount >= $oContractTerms->exit_fee_minimum_invoices) {
@@ -170,92 +145,56 @@ class Service_Rate_Plan extends ORM_Cached
 		return null;
 	}
 	
-	/**
-	 * getInvoiceCount()
-	 *
-	 * Gets the number of Invoices this Service Rate Plan record has been included in
-	 * 
-	 * @return	integer
-	 * 
-	 * @method
-	 */
-	public function getInvoiceCount($bolInclusive=true)
-	{
-		$selInvoiceCount	= self::_preparedStatement('selInvoiceCount');
-		if ($selInvoiceCount->Execute($this->toArray()) === false)
-		{
+	public function getInvoiceCount($bolInclusive=true) {
+		$selInvoiceCount = self::_preparedStatement('selInvoiceCount');
+		if ($selInvoiceCount->Execute($this->toArray()) === false) {
 			throw new Exception_Database($selInvoiceCount->Error());
-		}
-		else
-		{
-			$arrInvoiceCount	= $selInvoiceCount->Fetch();
+		} else {
+			$arrInvoiceCount = $selInvoiceCount->Fetch();
 			return $arrInvoiceCount['invoice_count'];
 		}
 	}
 
-        public static function getActiveForService($iServiceId, $sDateTime = null)
-        {
-            if ($sDateTime === null)
-                $sDateTime = Data_Source_Time::currentTimestamp ();
-            $oStatement = self::_preparedStatement('selActiveForServiceId');
-            if ($oStatement->Execute(array('iServiceId'=>$iServiceId, 'sDateTime'=>$sDateTime)) === false)
-            {
-                    throw new Exception_Database($selFNNInstances->Error());
-            }
-
-            $aRecords = $oStatement->FetchAll();
-            return count($aRecords) > 0 ? new self($aRecords[0]) : null;
-        }
-	
-	//------------------------------------------------------------------------//
-	// _preparedStatement
-	//------------------------------------------------------------------------//
-	/**
-	 * _preparedStatement()
-	 *
-	 * Access a Static Cache of Prepared Statements used by this Class
-	 *
-	 * Access a Static Cache of Prepared Statements used by this Class
-	 * 
-	 * @param	string		$strStatement						Name of the statement
-	 * 
-	 * @return	Statement										The requested Statement
-	 *
-	 * @method
-	 */
-	protected static function _preparedStatement($strStatement)
-	{
-		static	$arrPreparedStatements	= Array();
-		if (isset($arrPreparedStatements[$strStatement]))
-		{
-			return $arrPreparedStatements[$strStatement];
+	public static function getActiveForService($iServiceId, $sDateTime = null) {
+		if ($sDateTime === null)
+			$sDateTime = Data_Source_Time::currentTimestamp ();
+		$oStatement = self::_preparedStatement('selActiveForServiceId');
+		if ($oStatement->Execute(array('iServiceId'=>$iServiceId, 'sDateTime'=>$sDateTime)) === false) {
+			throw new Exception_Database($selFNNInstances->Error());
 		}
-		else
-		{
-			switch ($strStatement)
-			{
+
+		$aRecords = $oStatement->FetchAll();
+		return count($aRecords) > 0 ? new self($aRecords[0]) : null;
+	}
+	
+	protected static function _preparedStatement($strStatement) {
+		static $arrPreparedStatements = Array();
+		if (isset($arrPreparedStatements[$strStatement])) {
+			return $arrPreparedStatements[$strStatement];
+		} else {
+			switch ($strStatement) {
 				// SELECTS
-                case 'selActiveForServiceId':
-					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "Service = <iServiceId> AND <sDateTime> BETWEEN StartDateTime AND EndDatetime", 'CreatedOn DESC, Id DESC', 1);
+				case 'selActiveForServiceId':
+					$arrPreparedStatements[$strStatement] = new StatementSelect(self::$_strStaticTableName, "*", "Service = <iServiceId> AND <sDateTime> BETWEEN StartDateTime AND EndDatetime", 'CreatedOn DESC, Id DESC', 1);
 					break;
 				case 'selById':
-					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "id = <Id>", NULL, 1);
+					$arrPreparedStatements[$strStatement] = new StatementSelect(self::$_strStaticTableName, "*", "id = <Id>", NULL, 1);
 					break;
 				case 'selAll':
-					$arrPreparedStatements[$strStatement]	= new StatementSelect(self::$_strStaticTableName, "*", "1", "id ASC");
+					$arrPreparedStatements[$strStatement] = new StatementSelect(self::$_strStaticTableName, "*", "1", "id ASC");
 					break;
 				case 'selInvoiceCount':
-					$arrPreparedStatements[$strStatement]	= new StatementSelect(	"ServiceTotal JOIN InvoiceRun ON InvoiceRun.Id = ServiceTotal.invoice_run_id", "COUNT(ServiceTotal.Id) AS invoice_count", "service_rate_plan = <Id> AND InvoiceRun.invoice_run_status_id = ".INVOICE_RUN_STATUS_COMMITTED);
+					$arrPreparedStatements[$strStatement] = new StatementSelect( "ServiceTotal JOIN InvoiceRun ON InvoiceRun.Id = ServiceTotal.invoice_run_id", "COUNT(ServiceTotal.Id) AS invoice_count", "service_rate_plan = <Id> AND InvoiceRun.invoice_run_status_id = ".INVOICE_RUN_STATUS_COMMITTED);
 					break;
 
 				// INSERTS
 				case 'insSelf':
-					$arrPreparedStatements[$strStatement]	= new StatementInsert(self::$_strStaticTableName);
+					$arrPreparedStatements[$strStatement] = new StatementInsert(self::$_strStaticTableName);
 					break;
 
 				// UPDATE BY IDS
 				case 'ubiSelf':
-					$arrPreparedStatements[$strStatement]	= new StatementUpdateById(self::$_strStaticTableName);
+					$arrPreparedStatements[$strStatement] = new StatementUpdateById(self::$_strStaticTableName);
 					break;
 				
 				// UPDATES
@@ -267,4 +206,3 @@ class Service_Rate_Plan extends ORM_Cached
 		}
 	}
 }
-?>
