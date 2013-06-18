f232b3d2d0a50028db24781fc804b5918896b3e9
Rod McTainsh <rmctainsh@ybs.net.au>
2013-06-18 13:04:29 +1000

    bugfix: report events now honour the configured email_notification, rollout: removed collection_event_report_output.file_type_id (not necessary and incorrectly placed)

--- lib/classes/logic/collection/event/Logic_Collection_Event_Report.php
+++ lib/classes/logic/collection/event/Logic_Collection_Event_Report.php
@@ -1,62 +1,27 @@
 <?php
-/**
- * Description of Collection_Logic_Event_Report
- *
- * @author JanVanDerBreggen
- */
-class Logic_Collection_Event_Report extends Logic_Collection_Event
-{
+
+class Logic_Collection_Event_Report extends Logic_Collection_Event {
 	protected $oDO;
 	
-
-	public function __construct($mDefinition)
-	{	   
-		if ($mDefinition instanceof Logic_Collection_Event_Instance)
-		{
+	public function __construct($mDefinition) {
+		if ($mDefinition instanceof Logic_Collection_Event_Instance) {
 		   $this->oCollectionEventInstance = $mDefinition;
 		   $this->oParentDO = Collection_Event::getForId($mDefinition->collection_event_id);
 		   $this->oDO = Collection_Event_Report::getForCollectionEventId($this->oParentDO->id);
-		}
-		else if (is_numeric($mDefinition))
-		{
+		} else if (is_numeric($mDefinition)) {
 			$this->oParentDO = Collection_Event::getForId($mDefinition);
 			$this->oDO = Collection_Event_Report::getForCollectionEventId($this->oParentDO->id);
-		}
-		else
-		{
+		} else {
 		   throw new Exception('bad parameter passed into Collection_Logic_Event_Report constructor');
 		}
 	}
 
-	public function getFileExtension()
-	{
-		$oOutputOrm = Collection_Event_Report_Output::getForId($this->collection_event_report_output_id);
-		$oFileTypeOrm = File_Type::getForId($oOutputOrm->file_type_id);
-		return $oFileTypeOrm->extension;
-
+	protected function _invoke($aParameters=null) {
+		// ...
 	}
 
-	public function getReportOutPutId()
-	{
-		return $this->collection_event_report_output_id;
-	}
-
-	public function getMimeType()
-	{
-		$oOutputOrm = Collection_Event_Report_Output::getForId($this->collection_event_report_output_id);
-		$oFileTypeOrm = File_Type::getForId($oOutputOrm->file_type_id);
-		return $oFileTypeOrm->getPreferredMIMEType();
-	}
-
-	protected function _invoke($aParameters = null)
-	{
-
-	}
-
-	 public function __get($sField)
-	{
-		switch ($sField)
-		{
+	public function __get($sField) {
+		switch ($sField) {
 			case 'name':
 			case 'collection_event_type_id':
 				return $this->oParentDO->$sField;
@@ -65,105 +30,81 @@ class Logic_Collection_Event_Report extends Logic_Collection_Event
 		}
 	}
 
-	/**
-	 *
-	 * @param <type> $aEventInstances
-	 */
+	public static function complete($aEventInstances) {
+		// Get the sql for the report
+		$oEventInstance	= $aEventInstances[0];
+		if ($oEventInstance === NULL) {
+			// Nothing to complete
+			return;
+		}
 
-	public static function complete($aEventInstances)
-	{
-		//get the sql for the report
-		$oEventInstance		= $aEventInstances[0];
-		if ($oEventInstance === NULL)
-			return; //nothing to complete
-		$sEventName			= $oEventInstance->getEventName();
-		$oEventObject		= self::getForEventInstance($oEventInstance);
-		$sSql				=  $oEventObject->report_sql;
-		$iEmailNotification = $oEventObject->email_notification_id;
-		$sFileExtension		= $oEventObject->getFileExtension();
-		$iReportOutput		= $oEventObject->getReportOutPutId();
-		$sFileType			= $iReportOutput==COLLECTION_EVENT_REPORT_OUTPUT_CSV ? 'CSV' : ($iReportOutput==COLLECTION_EVENT_REPORT_OUTPUT_EXCEL ? 'Excel5' : 'Excel2007');
-		$sMimeType			= $oEventObject->getMimeType()->mime_content_type;
-		$aAccountIds		= array();
+		$sEventName = $oEventInstance->getEventName();
+		$oEventObject = self::getForEventInstance($oEventInstance);
+		switch ($oEventObject->collection_event_report_output_id) {
+			case COLLECTION_EVENT_REPORT_OUTPUT_CSV:
+				$sFileExtension = 'csv';
+				$sMimeType = 'text/plain';
+				$sFileType = 'CSV';
+				break;
+			case COLLECTION_EVENT_REPORT_OUTPUT_EXCEL:
+				$sFileExtension = 'xls';
+				$sMimeType = 'application/excel';
+				$sFileType = 'Excel5';
+				break;
+			case COLLECTION_EVENT_REPORT_OUTPUT_EXCEL_2007:
+				$sFileExtension = 'xlsx';
+				$sMimeType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+				$sFileType = 'Excel2007';
+				break;
+		}
 
-		foreach ($aEventInstances as $oEventInstance)
-		{		   
+		$aAccountIds = array();
+		foreach ($aEventInstances as $oEventInstance) {
 			$aAccountIds[] = $oEventInstance->account_id;
 		}
 
-		$sSql				= str_replace("<ACCOUNTS>", implode(",", $aAccountIds), $sSql);
-		$oQuery				= new Query();
-		$mResult			= $oQuery->Execute($sSql);
-		$aResult			= array();
-		if ($mResult)
-		{
-			while ($aRecord = $mResult->fetch_assoc())
-			{
-			   $aResult[]	= $aRecord;
+		$sSql = $oEventObject->report_sql;
+		$sSql = str_replace("<ACCOUNTS>", implode(",", $aAccountIds), $sSql);
+		$oQuery = new Query();
+		$mResult = $oQuery->Execute($sSql);
+		$aResult = array();
+		if ($mResult) {
+			while ($aRecord = $mResult->fetch_assoc()) {
+			   $aResult[] = $aRecord;
 			}
 		}
 
-		$oEmail				= Correspondence_Email::getForEmailNotificationSystemName('LATE_NOTICE_LIST');
+		$oEmail = new Correspondence_Email($oEventObject->email_notification_id);
 		$oEmail->setSubject("$sEventName (Collection Event id $oEventInstance->collection_event_id)");
-		$body				= $oEmail->getBody();
-		$oEmail->addTextHeader(3, $sEventName." (Collection Event id $oEventInstance->collection_event_id)");
-		$sMessage;
-
-		$oSpreadsheet		= new Logic_Spreadsheet(array_keys($aResult[0]), $aResult, $sFileType);
-		$sPath				= FILES_BASE_PATH.'temp/';
-
-		$sTimeStamp			= str_replace(array(' ',':','-'), '',Data_Source_Time::currentTimestamp());
-		$sFilename			= "$sEventName"."_$sTimeStamp.$sFileExtension";
-
-		$oSpreadsheet->saveAs( $sPath.$sFilename, $sFileType);
-		//send the email
-		$sFile				= file_get_contents($sPath.$sFilename);
-		$oEmail->addAttachment($sFile, $sFilename, $sMimeType);
-		$oEmail->addTextHeader(4, "Please find the report attached. Summary: ");
-		$table				=& $oEmail->setTable();
-		$oEmail->addPivotTableRow("Number of Rows", $oSpreadsheet->getRowCount());
-		$oEmployee			= Employee::getForId(Flex::getUserId());
-		if ($oEmployee!= null && $oEmployee->email!=null)
-			$oEmail->addTo($oEmployee->Email, $name=$oEmployee->FirstName.' '.$oEmployee->LastName);
-
-		$oEmail->appendSignature();
-		$oEmail->setBodyHTML();
-		$oEmail->send();
-		
-		foreach ($aEventInstances as $oInstance)
-		{
-			$oInstance->complete();
-		}		
-	}
-
-	public static function emailReport($sEventName)
-	{
-		$oEmail = Correspondence_Email::getForEmailNotificationSystemName(LATE_NOTICE_LIST);
-		$oEmail->setSubject("Report for Collection Event '$sEventName'");
 		$body = $oEmail->getBody();
-		$oEmail->addTextHeader(3, $sEventName." Report");
-		$sMessage;
-
+		$oEmail->addTextHeader(3, $sEventName." (Collection Event id $oEventInstance->collection_event_id)");
+		
 		$oSpreadsheet = new Logic_Spreadsheet(array_keys($aResult[0]), $aResult, $sFileType);
 		$sPath = FILES_BASE_PATH.'temp/';
-
 		$sTimeStamp = str_replace(array(' ',':','-'), '',Data_Source_Time::currentTimestamp());
-		$sFilename	= "$sEventName"."_$sTimeStamp.$sFileExtension";
+		$sFilename = "$sEventName"."_$sTimeStamp.$sFileExtension";
+		$oSpreadsheet->saveAs($sPath.$sFilename, $sFileType);
 
-		$oSpreadsheet->saveAs( $sPath.$sFilename, $sFileType);
-		//send the email
+		// Send the email
 		$sFile = file_get_contents($sPath.$sFilename);
 		$oEmail->addAttachment($sFile, $sFilename, $sMimeType);
 		$oEmail->addTextHeader(4, "Please find the report attached. Summary: ");
 		$table =& $oEmail->setTable();
 		$oEmail->addPivotTableRow("Number of Rows", $oSpreadsheet->getRowCount());
+		
 		$oEmployee = Employee::getForId(Flex::getUserId());
-		if ($oEmployee!= null && $oEmployee->email!=null)
-		$oEmail->addTo($oEmployee->Email, $name=$oEmployee->FirstName.' '.$oEmployee->LastName);
+		if (($oEmployee != null) && ($oEmployee->email != null)) {
+			$oEmail->addTo($oEmployee->Email, $oEmployee->FirstName.' '.$oEmployee->LastName);
+		}
 
 		$oEmail->appendSignature();
 		$oEmail->setBodyHTML();
 		$oEmail->send();
+		
+		foreach ($aEventInstances as $oInstance) {
+			$oInstance->complete();
+		}		
 	}
 }
+
 ?>
