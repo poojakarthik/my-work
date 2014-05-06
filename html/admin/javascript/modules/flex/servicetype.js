c3f351c0a5d71259cc0e02fc7bf643128380bb50
Rich Davis <richjddavis@gmail.com>
2014-05-07 09:59:42 +1000

    Add UI for more Service Types & Service Type-related data to Invoice XML

--- html/admin/javascript/modules/flex/servicetype.js
+++ html/admin/javascript/modules/flex/servicetype.js
@@ -6,7 +6,7 @@ var _serviceTypeModuleBasePath = 'flex/servicetype/';
 return {
 	getModule: function (serviceTypeModule) {
 		return promise(function (fulfill, reject) {
-			var serviceTypeModulePath = _serviceTypeModuleBasePath + '/' + serviceTypeModule.toLowerCase().replace('_', '/');
+			var serviceTypeModulePath = _serviceTypeModuleBasePath + '/' + serviceTypeModule.toLowerCase().replace(/_/g, '/');
 			module.provide([serviceTypeModulePath], function (provideError) {
 				if (provideError) {
 					reject(provideError);
