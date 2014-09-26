06246838a210f88423ff8d65fcb9e085343e191c
Sanjeev Maharjan <sanjeev@smartbusinesstelecom.com.au>
2014-09-26 12:29:21 +1000

    Adding Missing Permission Check in JSON Handler Files

--- html/admin/classes/json/handler/report/category/JSON_Handler_Report_Category.php
+++ html/admin/classes/json/handler/report/category/JSON_Handler_Report_Category.php
@@ -1,6 +1,9 @@
 <?php
 class JSON_Handler_Report_Category extends JSON_Handler implements JSON_Handler_Loggable, JSON_Handler_Catchable {
 	public function getAll() {
+		// Check user authorization and permissions
+		AuthenticatedUser()->CheckAuth();
+		AuthenticatedUser()->PermissionOrDie(PERMISSION_PROPER_ADMIN);
 		$aReportCategory = Report_Category::getAll();
 		$aResultSet = array();
 		foreach ($aReportCategory as $iKey=>$oReportCategory) {
--- html/admin/classes/json/handler/report/constraint/JSON_Handler_Report_Constraint_Type.php
+++ html/admin/classes/json/handler/report/constraint/JSON_Handler_Report_Constraint_Type.php
@@ -1,6 +1,9 @@
 <?php
 class JSON_Handler_Report_Constraint_Type extends JSON_Handler implements JSON_Handler_Loggable, JSON_Handler_Catchable {
 	public function getAll() {
+		// Check user authorization and permissions
+		AuthenticatedUser()->CheckAuth();
+		AuthenticatedUser()->PermissionOrDie(PERMISSION_PROPER_ADMIN);
 		$aReportConstraintType = Report_Constraint_Type::getAll();
 		$aResultSet = array();
 		foreach ($aReportConstraintType as $iKey=>$oReportConstraintType) {
